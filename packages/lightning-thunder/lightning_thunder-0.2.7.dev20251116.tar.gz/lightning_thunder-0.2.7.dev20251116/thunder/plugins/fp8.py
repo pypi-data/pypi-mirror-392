7f1cb8887ece26d39941b8c7a2226f193a731dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 15:05:37 +0000

    Add package lightning-thunder/0.2.7.dev20251116/lightning_thunder-0.2.7.dev20251116.tar.gz

---
+++ packages/lightning-thunder/lightning_thunder-0.2.7.dev20251116.tar.gz/lightning_thunder-0.2.7.dev20251116/thunder/plugins/fp8.py
@@ -0,0 +1,21 @@
+from thunder import Plugin
+
+
+class FP8(Plugin):
+    """
+    Plugin for enabling FP8 precision via NVIDIA Transformer Engine, enabling higher throughput of matrix operations in FP8.
+
+    See `lightning-thunder/thunder/executors/transformer_engine_v1ex.py` for implementation details.
+    """
+
+    def setup_executors(self):
+        """
+        Imports the Transformer Engine executor.
+
+        Returns:
+            list[Executor]: A list containing the Transformer Engine executor.
+
+        """
+        from thunder.executors.transformer_engine_v1ex import transformer_engine_v1_ex
+
+        return [transformer_engine_v1_ex]
