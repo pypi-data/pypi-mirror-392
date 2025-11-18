57dda70d0865eb059b27ac34b42b4ae3407cc244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 12:05:26 +0000

    Add package rust_neotools/0.0.5/rust_neotools-0.0.5-cp314-cp314-musllinux_1_2_i686.whl

---
+++ packages/rust_neotools/rust_neotools-0.0.5-cp314-cp314-musllinux_1_2_i686.whl/rust_neotools/__init__.py
@@ -0,0 +1,12 @@
+__title__ = "rust_neotools"
+__author__ = "diceroll123"
+__license__ = "MIT"
+__copyright__ = "Copyright 2023-present diceroll123"
+__version__ = "0.0.3"
+
+from .rust_neotools import IslandMystic, Symol  # noqa: F401
+
+__all__ = (
+    "IslandMystic",
+    "Symol",
+)
