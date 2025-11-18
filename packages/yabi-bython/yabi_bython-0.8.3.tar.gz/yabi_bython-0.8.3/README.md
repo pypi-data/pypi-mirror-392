acf4e113245297f0415632d781f46bafde693a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 20:06:58 +0000

    Add package yabi-bython/0.8.3/yabi_bython-0.8.3.tar.gz

---
+++ packages/yabi-bython/yabi_bython-0.8.3.tar.gz/yabi_bython-0.8.3/README.md
@@ -0,0 +1,59 @@
+# YABI
+
+Ever wanted to use braces instead of indentation in Python? No? Well, it's possible now!
+
+## Installing
+
+```bash
+pip install yabi-bython
+```
+
+## Running
+
+```bash
+# run file
+yabi file.by
+# run module
+yabi -m module
+# run interactive console
+yabi
+# run code from command line
+yabi -c "import os; try { os.remove('/') } except { print('Bruh') }"
+```
+
+Run `yabi --help` to see other options.
+
+## Is YABI better than other implementations?
+
+- Doesn't have problems with dicts
+- Has full `import` support (any `.by` file can be imported)
+- Has `.pyc` support (code is cached after translation)
+
+It can have bugs though, I'd be glad if you opened an issue in case you encounter one.
+
+If your condition contains `{` or `lambda`, remember to parenthesise it:
+```python
+# Wrong
+if x := {a for a in range(n)} { print("Hello") }
+# Correct
+if (x := {a for a in range(n)}) { print("Hello") }
+```
+
+## Perfect for one-liners
+
+Braces and semicolons allow to make any code a one-liner.
+But, like, why would you do that?
+
+## Converting Python files
+
+You don't need to convert anything because `.py` files can be imported into `.by`.
+YABI provides `yabi-convert` in case you still want to convert.
+The output may be a little weird but should still be correct.
+
+```bash
+yabi-convert something.py > something.by
+```
+
+## Acknowledgements
+
+Inspired by https://github.com/mathialo/bython and my friend ByThon.
---
+++ packages/yabi-bython/yabi_bython-0.8.3.tar.gz/yabi_bython-0.8.3/tests/test_yabi.py
@@ -0,0 +1,165 @@
+import os
+import sys
+from io import StringIO
+from unittest.mock import patch
+
+from pytest import mark
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
+from yabi import main, to_bython, to_pure_python  # noqa: E402
+from yabi.console import YabiConsole  # noqa: E402
+
+
+sys.dont_write_bytecode = True
+
+
+def check_file(file, expexted_output):
+    with patch("sys.stdout", new=StringIO()):
+        main([file])
+        assert sys.stdout.getvalue() == expexted_output
+
+
+def check_console(code, expexted_output):
+    with patch("sys.stdout", new=StringIO()), patch(
+        "sys.stdin", new=StringIO(code)
+    ):
+        YabiConsole().interact()
+        assert sys.stdout.getvalue() == expexted_output
+
+
+def test_regular():
+    check_file("tests/hello.by", "Hello\n")
+
+
+def test_mixed():
+    check_file("tests/mixed.by", "3\n")
+
+
+def test_dict():
+    check_file("tests/dict.by", "1\n2\n3\nok\n")
+
+
+def test_except():
+    check_file(
+        "tests/except.by", "e\ndivision by zero\ne\ne\ndivision by zero\n"
+    )
+
+
+def test_inline_if():
+    check_file("tests/inline_if.by", "a\n")
+
+
+def test_async():
+    check_file("tests/async.by", "hello\n")
+
+
+def test_nl_after_kw():
+    check_file("tests/nl_after_kw.by", "1\n")
+
+
+def test_lambda():
+    check_file("tests/lambda.by", "Hello\n")
+
+
+@mark.skipif(
+    sys.version_info < (3, 10), reason="requires python3.10 or higher"
+)
+def test_match():
+    check_file("tests/match.by", "1\n2\n")
+
+
+@mark.skipif(
+    sys.implementation.name == "pypy" and sys.version_info < (3, 10),
+    reason="pypy's console is weird on older versions",
+)
+def test_console():
+    code = (
+        """
+from __future__ import annotations
+a: lol
+1; 2; 3
+for i in range(
+    10
+) {
+    if i % 2 { print(i) }
+}
+
+if True:
+    print(1)
+    print(2)
+    print(3)
+    """.strip()
+        + "\n\n"
+    )
+    expexted_output = (
+        """
+>>> >>> >>> 1
+2
+3
+>>> ... ... ... ... ... 1
+3
+5
+7
+9
+>>> ... ... ... ... 1
+2
+3
+>>>
+        """.strip()
+        + " "
+    )
+    check_console(code, expexted_output)
+
+
+def test_console_linecont():
+    code = (
+        """
+if \\
+  1: pass
+    """.strip()
+        + "\n\n"
+    )
+    expexted_output = ">>> ... ... >>> "
+    check_console(code, expexted_output)
+
+
+def test_to_bython():
+    assert (
+        to_bython(
+            """
+for i in {1, 2, 3}:
+    if i % 2 == 1:
+        print("Yes")
+    else:
+        print("No")
+    """.strip()
+        )
+        == """
+for (i in {1, 2, 3}) {
+    if i % 2 == 1 {
+        print("Yes")
+    }
+    else {
+        print("No")
+    }
+}
+    """.strip()
+        + "\n"
+    )
+
+
+def test_comment_with_semicolon():
+    assert to_pure_python("# a; b").rstrip("\n") == "# a; b"
+
+
+def test_semicolon_parse():
+    assert to_pure_python("; while True {}") == "while True:\n    pass\n"
+
+
+def test_inline_comment():
+    code = """
+if 1:  # 2
+    3
+    """.strip()
+    assert to_pure_python(code).strip() == code
