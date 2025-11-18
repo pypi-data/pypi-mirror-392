e27c0d1faef6bf3e5b04d05ab790d9276e98af43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 22:07:57 +0000

    Add package adios2/2.11.0.1012/adios2-2.11.0.1012-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/adios2/adios2-2.11.0.1012-cp310-cp310-manylinux_2_28_x86_64.whl/adios2/include/adios2/cxx11/VariableNT.h
@@ -0,0 +1,23 @@
+/*
+ * Distributed under the OSI-approved Apache License, Version 2.0.  See
+ * accompanying file Copyright.txt for details.
+ *
+ * VARIABLENT_H : deprecated wrapper - use bindings/CXX/adios2/cxx instead
+ *
+ * DEPRECATION WARNING: The bindings/CXX/adios2/cxx11 directory has been
+ * renamed to bindings/CXX/adios2/cxx. Please update your includes to use
+ * bindings/CXX/adios2/cxx instead of bindings/CXX/adios2/cxx11.
+ */
+
+#ifndef ADIOS2_BINDINGS_CXX_CXX11_VARIABLENT_H_
+#define ADIOS2_BINDINGS_CXX_CXX11_VARIABLENT_H_
+
+#if defined(_MSC_VER)
+#pragma message("DEPRECATED: bindings/CXX/adios2/cxx11 has been renamed to bindings/CXX/adios2/cxx. Please update your includes.")
+#else
+#warning "DEPRECATED: bindings/CXX/adios2/cxx11 has been renamed to bindings/CXX/adios2/cxx. Please update your includes."
+#endif
+
+#include "../cxx/VariableNT.h"
+
+#endif /* ADIOS2_BINDINGS_CXX_CXX11_VARIABLENT_H_ */
---
+++ packages/adios2/adios2-2.11.0.1012-cp310-cp310-manylinux_2_28_x86_64.whl/adios2/include/adios2/toolkit/derived/DerivedData.h
@@ -0,0 +1,26 @@
+#ifndef ADIOS2_DERIVED_Data_H_
+#define ADIOS2_DERIVED_Data_H_
+
+#include "adios2/common/ADIOSTypes.h"
+
+namespace adios2
+{
+namespace derived
+{
+struct DerivedData
+{
+    void *Data;
+    Dims Start;
+    Dims Count;
+    DataType Type;
+};
+
+struct ExprData
+{
+    std::vector<DerivedData> Data;
+    std::vector<std::string> Const;
+    DataType OutType;
+};
+}
+}
+#endif
