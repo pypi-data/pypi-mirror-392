2590dc9d2b65818d7df4c05d15c0f8779ba61a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:05:41 +0000

    Add package pyleri/1.5.0/pyleri-1.5.0-py3-none-any.whl

---
+++ packages/pyleri/pyleri-1.5.0-py3-none-any.whl/pyleri/endofstatement.py
@@ -0,0 +1,17 @@
+"""end_of_statement Variable.
+
+end_of_statement is an instance of _EndOfStatement and will be added to
+an 'expecting' in a node result when an 'End of Statement' is possible.
+
+:copyright: 2021, Jeroen van der Heijden <jeroen@cesbit.com>
+"""
+from .elements import Element
+
+
+class _EndOfStatement(Element):
+
+    def __repr__(self):
+        return 'end_of_statement'
+
+
+end_of_statement = _EndOfStatement()
