4a01d54361341268ec478481c72b04ec6df2c9dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:05:35 +0000

    Add package san-cli/1.0.7/san_cli-1.0.7.tar.gz

---
+++ packages/san-cli/san_cli-1.0.7.tar.gz/san_cli-1.0.7/neuron_cli/version.py
@@ -0,0 +1,5 @@
+"""Version information for Neuron CLI"""
+
+__version__ = "1.0.6"  # SAN CLI (SPACE Agent Neuron CLI) - Production Release
+__author__ = "Kai Gartner (https://linkedin.com/in/kaigartner)"
+__license__ = "Commercial - All Rights Reserved"
