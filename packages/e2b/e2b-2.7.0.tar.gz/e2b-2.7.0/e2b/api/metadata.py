dd0510f15de87e0b2e86550e34ed1fda21666262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:16 +0000

    Add package e2b/2.7.0/e2b-2.7.0.tar.gz

---
+++ packages/e2b/e2b-2.7.0.tar.gz/e2b-2.7.0/e2b/api/metadata.py
@@ -0,0 +1,18 @@
+import platform
+
+from importlib import metadata
+
+package_version = metadata.version("e2b")
+
+default_headers = {
+    "lang": "python",
+    "lang_version": platform.python_version(),
+    "machine": platform.machine(),
+    "os": platform.platform(),
+    "package_version": metadata.version("e2b"),
+    "processor": platform.processor(),
+    "publisher": "e2b",
+    "release": platform.release(),
+    "sdk_runtime": "python",
+    "system": platform.system(),
+}
