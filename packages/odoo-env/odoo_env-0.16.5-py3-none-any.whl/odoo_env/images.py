e81d9a7c4b98097cb763db1704458e245a1a44e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 13:14:50 +0000

    Add package odoo-env/0.16.5/odoo_env-0.16.5-py3-none-any.whl

---
+++ packages/odoo-env/odoo_env-0.16.5-py3-none-any.whl/odoo_env/images.py
@@ -0,0 +1,62 @@
+from odoo_env.messages import Msg
+
+msg = Msg()
+
+
+class Image:
+    def __init__(self, values):
+        self._dict = values
+
+    @property
+    def short_name(self):
+        return self._dict.get("name")
+
+    @property
+    def version(self):
+        return self._dict.get("ver")
+
+    @property
+    def name(self):
+        ret = self._dict.get("usr")
+        image = self._dict.get("img")
+        ver = self._dict.get("ver")
+
+        if image:
+            ret += "/" + image
+        if ver:
+            ret += ":" + ver
+
+        return ret
+
+
+class Image2:
+    def __init__(self, values, debug=False):
+        _odoo_image = "odoo" in values
+        values = values.split()
+        if len(values) != 2:
+            msg.err(f"Bad image definition {values}")
+        self._name = values[0]
+        self._url = values[1]
+        if _odoo_image and debug:
+            self._url += ".debug"
+
+    @property
+    def short_name(self):
+        return self._name
+
+    @property
+    def version(self):
+        a = self._url
+        ver = a.split(":")
+        if len(ver) == 2:
+            return ver[1]
+        return ""
+
+    @property
+    def name(self):
+        return self._url
+
+    @property
+    def numeric_ver(self):
+        s = self._url
+        return int(float(s.split(":")[1].split("-")[0]))
---
+++ packages/odoo-env/odoo_env-0.16.5-py3-none-any.whl/odoo_env/options.py
@@ -0,0 +1,75 @@
+from odoo_env.config import OeConfig
+from odoo_env.messages import Msg
+
+
+def get_client(args):
+    if args.client:
+        OeConfig().save_client(args.client[0])
+        return args.client[0]
+    client = OeConfig().get_client()
+    if client:
+        return client
+    Msg().err("Need -c option (client name). Process aborted")
+
+
+def get_database(args):
+    if args.database:
+        return args.database[0]
+
+    client = get_client(args)
+    if client:
+        suffix = "_test" if args.quality_assurance else "_prod"
+        default_database = client + suffix
+        Msg().inf(
+            f"Using default database: {default_database}, use -d to "
+            "specify another database."
+        )
+        return default_database
+    Msg().err("Need -c option (client name). Process aborted")
+
+
+def get_module(args):
+    if args.module:
+        return args.module
+    Msg().inf(
+        "Updating all modules. Use -m to specify single module "
+        "or a comma separated list of modules."
+    )
+    return ["all"]
+
+
+def get_backup_file(args):
+    if args.backup_file:
+        return args.backup_file[0]
+
+    Msg().inf("Restoring newest LOCAL backup. Use -f to store specific one.")
+    return False
+
+
+def get_prod_backup_file(args):
+    if args.backup_file:
+        return args.backup_file[0]
+
+    Msg().inf("Restoring newest SERVER backup. Use -f to store specific one.")
+    return False
+
+
+def get_param(args, param):
+    if param == "client":
+        return get_client(args)
+
+    if param == "database":
+        return get_database(args)
+
+    if param == "module":
+        return get_module(args)
+
+    if param == "backup_file":
+        if args.from_prod:
+            return get_prod_backup_file(args)
+        return get_backup_file(args)
+
+    if param == "no-deactivate":
+        if args.no_deactivate:
+            return args.no_deactivate
+        return False
