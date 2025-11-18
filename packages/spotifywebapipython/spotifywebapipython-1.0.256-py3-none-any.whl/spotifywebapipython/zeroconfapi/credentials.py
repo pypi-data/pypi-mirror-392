0d2e7c61efb6095e6e67674f2a193e97a56a39f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 07:05:10 +0000

    Add package spotifywebapipython/1.0.256/spotifywebapipython-1.0.256-py3-none-any.whl

---
+++ packages/spotifywebapipython/spotifywebapipython-1.0.256-py3-none-any.whl/spotifywebapipython/zeroconfapi/credentials.py
@@ -0,0 +1,37 @@
+# our package imports.
+from .authenticationtypes import AuthenticationTypes
+
+class Credentials:
+    """
+    Credentials used by the Spotify SpConnectionLoginBlob method.
+    """
+
+    def __init__(
+            self, 
+            username:str, 
+            password:str,
+            authenticationType:AuthenticationTypes=AuthenticationTypes.USER_PASS,
+            ):
+        """
+        Initializes a new instance of the class.
+        
+        Args:
+            username (str):
+                Spotify Connect user name to login with.  
+            password (str):
+                Spotify Connect user password to login with.  
+            authenticationType (AuthenticationTypes):
+                Authentication type.
+        """
+        # validations.
+        if (authenticationType is None) or (not isinstance(authenticationType, AuthenticationTypes)):
+            authenticationType = AuthenticationTypes.USER_PASS
+        
+        # initialize storage.
+        self.username: bytes = bytes(username, 'ascii')
+        if (isinstance(password, bytes)):
+            self.password: bytes = password
+        else:
+            self.password: bytes = bytes(password, 'ascii')
+        self.auth_type: AuthenticationTypes = authenticationType
+        
