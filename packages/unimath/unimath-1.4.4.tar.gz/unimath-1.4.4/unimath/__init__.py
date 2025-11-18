0edeb938d7d80695ee1d731b7ace36c8aca047ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 18:06:29 +0000

    Add package unimath/1.4.4/unimath-1.4.4.tar.gz

---
+++ packages/unimath/unimath-1.4.4.tar.gz/unimath-1.4.4/unimath/__init__.py
@@ -0,0 +1,34 @@
+from .calculus import sigmanotation,productnatation,factorial
+from .combinatorics import permutation,combination,posibilty
+from .errors import NonCompliancaRecognition,DefinitionError,SizeLimitExceededError,WrongDataTypeError,NonSymbolicValue,RequiredModule
+from .matrix import Matrix
+from .plane_geometry import Vector,Line 
+from .sets import Bool_Integer,Bool_Natural,Bool_RealNumber,Integer
+from .symbolic import SymbolicVariable,SymbolicMatrix,SymbolicExpression,Sequence
+
+__version__ = "1.4.4"
+
+__all__ = [
+    "SymbolicVariable",
+    "SymbolicMatrix",
+    "SymbolicExpression",
+    "Sequence",
+    "Bool_Integer",
+    "Bool_Natural",
+    "Bool_RealNumber",
+    "Integer",
+    "Line",
+    "Vector",
+    "Matrix",
+    "NonSymbolicValue",
+    "RequiredModule",
+    "WrongDataTypeError",
+    "SizeLimitExceededError",
+    "DefinitionError",
+    "NonCompliancaRecognition",
+    "posibilty",
+    "sigmanotation",
+    "productnatation",
+    "permutation",
+    "factorial",
+    "combination"]
