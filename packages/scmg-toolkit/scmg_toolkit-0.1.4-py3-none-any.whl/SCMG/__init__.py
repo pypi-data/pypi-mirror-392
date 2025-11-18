1144a79ef2ccea5bc4813f8c11ec8452740c2b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 16:06:08 +0000

    Add package scmg-toolkit/0.1.4/scmg_toolkit-0.1.4-py3-none-any.whl

---
+++ packages/scmg-toolkit/scmg_toolkit-0.1.4-py3-none-any.whl/SCMG/__init__.py
@@ -0,0 +1,9 @@
+#
+# from ._version import get_versions
+# __version__ = get_versions()['version']
+# del get_versions
+__version__ = "0.1.2"
+
+# Optional: re-export something from models for convenience
+# from .models import *
+from .models.Transformer.model import *
