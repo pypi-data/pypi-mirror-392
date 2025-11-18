06cef5e1fae4068d1bf8a0a1755b9e441b93822a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 03:24:08 +0000

    Add package syside/0.8.2/syside-0.8.2-cp312-abi3-macosx_11_0_arm64.whl

---
+++ packages/syside/syside-0.8.2-cp312-abi3-macosx_11_0_arm64.whl/syside/core/version/__init__.pyi
@@ -0,0 +1,15 @@
+import datetime
+
+
+def date() -> datetime.datetime:
+    """Datetime of when Syside was built"""
+
+major: int = 0
+
+minor: int = 8
+
+patch: int = 2
+
+sha: str = '88a33b518f8cb3111f0cb15ebfee85f882efe4bc'
+
+timestamp: str = '2025-11-14T09:06:35Z'
