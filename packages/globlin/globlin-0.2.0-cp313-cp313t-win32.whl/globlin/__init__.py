a425d1b5159b20801fc998b041de42704d5ba17a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 05:04:06 +0000

    Add package globlin/0.2.0/globlin-0.2.0-cp313-cp313t-win32.whl

---
+++ packages/globlin/globlin-0.2.0-cp313-cp313t-win32.whl/globlin/__init__.py
@@ -0,0 +1,7 @@
+# noqa: D104
+# SPDX-FileCopyrightText: 2025 László Vaskó <opensource@vlaci.email.com>
+#
+# SPDX-License-Identifier: EUPL-1.2
+
+from .globlin import Flag as Flag
+from .globlin import fnmatch as fnmatch
