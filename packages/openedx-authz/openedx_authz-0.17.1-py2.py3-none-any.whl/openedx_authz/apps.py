33a92ff45ce52df9c56ada0259b0a76f83b5bab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 08:04:20 +0000

    Add package openedx-authz/0.17.1/openedx_authz-0.17.1-py2.py3-none-any.whl

---
+++ packages/openedx-authz/openedx_authz-0.17.1-py2.py3-none-any.whl/openedx_authz/apps.py
@@ -0,0 +1,46 @@
+"""
+openedx_authz Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class OpenedxAuthzConfig(AppConfig):
+    """
+    Configuration for the openedx_authz Django application.
+    """
+
+    name = "openedx_authz"
+    verbose_name = "Open edX AuthZ"
+    default_auto_field = "django.db.models.BigAutoField"
+
+    plugin_app = {
+        "url_config": {
+            "lms.djangoapp": {
+                "namespace": "openedx-authz",
+                "regex": r"^api/",
+                "relative_path": "urls",
+            },
+            "cms.djangoapp": {
+                "namespace": "openedx-authz",
+                "regex": r"^api/",
+                "relative_path": "urls",
+            },
+        },
+        "settings_config": {
+            "lms.djangoapp": {
+                "test": {"relative_path": "settings.test"},
+                "common": {"relative_path": "settings.common"},
+                "production": {"relative_path": "settings.production"},
+            },
+            "cms.djangoapp": {
+                "test": {"relative_path": "settings.test"},
+                "common": {"relative_path": "settings.common"},
+                "production": {"relative_path": "settings.production"},
+            },
+        },
+    }
+
+    def ready(self):
+        """Import signal handlers when Django starts."""
+        import openedx_authz.handlers  # pylint: disable=import-outside-toplevel,unused-import
