59a52fb02010fcbe3c5c318fd98f1bd017ad2f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:04:56 +0000

    Add package tmux-language-server/0.0.15/tmux_language_server-0.0.15.tar.gz

---
+++ packages/tmux-language-server/tmux_language_server-0.0.15.tar.gz/tmux_language_server-0.0.15/src/tmux_language_server/server.py
@@ -0,0 +1,186 @@
+r"""Server
+==========
+"""
+
+from typing import Any
+
+from lsp_tree_sitter.complete import get_completion_list_by_enum
+from lsp_tree_sitter.diagnose import get_diagnostics
+from lsp_tree_sitter.finders import PositionFinder
+from lsprotocol.types import (
+    TEXT_DOCUMENT_COMPLETION,
+    TEXT_DOCUMENT_DID_CHANGE,
+    TEXT_DOCUMENT_DID_OPEN,
+    TEXT_DOCUMENT_DOCUMENT_LINK,
+    TEXT_DOCUMENT_HOVER,
+    CompletionItem,
+    CompletionItemKind,
+    CompletionList,
+    CompletionParams,
+    DidChangeTextDocumentParams,
+    DocumentLink,
+    DocumentLinkParams,
+    Hover,
+    MarkupContent,
+    MarkupKind,
+    PublishDiagnosticsParams,
+    TextDocumentPositionParams,
+)
+from pygls.lsp.server import LanguageServer
+
+from .finders import DIAGNOSTICS_FINDER_CLASSES, ImportTmuxFinder
+from .utils import get_schema, parser
+
+
+class TmuxLanguageServer(LanguageServer):
+    r"""Tmux language server."""
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        r"""Init.
+
+        :param args:
+        :type args: Any
+        :param kwargs:
+        :type kwargs: Any
+        :rtype: None
+        """
+        super().__init__(*args, **kwargs)
+        self.trees = {}
+
+        @self.feature(TEXT_DOCUMENT_DID_OPEN)
+        @self.feature(TEXT_DOCUMENT_DID_CHANGE)
+        def did_change(params: DidChangeTextDocumentParams) -> None:
+            r"""Did change.
+
+            :param params:
+            :type params: DidChangeTextDocumentParams
+            :rtype: None
+            """
+            document = self.workspace.get_text_document(
+                params.text_document.uri
+            )
+            self.trees[document.uri] = parser.parse(document.source.encode())
+            diagnostics = get_diagnostics(
+                document.uri,
+                self.trees[document.uri],
+                DIAGNOSTICS_FINDER_CLASSES,
+                "tmux",
+            )
+            self.text_document_publish_diagnostics(
+                PublishDiagnosticsParams(
+                    params.text_document.uri,
+                    diagnostics,
+                )
+            )
+
+        @self.feature(TEXT_DOCUMENT_DOCUMENT_LINK)
+        def document_link(params: DocumentLinkParams) -> list[DocumentLink]:
+            r"""Get document links.
+
+            :param params:
+            :type params: DocumentLinkParams
+            :rtype: list[DocumentLink]
+            """
+            document = self.workspace.get_text_document(
+                params.text_document.uri
+            )
+            return ImportTmuxFinder().get_document_links(
+                document.uri, self.trees[document.uri]
+            )
+
+        @self.feature(TEXT_DOCUMENT_HOVER)
+        def hover(params: TextDocumentPositionParams) -> Hover | None:
+            r"""Hover.
+
+            :param params:
+            :type params: TextDocumentPositionParams
+            :rtype: Hover | None
+            """
+            document = self.workspace.get_text_document(
+                params.text_document.uri
+            )
+            uni = PositionFinder(params.position).find(
+                document.uri, self.trees[document.uri]
+            )
+            if uni is None:
+                return None
+            text = uni.text
+            result = None
+            if uni.node.range.start_point[1] == 0:
+                result = get_schema()["properties"].get(text)
+            elif uni.node.type == "option":
+                result = get_schema()["properties"]["set-option"][
+                    "properties"
+                ].get(text)
+            if result is None:
+                return None
+            return Hover(
+                MarkupContent(MarkupKind.Markdown, result["description"]),
+                uni.range,
+            )
+
+        @self.feature(TEXT_DOCUMENT_COMPLETION)
+        def completions(params: CompletionParams) -> CompletionList:
+            r"""Completions.
+
+            :param params:
+            :type params: CompletionParams
+            :rtype: CompletionList
+            """
+            document = self.workspace.get_text_document(
+                params.text_document.uri
+            )
+            uni = PositionFinder(params.position, right_equal=True).find(
+                document.uri, self.trees[document.uri]
+            )
+            if uni is None:
+                return CompletionList(False, [])
+            text = uni.text
+            if uni.node.range.start_point[1] == 0:
+                return CompletionList(
+                    False,
+                    [
+                        CompletionItem(
+                            x,
+                            kind=CompletionItemKind.Keyword,
+                            documentation=MarkupContent(
+                                MarkupKind.Markdown, property["description"]
+                            ),
+                            insert_text=x,
+                        )
+                        for x, property in get_schema()["properties"].items()
+                        if x.startswith(text)
+                    ],
+                )
+            elif uni.node.type == "option":
+                return CompletionList(
+                    False,
+                    [
+                        CompletionItem(
+                            x,
+                            kind=CompletionItemKind.Variable,
+                            documentation=MarkupContent(
+                                MarkupKind.Markdown, property["description"]
+                            ),
+                            insert_text=x,
+                        )
+                        for x, property in get_schema()["properties"][
+                            "set-option"
+                        ]["properties"].items()
+                        if x.startswith(text)
+                    ],
+                )
+            parent = uni.node
+            while parent and not (
+                len(parent.children) > 2
+                and parent.children[-2].type == "option"
+            ):
+                parent = parent.parent
+            if parent:
+                return get_completion_list_by_enum(
+                    text,
+                    get_schema()["properties"]["set-option"]["properties"].get(
+                        uni.node2text(parent.children[-2]), {}
+                    ),
+                )
+            return CompletionList(False, [])
