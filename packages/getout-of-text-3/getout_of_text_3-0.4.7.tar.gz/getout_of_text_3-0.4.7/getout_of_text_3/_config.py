d85ef8386135872d1e28ce7a1e75aa9474186830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 08:04:25 +0000

    Add package getout-of-text-3/0.4.7/getout_of_text_3-0.4.7.tar.gz

---
+++ packages/getout-of-text-3/getout_of_text_3-0.4.7.tar.gz/getout_of_text_3-0.4.7/getout_of_text_3/_config.py
@@ -0,0 +1,13 @@
+
+
+class Options:
+
+# basic options boilerplate here
+    def __init__(self):
+        self.verbose = False
+        self.log_file = None
+        self.temp_dir = None
+        self.data_dir = None
+
+
+options = Options()
