37c469f7c9674fa0c7aaf373da4831d8b4d92638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 23:05:23 +0000

    Add package ssnipbox/1.0.0/ssnipbox-1.0.0-py3-none-any.whl

---
+++ packages/ssnipbox/ssnipbox-1.0.0-py3-none-any.whl/ssnipbox/__init__.py
@@ -0,0 +1,14 @@
+"""
+ssnipbox package
+A simple package that returns code snippets for questions q1–q14.
+"""
+
+from .questions import (
+    q1, q2, q3, q4, q5, q6, q7,
+    q8, q9, q10, q11, q12, q13, q14
+)
+
+__all__ = [
+    "q1", "q2", "q3", "q4", "q5", "q6", "q7",
+    "q8", "q9", "q10", "q11", "q12", "q13", "q14"
+]
