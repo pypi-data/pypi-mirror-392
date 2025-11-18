03bd081056af2576519b8a5c8305ae51e7cd2837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 20:06:29 +0000

    Add package 2025-assignment2-vaultPasswordManager/1.0.0/2025_assignment2_vaultpasswordmanager-1.0.0.tar.gz

---
+++ packages/2025-assignment2-vaultPasswordManager/2025_assignment2_vaultpasswordmanager-1.0.0.tar.gz/2025_assignment2_vaultpasswordmanager-1.0.0/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+try:
+    README = (HERE / "README.md").read_text()
+except FileNotFoundError:
+    README = "Python Password Manager"
+
+setup(
+    name='2025_assignment2_vaultPasswordManager',
+    version='1.0.0',  
+    description='Python Password Manager',
+    long_description=README,
+    long_description_content_type='text/markdown', 
+    author='EmacsSoftwares',
+    author_email='matteo.broglio3@gmail.com',
+    url='https://gitlab.com/mbroglio/2025_assignment2_vaultPasswordManager',
+    license='MIT',
+
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+
+    install_requires=[
+        'pycryptodome==3.20.0',
+        'pyperclip',
+        'tabulate',
+        'passwordgenerator',
+        'SQLAlchemy==1.4.41',
+        'sqlcipher3==0.5.4'
+    ],
+    entry_points={
+        'console_scripts': [
+            'vault = vault.vault:main',
+        ],
+    },
+    classifiers=[
+        'Topic :: Security',
+        'Topic :: Security :: Cryptography',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+    ],
+)
