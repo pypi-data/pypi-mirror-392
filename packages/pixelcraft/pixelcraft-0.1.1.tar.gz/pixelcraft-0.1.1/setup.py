b9d980497d1bae839d5630f51a99723be4113253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 18:06:12 +0000

    Add package pixelcraft/0.1.1/pixelcraft-0.1.1.tar.gz

---
+++ packages/pixelcraft/pixelcraft-0.1.1.tar.gz/pixelcraft-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pixelcraft",
+    version="0.1.1",
+    author="Shrey Kalkhnday",
+    author_email="developershreyk@gmail.com",
+    description="A lightweight Python gaming utility library with vector math, collisions, timers, and movement helpers.",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Games/Entertainment",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
