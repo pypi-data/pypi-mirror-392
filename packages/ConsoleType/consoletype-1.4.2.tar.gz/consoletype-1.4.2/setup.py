4266e033803f8df0fee09ea6652e9609f6671a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 06:05:18 +0000

    Add package ConsoleType/1.4.2/consoletype-1.4.2.tar.gz

---
+++ packages/ConsoleType/consoletype-1.4.2.tar.gz/consoletype-1.4.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ConsoleType',
+    version='1.4.2',
+    packages=find_packages(),
+    description='Update README',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Vadim | Mur Studio',
+    author_email='somerare23@gmail.com',
+    url='https://github.com/Vaddlkk',
+    install_requires=[],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8, <3.15"
+)
