4364153d22ef710c35e39d9cdbfefeaf182b9a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 05:05:35 +0000

    Add package subnoto-api-client/1.0.6/subnoto_api_client-1.0.6-py3-none-manylinux2014_x86_64.whl

---
+++ packages/subnoto-api-client/subnoto_api_client-1.0.6-py3-none-manylinux2014_x86_64.whl/subnoto_api_client/generated/models/post_public_contact_get_response_200.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+from collections.abc import Mapping
+from typing import TYPE_CHECKING, Any, TypeVar
+
+from attrs import define as _attrs_define
+
+if TYPE_CHECKING:
+    from ..models.post_public_contact_get_response_200_contact import (
+        PostPublicContactGetResponse200Contact,
+    )
+
+
+T = TypeVar("T", bound="PostPublicContactGetResponse200")
+
+
+@_attrs_define
+class PostPublicContactGetResponse200:
+    """
+    Attributes:
+        contact (PostPublicContactGetResponse200Contact):
+    """
+
+    contact: PostPublicContactGetResponse200Contact
+
+    def to_dict(self) -> dict[str, Any]:
+        contact = self.contact.to_dict()
+
+        field_dict: dict[str, Any] = {}
+
+        field_dict.update(
+            {
+                "contact": contact,
+            }
+        )
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: type[T], src_dict: Mapping[str, Any]) -> T:
+        from ..models.post_public_contact_get_response_200_contact import (
+            PostPublicContactGetResponse200Contact,
+        )
+
+        d = dict(src_dict)
+        contact = PostPublicContactGetResponse200Contact.from_dict(d.pop("contact"))
+
+        post_public_contact_get_response_200 = cls(
+            contact=contact,
+        )
+
+        return post_public_contact_get_response_200
