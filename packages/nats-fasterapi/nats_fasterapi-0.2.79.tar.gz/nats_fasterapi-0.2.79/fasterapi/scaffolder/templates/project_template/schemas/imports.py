0072c069448445d887d4b6680ca8b9314e5e4680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 06:04:41 +0000

    Add package nats-fasterapi/0.2.79/nats_fasterapi-0.2.79.tar.gz

---
+++ packages/nats-fasterapi/nats_fasterapi-0.2.79.tar.gz/nats_fasterapi-0.2.79/fasterapi/scaffolder/templates/project_template/schemas/imports.py
@@ -0,0 +1,8 @@
+from bson import ObjectId
+from pydantic import GetJsonSchemaHandler
+from pydantic import BaseModel, EmailStr, Field,model_validator
+from pydantic_core import core_schema
+from datetime import datetime,timezone
+from typing import Optional,List,Any
+from enum import Enum
+import time
