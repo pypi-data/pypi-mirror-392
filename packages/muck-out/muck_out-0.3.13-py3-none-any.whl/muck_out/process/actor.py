7a8f558f7a7164d923eb428c2e6c8b255c132c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 19:05:13 +0000

    Add package muck-out/0.3.13/muck_out-0.3.13-py3-none-any.whl

---
+++ packages/muck-out/muck_out-0.3.13-py3-none-any.whl/muck_out/process/actor.py
@@ -0,0 +1,52 @@
+import logging
+from typing import Any
+from urllib.parse import urlparse
+
+from muck_out.types import ActorStub, Actor
+
+logger = logging.getLogger(__name__)
+
+
+def calculate_identifiers(stub: ActorStub) -> list[str]:
+    if not stub.id:
+        return []
+    try:
+        domain = urlparse(stub.id).netloc
+
+        if stub.preferred_username:
+            return [f"acct:{stub.preferred_username}@{domain}", stub.id]
+
+        return [stub.id]
+    except Exception:
+        return []
+
+
+def actor_stub(data: dict[str, Any]) -> ActorStub:
+    """Returns the stub actor"""
+    stub = ActorStub.model_validate(data)
+
+    if data.get("identifiers") is None:
+        stub.identifiers = calculate_identifiers(stub)
+
+    return stub
+
+
+def normalize_actor(data: dict[str, Any]) -> Actor | None:
+    """Normalizes an ActivityPub actor"""
+
+    try:
+        stub = actor_stub(data)
+
+        if stub.inbox is None:
+            return None
+
+        if stub.identifiers is None or len(stub.identifiers) == 0:
+            if stub.id is None:
+                return
+            stub.identifiers = [stub.id]
+
+        return Actor.model_validate(stub.model_dump(by_alias=True))
+
+    except Exception as e:
+        logger.info(e)
+        return None
