00d7d0c696ab933727f1020d8761e30dfeeede5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 13:14:46 +0000

    Add package starco-dj-utils/1.0.2/starco_dj_utils-1.0.2-py3-none-any.whl

---
+++ packages/starco-dj-utils/starco_dj_utils-1.0.2-py3-none-any.whl/dj_utils/tasks_config.py
@@ -0,0 +1,11 @@
+try:
+    from core.celery_config import app
+    import os
+    app.conf.beat_schedule = {
+        'run-backup-task': {
+            'task': 'utils.tasks.backup_database',
+            'schedule': int(os.getenv('BACKUP_EVERY_SECOND_EV')),  # seconds
+        },
+
+    }
+except:pass
