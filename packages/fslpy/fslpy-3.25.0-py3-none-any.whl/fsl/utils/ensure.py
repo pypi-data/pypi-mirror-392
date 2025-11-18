05fb57c19caf5e337cf577dd57ad98c17be44a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 03:23:16 +0000

    Add package fslpy/3.25.0/fslpy-3.25.0-py3-none-any.whl

---
+++ packages/fslpy/fslpy-3.25.0-py3-none-any.whl/fsl/utils/ensure.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#
+# ensure.py - Functions which ensure things.
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+"""This module contains a handful of utility functions which attempt to ensure
+that some condition is met.
+
+.. autosummary::
+   :nosignatures:
+
+   ensureIsImage
+"""
+
+import pathlib
+
+import nibabel as nib
+
+import fsl.data.image as fslimage
+
+
+def ensureIsImage(img):
+    """Ensures that the given ``img`` is an in-memory ``nibabel`` object.
+    """
+    if isinstance(img, (str, pathlib.Path)):
+        img = fslimage.addExt(img)
+        img = nib.load(img)
+    return img
