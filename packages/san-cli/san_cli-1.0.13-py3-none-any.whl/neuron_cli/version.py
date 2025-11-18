ab4330ab13e0869b6280bebc4dc11ffa94a6b925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:04:56 +0000

    Add package san-cli/1.0.13/san_cli-1.0.13-py3-none-any.whl

---
+++ packages/san-cli/san_cli-1.0.13-py3-none-any.whl/neuron_cli/version.py
@@ -0,0 +1,5 @@
+"""Version information for Neuron CLI"""
+
+__version__ = "1.0.13"  # SAN CLI (SPACE Agent Neuron CLI) - Secure Registry + Auto-Config
+__author__ = "Kai Gartner (https://linkedin.com/in/kaigartner)"
+__license__ = "Commercial - All Rights Reserved"
