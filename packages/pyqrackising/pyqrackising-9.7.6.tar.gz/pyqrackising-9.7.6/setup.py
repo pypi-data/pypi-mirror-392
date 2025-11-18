4b125ea583918d852696097b3b9ef086512e22f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:05:18 +0000

    Add package pyqrackising/9.7.6/pyqrackising-9.7.6.tar.gz

---
+++ packages/pyqrackising/pyqrackising-9.7.6.tar.gz/pyqrackising-9.7.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+
+README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')
+with open(README_PATH) as readme_file:
+    README = readme_file.read()
+
+setup(
+    name='pyqrackising',
+    version='9.7.6',
+    author='Dan Strano',
+    author_email='stranoj@gmail.com',
+    description='Fast MAXCUT, TSP, and sampling heuristics from near-ideal transverse field Ising model (TFIM)',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url="https://github.com/vm6502q/PyQrackIsing",
+    license="LGPL-3.0-or-later",
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: C++",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering",
+    ],
+    packages=['pyqrackising'],
+    zip_safe=False,
+)
