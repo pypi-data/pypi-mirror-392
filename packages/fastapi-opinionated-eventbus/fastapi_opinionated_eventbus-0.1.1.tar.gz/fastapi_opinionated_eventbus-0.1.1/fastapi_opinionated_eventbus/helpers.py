7a33c0aa57c46356c84c332d3b56f2b015edce17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 13:14:51 +0000

    Add package fastapi-opinionated-eventbus/0.1.1/fastapi_opinionated_eventbus-0.1.1.tar.gz

---
+++ packages/fastapi-opinionated-eventbus/fastapi_opinionated_eventbus-0.1.1.tar.gz/fastapi_opinionated_eventbus-0.1.1/fastapi_opinionated_eventbus/helpers.py
@@ -0,0 +1,50 @@
+#fastapi-opinionated-eventbus/fastapi_opinionated_eventbus/helpers.py
+import asyncio
+import traceback
+import inspect
+from fastapi_opinionated.shared.logger import ns_logger
+from fastapi_opinionated.exceptions.plugin_exception import PluginException
+from fastapi_opinionated.app import App
+from fastapi_opinionated.registry.plugin_store import PluginRegistryStore
+from fastapi_opinionated.registry.plugin import PluginRegistry
+logger = ns_logger("EventBus")
+
+class _EventBus:
+    @staticmethod
+    async def emit(event_name: str, /, *args, **kwargs):
+        from fastapi_opinionated_eventbus.plugin import EventBusPlugin
+        
+        try:
+            handlers = EventBusPlugin._handlers
+            tasks = []
+            for handler in handlers:
+                # jika async → await
+                if inspect.iscoroutinefunction(handler):
+                    tasks.append(handler(*args, **kwargs))
+                else:
+                    # jika sync, jalanin di event loop
+                    tasks.append(asyncio.to_thread(handler, *args, **kwargs))
+
+            if tasks:
+                logger.info(f"Emitting event '{event_name}' to {len(tasks)} handlers.")
+                await asyncio.gather(*tasks)
+        except Exception as e:
+            traceback.print_exc()
+            raise PluginException(EventBusPlugin.public_name, f"Error emitting event '{event_name}': {e}") from e
+        
+def eventbus_api()->_EventBus:
+    from fastapi_opinionated_eventbus.plugin import EventBusPlugin
+    PluginRegistry.ensure_enabled(EventBusPlugin.public_name)
+    return App.plugin.eventbus
+
+def OnInternalEvent(event_name: str):
+    from fastapi_opinionated_eventbus.plugin import EventBusPlugin
+    PluginRegistry.ensure_enabled(EventBusPlugin.public_name)
+    def wrapper(func):
+        PluginRegistryStore.add(EventBusPlugin.public_name, "internal_event_handlers", {
+            "event": event_name,
+            "handler": func,
+        })
+        return func
+    return wrapper
+    
