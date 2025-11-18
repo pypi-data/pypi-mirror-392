c0d7530273cdb4e146e53e3ebef60cd4d0edab72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 13:14:47 +0000

    Add package maleo-metadata/0.6.50/maleo_metadata-0.6.50-py3-none-any.whl

---
+++ packages/maleo-metadata/maleo_metadata-0.6.50-py3-none-any.whl/maleo/metadata/constants/service.py
@@ -0,0 +1,7 @@
+from maleo.schemas.resource import Resource, ResourceIdentifier
+
+
+SERVICE_RESOURCE = Resource(
+    identifiers=[ResourceIdentifier(key="service", name="Service", slug="services")],
+    details=None,
+)
