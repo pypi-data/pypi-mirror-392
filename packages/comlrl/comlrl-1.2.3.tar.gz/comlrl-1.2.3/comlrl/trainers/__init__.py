fedec53423fa81b938d590b0b441cbe0ea67e47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:16 +0000

    Add package comlrl/1.2.3/comlrl-1.2.3.tar.gz

---
+++ packages/comlrl/comlrl-1.2.3.tar.gz/comlrl-1.2.3/comlrl/trainers/__init__.py
@@ -0,0 +1,5 @@
+from .ippo import IPPOConfig, IPPOTrainer
+from .magrpo import MAGRPOConfig, MAGRPOTrainer
+from .mareinforce import MAREINFORCEConfig, MAREINFORCETrainer
+from .maremax import MAReMaxTrainer
+from .marloo import MARLOOTrainer
