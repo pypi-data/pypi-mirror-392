7179d6908140d52faef985b7d504fd86b8df27f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 08:04:18 +0000

    Add package vercel-workers/0.0.1/vercel_workers-0.0.1-py3-none-any.whl

---
+++ packages/vercel-workers/vercel_workers-0.0.1-py3-none-any.whl/vercel/workers/__init__.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from .client import (
+    MessageMetadata,
+    WorkerTimeoutResult,
+    has_subscriptions,
+    subscribe,
+    wsgi_app,
+)
+
+__all__ = [
+    "MessageMetadata",
+    "WorkerTimeoutResult",
+    "subscribe",
+    "wsgi_app",
+    "has_subscriptions",
+]
+
+
+
