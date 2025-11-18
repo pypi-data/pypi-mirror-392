d4503ae45377e5dc0c35a7f6cf9e6a850d02fd9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 19:05:08 +0000

    Add package matplotlab/0.1.0/matplotlab-0.1.0-py3-none-any.whl

---
+++ packages/matplotlab/matplotlab-0.1.0-py3-none-any.whl/speech/__init__.py
@@ -0,0 +1,17 @@
+"""
+Speech Processing Module
+========================
+
+Coming soon: Implementations from Speech Processing coursework
+
+Planned features:
+- Audio signal processing
+- Feature extraction (MFCC, spectrograms)
+- Speech recognition utilities
+- Audio preprocessing
+"""
+
+__version__ = "0.1.0"
+
+# Functions will be added during Speech module development
+__all__ = []
