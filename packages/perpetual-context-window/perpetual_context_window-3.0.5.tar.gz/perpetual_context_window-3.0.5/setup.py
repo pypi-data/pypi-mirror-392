31d5108e51541edda0ac4d5c207e065b37d67006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 20:06:23 +0000

    Add package perpetual-context-window/3.0.5/perpetual_context_window-3.0.5.tar.gz

---
+++ packages/perpetual-context-window/perpetual_context_window-3.0.5.tar.gz/perpetual_context_window-3.0.5/setup.py
@@ -0,0 +1,22 @@
+# Module developed by Sapiens Technology® for building language model algorithms with a perpetual context window without complete forgetting of initial, intermediate, and final information.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from setuptools import setup, find_packages
+package_name = 'perpetual_context_window'
+version = '3.0.5'
+setup(
+    name=package_name,
+    version=version,
+    author='SAPIENS TECHNOLOGY',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'infinite-context==3.0.4',
+        'perpetual-context==2.0.3',
+        'sapiens-infinite-context-window==1.0.5',
+        'certifi==2024.2.2'
+    ],
+    url='https://github.com/sapiens-technology/perpetual_context_window',
+    license='Proprietary Software'
+)
+# Module developed by Sapiens Technology® for building language model algorithms with a perpetual context window without complete forgetting of initial, intermediate, and final information.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
