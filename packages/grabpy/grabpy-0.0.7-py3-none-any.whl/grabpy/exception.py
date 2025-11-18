8590c0411f9f8efbf6d32d2afba23fb643a20678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 13:14:49 +0000

    Add package grabpy/0.0.7/grabpy-0.0.7-py3-none-any.whl

---
+++ packages/grabpy/grabpy-0.0.7-py3-none-any.whl/grabpy/exception.py
@@ -0,0 +1,71 @@
+from typing import IO
+
+
+class GrabpyException(Exception):
+    """Base Exception class for Grabpy package."""
+
+    def __init__(self, msg: str) -> None:
+        self.message: str = msg
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class HTTPError(GrabpyException):
+    """HTTP Exception class for Grabpy package."""
+
+    def __init__(self, msg: str, url: str) -> None:
+        super().__init__(msg)
+        self.url: str = url
+
+    def __str__(self) -> str:
+        return f'[{self.url}] {self.message}'
+
+
+class FileError(GrabpyException):
+    """File Exception class for Grabpy package."""
+
+    def __init__(self, msg: str, file: IO) -> None:
+        super().__init__(msg)
+        self.file: IO = file
+
+    def __str__(self) -> str:
+        return f'[{self.file.name}] {self.message}'
+
+
+class HTTPStatusError(HTTPError):
+    """HTTP Exception Status class for Grabpy package."""
+
+    def __init__(self, msg: str, url: str, code: int) -> None:
+        super().__init__(msg, url)
+        self.code = code
+
+    def __str__(self) -> str:
+        return f'[{self.url}<code:{self.code}>] {self.message}'
+
+
+class FileNotSeekableError(GrabpyException):
+    """Exception raised when file.seekable() returns False."""
+
+    def __init__(self, file: IO) -> None:
+        super().__init__('Not seekable.')
+        self.file = file
+
+
+class HTTPTimeoutError(HTTPError):
+    """Exception raised when request times out."""
+
+    def __init__(self, url: str, timeout: float | tuple[float, float]) -> None:
+        super().__init__('Timed out.', url)
+        self.timeout: float = timeout
+
+    def __str__(self) -> str:
+        return f'[{self.url}<timeout:{self.timeout}>] {self.message}'
+
+
+class HTTPNotFoundError(HTTPStatusError):
+    """Exception raised when server returns a 404 status code."""
+
+    def __init__(self, url: str) -> None:
+        super().__init__('Not found.', url, 404)
+
---
+++ packages/grabpy/grabpy-0.0.7-py3-none-any.whl/grabpy/scraper.py
@@ -0,0 +1,55 @@
+from .request import Requester
+from .robots import RobotsParser
+from .exception import GrabpyException
+from functools import lru_cache
+from uuid import uuid4
+import shutil
+import os
+
+
+class Grabber:
+    def __init__(self, useragent: str, retries: int = 3) -> None:
+        """Set retries to -1 to retry indefinitely"""
+
+        self.robots_parser = RobotsParser(useragent)
+        self.requester = Requester(retries)
+
+    def __enter__(self) -> 'Grabber':
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        return
+
+    @lru_cache(maxsize=8, typed=True)
+    def get(self, url: str) -> bytes:
+        parser = self.robots_parser.get_parser(url)
+
+        if not self.robots_parser.can_scrape(parser, url):
+            return b''
+
+        delay: float = self.robots_parser.scrape_delay(parser)
+
+        return self.requester.fetch(url, delay=delay)
+
+    def download(self, url: str, fp: str) -> bool:
+        parser = self.robots_parser.get_parser(url)
+
+        if not self.robots_parser.can_scrape(parser, url):
+            return False
+
+        delay: float = self.robots_parser.scrape_delay(parser)
+        temp: str = f'{uuid4()}.parts'
+        succeeded: bool = True
+
+        with open(temp, 'wb') as f:
+            try:
+                self.requester.stream(url, delay, f)
+            except GrabpyException:
+                succeeded = False
+
+        if not succeeded:
+            os.remove(temp)
+            return False
+        else:
+            shutil.move(temp, fp)
+            return True
