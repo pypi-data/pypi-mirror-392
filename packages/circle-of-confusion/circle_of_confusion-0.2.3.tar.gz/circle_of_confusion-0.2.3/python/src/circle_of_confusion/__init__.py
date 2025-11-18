e9d4cb564b8a7c24970f4160b57159515077fffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 06:04:41 +0000

    Add package circle-of-confusion/0.2.3/circle_of_confusion-0.2.3.tar.gz

---
+++ packages/circle-of-confusion/circle_of_confusion-0.2.3.tar.gz/circle_of_confusion-0.2.3/python/src/circle_of_confusion/__init__.py
@@ -0,0 +1,19 @@
+"""Library to calculate the Circle of Confusion for specified variables."""
+
+# ruff: noqa: F401
+
+from _circle_of_confusion.circle_of_confusion_pb2 import (
+    CameraData,
+    Filmback,
+    Math,
+    Resolution,
+    Settings,
+    WorldUnit,
+)
+
+from circle_of_confusion._exception import CircleOfConfusionError
+from circle_of_confusion._ffi import (
+    Calculator,
+    calculate,
+    initialize_calculator,
+)
