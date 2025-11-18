a2e59b8e1ed8d8e0b1fe1df557691b6d177fd26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 05:04:06 +0000

    Add package virus-simulation/1.0.2/virus_simulation-1.0.2.tar.gz

---
+++ packages/virus-simulation/virus_simulation-1.0.2.tar.gz/virus_simulation-1.0.2/virus_simulation/2d.py
@@ -0,0 +1,108 @@
+from typing import List
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
+
+class Human:
+    """
+    'infection_chance' is invert of the probability, for example if infection probability is 1% then infection_chance = 100
+    'death_chance' is invert of the probability, for example if death probability is 1% then death_chance = 100
+    """
+    def __init__(self, infection_chance: int = 100, death_chance: int = 100):
+        self.infected_until = 0
+        self.immune_until = 0
+        self.dead = False
+        self.infection_chance = infection_chance
+        self.death_chance = death_chance
+
+    def infect(self, current_time, infection_duration, immunity_duration):
+        if self.dead:
+            return
+        if self.immune_until < current_time and self.immune_until != -1 and np.random.randint(self.infection_chance) == 0:
+            self.infected_until = current_time + infection_duration
+            self.immune_until = current_time + infection_duration + immunity_duration
+            if self.death_chance > 0 and np.random.randint(self.death_chance) == 0:
+                self.dead = True
+
+    @classmethod
+    def infect_neighbours(cls, x, y, current_time, infection_duration, immunity_duration):
+        if y-1 >= 0:
+            grid[y-1][x].infect(current_time, infection_duration, immunity_duration)
+        if y+1 < SIZE:
+            grid[y+1][x].infect(current_time, infection_duration, immunity_duration)
+        if x-1 >= 0:
+            grid[y][x-1].infect(current_time, infection_duration, immunity_duration)
+        if x+1 < SIZE:
+            grid[y][x+1].infect(current_time, infection_duration, immunity_duration)
+
+    def step(self, x, y, current_time, infection_duration, immunity_duration):
+        if self.dead:
+            return
+        if self.infected_until >= current_time and self.infected_until != current_time + infection_duration:
+            Human.infect_neighbours(x, y, current_time, infection_duration, immunity_duration)
+
+    def __int__(self):
+        return int(self.infected_until >= current_time) if not self.dead else 2
+
+
+SIZE = int(input("Grid size: "))
+if SIZE < 1:
+    raise ValueError("Size < 1")
+INFECTION_DURATION = int(input("Infection duration: "))
+if INFECTION_DURATION < 0:
+    raise ValueError("Infection duration < 0")
+IMMUNITY_DURATION = int(input("Immunity duration: "))
+if IMMUNITY_DURATION < 0:
+    raise ValueError("Immunity duration < 0")
+INFECTION_CHANCE = int(input("Infection probability: 1/"))
+if INFECTION_CHANCE < 1:
+    raise ValueError("Infection chance < 1")
+_DEATH_CHANCE = input("Death probability (Press Enter to skip): 1/")
+TIME_DURATION = float(input("1 Unit of time (in seconds): "))
+if TIME_DURATION < 0:
+    raise ValueError("Time duration < 0")
+
+if len(_DEATH_CHANCE) == 0:
+    DEATH_CHANCE = 0
+else:
+    DEATH_CHANCE = int(_DEATH_CHANCE)
+    if DEATH_CHANCE < 0:
+        raise ValueError("Death chance < 0")
+
+grid: List[List[Human]] = [[Human(INFECTION_CHANCE, DEATH_CHANCE) for __ in range(SIZE)] for _ in range(SIZE)]
+current_time = 0
+total_infected = 0
+
+grid[np.random.randint(SIZE)][np.random.randint(SIZE)].infected_until = INFECTION_DURATION + 1
+
+fig, ax = plt.subplots()
+cmap = ListedColormap(["green", "red", "black"])
+im = ax.imshow([[0]], cmap=cmap, vmin=0, vmax=2)
+text = fig.text(0.02, 0.9, "Current time: 0\nTotal infected: 0")
+plt.axis('off')
+plt.ion()
+plt.show()
+
+try:
+    while True:
+        current_time += 1
+
+        for x in range(SIZE):
+            for y in range(SIZE):
+                grid[y][x].step(x, y, current_time, INFECTION_DURATION, IMMUNITY_DURATION)
+
+        total_infected = 0
+        dead = 0
+        for x in range(SIZE):
+            for y in range(SIZE):
+                if grid[y][x].dead:
+                    dead += 1
+                elif grid[y][x].infected_until >= current_time:
+                    total_infected += 1
+
+        n_grid = [[int(human) for human in column] for column in grid]
+        im.set_data(n_grid)
+        text.set_text(f"Current time: {current_time} | Infected: {total_infected} | Dead: {dead} | Alive-uninfected: {SIZE*SIZE-total_infected-dead}")
+        plt.pause(TIME_DURATION)
+except KeyboardInterrupt:
+    exit()
