91d4f59debe4b3632d7bdccec5e0da9ce60f0a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 22:08:14 +0000

    Add package matplotlab/0.1.2/matplotlab-0.1.2-py3-none-any.whl

---
+++ packages/matplotlab/matplotlab-0.1.2-py3-none-any.whl/matplotlab/__init__.py
@@ -0,0 +1,23 @@
+"""
+Matplotlab - Extended Plotting and ML Utilities
+================================================
+
+A comprehensive library covering:
+- Reinforcement Learning (rl)
+- Artificial Neural Networks (ann)
+
+Usage:
+    from matplotlab import rl, ann
+
+Author: ML Community
+Date: 2025
+"""
+
+__version__ = "0.1.0"
+__author__ = "ML Community"
+
+# Import submodules
+from . import rl
+from . import ann
+
+__all__ = ["rl", "ann"]
