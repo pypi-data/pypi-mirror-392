3130ebca4e1c518e75badd2564fadc09c2dfc8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 05:04:41 +0000

    Add package e-invoice-api/1.17.0/e_invoice_api-1.17.0.tar.gz

---
+++ packages/e-invoice-api/e_invoice_api-1.17.0.tar.gz/e_invoice_api-1.17.0/src/e_invoice_api/types/webhook_response.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+
+from .._models import BaseModel
+
+__all__ = ["WebhookResponse"]
+
+
+class WebhookResponse(BaseModel):
+    id: str
+
+    events: List[str]
+
+    secret: str
+
+    url: str
+
+    enabled: Optional[bool] = None
