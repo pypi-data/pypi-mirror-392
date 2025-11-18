4c9a746730bbab79f25f96ddf192fad3b8d19cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:05:50 +0000

    Add package allianceauth-auth-stats/0.0.2/allianceauth_auth_stats-0.0.2.tar.gz

---
+++ packages/allianceauth-auth-stats/allianceauth_auth_stats-0.0.2.tar.gz/allianceauth_auth_stats-0.0.2/authstats/api.py
@@ -0,0 +1,147 @@
+from __future__ import annotations
+
+import json
+import logging
+from datetime import timedelta
+from typing import List
+
+from ninja import Field, NinjaAPI, Schema
+from ninja.pagination import LimitOffsetPagination
+from ninja.security import django_auth
+from ninja.types import DictStrAny
+
+from django.contrib.auth.models import User
+from django.db.models import QuerySet
+from django.utils import timezone
+
+from allianceauth.eveonline.models import EveCharacter, EveCorporationInfo
+from esi.models import Token
+
+from authstats import schema
+from authstats.tasks import run_report_for_corp
+
+from . import providers
+from .models import AuthReportsConfiguration, Report, ReportResults
+
+logger = logging.getLogger(__name__)
+
+
+class Paginator(LimitOffsetPagination):
+    class Input(Schema):
+        limit: int = Field(1000000, ge=1)
+        offset: int = Field(0, ge=0)
+
+    def paginate_queryset(
+        self,
+        queryset: QuerySet,
+        pagination: Input,
+        **params: DictStrAny,
+    ) -> any:
+        offset = pagination.offset
+        limit: int = pagination.limit
+        return {
+            "items": queryset[offset: offset + limit],
+            "count": self._items_count(queryset),
+        }
+
+
+api = NinjaAPI(
+    title="Auth Stats API",
+    version="0.0.3",
+    urls_namespace='authstats:api',
+    auth=django_auth
+)
+# openapi_url=settings.DEBUG and "/openapi.json" or "")
+
+
+@api.get(
+    "/get_report/{report_id}/{corp_id}",
+    response={200: dict, 403: str},
+    tags=["Report"]
+)
+def get_report_for_corp(request, report_id: int, corp_id: int):
+    if not AuthReportsConfiguration.get_solo().visible_corps_for_user(request.user).filter(corporation_id=corp_id).exists():
+        return 403, f"Access Denied to get_report_for_corp {report_id}/{corp_id} for {request.user}"
+    try:
+        report = ReportResults.objects.get(
+            corporation__corporation_id=corp_id, report_id=report_id)
+        if report.report.restricted and not request.user.has_perm("authstats.restricted_reports"):
+            return 403, f"Access Denied to get_report_for_corp {report_id}/{corp_id} for {request.user}"
+        if report.last_update < timezone.now() - timedelta(hours=1):
+            run_report_for_corp.delay(corp_id, report_id)
+        return json.loads(report.results)
+    except ReportResults.DoesNotExist:
+        corp = EveCorporationInfo.objects.get(corporation_id=corp_id)
+        report = Report.objects.get(id=report_id)
+        # send task!
+        run_report_for_corp.delay(corp_id, report_id)
+        return {"report": {"name": f"{report.name}",
+                           "corporation": corp.corporation_name},
+                "data": False,
+                "members": 0,
+                "unknowns": 0}
+
+
+@api.get(
+    "/get_corps",
+    response={200: List[schema.Corporation]},
+    tags=["Report"]
+)
+def get_corps(request):
+    return AuthReportsConfiguration.get_solo().visible_corps_for_user(request.user)
+
+
+@api.get(
+    "/get_reports",
+    response={200: List[schema.Report], 403: str},
+    tags=["Report"]
+)
+def get_reports(request):
+    if not request.user.has_perm("authstats.basic_access"):
+        return 403, f"Access Denied to get_reports for {request.user}"
+
+    _reports = Report.objects.all()
+    if not request.user.has_perm("authstats.restricted_reports"):
+        _reports = _reports.filter(restricted=False)
+    return _reports
+
+
+@api.get(
+    "/get_unknowns/{corp_id}",
+    response={200: dict, 403: str},
+    tags=["Report"]
+)
+def get_orphans_for_corp(request, corp_id: int):
+    if not AuthReportsConfiguration.get_solo().visible_corps_for_user(request.user).filter(corporation_id=corp_id).exists():
+        return 403, f"Access Denied to get_orphans_for_corp for {request.user}"
+
+    corp = EveCorporationInfo.objects.get(corporation_id=corp_id)
+
+    mains = User.objects.filter(
+        profile__main_character__corporation_id=corp_id)
+
+    known_character_ids = EveCharacter.objects.filter(
+        corporation_id=corp_id, character_ownership__isnull=False).values_list("character_id", flat=True)
+    names = []
+    try:
+        scopes = "esi-corporations.read_corporation_membership.v1"
+        token = Token.objects.filter(
+            character_id__in=EveCharacter.objects.filter(
+                corporation_id=corp_id).values('character_id')).require_scopes(scopes)
+        if not token.exists():
+            raise Exception("No Tokens")
+        unknown_member_list = set(providers.esi.client.Corporation.get_corporations_corporation_id_members(
+            corporation_id=corp_id, token=token.first().valid_access_token()).results())
+        for m in known_character_ids:
+            try:
+                unknown_member_list.remove(m)
+            except KeyError:
+                pass
+        names = providers.esi.client.Universe.post_universe_names(
+            ids=list(unknown_member_list)).results()
+    except Exception as e:
+        logger.exception(e)
+
+    return {"report": {"name": "Orphan Characters",
+                       "corporation": corp.corporation_name},
+            "members": mains.count(), "data": names}
---
+++ packages/allianceauth-auth-stats/allianceauth_auth_stats-0.0.2.tar.gz/allianceauth_auth_stats-0.0.2/authstats/tasks.py
@@ -0,0 +1,109 @@
+import json
+import logging
+
+from celery import shared_task
+
+from django.contrib.auth.models import User
+from django.core.cache import cache
+from django.core.serializers.json import DjangoJSONEncoder
+from django.utils import timezone
+
+from allianceauth.eveonline.models import EveCharacter, EveCorporationInfo
+from allianceauth.services.tasks import QueueOnce
+from esi.models import Token
+
+from . import providers
+from .models import Report, ReportDataThrough, ReportResults
+
+TZ_STRING = "%Y-%m-%dT%H:%M:%SZ"
+
+logger = logging.getLogger(__name__)
+
+
+@shared_task(bind=True, base=QueueOnce, max_retries=None)
+def run_report_for_corp(self, corp_id, report_id):
+    report = Report.objects.get(id=report_id)
+    corp = EveCorporationInfo.objects.get(corporation_id=corp_id)
+
+    fields = ReportDataThrough.objects.filter(report=report).order_by("rank")
+
+    mains = User.objects.filter(
+        profile__main_character__corporation_id=corp_id)
+
+    known_character_ids = EveCharacter.objects.filter(
+        corporation_id=corp_id, character_ownership__isnull=False).values_list("character_id", flat=True)
+    unknown_member_list = []
+
+    try:
+        scopes = "esi-corporations.read_corporation_membership.v1"
+        token = Token.objects.filter(
+            character_id__in=EveCharacter.objects.filter(
+                corporation_id=corp_id).values('character_id')).require_scopes(scopes)
+        if not token.exists():
+            raise Exception("No Tokens")
+        unknown_member_list = set(providers.esi.client.Corporation.get_corporations_corporation_id_members(
+            corporation_id=corp_id, token=token.first().valid_access_token()).results())
+        for m in known_character_ids:
+            try:
+                unknown_member_list.remove(m)
+            except KeyError:
+                pass
+    except Exception as e:
+        logger.exception(e)
+
+    output = {"report": {"name": report.name,
+                         "corporation": corp.corporation_name},
+              "headers": {},
+              "data": {},
+              "members": mains.count(),
+              "unknowns": len(unknown_member_list),
+              "updated": timezone.now(),
+              "show_avatar": report.show_character_image
+              }
+
+    for f in fields:
+        output['headers'][f.id] = {"rank": f.rank, "header": f.header,
+                                   "field": f"field-{f.id}",
+                                   "checkbox": f.checkbox_only,
+                                   "aggregate": f.pass_fail_aggregate,
+                                   "pass": 0,
+                                   "description": f.long_descriptions,
+                                   "allow_sort": f.allow_sort}
+
+        try:  # TODO create new method for clean table data
+            _f = f.report_Data_source.filter_object
+            f_data = {}
+            if hasattr(_f, "process_field"):
+                f_data = _f.process_field(mains)
+            elif hasattr(_f, "audit_filter"):
+                logger.debug(
+                    f"{_f} has no process_field, falling back to audit_filter")
+                f_data = _f.audit_filter(mains)
+            for u in mains:
+                if u.id not in output['data']:
+                    output['data'][u.id] = {"character": {
+                        "name": u.profile.main_character.character_name, "id": u.profile.main_character.character_id}}
+                output['data'][u.id][f"field-{f.id}"] = {
+                    "check": f_data[u.id]['check'],
+                    "data": "" if f.checkbox_only else f_data[u.id]['message']
+                }
+                output['headers'][f.id]['pass'] += 1 if f_data[u.id]['check'] else 0
+
+        except Exception as e:
+            logger.error(f"Error in Filter {e}")
+
+    # strip the uid's from the data its just annoying to deal with.
+    output['data'] = list(output['data'].values())
+    output['headers'] = list(output['headers'].values())
+    ReportResults.objects.update_or_create(corporation=corp, report=report,
+                                           defaults={"results": json.dumps(output, cls=DjangoJSONEncoder)})
+
+    return output
+
+
+# @shared_task(bind=True, base=QueueOnce, max_retries=None)
+# def run_reports_for_exporting(self, report_id, webhook_url):
+#     """
+#         Export the Stats for this report for all corps to a discord webhook as json?
+#     """
+#     pass...
