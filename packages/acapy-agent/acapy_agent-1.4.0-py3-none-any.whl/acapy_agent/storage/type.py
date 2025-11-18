47c28037677a5c07c67a0c6d9344c0cb1d7425a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:18 +0000

    Add package acapy-agent/1.4.0/acapy_agent-1.4.0-py3-none-any.whl

---
+++ packages/acapy-agent/acapy_agent-1.4.0-py3-none-any.whl/acapy_agent/storage/type.py
@@ -0,0 +1,9 @@
+"""Library version information."""
+
+RECORD_TYPE_ACAPY_STORAGE_TYPE = "acapy_storage_type"
+RECORD_TYPE_ACAPY_UPGRADING = "acapy_upgrading"
+
+STORAGE_TYPE_VALUE_ANONCREDS = "askar-anoncreds"
+STORAGE_TYPE_VALUE_ASKAR = "askar"
+STORAGE_TYPE_VALUE_KANON = "kanon"
+STORAGE_TYPE_VALUE_KANON_ANONCREDS = "kanon-anoncreds"
