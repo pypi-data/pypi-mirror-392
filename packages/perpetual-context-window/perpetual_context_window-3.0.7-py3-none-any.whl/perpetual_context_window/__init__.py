9e96d097a5f3aa82790f688db85f184453f22a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 20:06:23 +0000

    Add package perpetual-context-window/3.0.7/perpetual_context_window-3.0.7-py3-none-any.whl

---
+++ packages/perpetual-context-window/perpetual_context_window-3.0.7-py3-none-any.whl/perpetual_context_window/__init__.py
@@ -0,0 +1,5 @@
+# Module developed by Sapiens Technology® for building language model algorithms with a perpetual context window without complete forgetting of initial, intermediate, and final information.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from .perpetual_context_window import *
+# Module developed by Sapiens Technology® for building language model algorithms with a perpetual context window without complete forgetting of initial, intermediate, and final information.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
