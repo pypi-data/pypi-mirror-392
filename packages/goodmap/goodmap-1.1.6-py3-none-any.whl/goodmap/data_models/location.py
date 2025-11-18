17607c1bbc12b11262eb4966e1c6855603eaacfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:51 +0000

    Add package goodmap/1.1.6/goodmap-1.1.6-py3-none-any.whl

---
+++ packages/goodmap/goodmap-1.1.6-py3-none-any.whl/goodmap/data_models/location.py
@@ -0,0 +1,41 @@
+from typing import Any, Type
+
+from pydantic import BaseModel, Field, create_model, field_validator
+
+
+class LocationBase(BaseModel, extra="allow"):
+    position: tuple[float, float]
+    uuid: str
+
+    @field_validator("position")
+    @classmethod
+    def position_must_be_valid(cls, v):
+        if v[0] < -90 or v[0] > 90:
+            raise ValueError("latitude must be in range -90 to 90")
+        if v[1] < -180 or v[1] > 180:
+            raise ValueError("longitude must be in range -180 to 180")
+        return v
+
+    def basic_info(self):
+        return {
+            "uuid": self.uuid,
+            "position": self.position,
+            "remark": bool(getattr(self, "remark", False)),
+        }
+
+
+def create_location_model(obligatory_fields: list[tuple[str, Type[Any]]]) -> Type[BaseModel]:
+    fields = {
+        field_name: (field_type, Field(...)) for (field_name, field_type) in obligatory_fields
+    }
+
+    return create_model(
+        "Location",
+        __config__=None,
+        __doc__=None,
+        __module__="Location",
+        __validators__=None,
+        __base__=LocationBase,
+        __cls_kwargs__=None,
+        **fields,
+    )
