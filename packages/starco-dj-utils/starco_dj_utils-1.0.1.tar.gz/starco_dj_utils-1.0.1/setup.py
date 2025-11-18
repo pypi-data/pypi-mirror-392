bdc0d2a053f2e89003dad65fd6073ad1a094038b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 13:14:45 +0000

    Add package starco-dj-utils/1.0.1/starco_dj_utils-1.0.1.tar.gz

---
+++ packages/starco-dj-utils/starco_dj_utils-1.0.1.tar.gz/starco_dj_utils-1.0.1/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='starco-dj-utils',
+    version='1.0.1',
+    packages=['dj_utils'],
+    include_package_data=True,
+    license='MIT',
+    description='A Django pluggable app for utilities and database management',
+    author='Mojtaba',
+    author_email='m.tahmasbi0111@yahoo.com',
+    install_requires=[
+        'Django>=4.0',
+    ],
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+    ],
+)
