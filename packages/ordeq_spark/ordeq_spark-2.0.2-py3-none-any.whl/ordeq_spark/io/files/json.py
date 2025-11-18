4e8ff3540736c788173d9dcf47ea9ee6a53ec48f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 19:05:10 +0000

    Add package ordeq_spark/2.0.2/ordeq_spark-2.0.2-py3-none-any.whl

---
+++ packages/ordeq_spark/ordeq_spark-2.0.2-py3-none-any.whl/ordeq_spark/io/files/json.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+
+from ordeq import IO
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql.types import StructType
+
+from ordeq_spark.io.files.utils import _save_single_file
+from ordeq_spark.utils import apply_schema
+
+
+@dataclass(frozen=True, kw_only=True)
+class SparkJSON(IO[DataFrame]):
+    """IO for loading and saving JSON using Spark.
+
+    Example:
+
+    ```pycon
+    >>> from ordeq_spark import SparkJSON
+    >>> json = SparkJSON(
+    ...     path="to.json"
+    ... )
+
+    ```
+
+    By default, Spark creates a directory on save.
+    Use `single_file` if you want to write to a file instead:
+
+    ```pycon
+    >>> from ordeq_spark import SparkJSON
+    >>> json = SparkJSON(
+    ...     path="to.json"
+    ... ).with_load_options(single_file=True)
+
+    ```
+
+    """
+
+    path: str
+    format: str = "json"
+    schema: StructType | None = None
+
+    def load(self, **load_options) -> DataFrame:
+        df = SparkSession.builder.getOrCreate().read.load(
+            path=self.path, format=self.format, **load_options
+        )
+        return apply_schema(df, self.schema) if self.schema else df
+
+    def save(
+        self, df: DataFrame, single_file: bool = False, **save_options
+    ) -> None:
+        df = apply_schema(df, self.schema) if self.schema else df
+        if single_file:
+            _save_single_file(df, self.path, self.format, **save_options)
+        else:
+            df.write.save(path=self.path, format=self.format, **save_options)
