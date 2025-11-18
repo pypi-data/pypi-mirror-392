343369bf00c742cc7036734868fb36fc7900f9ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 04:05:52 +0000

    Add package kognitos-bdk-linter/1.30.0/kognitos_bdk_linter-1.30.0-py3-none-any.whl

---
+++ packages/kognitos-bdk-linter/kognitos_bdk_linter-1.30.0-py3-none-any.whl/kognitos/bdk/linter/procedure/rules/__init__.py
@@ -0,0 +1,4 @@
+from .procedure_rules import (ProcedureConnectionRequiredRule,
+                              ProcedureExamplesRule, ProcedureRule)
+
+__all__ = ["ProcedureExamplesRule", "ProcedureConnectionRequiredRule", "ProcedureRule"]
