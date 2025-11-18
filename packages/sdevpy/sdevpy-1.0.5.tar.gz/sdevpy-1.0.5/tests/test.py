ed09e5a17655135a166e43f64d73e77e0d109f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 12:05:02 +0000

    Add package sdevpy/1.0.5/sdevpy-1.0.5.tar.gz

---
+++ packages/sdevpy/sdevpy-1.0.5.tar.gz/sdevpy-1.0.5/tests/test.py
@@ -0,0 +1,13 @@
+import sdevpy as sd
+from sdevpy import example
+from sdevpy import settings
+from sdevpy.projects import datafiles
+
+
+a = example.add_one(2)
+print(a)
+
+print(settings.WORKFOLDER)
+
+print(sd.__version__)
+
