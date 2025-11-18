c5f02da776895e3863cad26229c824b2f9bada80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 05:04:48 +0000

    Add package weixinautox4/1.0.4/weixinautox4-1.0.4.tar.gz

---
+++ packages/weixinautox4/weixinautox4-1.0.4.tar.gz/weixinautox4-1.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="weixinautox4",  # 包名
+    version="1.0.4",  # 版本号
+    description="WeChat automation SDK with license management.",
+    author="agentaibot",
+    author_email="zhilongfeng66@gmail.com",
+    packages=find_packages(),  # 自动发现包，确保包括所有子目录
+    install_requires=[
+        "requests>=2.24.0",
+        "Cython>=0.29.21",
+        "pydantic>=1.8.2",
+        "pywin32==311",
+        "uiautomation==2.0.29",
+    ],
+    include_package_data=True,
+    package_data={
+        "weixinauto": [
+            "secure/*.pyd",
+            "infra/uia/selectors/wechat_4_1_plus.json",  
+        ],
+        
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
