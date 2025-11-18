222c0d93910cc4157775b37cb2853875610f0e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:05:05 +0000

    Add package eidosui/0.12.2/eidosui-0.12.2.tar.gz

---
+++ packages/eidosui/eidosui-0.12.2.tar.gz/eidosui-0.12.2/eidos/components/__init__.py
@@ -0,0 +1,19 @@
+"""EidosUI Components Package
+
+Higher-level components built on top of the base tags.
+"""
+
+from .headers import EidosHeaders
+from .navigation import NavBar
+from .table import DataTable
+from .tabs import AlpineTabs, HTMXTabs
+from .theme import ThemeSwitch
+
+__all__ = [
+    "DataTable",
+    "NavBar",
+    "EidosHeaders",
+    "AlpineTabs",
+    "HTMXTabs",
+    "ThemeSwitch",
+]
