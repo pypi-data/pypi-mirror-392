c3aa208a07aed0de5c6cdee55b35f7c90bb91461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 06:04:26 +0000

    Add package stalcraft-api/1.2.0/stalcraft_api-1.2.0.tar.gz

---
+++ packages/stalcraft-api/stalcraft_api-1.2.0.tar.gz/stalcraft_api-1.2.0/stalcraft/asyncio/__init__.py
@@ -0,0 +1,8 @@
+from .auth import AsyncAppAuth, AsyncUserAuth
+
+from .client import AsyncAppClient, AsyncUserClient
+
+__all__ = (
+    "AsyncAppAuth", "AsyncUserAuth",
+    "AsyncAppClient", "AsyncUserClient"
+)
