bc21f08d079b5ebad7e5f6d7fb4168b14cd03caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 08:04:24 +0000

    Add package ominfra/0.0.0.dev477/ominfra-0.0.0.dev477.tar.gz

---
+++ packages/ominfra/ominfra-0.0.0.dev477.tar.gz/ominfra-0.0.0.dev477/ominfra/__about__.py
@@ -0,0 +1,32 @@
+from omlish.__about__ import ProjectBase
+from omlish.__about__ import SetuptoolsBase
+from omlish.__about__ import __version__
+
+
+class Project(ProjectBase):
+    name = 'ominfra'
+    description = 'ominfra'
+
+    dependencies = [
+        f'omdev == {__version__}',
+        f'omlish == {__version__}',
+    ]
+
+    optional_dependencies = {
+        'ssh': [
+            'paramiko ~= 4.0',  # !! LGPL
+
+            'asyncssh ~= 2.21',  # cffi
+        ],
+    }
+
+    entry_points = {
+        'omlish.manifests': {name: name},
+    }
+
+
+class Setuptools(SetuptoolsBase):
+    find_packages = {
+        'include': [Project.name, f'{Project.name}.*'],
+        'exclude': [*SetuptoolsBase.find_packages['exclude']],
+    }
