fdafdce45709cd63f58d943d8c97086b40abcef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 07:04:23 +0000

    Add package bannerify/0.3.0/bannerify-0.3.0-py3-none-any.whl

---
+++ packages/bannerify/bannerify-0.3.0-py3-none-any.whl/bannerify/__init__.py
@@ -0,0 +1,7 @@
+"""Bannerify Python SDK - Generate images and PDFs at scale"""
+
+from .client import BannerifyClient
+from .models import Modification
+
+__version__ = "0.1.0"
+__all__ = ["BannerifyClient", "Modification"]
