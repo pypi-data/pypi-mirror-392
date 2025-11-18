194f250a65fdf256acb149b87622c57e84e24d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 18:06:29 +0000

    Add package EFN/4.1/efn-4.1.tar.gz

---
+++ packages/EFN/efn-4.1.tar.gz/efn-4.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="EFN",
+    version="4.1",
+    packages=find_packages(),
+    author="Andrisgalaxy",
+    description="A programming language in a Python library.",
+    python_requires=">=3.8",
+    install_requires=[
+        "cefpython3",
+        "requests",
+        "beautifulsoup4",
+        "Pillow",
+        "psutil",
+        "qrcode",
+        "tkhtmlview",
+        "pyttsx3",
+        "pywebview",
+        "ursina",
+        "pygame",
+        "discord.py",
+        "deep_translator",
+        "google",
+        "pandas",
+        "phonenumbers",
+        "fastapi",
+        "keyboard",
+        "google-genai; python_version >= '3.9'",
+        "kivy",
+        "pytz"
+    ]
+)
