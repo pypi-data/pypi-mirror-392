32d341f255a457b195fe51463a4cace38bd8fdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:46 +0000

    Add package cylindra/1.0.2/cylindra-1.0.2-cp313-cp313-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/cylindra/cylindra-1.0.2-cp313-cp313-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/cylindra/cli/run.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+from cylindra.cli._base import ParserBase, coerce_output_filename
+from cylindra.core import read_project, start
+
+
+class ParserRun(ParserBase):
+    """
+    cylindra run [bold green]path[/bold green] [bold cyan]options[/bold cyan]
+
+    [u bold green]path[/u bold green]
+        Path to the project/python file.
+
+    [u bold cyan]options[/u bold cyan]
+        [bold]--headless[/bold]
+            Run in headless mode.
+
+        [bold]--output, -o[/bold]
+            Output file name.
+    """
+
+    def __init__(self):
+        super().__init__(prog="cylindra run", description="Run a script.")
+        self.add_argument("path", type=str, help="Python script to run.")
+        self.add_argument("--headless", action="store_true")
+        self.add_argument("--output", "-o", type=str, default=None)
+
+    def run_action(
+        self, path: str, headless: bool = False, output: str | None = None, **kwargs
+    ):
+        from runpy import run_path
+
+        ui = start(viewer=self.viewer, headless=headless)
+        project_path = None
+        if Path(path).suffix in ("", ".tar", ".zip", ".json"):
+            prj = read_project(path)
+            with prj.open_project() as d:
+                py_path = str(prj._script_py_path(d))
+                out_globs = run_path(py_path, {"ui": ui})
+            is_project = True
+            project_path = prj.project_path
+        else:
+            out_globs = run_path(path, {"ui": ui})
+            is_project = False
+        if callable(main := out_globs.get("main")):
+            main(ui)  # script.py style
+        if project_path is not None:
+            ui._project_dir = project_path
+        if output is None:
+            if is_project:
+                ui.overwrite_project()
+            else:
+                output = coerce_output_filename("output", ext="")
+                ui.save_project(output, molecules_ext=".parquet")
+        else:
+            ui.save_project(output, molecules_ext=".parquet")
