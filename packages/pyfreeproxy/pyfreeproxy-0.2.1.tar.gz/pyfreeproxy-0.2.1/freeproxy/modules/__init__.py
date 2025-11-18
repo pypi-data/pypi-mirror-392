371ac128e02620bcea3592b0fe3e9cb15ec82288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 15:06:56 +0000

    Add package pyfreeproxy/0.2.1/pyfreeproxy-0.2.1.tar.gz

---
+++ packages/pyfreeproxy/pyfreeproxy-0.2.1.tar.gz/pyfreeproxy-0.2.1/freeproxy/modules/__init__.py
@@ -0,0 +1,3 @@
+'''initialize'''
+from .proxies import BaseProxiedSession, ProxiedSessionBuilder, BuildProxiedSession
+from .utils import BaseModuleBuilder, LoggerHandle, printtable, colorize, touchdir, ensureplaywrightchromium, ensurevalidrequestsproxies
