d33cf31deda3ab15155f29b98582fd52684ef824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:05:08 +0000

    Add package ry/0.0.68/ry-0.0.68-cp314-cp314t-manylinux_2_28_s390x.whl

---
+++ packages/ry/ry-0.0.68-cp314-cp314t-manylinux_2_28_s390x.whl/ry/JSON.pyi
@@ -0,0 +1,17 @@
+"""ry.JSON"""
+
+from ry.ryo3.JSON import cache_clear as cache_clear
+from ry.ryo3.JSON import cache_usage as cache_usage
+from ry.ryo3.JSON import dumps as dumps
+from ry.ryo3.JSON import loads as loads
+from ry.ryo3.JSON import parse as parse
+from ry.ryo3.JSON import stringify as stringify
+
+__all__ = (
+    "cache_clear",
+    "cache_usage",
+    "dumps",
+    "loads",
+    "parse",
+    "stringify",
+)
