21d490f4857595185c7a799b628f7acc29b2185b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 08:04:24 +0000

    Add package steer-materials/0.1.15/steer_materials-0.1.15-py3-none-any.whl

---
+++ packages/steer-materials/steer_materials-0.1.15-py3-none-any.whl/steer_materials/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.15"
+
+# import base materials
+from .CellMaterials.Base import TapeMaterial, SeparatorMaterial, CurrentCollectorMaterial, InsulationMaterial
+from .CellMaterials.Electrode import AnodeMaterial, CathodeMaterial, ConductiveAdditive, Binder
+
