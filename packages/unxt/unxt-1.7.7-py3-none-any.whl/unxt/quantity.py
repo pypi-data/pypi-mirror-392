a9091b822c0909748f3bc6f8c7d91e8b19cb1630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 22:08:00 +0000

    Add package unxt/1.7.7/unxt-1.7.7-py3-none-any.whl

---
+++ packages/unxt/unxt-1.7.7-py3-none-any.whl/unxt/quantity.py
@@ -0,0 +1,67 @@
+"""Quantities in JAX.
+
+This module provides a way to work with quantities in JAX.
+
+The main features are:
+
+- ``unxt.quantity.Quantity``: A class representing a quantity with units.
+- ``unxt.quantity.uconvert``: A function to convert a quantity to a different
+  unit.
+- ``unxt.quantity.ustrip``: A function to strip the units from a quantity.
+
+"""
+
+__all__ = (
+    # Core
+    "Quantity",
+    # Base
+    "AbstractQuantity",
+    # Fast
+    "BareQuantity",
+    "UncheckedQuantity",
+    # Angles
+    "AbstractAngle",
+    "Angle",
+    # Base Parametric
+    "AbstractParametricQuantity",
+    # Functional
+    "uconvert",
+    "ustrip",
+    "is_unit_convertible",
+    "wrap_to",
+    "is_any_quantity",
+    "convert_to_quantity_value",
+    "AllowValue",
+)
+
+
+from .setup_package import install_import_hook
+
+with install_import_hook("unxt.quantity"):
+    from ._src.quantity.angle import Angle
+    from ._src.quantity.api import is_unit_convertible, uconvert, ustrip, wrap_to
+    from ._src.quantity.base import AbstractQuantity, is_any_quantity
+    from ._src.quantity.base_angle import AbstractAngle
+    from ._src.quantity.base_parametric import AbstractParametricQuantity
+    from ._src.quantity.flag import AllowValue
+    from ._src.quantity.quantity import Quantity
+    from ._src.quantity.unchecked import BareQuantity, UncheckedQuantity
+    from ._src.quantity.value import convert_to_quantity_value
+
+    # isort: split
+    # Register dispatches and conversions
+    from ._src.quantity import (
+        register_api,
+        register_conversions,
+        register_dispatches,
+        register_primitives,
+    )
+
+# Clean up namespace
+del (
+    register_conversions,
+    register_api,
+    register_dispatches,
+    register_primitives,
+    install_import_hook,
+)
