66062c83187554345047a8c6c5d414d8ae00161a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:50 +0000

    Add package osism/0.20251115.0/osism-0.20251115.0.tar.gz

---
+++ packages/osism/osism-0.20251115.0.tar.gz/osism-0.20251115.0/osism/tasks/netbox.py
@@ -0,0 +1,193 @@
+# SPDX-License-Identifier: Apache-2.0
+
+from celery import Celery
+from loguru import logger
+
+from osism import settings, utils
+from osism.tasks import Config, run_command
+
+app = Celery("netbox")
+app.config_from_object(Config)
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    pass
+
+
+@app.task(bind=True, name="osism.tasks.netbox.run")
+def run(self, action, arguments):
+    # Check if tasks are locked before execution
+    utils.check_task_lock_and_exit()
+
+    pass
+
+
+# NOTE: While `get_*` tasks only operate on the NetBox configured in NETBOX_URL,
+#       `set_*` tasks additionally operate on all NetBox instances listed in
+#       NETBOX_SECONDARIES
+
+
+@app.task(bind=True, name="osism.tasks.netbox.set_maintenance")
+def set_maintenance(self, device_name, state=True):
+    """Set the maintenance state for a device in the NetBox."""
+    # Check if tasks are locked before execution
+    utils.check_task_lock_and_exit()
+
+    lock = utils.create_redlock(
+        key=f"lock_osism_tasks_netbox_set_maintenance_{device_name}",
+        auto_release_time=60,
+    )
+    if lock.acquire(timeout=20):
+        try:
+            for nb in [utils.nb] + utils.secondary_nb_list:
+                logger.info(
+                    f"Set maintenance state of device {device_name} = {state} on {nb.base_url}"
+                )
+                device = nb.dcim.devices.get(name=device_name)
+                if device:
+                    device.custom_fields.update({"maintenance": state})
+                    device.save()
+                else:
+                    logger.error(
+                        f"Could not set maintenance for {device_name} on {nb.base_url}"
+                    )
+        finally:
+            lock.release()
+    else:
+        logger.error("Could not acquire lock for node {device_name}")
+
+
+@app.task(bind=True, name="osism.tasks.netbox.set_provision_state")
+def set_provision_state(self, device_name, state):
+    """Set the provision state for a device in the NetBox."""
+    # Check if tasks are locked before execution
+    utils.check_task_lock_and_exit()
+
+    lock = utils.create_redlock(
+        key=f"lock_osism_tasks_netbox_set_provision_state_{device_name}",
+        auto_release_time=60,
+    )
+    if lock.acquire(timeout=20):
+        try:
+
+            for nb in [utils.nb] + utils.secondary_nb_list:
+                logger.info(
+                    f"Set provision state of device {device_name} = {state} on {nb.base_url}"
+                )
+                device = nb.dcim.devices.get(name=device_name)
+                if device:
+                    device.custom_fields.update({"provision_state": state})
+                    device.save()
+                else:
+                    logger.error(
+                        f"Could not set provision state for {device_name} on {nb.base_url}"
+                    )
+        finally:
+            lock.release()
+    else:
+        logger.error("Could not acquire lock for node {device_name}")
+
+
+@app.task(bind=True, name="osism.tasks.netbox.set_power_state")
+def set_power_state(self, device_name, state):
+    """Set the provision state for a device in the NetBox."""
+    # Check if tasks are locked before execution
+    utils.check_task_lock_and_exit()
+
+    lock = utils.create_redlock(
+        key=f"lock_osism_tasks_netbox_set_provision_state_{device_name}",
+        auto_release_time=60,
+    )
+    if lock.acquire(timeout=20):
+        try:
+            for nb in [utils.nb] + utils.secondary_nb_list:
+                logger.info(
+                    f"Set power state of device {device_name} = {state} on {nb.base_url}"
+                )
+                device = nb.dcim.devices.get(name=device_name)
+                if device:
+                    device.custom_fields.update({"power_state": state})
+                    device.save()
+                else:
+                    logger.error(
+                        f"Could not set power state for {device_name} on {nb.base_url}"
+                    )
+        finally:
+            lock.release()
+    else:
+        logger.error("Could not acquire lock for node {device_name}")
+
+
+@app.task(bind=True, name="osism.tasks.netbox.get_location_id")
+def get_location_id(self, location_name):
+    try:
+        location = utils.nb.dcim.locations.get(name=location_name)
+    except ValueError:
+        return None
+    if location:
+        return location.id
+    else:
+        return None
+
+
+@app.task(bind=True, name="osism.tasks.netbox.get_rack_id")
+def get_rack_id(self, rack_name):
+    try:
+        rack = utils.nb.dcim.racks.get(name=rack_name)
+    except ValueError:
+        return None
+    if rack:
+        return rack.id
+    else:
+        return None
+
+
+@app.task(bind=True, name="osism.tasks.netbox.get_devices")
+def get_devices(self, **query):
+    return utils.nb.dcim.devices.filter(**query)
+
+
+@app.task(bind=True, name="osism.tasks.netbox.get_device_by_name")
+def get_device_by_name(self, name):
+    return utils.nb.dcim.devices.get(name=name)
+
+
+@app.task(bind=True, name="osism.tasks.netbox.get_interfaces_by_device")
+def get_interfaces_by_device(self, device_name):
+    return utils.nb.dcim.interfaces.filter(device=device_name)
+
+
+@app.task(bind=True, name="osism.tasks.netbox.get_addresses_by_device_and_interface")
+def get_addresses_by_device_and_interface(self, device_name, interface_name):
+    return utils.nb.dcim.addresses.filter(device=device_name, interface=interface_name)
+
+
+@app.task(bind=True, name="osism.tasks.netbox.manage")
+def manage(self, *arguments, publish=True, locking=False, auto_release_time=3600):
+    # Check if tasks are locked before execution
+    utils.check_task_lock_and_exit()
+
+    netbox_manager_env = {
+        "NETBOX_MANAGER_URL": str(settings.NETBOX_URL),
+        "NETBOX_MANAGER_TOKEN": str(settings.NETBOX_TOKEN),
+        "NETBOX_MANAGER_IGNORE_SSL_ERRORS": str(settings.IGNORE_SSL_ERRORS),
+        "NETBOX_MANAGER_VERBOSE": "true",
+    }
+
+    return run_command(
+        self.request.id,
+        "/usr/local/bin/netbox-manager",
+        netbox_manager_env,
+        *arguments,
+        publish=publish,
+        locking=locking,
+        auto_release_time=auto_release_time,
+    )
+
+
+@app.task(bind=True, name="osism.tasks.netbox.ping")
+def ping(self):
+    status = utils.nb.status()
+
+    return status
---
+++ packages/osism/osism-0.20251115.0.tar.gz/osism-0.20251115.0/osism/utils/__init__.py
@@ -0,0 +1,325 @@
+# SPDX-License-Identifier: Apache-2.0
+
+import time
+import os
+from contextlib import redirect_stdout, redirect_stderr
+from cryptography.fernet import Fernet
+import keystoneauth1
+from loguru import logger
+import openstack
+import pynetbox
+from pottery import Redlock
+from redis import Redis
+import urllib3
+import yaml
+
+from osism import settings
+
+
+def get_netbox_connection(netbox_url, netbox_token, ignore_ssl_errors=False):
+    if netbox_url and netbox_token:
+        nb = pynetbox.api(netbox_url, token=netbox_token)
+
+        if ignore_ssl_errors and nb:
+            import requests
+
+            urllib3.disable_warnings()
+            session = requests.Session()
+            session.verify = False
+            nb.http_session = session
+
+    else:
+        nb = None
+
+    return nb
+
+
+redis = Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB,
+    socket_keepalive=True,
+)
+redis.ping()
+
+nb = get_netbox_connection(
+    settings.NETBOX_URL, settings.NETBOX_TOKEN, settings.IGNORE_SSL_ERRORS
+)
+
+try:
+    secondary_nb_settings_list = yaml.safe_load(settings.NETBOX_SECONDARIES)
+    supported_secondary_nb_keys = ["NETBOX_URL", "NETBOX_TOKEN", "IGNORE_SSL_ERRORS"]
+    secondary_nb_list = []
+    if type(secondary_nb_settings_list) is not list:
+        raise TypeError(
+            f"Setting NETBOX_SECONDARIES needs to be an array of mappings containing supported NetBox API configuration: {supported_secondary_nb_keys}"
+        )
+    for secondary_nb_settings in secondary_nb_settings_list:
+        if type(secondary_nb_settings) is not dict:
+            raise TypeError(
+                f"Elements in setting NETBOX_SECONDARIES need to be mappings containing supported NetBox API configuration: {supported_secondary_nb_keys}"
+            )
+        for key in list(secondary_nb_settings.keys()):
+            if key not in supported_secondary_nb_keys:
+                raise ValueError(
+                    f"Unknown key in element of setting NETBOX_SECONDARIES. Supported keys: {supported_secondary_nb_keys}"
+                )
+        if (
+            "NETBOX_URL" not in secondary_nb_settings
+            or not secondary_nb_settings["NETBOX_URL"]
+        ):
+            raise ValueError(
+                "All NETBOX_URL values in the elements of setting NETBOX_SECONDARIES need to be valid NetBox URLs"
+            )
+        if (
+            "NETBOX_TOKEN" not in secondary_nb_settings
+            or not str(secondary_nb_settings["NETBOX_TOKEN"]).strip()
+        ):
+            raise ValueError(
+                "All NETBOX_TOKEN values in the elements of setting NETBOX_SECONDARIES need to be valid NetBox tokens"
+            )
+
+        secondary_nb_list.append(
+            get_netbox_connection(
+                secondary_nb_settings["NETBOX_URL"],
+                str(secondary_nb_settings["NETBOX_TOKEN"]),
+                secondary_nb_settings.get("IGNORE_SSL_ERRORS", True),
+            )
+        )
+except (yaml.YAMLError, TypeError, ValueError) as exc:
+    logger.error(f"Error parsing settings NETBOX_SECONDARIES: {exc}")
+    secondary_nb_list = []
+
+
+def get_openstack_connection():
+    try:
+        conn = openstack.connect()
+    except keystoneauth1.exceptions.auth_plugins.MissingRequiredOptions:
+        pass
+
+    return conn
+
+
+def get_ansible_vault_password():
+    keyfile = "/share/ansible_vault_password.key"
+
+    try:
+        with open(keyfile, "r") as fp:
+            key = fp.read()
+        f = Fernet(key)
+
+        encrypted_ansible_vault_password = redis.get("ansible_vault_password")
+        if encrypted_ansible_vault_password is None:
+            raise ValueError("Ansible vault password is not set in Redis")
+
+        ansible_vault_password = f.decrypt(encrypted_ansible_vault_password)
+        password = ansible_vault_password.decode("utf-8")
+
+        if not password or password.strip() == "":
+            raise ValueError(
+                "Ansible vault password is empty or contains only whitespace"
+            )
+
+        return password
+    except Exception as exc:
+        logger.error("Unable to get ansible vault password")
+        raise exc
+
+
+# https://stackoverflow.com/questions/2361426/get-the-first-item-from-an-iterable-that-matches-a-condition
+def first(iterable, condition=lambda x: True):
+    """
+    Returns the first item in the `iterable` that
+    satisfies the `condition`.
+
+    If the condition is not given, returns the first item of
+    the iterable.
+
+    Raises `StopIteration` if no item satysfing the condition is found.
+
+    >>> first( (1,2,3), condition=lambda x: x % 2 == 0)
+    2
+    >>> first(range(3, 100))
+    3
+    >>> first( () )
+    Traceback (most recent call last):
+    ...
+    StopIteration
+    """
+
+    return next(x for x in iterable if condition(x))
+
+
+def fetch_task_output(
+    task_id, timeout=os.environ.get("OSISM_TASK_TIMEOUT", 300), enable_play_recap=False
+):
+    rc = 0
+    stoptime = time.time() + timeout
+    last_id = 0
+    while time.time() < stoptime:
+        data = redis.xread({str(task_id): last_id}, count=1, block=(timeout * 1000))
+        if data:
+            stoptime = time.time() + timeout
+            messages = data[0]
+            for message_id, message in messages[1]:
+                last_id = message_id.decode()
+                message_type = message[b"type"].decode()
+                message_content = message[b"content"].decode()
+
+                logger.debug(f"Processing message {last_id} of type {message_type}")
+                redis.xdel(str(task_id), last_id)
+
+                if message_type == "stdout":
+                    print(message_content, end="")
+                    if enable_play_recap and "PLAY RECAP" in message_content:
+                        logger.info(
+                            "Play has been completed. There may now be a delay until "
+                            "all logs have been written."
+                        )
+                        logger.info("Please wait and do not abort execution.")
+                elif message_type == "rc":
+                    rc = int(message_content)
+                elif message_type == "action" and message_content == "quit":
+                    redis.close()
+                    return rc
+    raise TimeoutError
+
+
+def push_task_output(task_id, line):
+    redis.xadd(task_id, {"type": "stdout", "content": line})
+
+
+def finish_task_output(task_id, rc=None):
+    if rc:
+        redis.xadd(task_id, {"type": "rc", "content": rc})
+    redis.xadd(task_id, {"type": "action", "content": "quit"})
+
+
+def revoke_task(task_id):
+    """
+    Revoke a running Celery task.
+
+    Args:
+        task_id (str): The ID of the task to revoke
+
+    Returns:
+        bool: True if revocation was successful, False otherwise
+    """
+    try:
+        from celery import Celery
+        from osism.tasks import Config
+
+        app = Celery("task")
+        app.config_from_object(Config)
+        app.control.revoke(task_id, terminate=True)
+        return True
+    except Exception as e:
+        logger.error(f"Failed to revoke task {task_id}: {e}")
+        return False
+
+
+def create_redlock(key, auto_release_time=3600):
+    """
+    Create a Redlock instance with output suppression during initialization.
+
+    Args:
+        key (str): The lock key
+        auto_release_time (int): Auto release time in seconds (default: 3600)
+
+    Returns:
+        Redlock: The configured Redlock instance
+    """
+    import logging
+
+    # Permanently suppress pottery logger output
+    pottery_logger = logging.getLogger("pottery")
+    pottery_logger.setLevel(logging.CRITICAL)
+
+    with open(os.devnull, "w") as devnull:
+        with redirect_stdout(devnull), redirect_stderr(devnull):
+            return Redlock(
+                key=key,
+                masters={redis},
+                auto_release_time=auto_release_time,
+            )
+
+
+def set_task_lock(user=None, reason=None):
+    """
+    Set task lock to prevent new tasks from starting.
+
+    Args:
+        user (str): User who set the lock (optional)
+        reason (str): Reason for the lock (optional)
+
+    Returns:
+        bool: True if lock was set successfully
+    """
+    try:
+        import json
+        from datetime import datetime
+
+        lock_data = {
+            "locked": True,
+            "timestamp": datetime.now().isoformat(),
+            "user": user or "dragon",
+            "reason": reason,
+        }
+
+        redis.set("osism:task_lock", json.dumps(lock_data))
+        return True
+    except Exception as e:
+        logger.error(f"Failed to set task lock: {e}")
+        return False
+
+
+def remove_task_lock():
+    """
+    Remove task lock to allow new tasks to start.
+
+    Returns:
+        bool: True if lock was removed successfully
+    """
+    try:
+        redis.delete("osism:task_lock")
+        return True
+    except Exception as e:
+        logger.error(f"Failed to remove task lock: {e}")
+        return False
+
+
+def is_task_locked():
+    """
+    Check if tasks are currently locked.
+
+    Returns:
+        dict: Lock status information or None if not locked
+    """
+    try:
+        import json
+
+        lock_data = redis.get("osism:task_lock")
+        if lock_data:
+            return json.loads(lock_data.decode("utf-8"))
+        return None
+    except Exception as e:
+        logger.error(f"Failed to check task lock status: {e}")
+        return None
+
+
+def check_task_lock_and_exit():
+    """
+    Check if tasks are locked and exit with error message if they are.
+    Used by commands that should not run when tasks are locked.
+    """
+    lock_info = is_task_locked()
+    if lock_info and lock_info.get("locked"):
+        user = lock_info.get("user", "unknown")
+        timestamp = lock_info.get("timestamp", "unknown")
+        reason = lock_info.get("reason")
+
+        logger.error(f"Tasks are currently locked by {user} at {timestamp}")
+        if reason:
+            logger.error(f"Reason: {reason}")
+        logger.error("Use 'osism unlock' to remove the lock")
+        exit(1)
