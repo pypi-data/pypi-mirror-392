e1932dcea39c125ff5130298ed6d8a6abdfb27e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:04:37 +0000

    Add package astraray/0.1.1/astraray-0.1.1.tar.gz

---
+++ packages/astraray/astraray-0.1.1.tar.gz/astraray-0.1.1/astraray/utils/del_jupyter_temp.py
@@ -0,0 +1,33 @@
+﻿import argparse
+import shutil
+from pathlib import Path
+
+parser = argparse.ArgumentParser("delete_jupyter_temp_dirs")
+parser.add_argument(
+    "-r", "--root", required=True, help="scan the temp dirs in the path"
+)
+parser.add_argument("-o", "--op", default="del", help="operation:show or del")
+parser.add_argument(
+    "-n", "--name", default="*ipynb_checkpoints", help="name key words"
+)
+args = parser.parse_args()
+
+
+def del_jupyter_temp() -> None:
+    root_path = Path(args.root)
+    # 使用 rglob 递归查找匹配的路径
+    matches = list(root_path.rglob(args.name))
+
+    if args.op == "show":
+        print([str(match) for match in matches])
+    elif args.op == "del":
+        for match in matches:
+            if match.is_file():
+                match.unlink()  # 删除文件
+            else:
+                shutil.rmtree(match)  # 删除目录树
+            print(match)
+
+
+if __name__ == "__main__":
+    del_jupyter_temp()
