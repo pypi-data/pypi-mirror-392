2e9d199eb69f021b82d773f3792655e641d5adc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 18:06:29 +0000

    Add package unimath/1.4.2/unimath-1.4.2.tar.gz

---
+++ packages/unimath/unimath-1.4.2.tar.gz/unimath-1.4.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="unimath",
+    version="1.4.2",
+    packages=find_packages(),
+    install_requires=[
+        "sympy","matplotlib"
+    ],
+    author="Poyraz Soylu",
+    author_email="psoylu3467@gmail.com",
+    description="Mathematical Environment Library in Python",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jolankaa/unimath",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
