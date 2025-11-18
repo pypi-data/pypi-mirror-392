40b9dc4696bf72881f59b924c0f2b20dc8ac5d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:05:55 +0000

    Add package django-telegram-app/0.3.0/django_telegram_app-0.3.0-py3-none-any.whl

---
+++ packages/django-telegram-app/django_telegram_app-0.3.0-py3-none-any.whl/django_telegram_app/management/base.py
@@ -0,0 +1,73 @@
+"""Base command module to start telegram commands.
+
+Note:
+This module's command-class does not create an actual CLI command, but can be used by actual commands.
+"""
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.utils.translation import override
+
+from django_telegram_app import get_telegram_settings_model
+from django_telegram_app.bot.base import BaseCommand as BaseBotCommand
+from django_telegram_app.bot.bot import handle_update
+from django_telegram_app.conf import settings as app_settings
+
+
+class BaseTelegramCommand(BaseCommand):
+    """Base command class to start Telegram bot commands."""
+
+    command: type[BaseBotCommand] | None = None
+
+    def add_arguments(self, parser):
+        """Add command arguments."""
+        parser.add_argument(
+            "--force",
+            action="store_true",
+            default=False,
+            help="Force the command to run, regardless of the should_run outcome.",
+        )
+
+    def handle(self, *_args, **options):
+        """Start the configured telegram command.
+
+        Note:
+        The chat_id is configured on the user as a TelegramSetting.
+        A fake update is created with a message containing the command, this is not persisted.
+        """
+        if not self.command:
+            raise ValueError("The attribute `command` must be set.")
+        command_text = self.command.get_command_string()
+
+        if not options["force"] and not self.should_run():
+            self.stdout.write(self.style.NOTICE(f"Command '{command_text}' skipped as should_run returned False."))
+            return
+
+        for telegram_settings in get_telegram_settings_model().objects.filter(user__is_active=True):
+            user_language = get_user_language(telegram_settings.user)
+            with override(user_language, deactivate=True):
+                update = {"message": {"chat": {"id": telegram_settings.chat_id}, "text": command_text}}
+                handle_update(update=update)
+            self.stdout.write(self.style.SUCCESS(f"Started the command for {telegram_settings.user}."))
+        else:
+            self.stdout.write(self.style.NOTICE("No Telegram-enabled users found. Nothing to do."))
+
+    def should_run(self) -> bool:
+        """Determine if the command should run."""
+        return True
+
+
+def get_user_language(user) -> str:
+    """Get the language code for a user.
+
+    Look for various attributes on the user model to determine the preferred language.
+    If no language attribute is found, the default LANGUAGE_CODE from settings is returned.
+
+    The attribute names to look for can be configured in the TELEGRAM.USER_LANGUAGE_ATTRS setting.
+    """
+    for attr in app_settings.USER_LANGUAGE_ATTRS:
+        value = getattr(user, attr, "")
+        if value:
+            return value
+
+    return settings.LANGUAGE_CODE
