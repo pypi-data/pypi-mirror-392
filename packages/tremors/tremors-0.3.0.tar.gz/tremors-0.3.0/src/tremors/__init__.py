982c71eb288dfedb72e557fbf1e1bb4f335ebb6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 20:06:24 +0000

    Add package tremors/0.3.0/tremors-0.3.0.tar.gz

---
+++ packages/tremors/tremors-0.3.0.tar.gz/tremors-0.3.0/src/tremors/__init__.py
@@ -0,0 +1,10 @@
+"""The tremors package."""
+
+from tremors.decorator import from_logged, logged
+from tremors.logger import EXTRA_KEY, Collector, Logger
+
+__authors__ = ["Narvin Singh"]
+__project__ = "Tremors"
+__version__ = "0.3.0"
+
+__all__ = ["EXTRA_KEY", "Collector", "Logger", "__version__", "from_logged", "logged"]
