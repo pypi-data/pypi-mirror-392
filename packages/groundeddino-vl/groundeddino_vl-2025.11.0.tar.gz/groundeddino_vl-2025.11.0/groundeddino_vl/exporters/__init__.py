94cda09e68379ab0bd63e81e0d9ba5b843b69ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 23:05:37 +0000

    Add package groundeddino-vl/2025.11.0/groundeddino_vl-2025.11.0.tar.gz

---
+++ packages/groundeddino-vl/groundeddino_vl-2025.11.0.tar.gz/groundeddino_vl-2025.11.0/groundeddino_vl/exporters/__init__.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------
+# GroundedDINO-VL - Model Exporters
+# Copyright (c) 2025 GhostCipher. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+
+"""Model export utilities for ONNX, TensorRT, etc."""
+
+# Placeholder for future export functionality
+__all__ = []
---
+++ packages/groundeddino-vl/groundeddino_vl-2025.11.0.tar.gz/groundeddino_vl-2025.11.0/groundeddino_vl/models/grounding_dino/__init__.py
@@ -0,0 +1,12 @@
+# ------------------------------------------------------------------------
+# GroundedDINO-VL - Grounding DINO Model
+# Based on Grounding DINO by IDEA-Research
+# Copyright (c) 2025 GhostCipher. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+# Original Copyright (c) 2023 IDEA. All Rights Reserved.
+# ------------------------------------------------------------------------
+
+from .model import GroundingDINO, build_groundingdino
+
+__all__ = ["GroundingDINO", "build_groundingdino"]
