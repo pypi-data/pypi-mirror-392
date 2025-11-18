67a4f088872746895da2d4ecd9e2146d264ff1a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 07:05:41 +0000

    Add package WebDesignTaal/1.1.4/webdesigntaal-1.1.4-py3-none-any.whl

---
+++ packages/WebDesignTaal/webdesigntaal-1.1.4-py3-none-any.whl/WebDesignTaal/__init__.py
@@ -0,0 +1,8 @@
+"""WebDesignTaal
+
+Een programma gemaakt om .wdt bestanden of strings om te zetten naar ondersteunde varianten.
+
+"""
+
+from .file_handler import file_conversion
+from .wdt_parser import render_code
