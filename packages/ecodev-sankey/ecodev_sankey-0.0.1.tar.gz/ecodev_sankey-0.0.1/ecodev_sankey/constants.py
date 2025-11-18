90b1eec2a89d9d4ef4527953727d9b1dc6cce0c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 03:23:51 +0000

    Add package ecodev-sankey/0.0.1/ecodev_sankey-0.0.1.tar.gz

---
+++ packages/ecodev-sankey/ecodev_sankey-0.0.1.tar.gz/ecodev_sankey-0.0.1/ecodev_sankey/constants.py
@@ -0,0 +1,15 @@
+"""
+Module listing all sankey related constants
+"""
+LABEL = 'label'
+NODE_ID = 'node-id'
+COLOR = 'color'
+ROOT = 'root'
+VALUE = 'value'
+NUM_DATAPOINTS = 'num_datapoints'
+NUM_CHILDREN = 'num-children'
+ID = 'id'
+DESCRIPTION = 'description'
+HIERARCHIES_SHEET = 'Hierarchies'
+HIERARCHY = 'hierarchy'
+DEPTH = 'depth'
