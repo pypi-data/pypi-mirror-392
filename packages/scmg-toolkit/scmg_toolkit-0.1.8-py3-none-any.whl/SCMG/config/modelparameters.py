e725cc3d13c16fb2e3025809c9d28dd57d7ecfe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 16:06:21 +0000

    Add package scmg-toolkit/0.1.8/scmg_toolkit-0.1.8-py3-none-any.whl

---
+++ packages/scmg-toolkit/scmg_toolkit-0.1.8-py3-none-any.whl/SCMG/config/modelparameters.py
@@ -0,0 +1,21 @@
+# class ModelParameters():
+#     def __init__(self):
+#         self.NUM_LAYERS = "num_layers"
+#         self.NUM_HEADS = "num_heads"
+#         self.DIM_ATTENTION = "dim_attention"
+#         self.DIM_FEEDFORWARD = "dim_feedforward"
+#         self.DIM_LSTM = "dim_lstm"
+#         self.DIM_EMBEDDING = "dim_embedding"
+#         self.DIM_OUTPUT = "dim_output"
+#         self.RATE_DROPOUT = "rate_dropout"
+#         return
+#
+
+NUM_LAYERS = "num_layers"
+NUM_HEADS = "num_heads"
+DIM_ATTENTION = "dim_attention"
+DIM_FEEDFORWARD = "dim_feedforward"
+DIM_LSTM = "dim_lstm"
+DIM_EMBEDDING = "dim_embedding"
+DIM_OUTPUT = "dim_output"
+RATE_DROPOUT = "rate_dropout"
