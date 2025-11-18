4a11ce6e868c0e9cb73fb50c9f401a57dfdecb02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 22:07:30 +0000

    Add package wsi-toolbox/0.1.1/wsi_toolbox-0.1.1-py3-none-any.whl

---
+++ packages/wsi-toolbox/wsi_toolbox-0.1.1-py3-none-any.whl/wsi_toolbox/__init__.py
@@ -0,0 +1,117 @@
+"""
+WSI-toolbox: Whole Slide Image analysis toolkit
+
+A comprehensive toolkit for WSI processing, feature extraction, and clustering.
+
+Basic Usage:
+    >>> import wsi_toolbox as wt
+    >>>
+    >>> # Convert WSI to HDF5
+    >>> wt.set_default_progress('tqdm')
+    >>> cmd = wt.Wsi2HDF5Command(patch_size=256)
+    >>> result = cmd('input.ndpi', 'output.h5')
+    >>>
+    >>> # Extract features with preset model
+    >>> wt.set_default_model_preset('gigapath')
+    >>> wt.set_default_device('cuda')
+    >>> emb_cmd = wt.PatchEmbeddingCommand(batch_size=256)
+    >>> emb_result = emb_cmd('output.h5')
+    >>>
+    >>> # Or use custom model
+    >>> custom_model = wt.create_model('uni')
+    >>> wt.set_default_model(custom_model, name='my_uni', label='My UNI')
+    >>>
+    >>> # Clustering
+    >>> cluster_cmd = wt.ClusteringCommand(resolution=1.0, use_umap=True)
+    >>> cluster_result = cluster_cmd(['output.h5'])
+    >>>
+    >>> # Plot UMAP
+    >>> umap_embs = cluster_cmd.get_umap_embeddings()
+    >>> fig = wt.plot_umap(umap_embs, cluster_cmd.total_clusters)
+    >>> fig.savefig('umap.png')
+"""
+
+# Version info
+__version__ = "0.1.0"
+
+# Configuration
+# Commands
+from .commands import (
+    ClusteringCommand,
+    DziExportCommand,
+    PatchEmbeddingCommand,
+    PreviewClustersCommand,
+    PreviewLatentClusterCommand,
+    PreviewLatentPCACommand,
+    PreviewScoresCommand,
+    Wsi2HDF5Command,
+)
+from .commands.clustering import ClusteringResult
+from .commands.patch_embedding import PatchEmbeddingResult
+
+# Command result types
+from .commands.wsi import Wsi2HDF5Result
+from .common import (
+    get_config,
+    set_default_device,
+    set_default_model,
+    set_default_model_preset,
+    set_default_progress,
+    set_verbose,
+)
+
+# Models
+from .models import (
+    MODEL_LABELS,
+    create_model,
+)
+
+# Utility functions
+from .utils import plot_umap
+from .utils.analysis import leiden_cluster
+
+# WSI file classes
+from .wsi_files import (
+    OpenSlideFile,
+    StandardImage,
+    TiffFile,
+    WSIFile,
+    create_wsi_file,
+)
+
+__all__ = [
+    # Version
+    "__version__",
+    # Configuration functions
+    "get_config",
+    "set_default_progress",
+    "set_default_model",
+    "set_default_model_preset",
+    "set_default_device",
+    "set_verbose",
+    # Commands
+    "Wsi2HDF5Command",
+    "PatchEmbeddingCommand",
+    "ClusteringCommand",
+    "PreviewClustersCommand",
+    "PreviewScoresCommand",
+    "PreviewLatentPCACommand",
+    "PreviewLatentClusterCommand",
+    "DziExportCommand",
+    # Result types
+    "Wsi2HDF5Result",
+    "PatchEmbeddingResult",
+    "ClusteringResult",
+    # WSI files
+    "WSIFile",
+    "OpenSlideFile",
+    "TiffFile",
+    "StandardImage",
+    "create_wsi_file",
+    # Models
+    "MODEL_LABELS",
+    "create_model",
+    # Utilities
+    "plot_umap",
+    "leiden_cluster",
+]
