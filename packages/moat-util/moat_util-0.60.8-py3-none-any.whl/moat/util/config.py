512e7ce57e83e2353c6d4076b69175d602953e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 18:06:21 +0000

    Add package moat-util/0.60.8/moat_util-0.60.8-py3-none-any.whl

---
+++ packages/moat-util/moat_util-0.60.8-py3-none-any.whl/moat/util/config.py
@@ -0,0 +1,61 @@
+"""
+This module imports possibly-partial configuration snippets.
+"""
+
+from __future__ import annotations
+
+from importlib import import_module
+from pathlib import Path as FSPath
+
+from .dict import attrdict
+from .merge import merge
+from .yaml import yload
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .path import Path
+
+__all__ = ["CFG", "ensure_cfg"]
+CFG = attrdict()
+
+
+def ensure_cfg(path: str | Path, cfg=CFG) -> dict:
+    """
+    Ensure that a submodule's default configuration is available.
+    """
+    if isinstance(path, str):
+        path = path.split(".")
+
+    def _load(cfg, p):
+        ext = import_module(p)
+
+        try:
+            p = ext.__path__
+        except AttributeError:
+            p = (str(FSPath(ext.__file__).parent),)
+
+        for d in p:
+            fn = FSPath(d) / "_cfg.yaml"
+            if fn.is_file():
+                merge(cfg, yload(fn, attr=True), replace=False)
+
+    try:
+        EXT = cfg.setdefault("ext", attrdict())
+        EXT["moat"] = cfg
+
+        if "logging" not in cfg:
+            _load(cfg, "moat")
+
+        cc = EXT
+        for n in range(len(path)):
+            cc = cc.setdefault(path[n], attrdict())
+            if "_def" in cc:
+                continue
+            _load(cc, ".".join(path[: n + 1]))
+            cc["_def"] = True
+
+    finally:
+        del EXT["moat"]
+
+    return cfg
