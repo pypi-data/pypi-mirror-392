16174206792a1693ec9da35079d43e1c19ffa79a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:06:00 +0000

    Add package NepTrainKit/2.7.2/neptrainkit-2.7.2-cp310-cp310-win32.whl

---
+++ packages/NepTrainKit/neptrainkit-2.7.2-cp310-cp310-win32.whl/NepTrainKit/ui/views/_card/__init__.py
@@ -0,0 +1,40 @@
+﻿"""Expose registered card classes for the NEP UI."""
+
+from .super_cell_card import SuperCellCard
+from .perturb_card import PerturbCard
+# from .vibration_perturb_card import VibrationModePerturbCard
+from .magmom_rotation_card import MagneticMomentRotationCard
+from .cell_strain_card import CellStrainCard
+from .cell_scaling_card import CellScalingCard
+from .shear_matrix_card import ShearMatrixCard
+from .shear_angle_card import ShearAngleCard
+from .random_slab_card import RandomSlabCard
+from .random_doping_card import RandomDopingCard
+from .random_vacancy_card import RandomVacancyCard
+from .vacancy_defect_card import VacancyDefectCard
+from .stacking_fault_card import StackingFaultCard
+from .organic_mol_config_pbc_card import OrganicMolConfigPBCCard
+# from .interstitial_adsorbate_card import InsertDefectCard
+
+from .fps_filter_card import FilterDataCard
+from .card_group import CardGroup
+
+__all__ = [
+    "SuperCellCard",
+    "PerturbCard",
+    # "VibrationModePerturbCard",
+    "MagneticMomentRotationCard",
+    "CellStrainCard",
+    "ShearMatrixCard",
+    "ShearAngleCard",
+    "CellScalingCard",
+    "RandomSlabCard",
+    "RandomDopingCard",
+    "RandomVacancyCard",
+    "VacancyDefectCard",
+    "StackingFaultCard",
+    "OrganicMolConfigPBCCard",
+    # "InsertDefectCard",
+    "FilterDataCard",
+    "CardGroup",
+]
