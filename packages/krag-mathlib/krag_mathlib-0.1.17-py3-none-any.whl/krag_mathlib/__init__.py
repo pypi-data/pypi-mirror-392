ec6aee8caa1514f6585f620d6714c829d6b5fcdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:05:18 +0000

    Add package krag-mathlib/0.1.17/krag_mathlib-0.1.17-py3-none-any.whl

---
+++ packages/krag-mathlib/krag_mathlib-0.1.17-py3-none-any.whl/krag_mathlib/__init__.py
@@ -0,0 +1,20 @@
+from .core import (
+    add,
+    subtract,
+    multiply,
+    divide,
+    exponent,
+    log
+)
+
+from .utils import hello
+
+__all__ = [
+    "add",
+    "subtract",
+    "multiply",
+    "divide",
+    "exponent",
+    "log",
+    "hello",
+]
---
+++ packages/krag-mathlib/krag_mathlib-0.1.17-py3-none-any.whl/krag_mathlib/utils.py
@@ -0,0 +1,8 @@
+"""
+krag_mathlib.utils
+
+A collection of utility functions for the mathlib package.
+"""
+
+def hello():
+    return "Hello mathlib"
