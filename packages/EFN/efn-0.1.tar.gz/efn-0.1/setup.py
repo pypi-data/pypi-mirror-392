7509995b91e6d9358be690223a68586ba7acce90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 18:06:29 +0000

    Add package EFN/0.1/efn-0.1.tar.gz

---
+++ packages/EFN/efn-0.1.tar.gz/efn-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="EFN",
+    version="0.1",
+    packages=find_packages(),
+    author="Andrisgalaxy",
+    description="A programming language in a Python library.",
+    python_requires=">=3.6",
+    install_requires=[
+        "cefpython3",
+        "requests",
+        "beautifulsoup4",
+        "Pillow",
+        "psutil",
+        "qrcode",
+        "tkhtmlview",
+        "pyttsx3",
+        "pywebview"
+    ]
+)
