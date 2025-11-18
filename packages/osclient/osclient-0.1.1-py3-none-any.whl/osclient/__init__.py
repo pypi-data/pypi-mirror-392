e6b12b34a67b175e6ba6f0ca0e2c6cd3882a5645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 16:09:01 +0000

    Add package osclient/0.1.1/osclient-0.1.1-py3-none-any.whl

---
+++ packages/osclient/osclient-0.1.1-py3-none-any.whl/osclient/__init__.py
@@ -0,0 +1,10 @@
+"""
+osclient - OpenStack client library
+"""
+
+from .openstack import OpenStack
+from .openstack2 import OpenStack2
+
+__version__ = '0.1.0'
+__all__ = ['OpenStack', 'OpenStack2']
+
