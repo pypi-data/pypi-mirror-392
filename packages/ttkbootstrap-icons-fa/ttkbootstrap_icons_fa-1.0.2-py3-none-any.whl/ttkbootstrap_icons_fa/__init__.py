7ec981021443dbf0c3dbd51b3e90093f96519e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 23:05:21 +0000

    Add package ttkbootstrap-icons-fa/1.0.2/ttkbootstrap_icons_fa-1.0.2-py3-none-any.whl

---
+++ packages/ttkbootstrap-icons-fa/ttkbootstrap_icons_fa-1.0.2-py3-none-any.whl/ttkbootstrap_icons_fa/__init__.py
@@ -0,0 +1,4 @@
+from .icon import FAIcon
+from .provider import FontAwesomeFontProvider
+
+__all__ = ["FontAwesomeFontProvider", "FAIcon"]
---
+++ packages/ttkbootstrap-icons-fa/ttkbootstrap_icons_fa-1.0.2-py3-none-any.whl/ttkbootstrap_icons_fa/tools/__init__.py
@@ -0,0 +1,2 @@
+"""Developer tools for managing Font Awesome assets."""
+
