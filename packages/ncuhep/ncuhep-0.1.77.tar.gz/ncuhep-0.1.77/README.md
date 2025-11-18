579e6b65f6cbe43537da69919c00f83192547309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:06:02 +0000

    Add package ncuhep/0.1.77/ncuhep-0.1.77.tar.gz

---
+++ packages/ncuhep/ncuhep-0.1.77.tar.gz/ncuhep-0.1.77/README.md
@@ -0,0 +1,33 @@
+# NCUHEP
+
+This project provides modules for the purpose of muography data reconstruction and simulation 
+
+    ncuhep.muography.parser
+    -parser
+
+    ncuhep.muography.utils
+    -cart2slope
+    -cart2slope_vec
+    -slope2cart
+    -slope2cart_vec
+    -det2earth
+    -det2earth_vec
+    -earth2det
+    -earth2det_vec
+    -zenith_azimuth_deg
+    -zenith_azimuth_deg_vec
+    -effective_area
+    -solid_angle
+    -array2combo
+    -intercept_
+    -multiple_intercept_
+    -track_reconstruction
+    -homogenous_generator
+    -detection_simulation
+    -run_homogenous_simulation
+    -compute_basis
+
+## Installation
+
+```sh
+pip install ncuhep
---
+++ packages/ncuhep/ncuhep-0.1.77.tar.gz/ncuhep-0.1.77/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ncuhep',
+    version='0.1.77',
+    author='Kah Seng Phay',
+    author_email='phay_ks@icloud.com',
+    description='A Python project for NCU High Energy Physics.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/ncuhep',
+    packages=find_packages(),
+    license='MIT',  # Add this line
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'opencv-python',
+        'numpy',
+        "scipy",
+        "iminuit",
+        "matplotlib",
+        "tqdm",
+        "numba",
+        "pandas",
+    ],
+)
