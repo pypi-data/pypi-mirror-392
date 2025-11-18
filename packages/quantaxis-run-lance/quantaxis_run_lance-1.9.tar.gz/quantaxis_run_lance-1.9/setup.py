92817f774c457555ef71ccc821b49b5032abb99b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 18:06:08 +0000

    Add package quantaxis-run-lance/1.9/quantaxis_run_lance-1.9.tar.gz

---
+++ packages/quantaxis-run-lance/quantaxis_run_lance-1.9.tar.gz/quantaxis_run_lance-1.9/setup.py
@@ -0,0 +1,53 @@
+import codecs
+import io
+import os
+import re
+import sys
+import webbrowser
+import platform
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+
+NAME = "quantaxis_run-lance"
+"""
+名字，一般放你包的名字即可
+"""
+PACKAGES = ["quantaxis_run"]
+"""
+包含的包，可以多个，这是一个列表
+"""
+
+DESCRIPTION = "QUANTAXIS:Quantitative Financial Strategy Framework"
+KEYWORDS = ["quantaxis", "quant", "finance", "Backtest", 'Framework']
+AUTHOR_EMAIL = "yutiansut@qq.com"
+AUTHOR = 'yutiansut'
+URL = "https://github.com/yutiansut/quantaxis_run"
+
+
+LICENSE = "MIT"
+
+setup(
+    name=NAME,
+    version='1.9',
+    description=DESCRIPTION,
+    long_description='automatic run quantaxis program',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=['celery>4', 'quantaxis-lance', 'qaenv'],
+    keywords=KEYWORDS,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license=LICENSE,
+    packages=PACKAGES,
+    include_package_data=True,
+    zip_safe=True
+)
