4b117dc3a7ca6d4932d88bc4488ffaad922e7533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 13:14:40 +0000

    Add package mecha/0.100.0b4/mecha-0.100.0b4.tar.gz

---
+++ packages/mecha/mecha-0.100.0b4.tar.gz/mecha-0.100.0b4/src/mecha/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "0.100.0-beta.4"
+
+
+from .api import *  # noqa: F403
+from .ast import *  # noqa: F403
+from .config import *  # noqa: F403
+from .database import *  # noqa: F403
+from .diagnostic import *  # noqa: F403
+from .dispatch import *  # noqa: F403
+from .error import *  # noqa: F403
+from .parse import *  # noqa: F403
+from .plugin import *  # noqa: F403
+from .preprocess import *  # noqa: F403
+from .prototype import *  # noqa: F403
+from .resources import *  # noqa: F403
+from .serialize import *  # noqa: F403
+from .spec import *  # noqa: F403
