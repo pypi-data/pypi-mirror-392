25b92bd3575379877c763b98882f4079131c90a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 13:14:46 +0000

    Add package thermopro-ble/1.1.1/thermopro_ble-1.1.1-py3-none-any.whl

---
+++ packages/thermopro-ble/thermopro_ble-1.1.1-py3-none-any.whl/thermopro_ble/__init__.py
@@ -0,0 +1,35 @@
+"""Parser for ThermoPro BLE advertisements."""
+
+from __future__ import annotations
+
+from sensor_state_data import (
+    BinarySensorDeviceClass,
+    BinarySensorValue,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceClass,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .device import ThermoProDevice
+from .parser import ThermoProBluetoothDeviceData
+
+__version__ = "1.1.1"
+
+__all__ = [
+    "ThermoProDevice",
+    "ThermoProBluetoothDeviceData",
+    "BinarySensorDeviceClass",
+    "BinarySensorValue",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceClass",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+]
