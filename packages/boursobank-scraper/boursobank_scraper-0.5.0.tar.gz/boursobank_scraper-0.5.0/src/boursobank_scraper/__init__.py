e9738455e8a55cbe203cb84fce52437a3fa65e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:53 +0000

    Add package boursobank-scraper/0.5.0/boursobank_scraper-0.5.0.tar.gz

---
+++ packages/boursobank-scraper/boursobank_scraper-0.5.0.tar.gz/boursobank_scraper-0.5.0/src/boursobank_scraper/__init__.py
@@ -0,0 +1,9 @@
+from boursobank_scraper.account import BoursoAccount
+from boursobank_scraper.bourso_scraper import BoursoScraper
+from boursobank_scraper.models import BoursoApiOperation
+
+__all__ = [
+    "BoursoAccount",
+    "BoursoScraper",
+    "BoursoApiOperation",
+]
