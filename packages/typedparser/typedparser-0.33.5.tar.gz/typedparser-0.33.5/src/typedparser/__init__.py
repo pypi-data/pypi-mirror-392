d3ab1f32422481dc35f1b66add04b2510876075c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 12:04:59 +0000

    Add package typedparser/0.33.5/typedparser-0.33.5.tar.gz

---
+++ packages/typedparser/typedparser-0.33.5.tar.gz/typedparser-0.33.5/src/typedparser/__init__.py
@@ -0,0 +1,28 @@
+from ._typedattr import NamedTupleMixin, attrs_from_dict, definenumpy
+from ._typedparser import (
+    TaskSplitterArgs,
+    TypedParser,
+    VerboseQuietArgs,
+    add_argument,
+    split_list_for_processing,
+    split_list_given_task_splitter_args,
+)
+from .custom_format import CustomArgparseFmt
+from .objects import get_attr_names
+
+__all__ = [
+    "definenumpy",
+    "attrs_from_dict",
+    "NamedTupleMixin",
+    "add_argument",
+    "TypedParser",
+    "VerboseQuietArgs",
+    "CustomArgparseFmt",
+    "get_attr_names",
+    "TaskSplitterArgs",
+    "split_list_given_task_splitter_args",
+    "split_list_for_processing",
+]
+
+
+__version__ = "0.33.5"
