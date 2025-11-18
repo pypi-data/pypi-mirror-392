03865d44cc0581956475762cacc4073817058d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:51 +0000

    Add package cylindra/1.0.2/cylindra-1.0.2-cp313-cp313-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/cylindra/cylindra-1.0.2-cp313-cp313-manylinux_2_17_s390x.manylinux2014_s390x.whl/cylindra/core.py
@@ -0,0 +1,294 @@
+from __future__ import annotations
+
+import glob
+from contextlib import suppress
+from pathlib import Path
+from typing import TYPE_CHECKING, Iterable, Literal, Sequence, overload
+from weakref import WeakSet
+
+if TYPE_CHECKING:
+    import napari
+    from acryo import Molecules
+    from magicgui.widgets import Widget
+
+    from cylindra.components import CylSpline
+    from cylindra.project import CylindraProject, ProjectSequence
+    from cylindra.widgets import CylindraMainWidget
+
+    PathLike = str | Path
+
+_CURRENT_INSTANCE: CylindraMainWidget | None = None
+ACTIVE_WIDGETS: WeakSet[Widget] = WeakSet()
+
+
+def start(
+    project_file: str | None = None,
+    viewer: napari.Viewer | None = None,
+    *,
+    log_level: int | str = "INFO",
+    headless: bool = False,
+    add_main_widget: bool = True,
+    run: bool = True,
+) -> CylindraMainWidget:
+    """Start napari viewer and dock cylindra widget as a dock widget.
+
+    Parameters
+    ----------
+    project_file : path-like, optional
+        If given, load the project file.
+    viewer : napari.Viewer
+        Give a viewer object and this viewer will be used as the parent.
+    log_level : int or str, default "INFO"
+        Log level. See `logging` module for details.
+    headless : bool, default False
+        If True, do not show the viewer.
+    """
+    from cylindra.widgets import CylindraMainWidget  # noqa: I001
+    import impy as ip
+    import matplotlib.pyplot as plt
+    import napari
+    import numpy as np
+    import polars as pl
+    from IPython import get_ipython
+    from magicclass import logging
+
+    from cylindra._config import init_config
+
+    global _CURRENT_INSTANCE
+
+    if viewer is None:
+        viewer = napari.Viewer(show=not headless)
+    elif not isinstance(viewer, napari.Viewer):
+        raise TypeError(f"viewer must be a napari.Viewer object, got {type(viewer)}")
+
+    init_config()
+    ui = CylindraMainWidget()
+    ui.macro.options.max_undo = 16
+    ACTIVE_WIDGETS.add(ui)
+
+    # set logger
+    logger = logging.getLogger("cylindra")
+    formatter = logging.Formatter(fmt="%(levelname)s || %(message)s")
+    logger.widget.setFormatter(formatter)
+    logger.widget.min_height = 150
+
+    # set log level
+    if isinstance(log_level, str):
+        log_level = log_level.upper()
+        if log_level in ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"):
+            log_level = getattr(logging, log_level)
+        else:
+            raise ValueError(f"Invalid log level: {log_level}")
+    logger.setLevel(log_level)
+
+    # set polars display options
+    pl.Config().set_tbl_width_chars(120)
+
+    if add_main_widget:
+        dock = viewer.window.add_dock_widget(
+            ui, area="right", allowed_areas=["right"], name="cylindra"
+        )
+        dock.setMinimumHeight(300)
+    viewer.window.add_dock_widget(logger.widget, name="Log")
+    ui.macro.options.syntax_highlight = False
+
+    if project_file is not None:
+        ui.load_project(project_file)
+    _CURRENT_INSTANCE = ui
+
+    with suppress(Exception):
+        # update console namespace
+        viewer.window._qt_viewer.console.push(
+            {
+                ".ui": ui,  # only available from namespace dict
+                "ui": ui,
+                "np": np,
+                "ip": ip,
+                "pl": pl,
+                "plt": plt,
+                "Path": Path,
+            }
+        )
+
+    with suppress(Exception):  # This block uses private API.
+        # napari viewer does not disconnect layer events when the viewer is closed,
+        # so we need to do it manually
+        @viewer.window._qt_window.destroyed.connect
+        def _on_destroy():
+            viewer.layers.events.removing.disconnect()
+            viewer.layers.events.removed.disconnect()
+
+        # napari-console disables calltips by default. It's better to enable it.
+        viewer.window._qt_viewer.console.enable_calltips = True
+
+    with suppress(Exception):  # napari>=0.6.0
+        viewer.camera.orientation = ("away", "down", "right")
+
+    @viewer.bind_key("J", overwrite=True)
+    def _focus_up(v: napari.Viewer):
+        v.dims.set_current_step(0, v.dims.current_step[0] - 4)
+
+    @viewer.bind_key("K", overwrite=True)
+    def _focus_down(v: napari.Viewer):
+        v.dims.set_current_step(0, v.dims.current_step[0] + 4)
+
+    ui.show(run=run)
+    if add_main_widget:
+        try:  # Just in case
+            # avoid accidentally closing/hiding the dock widget
+            dock.title.close_button.disconnect()
+            dock.title.hide_button.disconnect()
+        except Exception:  # pragma: no cover
+            print("Failed to disconnect the close/hide button of the dock widget.")
+
+    # Programmatically run `%matplotlib inline` magic
+    if ipy := get_ipython():
+        ipy.run_line_magic("matplotlib", "inline")
+
+    return ui
+
+
+def start_as_plugin(run: bool = True):
+    """Start Cylindra as a napari plugin"""
+    import napari
+    from magicclass import logging
+
+    ui = start(
+        viewer=napari.current_viewer(),
+        add_main_widget=False,
+        run=run,
+    )
+    # float logger widget
+    logger = logging.getLogger("cylindra")
+    logger.widget.native.parentWidget().setFloating(True)
+    logger.widget.height = 160
+    return ui
+
+
+# fmt: off
+@overload
+def instance(create: Literal[False] = False) -> CylindraMainWidget | None: ...
+@overload
+def instance(create: Literal[True]) -> CylindraMainWidget: ...
+# fmt: on
+
+
+def instance(create=False):
+    """Get the current CylindraMainWidget instance."""
+    ins = _CURRENT_INSTANCE
+    if ins is None and create:
+        ins = start()
+    return ins
+
+
+def _delete_instance():
+    """Delete the current CylindraMainWidget instance."""
+    global _CURRENT_INSTANCE
+    _CURRENT_INSTANCE = None
+
+
+def view_project(project_file: PathLike, show: bool = True):
+    """View the Cylindra project file."""
+    from cylindra.project import CylindraProject
+
+    widget = CylindraProject.from_file(project_file).make_project_viewer()
+    if show:
+        widget.show(run=False)
+    ACTIVE_WIDGETS.add(widget)
+    return widget
+
+
+def read_project(file: PathLike) -> CylindraProject:
+    """Read the Cylindra project file."""
+    from cylindra.project import CylindraProject
+
+    return CylindraProject.from_file(file)
+
+
+def read_molecules(
+    file: PathLike,
+    pos_cols: Sequence[str] = ("z", "y", "x"),
+    rot_cols: Sequence[str] = ("zvec", "yvec", "xvec"),
+) -> Molecules:
+    """Read a molecules CSV or parquet file.
+
+    Parameters
+    ----------
+    file : PathLike
+        File path.
+    pos_cols : sequence of str, default ("z", "y", "x")
+        Column names for the molecule positions.
+    rot_cols : sequence of str, default ("zvec", "yvec", "xvec")
+        Column names for the molecule rotation vectors.
+
+    Returns
+    -------
+    Molecules
+        Molecules object.
+    """
+    from acryo import Molecules
+
+    path = Path(file)
+    return Molecules.from_file(path, pos_cols=list(pos_cols), rot_cols=list(rot_cols))
+
+
+def read_spline(file: PathLike) -> CylSpline:
+    """Read a spline file.
+
+    Parameters
+    ----------
+    file : PathLike
+        File path.
+
+    Returns
+    -------
+    CylSpline
+        CylSpline object.
+    """
+    from cylindra.components import CylSpline
+
+    return CylSpline.from_json(file)
+
+
+def collect_projects(
+    files: PathLike | Iterable[PathLike],
+    *,
+    skip_exc: bool = False,
+) -> ProjectSequence:
+    """Collect project files into a ProjectSequence object.
+
+    Examples
+    --------
+    ``` python
+    collect_projects("path/to/dir/*.json")
+    collect_projects(["path/to/dir-0/*.zip", "path/to/dir-1/*.zip"])
+    ```
+
+    Parameters
+    ----------
+    files : path-like or iterable of path-like
+        Project file paths or glob pattern(s).
+    """
+    from cylindra.project import ProjectSequence
+
+    if isinstance(files, (str, Path)):
+        if "*" in str(files):
+            _files = glob.glob(str(files))
+        else:
+            if not Path(files).exists():
+                raise FileNotFoundError(f"File not found: {files}")
+            _files = [files]
+    elif hasattr(files, "__iter__"):
+        _files = []
+        for f in files:
+            f = str(f)
+            if "*" not in f:
+                _files.append(f)
+            else:
+                _files.extend(list(glob.glob(f)))
+    else:
+        raise TypeError(f"files must be path or iterable of paths, got {type(files)}")
+    if len(_files) == 0:
+        raise FileNotFoundError(f"No project files found from the input {files!r}.")
+    seq = ProjectSequence.from_paths(_files, check_scale=False, skip_exc=skip_exc)
+    return seq
---
+++ packages/cylindra/cylindra-1.0.2-cp313-cp313-manylinux_2_17_s390x.manylinux2014_s390x.whl/cylindra/project/_widgets.py
@@ -0,0 +1,207 @@
+from typing import TYPE_CHECKING, Annotated
+
+import impy as ip
+import numpy as np
+import polars as pl
+from magicclass import (
+    MagicTemplate,
+    abstractapi,
+    field,
+    magicclass,
+    magicmenu,
+    set_design,
+)
+from magicclass.ext.polars import DataFrameView
+from magicclass.ext.vispy import Vispy3DCanvas
+from magicclass.types import Path
+from magicclass.utils import thread_worker
+from magicclass.widgets import ConsoleTextEdit, FrameContainer, Label, ToggleSwitch
+
+from cylindra.const import ImageFilter
+from cylindra.widget_utils import capitalize
+
+if TYPE_CHECKING:
+    from magicclass.ext.vispy._base import LayerItem
+
+    from cylindra.project._single import CylindraProject
+    from cylindra.widgets import CylindraMainWidget
+
+
+@magicclass(labels=False, widget_type="split", layout="horizontal", record=False)
+class TextInfo(MagicTemplate):
+    project_text = field(ConsoleTextEdit)
+
+    @magicclass(labels=False, record=False, widget_type="split")
+    class Right(MagicTemplate):
+        config = field(ConsoleTextEdit)
+        macro_script = field(ConsoleTextEdit)
+
+    def _from_project(self, project: "CylindraProject", dir: Path):
+        from cylindra.widget_utils import get_code_theme
+
+        theme = get_code_theme(self)
+        path = dir / "project.json"
+        if path.exists():
+            self.project_text.value = path.read_text()
+        else:
+            # NOTE: paths are alreadly resolved in project.json so this might be
+            # different from the original json file.
+            self.project_text.value = str(
+                project.model_dump_json(indent=4, separators=(", ", ": "))
+            )
+
+        self.project_text.read_only = True
+        self.project_text.syntax_highlight("json", theme=theme)
+        cfg_path = project._default_spline_config_path(dir)
+        if cfg_path.exists():
+            self.Right.config.value = cfg_path.read_text()
+        self.Right.config.read_only = True
+        self.Right.config.syntax_highlight("json", theme=theme)
+
+        macro_path = dir / "script.py"
+        if macro_path.exists():
+            self.Right.macro_script.value = macro_path.read_text()
+        self.Right.macro_script.read_only = True
+        self.Right.macro_script.syntax_highlight("python", theme=theme)
+
+
+@magicclass(labels=False, layout="horizontal", record=False)
+class ComponentsViewer(MagicTemplate):
+    canvas = field(Vispy3DCanvas)
+
+    @magicclass(labels=False, widget_type="scrollable", properties={"min_width": 220})
+    class components(MagicTemplate):
+        def _add_layer(self, layer: "LayerItem", visible: bool = True):
+            visible_btn = ToggleSwitch(value=visible, text="")
+            label = Label(value=layer.name)
+            cont = FrameContainer(
+                widgets=[visible_btn, label], layout="horizontal", labels=False
+            )
+            cont.margins = (0, 0, 0, 0)
+            cont.min_width = 200
+
+            @visible_btn.changed.connect
+            def _on_visible_change(value: bool):
+                layer.visible = value
+
+            layer.visible = visible
+            self.append(cont)
+
+    def _from_project(self, project: "CylindraProject", dir: Path):
+        self.canvas.layers.clear()
+        self.components.clear()
+
+        for i, spl in enumerate(project.iter_load_splines(dir)):
+            coords = spl.partition(100)
+            layer = self.canvas.add_curve(
+                coords, color="crimson", width=5.0, name=f"spline-{i}"
+            )
+            self.components._add_layer(layer)
+
+        for info, mole in project.iter_load_molecules(dir):
+            layer = self.canvas.add_points(mole.pos, face_color="lime", name=info.stem)
+            self.components._add_layer(layer, info.visible)
+
+        # draw edge
+        if project.image is None or not project.image.exists():
+            return
+        img = ip.lazy.imread(project.image)
+        nz, ny, nx = img.shape
+        for z in [0, nz]:
+            arr = (
+                np.array([[z, 0, 0], [z, 0, nx], [z, ny, nx], [z, ny, 0], [z, 0, 0]])
+                * img.scale.x
+            )
+            self.canvas.add_curve(arr, color="gray")
+        for y, x in [(0, 0), (0, nx), (ny, nx), (ny, 0)]:
+            arr = np.array([[0, y, x], [nz, y, x]]) * img.scale.x
+            self.canvas.add_curve(arr, color="gray")
+
+
+@magicclass(labels=False, widget_type="split", record=False)
+class Properties(MagicTemplate):
+    table_local = field(widget_type=DataFrameView)
+    table_global = field(widget_type=DataFrameView)
+
+    def _from_project(self, project: "CylindraProject", dir: Path):
+        localprops_path = project._localprops_path(dir)
+        globalprops_path = project._globalprops_path(dir)
+        if localprops_path.exists():
+            df = pl.read_csv(localprops_path)
+            self.table_local.value = df
+        else:
+            self.table_local.value = pl.DataFrame([["Not found"]])
+
+        if globalprops_path.exists():
+            df = pl.read_csv(globalprops_path)
+            self.table_global.value = df
+        else:
+            self.table_global.value = pl.DataFrame([["Not found"]])
+
+
+@magicclass(widget_type="tabbed", name="Project Viewer", record=False)
+class ProjectViewer(MagicTemplate):
+    @magicmenu
+    class Menu(MagicTemplate):
+        load_this_project = abstractapi()
+        preview_image = abstractapi()
+        close_window = abstractapi()
+
+    def __init__(self):
+        self._project: CylindraProject | None = None
+
+    info_viewer = field(TextInfo, name="Text files")
+    component_viewer = field(ComponentsViewer, name="Components")
+    properties = field(Properties, name="Properties")
+
+    def _from_project(self, project: "CylindraProject"):
+        self._project = project
+        with project.open_project() as dir:
+            self.info_viewer._from_project(project, dir)
+            self.component_viewer._from_project(project, dir)
+            self.properties._from_project(project, dir)
+
+    def _get_project_path(self, *_):
+        if self._project is None:
+            raise ValueError("Project path is not known.")
+        return self._project.project_path
+
+    @thread_worker
+    @set_design(text=capitalize, location=Menu)
+    def load_this_project(
+        self,
+        path: Annotated[str, {"bind": _get_project_path}],
+        filter: ImageFilter | None = ImageFilter.Lowpass,
+        read_image: Annotated[bool, {"label": "read image data"}] = True,
+        update_config: bool = False,
+    ):
+        """Load current project in main window."""
+        from cylindra import instance
+
+        ui: CylindraMainWidget | None = None
+
+        @thread_worker.callback
+        def _launch_ui():
+            nonlocal ui
+            ui = instance(create=True)
+            self.native.setParent(ui.native, self.native.windowFlags())
+
+        yield _launch_ui
+        _launch_ui.await_call()
+
+        if ui is None:
+            raise RuntimeError("Main window is not running.")
+        yield from ui.load_project.arun(path, filter, read_image, update_config)
+        return thread_worker.callback(self.close)
+
+    @set_design(text=capitalize, location=Menu)
+    def preview_image(self):
+        """Preview the tomogram image."""
+        from cylindra._previews import view_image
+
+        return view_image(self._project.image, parent=self)
+
+    @set_design(text="Close", location=Menu)
+    def close_window(self):
+        """Close this preview."""
+        return self.close()
