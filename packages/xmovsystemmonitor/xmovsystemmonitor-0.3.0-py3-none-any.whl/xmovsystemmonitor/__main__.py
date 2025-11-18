0b336c2baddc0cd070286949221813eca4235f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 18:06:12 +0000

    Add package xmovsystemmonitor/0.3.0/xmovsystemmonitor-0.3.0-py3-none-any.whl

---
+++ packages/xmovsystemmonitor/xmovsystemmonitor-0.3.0-py3-none-any.whl/xmovsystemmonitor/__main__.py
@@ -0,0 +1,45 @@
+"""命令行入口 - 显示系统监控信息"""
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import uvicorn
+import argparse
+from systemmonitor.system_info_monitor_thread import SystemInfoMonitorThread
+from systemmonitor import get_system_info
+
+from systemmonitor.logger import logger
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--host", type=str, default="0.0.0.0")
+parser.add_argument("--port", type=int, default=8000)
+args = parser.parse_args()
+
+app = FastAPI()
+background_thread = SystemInfoMonitorThread(interval=1.0)
+background_thread.start()
+
+@app.get("/")
+def read_root():
+    info = get_system_info()
+    return JSONResponse(content=info)
+
+@app.get("/start-monitoring")
+def start_monitoring():
+    """启动监控"""
+    background_thread.start_to_monitor()
+    return JSONResponse(content={"message": "监控已启动"})
+
+@app.get("/stop-monitoring")
+def stop_monitoring():
+    """停止监控"""
+    background_thread.stop_to_monitor()
+    return JSONResponse(content={"message": "监控已停止"})
+
+@app.get("/statistic")
+def statistic():
+    """获取监控统计信息"""
+    return JSONResponse(content=background_thread.statistic())
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=args.host, port=args.port)
