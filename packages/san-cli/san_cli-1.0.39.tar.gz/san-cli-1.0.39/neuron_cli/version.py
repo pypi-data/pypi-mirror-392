4e654140e99f48766aedd925ae938711fec17355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 22:08:12 +0000

    Add package san-cli/1.0.39/san_cli-1.0.39.tar.gz

---
+++ packages/san-cli/san_cli-1.0.39.tar.gz/san-cli-1.0.39/neuron_cli/version.py
@@ -0,0 +1,5 @@
+"""Version information for Neuron CLI"""
+
+__version__ = "1.0.39"  # SAN CLI (SPACE Agent Neuron CLI) - Add list_installed method to MarketplaceManager
+__author__ = "Kai Gartner (https://linkedin.com/in/kaigartner)"
+__license__ = "Commercial - All Rights Reserved"
