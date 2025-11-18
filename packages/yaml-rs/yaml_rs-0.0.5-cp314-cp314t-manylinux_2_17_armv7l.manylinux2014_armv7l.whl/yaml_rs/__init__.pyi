a1562c8560f7cedb914a6807837b411f7f7f971b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 19:05:13 +0000

    Add package yaml-rs/0.0.5/yaml_rs-0.0.5-cp314-cp314t-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/yaml-rs/yaml_rs-0.0.5-cp314-cp314t-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/yaml_rs/__init__.pyi
@@ -0,0 +1,19 @@
+from pathlib import Path
+from typing import Any, BinaryIO, TextIO
+
+__version__: str
+
+def load(
+    fp: BinaryIO | bytes | str,
+    /,
+    *,
+    parse_datetime: bool = True,
+    encoding: str | None = None,
+    encoder_errors: str | None = None,
+) -> dict[str, Any] | list[dict[str, Any]]: ...
+def loads(s: str, /, *, parse_datetime: bool = True) -> dict[str, Any] | list[dict[str, Any]]: ...
+def dump(obj: Any, file: str | Path | TextIO) -> int: ...
+def dumps(obj: Any) -> str: ...
+
+class YAMLDecodeError(ValueError): ...
+class YAMLEncodeError(TypeError): ...
