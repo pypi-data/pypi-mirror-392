3a31ac9e3ca7195ac0f73e678624377e415a9e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 15:05:11 +0000

    Add package lazyurl/1.0.0/lazyurl-1.0.0.tar.gz

---
+++ packages/lazyurl/lazyurl-1.0.0.tar.gz/lazyurl-1.0.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# coding = utf8
+"""
+@ Author : ZeroSeeker
+@ e-mail : zeroseeker@foxmail.com
+@ GitHub : https://github.com/ZeroSeeker
+@ Gitee : https://gitee.com/ZeroSeeker
+"""
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lazyurl",
+    version="1.0.0",
+    description="解析url",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="ZeroSeeker",
+    author_email="zeroseeker@foxmail.com",
+    url="https://gitee.com/ZeroSeeker/lazyurl",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[]
+)
