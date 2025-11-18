eaee453c0454c3d525cc24e13b08e19f16f8d08b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 05:04:58 +0000

    Add package TechTrash-s3file/0.1.0/techtrash_s3file-0.1.0-py3-none-any.whl

---
+++ packages/TechTrash-s3file/techtrash_s3file-0.1.0-py3-none-any.whl/s3file/__init__.py
@@ -0,0 +1,7 @@
+# Import de la classe OvhS3 depuis le module main
+# Cela permet d'utiliser: from s3file import OvhS3
+from .main import OvhS3
+
+# Liste des symboles exportés publiquement par le package
+__all__ = ["OvhS3"]
+
