36fe54c905d7a0850f68e6b870fe24349092e5d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:34 +0000

    Add package mjnf/0.1.0/mjnf-0.1.0.tar.gz

---
+++ packages/mjnf/mjnf-0.1.0.tar.gz/mjnf-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mjnf",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "mjnf = mjnf.main:main"
+        ]
+    },
+    author="zombyburger567777777",
+    description="Решение части 1 и полностью 2 номер из кр Сучкова линал",
+)
+
