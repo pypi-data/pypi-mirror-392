a74098f8e2aa56092618f26e7f298f2f925ceef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 18:06:12 +0000

    Add package pyquoks/2.0.1/pyquoks-2.0.1-py3-none-any.whl

---
+++ packages/pyquoks/pyquoks-2.0.1-py3-none-any.whl/pyquoks/__init__.py
@@ -0,0 +1,9 @@
+from . import models, data, utils, localhost, test
+
+__all__ = [
+    "models",
+    "data",
+    "utils",
+    "localhost",
+    "test",
+]
---
+++ packages/pyquoks/pyquoks-2.0.1-py3-none-any.whl/pyquoks/utils.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+import datetime, sys, os
+import psutil
+
+
+class _HasRequiredAttributes:
+    """
+    Assistive class for checking for required attributes
+
+    **Required attributes**::
+
+        _REQUIRED_ATTRIBUTES = {"_ATTRIBUTES", "_OBJECTS"}
+
+    Attributes:
+        _REQUIRED_ATTRIBUTES: Set of required attributes in the class
+    """
+
+    _REQUIRED_ATTRIBUTES: set[str]
+
+    def _check_attributes(self) -> None:
+        if hasattr(self, "_REQUIRED_ATTRIBUTES"):
+            for attribute in self._REQUIRED_ATTRIBUTES:
+                if not hasattr(self, attribute):
+                    raise AttributeError(f"The required class attribute is not set! ({attribute})")
+
+
+def get_path(relative_path: str, use_meipass: bool = False) -> str:
+    """
+    :param relative_path: Relative path of the file
+    :param use_meipass: Whether or not ``sys._MEIPASS`` should be used
+    :return: Absolute path for provided relative path
+    """
+
+    if use_meipass and hasattr(sys, "_MEIPASS"):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+def get_started_datetime() -> datetime.datetime:
+    """
+    :return: Datetime when the current process was started
+    """
+
+    process = psutil.Process(os.getpid())
+
+    return datetime.datetime.fromtimestamp(
+        timestamp=process.create_time(),
+    )
