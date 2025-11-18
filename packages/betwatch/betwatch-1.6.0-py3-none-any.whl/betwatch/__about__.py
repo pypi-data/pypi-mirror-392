af8ac3b43978ec5729062ef146ab0f939790c9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:06:20 +0000

    Add package betwatch/1.6.0/betwatch-1.6.0-py3-none-any.whl

---
+++ packages/betwatch/betwatch-1.6.0-py3-none-any.whl/betwatch/__about__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2022-present Jamie Watts <jamie@betwatch.com>
+#
+# SPDX-License-Identifier: MIT
+import importlib.metadata
+
+__version__ = importlib.metadata.version("betwatch")
