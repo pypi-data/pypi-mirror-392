adb0461acc0a0362087a2021ac4bb25ca3837f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 13:14:40 +0000

    Add package fpquantize/0.0.2/fpquantize-0.0.2-py3-none-any.whl

---
+++ packages/fpquantize/fpquantize-0.0.2-py3-none-any.whl/fpquantize/e5m2.py
@@ -0,0 +1,13 @@
+import torch
+class QuantizeFP8(torch.nn.Module):
+    class _QuantizeFP8(torch.autograd.Function):
+        @staticmethod
+        def forward(ctx, x):
+            return x.clamp(-61439., 61439.).to(torch.float8_e5m2fnuz)
+        @staticmethod
+        def backward(ctx, grad_output):
+            return grad_output
+    def __init__(self):
+        super(QuantizeFP8, self).__init__()
+    def forward(self, x):
+        return QuantizeFP8._QuantizeFP8.apply(x)
