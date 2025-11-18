4ad0391f178702485bdd2969d84a707a9fc883cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 03:24:03 +0000

    Add package wnet/0.9.4/wnet-0.9.4-cp311-cp311-win_arm64.whl

---
+++ packages/wnet/wnet-0.9.4-cp311-cp311-win_arm64.whl/wnet/__init__.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .distribution import Distribution_1D, Distribution
+from . import wnet_cpp
+from .wasserstein_network import WassersteinNetwork
+from .wasserstein import WassersteinDistance, TruncatedWassersteinDistance
