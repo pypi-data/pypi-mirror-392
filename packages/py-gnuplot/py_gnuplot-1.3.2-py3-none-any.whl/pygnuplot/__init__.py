bed8583d1d224e02867cd1225ed1b8d62735c9b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 23:05:13 +0000

    Add package py-gnuplot/1.3.2/py_gnuplot-1.3.2-py3-none-any.whl

---
+++ packages/py-gnuplot/py_gnuplot-1.3.2-py3-none-any.whl/pygnuplot/__init__.py
@@ -0,0 +1,13 @@
+# $Id: __init__.py,v 1.1.1.1 2024/02/02 07:06:20 shanshi Exp $
+
+"""gnuplot python wrapper."""
+
+__author__ = 'Yongping Guo <guoyoooping@163.com>'
+__copyright__ = 'Copyright (c) 2020 Yongping guo'
+__license__ = 'GPL'
+__url__ = 'http://www.gnuplot.info'
+__version__ = '0.1'
+
+#from gnuplotpy import *
+#import gnuplotpy
+__all__ = ['gnuplot', 'pyplot'] 
