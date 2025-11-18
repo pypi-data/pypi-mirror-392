eeb79d1af625539af2d2f17dee088dfa9358c2fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 07:05:20 +0000

    Add package garmin-to-fittrackee/0.11.1/garmin_to_fittrackee-0.11.1.tar.gz

---
+++ packages/garmin-to-fittrackee/garmin_to_fittrackee-0.11.1.tar.gz/garmin_to_fittrackee-0.11.1/garmin_to_fittrackee/fittrackee.py
@@ -0,0 +1,312 @@
+import json
+from pathlib import Path
+from typing import Union
+
+import pendulum
+import requests
+import typer
+import yaml
+from packaging.version import Version
+from requests_oauthlib import OAuth2Session
+from rich import print
+
+from garmin_to_fittrackee.logs import Log
+from garmin_to_fittrackee.workout import Workout
+
+log = Log(__name__)
+
+
+class Fittrackee:
+    def __init__(
+        self,
+        config_path: str,
+        client_id: str = None,
+        client_secret: str = None,
+        host: str = None,
+        timezone: str = None,
+        verify: bool = True,
+    ):
+        log.debug("Initializing FitTrackeeConnector")
+        self.config_path = config_path
+        self.tokens = None
+        self.host = host
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.api_url = None
+        self.sports = None
+        if timezone is None:
+            dt = pendulum.now()
+            timezone = dt.timezone.name
+        self.timezone = timezone
+        self.__load_config()
+        self.client = self.__auth()
+
+    def __save_config(self):
+        log.debug("Saving data")
+        data = {
+            "fittrackee": {
+                "host": self.host,
+                "client_id": self.client_id,
+                "client_secret": self.client_secret,
+            },
+            "tokens": self.tokens,
+        }
+        fittrackee_config = Path(f"{self.config_path}/fittrackee.yml")
+        with fittrackee_config.open("w", encoding="utf-8") as file:
+            yaml.dump(data, file, default_flow_style=False)
+        return
+
+    def __load_config(self):
+        fittrackee_config = Path(f"{self.config_path}/fittrackee.yml")
+        if fittrackee_config.is_file():
+            with fittrackee_config.open() as stream:
+                try:
+                    config = yaml.load(stream, Loader=yaml.Loader)
+                except (yaml.YAMLError, yaml.scanner.ScannerError) as exc:
+                    log.error(
+                        "Can't load configuration."
+                        "Please check {self.config_path}/fittrackee.yml"
+                        "or remove this file"
+                    )
+                    log.error(exc)
+                    raise typer.Exit(code=1) from None
+
+            self.tokens = config["tokens"]
+            self.client_id = config["fittrackee"]["client_id"]
+            self.client_secret = config["fittrackee"]["client_secret"]
+            self.host = config["fittrackee"]["host"]
+            self.api_url = f"https://{self.host}/api"
+
+    def __auth(self):
+        """
+        Checks if a valid access token exists in the token file;
+        if not, tries to get a new one via a refresh token (if present)
+        or prompts the user to authenticate in order to get a brand new
+        token.
+        """
+        log.debug("Setting up FitTrackee auth")
+        if self.tokens is None:
+            log.debug("No FitTrackee tokens found; fetching new ones")
+            return self.__web_application_flow()
+        else:
+            log.debug("Using existing FitTrackee tokens with self-refreshing client")
+            return self.__get_refreshing_client()
+
+    def __web_application_flow(self):
+        authorize_url = f"https://{self.host}/profile/apps/authorize"
+        self.api_url = f"https://{self.host}/api"
+
+        redirect_uri = "https://localhost/"
+        scope = "workouts:read workouts:write profile:read"
+        oauth = OAuth2Session(self.client_id, redirect_uri=redirect_uri, scope=scope)
+        authorization_url, state = oauth.authorization_url(authorize_url)
+        print(f"Please go to {authorization_url} and authorize access.\n")
+        authorization_response = typer.prompt(
+            "Enter the full callback URL from the browser address bar"
+            "after you are redirected and press <enter>"
+        )
+        print(authorization_response)
+        log.debug("Logging to fittrackee instance")
+        self.tokens = oauth.fetch_token(
+            f"{self.api_url}/oauth/token",
+            authorization_response=authorization_response,
+            client_secret=self.client_secret,
+            include_client_id=True,
+            verify=True,
+        )
+        log.info("Logging successfull. Saving configuration")
+        self.__save_config()
+        return oauth
+
+    def __get_refreshing_client(self):
+        refresh_params = {
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+        }
+        client = OAuth2Session(
+            self.client_id,
+            token=self.tokens,
+            auto_refresh_url=f"{self.api_url}/oauth/token",
+            auto_refresh_kwargs=refresh_params,
+            token_updater=self.__token_update,
+        )
+        return client
+
+    def __token_update(self, token):
+        log.debug("New token receive. Save it")
+        self.tokens = token
+        self.__save_config()
+
+    def is_workout_present(self):
+        try:
+            r = self.client.get(
+                f"{self.api_url}/workouts", params={"per_page": 1, "page": 1}
+            )
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as error:
+            error_code = error.response.status_code
+            log.debug(error.response.headers)
+            log.error(
+                "Failed to get all workouts."
+                f"Return code {error_code}. Error {error.response.text}"
+            )
+            return
+        except requests.RequestException as e:
+            log.error(str(e))
+            return
+        results = r.json()
+        log.debug(f"Workkouts: {results['data']['workouts']}")
+        log.debug(f"Count workkouts: {len(results['data']['workouts'])}")
+        return len(results["data"]["workouts"]) != 0
+
+    def get_all_workouts(self, pagination: int = 50):
+        workouts = []
+        log.info(f"Get all workout from fittrackee (in page of {pagination})")
+        results = {"pagination": {"has_next": True}}
+        page = 1
+        while results["pagination"]["has_next"]:
+            try:
+                r = self.client.get(
+                    f"{self.api_url}/workouts",
+                    params={"per_page": pagination, "page": page},
+                )
+                r.raise_for_status()
+            except requests.exceptions.HTTPError as error:
+                error_code = error.response.status_code
+                log.debug(error.response.headers)
+                log.error(
+                    "Failed to get all workouts."
+                    f"Return code {error_code}. Error {error.response.text}"
+                )
+                return
+            except requests.RequestException as e:
+                log.error(str(e))
+                return
+            results = r.json()
+            for workout in results["data"]["workouts"]:
+                workout_object = object.__new__(Workout)
+                workout_object.__dict__ = workout
+                workout_object.set_present_in_fittrackee()
+                workouts.append(workout_object)
+            log.debug(
+                f"Fetched page {page} of workouts (fetched {len(workouts)} so far)"
+            )
+            page += 1
+            return workouts
+
+    def get_last_workout(self):
+        try:
+            r = self.client.get(
+                f"{self.api_url}/workouts",
+                params={"per_page": 1, "page": 1, "order": "desc"},
+            )
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as error:
+            error_code = error.response.status_code
+            log.debug(error.response.headers)
+            log.error(
+                "Failed to get workout."
+                f"Return code {error_code}. Error {error.response.text}"
+            )
+            return
+        except requests.RequestException as e:
+            log.error(str(e))
+            return
+        results = r.json()
+        for workout in results["data"]["workouts"]:
+            workout_object = object.__new__(Workout)
+            workout_object.__dict__ = workout
+            workout_object.set_present_in_fittrackee()
+            return workout_object
+
+    def upload_workout(
+        self, file: Union[str, Path], sport_id: int, notes: str = None, name: str = ""
+    ):
+        """
+        Higly inspired of https://github.com/jat255/strava-to-fittrackee/blob/main/strava_to_fittrackee/s2f.py#L805
+        """
+        if not Path(file).is_file():
+            log.error(f"{file} is not exist or is not a file")
+            return
+
+        log.debug(f"posting {file} to FitTrackee")
+        data = {"sport_id": sport_id, "notes": "", "title": name}
+        gpx = Path(file)
+        try:
+            r = self.client.post(
+                f"{self.api_url}/workouts",
+                files=dict(file=gpx.open("rb")),
+                data=dict(data=json.dumps(data)),
+            )
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as error:
+            error_code = error.response.status_code
+            log.debug(error.response.headers)
+            log.error(
+                f"Failed to post {file}."
+                f"Return code {error_code}. Error {error.response.text}"
+            )
+            return
+        except requests.RequestException as e:
+            log.error(str(e))
+            return
+        results = r.json()
+        workout = object.__new__(Workout)
+        workout.__dict__ = results["data"]["workouts"][0]
+        workout.set_present_in_fittrackee()
+        workout.set_present_in_garmin()
+        log.info(f"Activity added on Fittrackee with id {workout.id}")
+        return workout
+
+    def get_sports(self):
+        """
+        Needed only during development
+        """
+        r = self.client.get(f"{self.api_url}/sports")
+        results = r.json()
+        return results
+
+    def delete_workout(self, workout_id: int):
+        try:
+            r = self.client.delete(f"{self.api_url}/workouts/{workout_id}")
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as error:
+            error_code = error.response.status_code
+            log.debug(error.response.headers)
+            log.error(
+                "Failed to get sports's list."
+                f"Return code {error_code}. Error {error.response.text}"
+            )
+            return
+        except requests.RequestException as e:
+            log.error(str(e))
+            return
+        log.warning(f"Workout {workout_id} deleted")
+
+    @staticmethod
+    def get_instance_config(host: str):
+        try:
+            r = requests.get(f"https://{host}/api/config")
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as error:
+            error_code = error.response.status_code
+            log.debug(error.response.headers)
+            log.error(
+                "Failed to get instance config."
+                f"Return code {error_code}. Error {error.response.text}"
+            )
+            return
+        except requests.RequestException as e:
+            log.error(str(e))
+            return
+        return r.json()
+
+    @staticmethod
+    def is_instance_is_supported(host: str):
+        config = Fittrackee.get_instance_config(host=host)
+        return (
+            config
+            and "data" in config
+            and "version" in config["data"]
+            and Version(config["data"]["version"]) >= Version("0.7.29")
+        )
---
+++ packages/garmin-to-fittrackee/garmin_to_fittrackee-0.11.1.tar.gz/garmin_to_fittrackee-0.11.1/garmin_to_fittrackee/main.py
@@ -0,0 +1,384 @@
+import os
+import sqlite3
+from pathlib import Path
+from typing import Annotated
+from urllib.parse import urlparse
+
+import pendulum
+import typer
+import yaml
+from garminconnect import Garmin
+
+from garmin_to_fittrackee.fittrackee import Fittrackee
+from garmin_to_fittrackee.logs import Log
+from garmin_to_fittrackee.sports import Sports
+
+log = Log(name=__name__)
+
+app = typer.Typer()
+
+GarminActivityFormatExtension = {
+    "ActivityDownloadFormat.ORIGINAL": ".zip",
+    "ActivityDownloadFormat.TCX": ".tcx",
+    "ActivityDownloadFormat.GPX": ".gpx",
+    "ActivityDownloadFormat.KML": ".kml",
+    "ActivityDownloadFormat.CSV": ".CSV",
+}
+
+GarminActivityFormat = {
+    "original": Garmin.ActivityDownloadFormat.ORIGINAL,
+    "tcx": Garmin.ActivityDownloadFormat.TCX,
+    "gpx": Garmin.ActivityDownloadFormat.GPX,
+    "kml": Garmin.ActivityDownloadFormat.KML,
+    "csv": Garmin.ActivityDownloadFormat.CSV,
+}
+
+
+def default_config_path():
+    home = str(Path.home())
+    return f"{home}/.config/garmin-to-fittrackee"
+
+
+def default_database_path():
+    home = str(Path.home())
+    return f"{home}/.local/share/garmin_to_fittrackee"
+
+
+config_path = os.environ.get("CONFIG_PATH", default_config_path())
+database_path = os.environ.get("DATABASE_PATH", default_database_path())
+default_tmp_path = os.environ.get("TMP_PATH", "/tmp")
+Path(config_path).mkdir(parents=True, exist_ok=True)
+
+setup = typer.Typer()
+app.add_typer(setup, name="setup")
+
+if Path(f"{config_path}/config.yml").is_file():
+    log.debug("Import config")
+    with open(f"{config_path}/config.yml") as file:
+        config = yaml.safe_load(file)
+
+    db = sqlite3.connect(f"{config['sqlite']['path']}/db.sqlite3")
+
+
+@app.command()
+def sync(
+    start_year: Annotated[
+        int,
+        typer.Option(
+            help=(
+                "Year of your first record."
+                "Only used when no workouts found on Fittrackee."
+            )
+        ),
+    ] = None,
+    activity_format: Annotated[
+        str,
+        typer.Option(
+            help=(
+                "File format download from Garmin."
+                "Can be original, gpx, tcx, kml, csv. Default is all."
+            )
+        ),
+    ] = None,
+    interactive: Annotated[
+        bool, typer.Option(help="Can ask question during the sync")
+    ] = True,
+):
+    """
+    Synchronise Garmin's activities in Fittrackee.
+    First run, all of gamin's activities will be fetch.
+    """
+    if not config_exists():
+        return
+
+    if activity_format and activity_format not in [
+        "original",
+        "tcx",
+        "gpx",
+        "kml",
+        "csv",
+    ]:
+        log.error(f"{activity_format} not in original, tcx, gpx, kml, csv")
+        raise typer.Exit(code=1)
+    fittrackee = Fittrackee(config_path)
+    if fittrackee.is_workout_present():
+        workout = fittrackee.get_last_workout()
+        start_datetime = pendulum.parse(workout.workout_date, strict=False)
+        log.debug(f"Start date is {start_datetime.isoformat()}")
+        start_datetime = start_datetime.add(minutes=1)
+        end_datetime = start_datetime.add(days=1)
+    else:
+        if interactive:
+            log.warning("No workout present on Fittrackee")
+            start_year = int(typer.prompt("What year was your first Garmin activity?"))
+            log.info(f"Okay, fetching activity on Garmin from year {start_year}")
+        elif not interactive and start_year is not None:
+            log.warning(
+                "No workout present on Fittrackee."
+                f"Fetch all workouts on Garmin from year {start_year}"
+            )
+        else:
+            log.error(
+                "Not workout found on Fittrackee and --no-interactive set."
+                "Please specify a year to start with --start-year"
+            )
+            raise typer.Exit(code=1)
+        if start_year < 1990:
+            log.error(
+                "Garmin was created at the end of the year 1989 only for US army."
+                "I'm sure you don't have GPX file from this date"
+            )
+            raise typer.Exit(code=1)
+        start_datetime = pendulum.datetime(start_year, 1, 1, 0, 0, 0)
+        end_datetime = start_datetime.add(days=1)
+
+    today = pendulum.now()
+    if today.diff(end_datetime, False).in_seconds() > 0:
+        end_datetime = today
+    garmin = Garmin()
+    garmin.login(f"{config_path}/garmintoken")
+    log.debug(
+        f"Condition in while : {today.diff(start_datetime, False).in_seconds()} < 0"
+    )
+    while today.diff(start_datetime, False).in_seconds() < 0:
+        log.info(
+            "Fetching activities on Garmin"
+            f"from {start_datetime.to_formatted_date_string()}"
+            f"to {end_datetime.to_formatted_date_string()}"
+        )
+        activities = garmin.get_activities_by_date(
+            start_datetime.to_date_string(), end_datetime.to_date_string()
+        )
+        if activities:
+            for activity in activities:
+                cur = db.cursor()
+                res = cur.execute(
+                    f"SELECT garmin_id "
+                    f"FROM activities_ids "
+                    f"WHERE garmin_id='{activity['activityId']}'"
+                )
+                if res.fetchone() is not None:
+                    continue
+                activityType_id = activity["activityType"]["typeId"]
+                fittrackee_sport_id = Sports.get_fittrackee_sport_by_garmin_id(
+                    activityType_id
+                )
+                for _, fileformat in GarminActivityFormat.items():
+                    if (
+                        activity_format
+                        and GarminActivityFormat[activity_format] != fileformat
+                    ):
+                        log.debug(f"{fileformat} is not {activity_format}")
+                        continue
+                    file = _fetch_garmin_activity_file(
+                        garmin=garmin,
+                        activity_id=activity["activityId"],
+                        GarminFileFormat=fileformat,
+                    )
+                    if not file:
+                        continue
+
+                    workout = fittrackee.upload_workout(
+                        file=file,
+                        sport_id=fittrackee_sport_id,
+                        name=activity.get("activityName"),
+                    )
+                    if workout is not None:
+                        log.debug(f"Deleting {file}")
+                        Path(file).unlink(missing_ok=True)
+                        if config["sqlite"]["use"]:
+                            log.debug(
+                                "Adding workout and activity matches in tool database"
+                            )
+                            log.debug(
+                                f"Using Fittrackee ID {workout.id}"
+                                f"and Garmin ID {activity['activityId']}"
+                            )
+                            data_insert = (workout.id, activity["activityId"])
+                            cur = db.cursor()
+                            cur.execute(
+                                "INSERT INTO activities_ids (fittrackee_id, garmin_id)"
+                                "VALUES(?, ?)",
+                                data_insert,
+                            )
+                            db.commit()
+                        break
+        start_datetime = start_datetime.add(days=2)
+        end_datetime = start_datetime.add(days=1)
+        if today.diff(end_datetime, False).in_seconds() > 0:
+            end_datetime = today
+
+
+def _fetch_garmin_activity_file(garmin, activity_id: int, GarminFileFormat):
+    data = garmin.download_activity(activity_id, GarminFileFormat)
+    file = (
+        f"{default_tmp_path}/{str(activity_id)}"
+        f"{GarminActivityFormatExtension[str(GarminFileFormat)]}"
+    )
+    with open(file, "wb") as fb:
+        fb.write(data)
+        log.info(f"Activity data downloaded to file {file}")
+    return file
+
+
+def _send_to_fittrackee():
+    pass
+
+
+@app.command()
+def reset(
+    force: Annotated[
+        bool,
+        typer.Option(help="Security reason, you need to force the reset"),
+    ] = False,
+):
+    """
+    Reset database and remove ALL of workouts in Fittrackee. (Usefull for development)
+    """
+    if not force:
+        log.warning("No force, no chocolate")
+        return
+    cur = db.cursor()
+    cur.execute("DROP TABLE activities_ids")
+    db.commit()
+    cur = db.cursor()
+    cur.execute(
+        "CREATE TABLE"
+        "activities_ids(fittrackee_id VARCHAR(255) UNIQUE,"
+        "garmin_id INTEGER(100) UNIQUE"
+        ")"
+    )
+    db.commit()
+    fittrackee = Fittrackee(config_path)
+    workouts = fittrackee.get_all_workouts()
+    if workouts:
+        for workout in workouts:
+            fittrackee.delete_workout(workout.id)
+
+
+@setup.command()
+def garmin(
+    email: Annotated[
+        str,
+        typer.Option(
+            help="Email of your Garmin account. If not specify, we use prompt."
+        )
+        == "",
+        typer.Option(prompt=True),
+    ],
+    password: Annotated[
+        str,
+        typer.Option(
+            help="Password of your Garmin account. If not specify, we use prompt."
+        )
+        == "",
+        typer.Option(prompt=True, hide_input=True),
+    ],
+    store: bool = True,
+):
+    garmin = Garmin(email, password)
+    garmin.login()
+    if store:
+        data = {"garmin": {"username": email, "password": password}}
+        with open(f"{config_path}/garmin.yml", "w") as file:
+            yaml.dump(data, file, default_flow_style=False)
+
+    garmin.garth.dump(f"{config_path}/garmintoken")
+
+
+@setup.command()
+def fittrackee(
+    client_id: Annotated[
+        str,
+        typer.Option(help="Client id of fittrackee. If not specify, we use prompt.")
+        == "",
+        typer.Option(prompt=True),
+    ],
+    client_secret: Annotated[
+        str,
+        typer.Option(help="Client secret of fittrackee. If not specify, we use prompt.")
+        == "",
+        typer.Option(prompt=True, hide_input=True),
+    ],
+    fittrackee_domain: Annotated[
+        str,
+        typer.Option(
+            help=(
+                "Domain of Fittrackee instance (without https)."
+                "If not specify, we use prompt."
+            )
+        )
+        == "",
+        typer.Option(prompt=True),
+    ],
+    force: Annotated[bool, typer.Option(help="Rewrite configuration file")] = False,
+):
+    if force:
+        log.warning("Rewrite configuration file")
+        Path(f"{config_path}/fittrackee.yml").unlink(missing_ok=True)
+    url = urlparse(fittrackee_domain)
+    if url.hostname:
+        fittrackee_domain = url.hostname
+    if not Fittrackee.is_instance_is_supported(host=fittrackee_domain):
+        log.error(
+            "Fittrackee instance isn't supported. "
+            "Please update your Fittrackee instance"
+        )
+        raise typer.Exit(code=1)
+    Fittrackee(
+        config_path=config_path,
+        client_id=client_id,
+        client_secret=client_secret,
+        host=fittrackee_domain,
+    )
+
+
+@setup.command()
+def config_tool(
+    database_path: Annotated[
+        str,
+        typer.Option(help="Database location. If not exist, path will be created."),
+    ] = database_path,
+    verbose_level: Annotated[
+        str,
+        typer.Option(help="Verbose level. Maybe DEBUG, INFO, WARNING, ERROR, CRITICAL"),
+    ] = "INFO",
+):
+    data = {
+        "sqlite": {"use": True, "path": database_path},
+        "log": {"level": verbose_level},
+    }
+    with open(f"{config_path}/config.yml", "w") as file:
+        yaml.dump(data, file, default_flow_style=False)
+    log.debug("Create database")
+    path = Path(database_path)
+    path.mkdir(parents=True, exist_ok=True)
+
+    db = sqlite3.connect(f"{database_path}/db.sqlite3")
+    cur = db.cursor()
+    cur.execute(
+        "CREATE TABLE "
+        "activities_ids(fittrackee_id VARCHAR(255) UNIQUE,"
+        "garmin_id INTEGER(100) UNIQUE)"
+    )
+
+
+def config_exists():
+    if (
+        not Path(f"{config_path}/garmintoken").is_dir()
+        or not Path(f"{config_path}/fittrackee.yml").is_file()
+        or not Path(f"{config_path}/config.yml").is_file()
+    ):
+        log.error(
+            "Config files aren't present."
+            "Start using garmin-to-fittrackee with `setup config-tool`,"
+            "`setup garmin` and `setup fittrackee` command"
+        )
+        return False
+    else:
+        return True
+
+
+if __name__ == "__main__":
+    config_exists()
+    app()
