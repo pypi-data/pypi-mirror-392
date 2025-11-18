e2850b381544c645e98c043e5a1e43942c1416e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:20 +0000

    Add package maleo-metadata/0.7.50/maleo_metadata-0.7.50-py3-none-any.whl

---
+++ packages/maleo-metadata/maleo_metadata-0.7.50-py3-none-any.whl/maleo/metadata/constants/system_role.py
@@ -0,0 +1,9 @@
+from maleo.schemas.resource import Resource, ResourceIdentifier
+
+
+SYSTEM_ROLE_RESOURCE = Resource(
+    identifiers=[
+        ResourceIdentifier(key="system_role", name="System Role", slug="system-roles")
+    ],
+    details=None,
+)
