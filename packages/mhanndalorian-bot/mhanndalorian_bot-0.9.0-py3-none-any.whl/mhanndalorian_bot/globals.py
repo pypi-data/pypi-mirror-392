7d98fb8357a98268aad0d84babe8a07bbb66548f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 16:06:14 +0000

    Add package mhanndalorian-bot/0.9.0/mhanndalorian_bot-0.9.0-py3-none-any.whl

---
+++ packages/mhanndalorian-bot/mhanndalorian_bot-0.9.0-py3-none-any.whl/mhanndalorian_bot/globals.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+"""
+Global settings and variable for the Mhanndalorian Bot API library
+"""
+
+from __future__ import annotations
+
+SWGOH_GG_BASE_URL = "https://swgoh.gg"
+SWGOH_GG_GUILD_PATH = 'g'
+
---
+++ packages/mhanndalorian-bot/mhanndalorian_bot-0.9.0-py3-none-any.whl/mhanndalorian_bot/models.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+"""
+Object models for the MBot API library
+"""
+
+from __future__ import annotations
+
+
+class Guild:
+    pass
