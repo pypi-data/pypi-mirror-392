f228e74e2b593608695adcb139ccf003ca572321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 18:06:29 +0000

    Add package EFN/3.0/efn-3.0-py3-none-any.whl

---
+++ packages/EFN/efn-3.0-py3-none-any.whl/EFN/__init__.py
@@ -0,0 +1,17 @@
+from .core import *
+from .coreforbrowser import fullgui, halfguiconsole, fullconsole
+from .coreforimg import showimage, createimagewidget
+from .coreforqrcode import qrcode
+from .coreforspeak import speak
+from .coreforweather import checkweather
+from .coreforinternet import *
+from .coreforhtml import createhtmlgui
+from .coreforurl import downloadfile, geturl, getip, starthttpserver, starthttpsserver, pingserver, httpget, httppost
+from .EFNC import *
+from .EFNASM import *
+from .EFNKERNEL import *
+from .corefor3d import *
+from .corefordiscordpy import *
+from .coreforai import *
+from .coreforphonenumbers import *
+from .coreforurml import *
