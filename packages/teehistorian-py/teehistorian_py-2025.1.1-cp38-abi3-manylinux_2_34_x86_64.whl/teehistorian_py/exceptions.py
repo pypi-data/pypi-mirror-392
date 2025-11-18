2042f4f45a457aac70510fc44b86915c40703993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 20:06:28 +0000

    Add package teehistorian-py/2025.1.1/teehistorian_py-2025.1.1-cp38-abi3-manylinux_2_34_x86_64.whl

---
+++ packages/teehistorian-py/teehistorian_py-2025.1.1-cp38-abi3-manylinux_2_34_x86_64.whl/teehistorian_py/exceptions.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Essential exception types for teehistorian library."""
+
+
+class TeehistorianError(Exception):
+    """Base exception for all teehistorian-related errors."""
+
+    pass
