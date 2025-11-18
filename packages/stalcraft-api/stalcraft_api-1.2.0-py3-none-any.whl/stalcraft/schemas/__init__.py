d72978f45918188edb05a967736dd97b93bab30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 06:04:26 +0000

    Add package stalcraft-api/1.2.0/stalcraft_api-1.2.0-py3-none-any.whl

---
+++ packages/stalcraft-api/stalcraft_api-1.2.0-py3-none-any.whl/stalcraft/schemas/__init__.py
@@ -0,0 +1,14 @@
+from .api import (
+    RegionInfo, Emission,
+    ClanInfo, ClanMember,
+    CharacterClan, CharacterInfo, Character,
+    CharacterStatistic, CharacterProfile,
+    Price, Lot,
+    OperationParticipant, OperationSession
+)
+
+from .auth import (
+    AppToken, UserToken, TokenInfo
+)
+
+from .ratelimit import RateLimit
