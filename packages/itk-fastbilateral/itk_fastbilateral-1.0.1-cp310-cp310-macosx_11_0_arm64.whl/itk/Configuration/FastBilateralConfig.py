7ed04d37aae28c0107844d3e78346609d81301e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:25 +0000

    Add package itk-fastbilateral/1.0.1/itk_fastbilateral-1.0.1-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/itk-fastbilateral/itk_fastbilateral-1.0.1-cp310-cp310-macosx_11_0_arm64.whl/itk/Configuration/FastBilateralConfig.py
@@ -0,0 +1,18 @@
+depends = ('ITKPyBase', 'ITKSmoothing', 'ITKImageSources', 'ITKCommon', )
+templates = (  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterISS2ISS2', True, 'itk::Image< signed short,2 >, itk::Image< signed short,2 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterISS3ISS3', True, 'itk::Image< signed short,3 >, itk::Image< signed short,3 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterISS4ISS4', True, 'itk::Image< signed short,4 >, itk::Image< signed short,4 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterIUC2IUC2', True, 'itk::Image< unsigned char,2 >, itk::Image< unsigned char,2 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterIUC3IUC3', True, 'itk::Image< unsigned char,3 >, itk::Image< unsigned char,3 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterIUC4IUC4', True, 'itk::Image< unsigned char,4 >, itk::Image< unsigned char,4 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterIUS2IUS2', True, 'itk::Image< unsigned short,2 >, itk::Image< unsigned short,2 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterIUS3IUS3', True, 'itk::Image< unsigned short,3 >, itk::Image< unsigned short,3 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterIUS4IUS4', True, 'itk::Image< unsigned short,4 >, itk::Image< unsigned short,4 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterIF2IF2', True, 'itk::Image< float,2 >, itk::Image< float,2 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterIF3IF3', True, 'itk::Image< float,3 >, itk::Image< float,3 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterIF4IF4', True, 'itk::Image< float,4 >, itk::Image< float,4 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterID2ID2', True, 'itk::Image< double,2 >, itk::Image< double,2 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterID3ID3', True, 'itk::Image< double,3 >, itk::Image< double,3 >'),
+  ('FastBilateralImageFilter', 'itk::FastBilateralImageFilter', 'itkFastBilateralImageFilterID4ID4', True, 'itk::Image< double,4 >, itk::Image< double,4 >'),
+)
+factories = ()
