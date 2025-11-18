c23c9b62c44665dd4c24ae7f67baf3265efde4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 06:04:23 +0000

    Add package chonkie/1.4.2/chonkie-1.4.2-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/chonkie/chonkie-1.4.2-cp312-cp312-macosx_11_0_arm64.whl/chonkie/embeddings/__init__.py
@@ -0,0 +1,28 @@
+"""Embeddings classes for text embedding."""
+
+from .auto import AutoEmbeddings
+from .azure_openai import AzureOpenAIEmbeddings
+from .base import BaseEmbeddings
+from .cohere import CohereEmbeddings
+from .gemini import GeminiEmbeddings
+from .jina import JinaEmbeddings
+from .model2vec import Model2VecEmbeddings
+from .openai import OpenAIEmbeddings
+from .registry import EmbeddingsRegistry
+from .sentence_transformer import SentenceTransformerEmbeddings
+from .voyageai import VoyageAIEmbeddings
+
+# Add all embeddings classes to __all__
+__all__ = [
+    "BaseEmbeddings",
+    "Model2VecEmbeddings",
+    "SentenceTransformerEmbeddings",
+    "OpenAIEmbeddings",
+    "AzureOpenAIEmbeddings",
+    "CohereEmbeddings",
+    "GeminiEmbeddings",
+    "AutoEmbeddings",
+    "JinaEmbeddings",
+    "VoyageAIEmbeddings",
+    "EmbeddingsRegistry",
+]
