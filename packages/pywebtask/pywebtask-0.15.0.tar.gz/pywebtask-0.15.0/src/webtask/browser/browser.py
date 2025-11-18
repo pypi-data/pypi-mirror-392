18e6daabfc731c81abc4032df17ad6efd0e9008d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 16:06:20 +0000

    Add package pywebtask/0.15.0/pywebtask-0.15.0.tar.gz

---
+++ packages/pywebtask/pywebtask-0.15.0.tar.gz/pywebtask-0.15.0/src/webtask/browser/browser.py
@@ -0,0 +1,53 @@
+"""Browser base class for browser lifecycle management."""
+
+from abc import ABC, abstractmethod
+
+
+class Browser(ABC):
+    """
+    Abstract base class for browser management.
+
+    Manages browser lifecycle (launch, close, etc.).
+    """
+
+    def __init__(self, headless: bool = False):
+        """
+        Initialize the browser.
+
+        Args:
+            headless: Whether to run browser in headless mode
+        """
+        self.headless = headless
+
+    @classmethod
+    @abstractmethod
+    async def create(cls, **kwargs):
+        """
+        Create and launch the browser instance (async factory method).
+
+        Returns:
+            Browser instance
+
+        Example:
+            >>> browser = await PlaywrightBrowser.create(headless=True)
+        """
+        pass
+
+    @abstractmethod
+    async def create_session(self, **kwargs):
+        """
+        Create a new session/context in this browser.
+
+        Returns:
+            Session instance
+
+        Example:
+            >>> browser = await PlaywrightBrowser.create()
+            >>> session = await browser.create_session()
+        """
+        pass
+
+    @abstractmethod
+    async def close(self):
+        """Close the browser instance."""
+        pass
