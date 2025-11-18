dd0d005debbd49b3e656895b94ac83995dfb086c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 20:07:00 +0000

    Add package ngff-zarr/0.19.1/ngff_zarr-0.19.1-py3-none-any.whl

---
+++ packages/ngff-zarr/ngff_zarr-0.19.1-py3-none-any.whl/ngff_zarr/methods/_itkwasm.py
@@ -0,0 +1,282 @@
+# SPDX-FileCopyrightText: Copyright (c) Fideus Labs LLC
+# SPDX-License-Identifier: MIT
+from typing import Tuple
+from itertools import product
+
+import numpy as np
+from dask.array import map_blocks, map_overlap
+import dask.array
+
+from ..ngff_image import NgffImage
+from ._support import (
+    _align_chunks,
+    _compute_sigma,
+    _dim_scale_factors,
+    _update_previous_dim_factors,
+    _get_block,
+    _spatial_dims,
+    _spatial_dims_last_zyx,
+    _next_scale_metadata,
+    _next_block_shape,
+)
+
+_image_dims: Tuple[str, str, str, str] = ("x", "y", "z", "t")
+
+
+def _itkwasm_blur_and_downsample(
+    image_data,
+    shrink_factors,
+    kernel_radius,
+    smoothing,
+    is_vector=False,
+):
+    """Blur and then downsample a given image chunk"""
+    import itkwasm
+
+    # chunk does not have metadata attached, values are ITK defaults
+    image = itkwasm.image_from_array(image_data, is_vector=is_vector)
+    from rich import print
+
+    print("is_vector:", is_vector)
+    print("ITK Image:", image)
+
+    # Skip this image block if it has 0 voxels
+    block_size = image.size
+    if any(block_len == 0 for block_len in block_size):
+        return None
+
+    if smoothing == "gaussian":
+        from itkwasm_downsample import downsample
+
+        downsampled = downsample(
+            image, shrink_factors=shrink_factors, crop_radius=kernel_radius
+        )
+    elif smoothing == "label_image":
+        from itkwasm_downsample import downsample_label_image
+
+        downsampled = downsample_label_image(
+            image, shrink_factors=shrink_factors, crop_radius=kernel_radius
+        )
+    else:
+        msg = f"Unknown smoothing method: {smoothing}"
+        raise ValueError(msg)
+
+    return downsampled.data
+
+
+def _itkwasm_chunk_bin_shrink(
+    image_data,
+    shrink_factors,
+    is_vector=False,
+):
+    """Compute the local mean and downsample on a given image chunk"""
+    import itkwasm
+    from itkwasm_downsample import downsample_bin_shrink
+
+    # chunk does not have metadata attached, values are ITK defaults
+    image = itkwasm.image_from_array(image_data, is_vector=is_vector)
+
+    # Skip this image block if it has 0 voxels
+    block_size = image.size
+    if any(block_len == 0 for block_len in block_size):
+        return None
+
+    downsampled = downsample_bin_shrink(image, shrink_factors=shrink_factors)
+    return downsampled.data
+
+
+def _downsample_itkwasm(
+    ngff_image: NgffImage, default_chunks, out_chunks, scale_factors, smoothing
+):
+    from itkwasm_downsample import gaussian_kernel_radius
+
+    multiscales = [
+        ngff_image,
+    ]
+    previous_image = ngff_image
+    dims = tuple(ngff_image.dims)
+    previous_dim_factors = {d: 1 for d in dims}
+    spatial_dims = [dim for dim in dims if dim in _spatial_dims]
+    spatial_dims = _image_dims[: len(spatial_dims)]
+    transposed_dims = False
+    for scale_factor in scale_factors:
+        dim_factors = _dim_scale_factors(dims, scale_factor, previous_dim_factors)
+        previous_dim_factors = _update_previous_dim_factors(
+            scale_factor, spatial_dims, previous_dim_factors
+        )
+        previous_image = _align_chunks(previous_image, default_chunks, dim_factors)
+
+        # Operate on a contiguous spatial block
+        previous_image = _spatial_dims_last_zyx(previous_image)
+        if tuple(previous_image.dims) != dims:
+            transposed_dims = True
+            reorder = [previous_image.dims.index(dim) for dim in dims]
+
+        translation, scale = _next_scale_metadata(
+            previous_image, dim_factors, spatial_dims
+        )
+
+        # Blocks 0, ..., N-2 have the same shape
+        block_0_input = _get_block(previous_image, 0)
+        next_block_0_shape = _next_block_shape(
+            previous_image, dim_factors, spatial_dims, block_0_input
+        )
+        block_0_size = []
+        for dim in spatial_dims:
+            if dim in previous_image.dims:
+                block_0_size.append(block_0_input.shape[previous_image.dims.index(dim)])
+            else:
+                block_0_size.append(1)
+        block_0_size.reverse()
+
+        # Block N-1 may be smaller than preceding blocks
+        block_neg1_input = _get_block(previous_image, -1)
+        next_block_neg1_shape = _next_block_shape(
+            previous_image, dim_factors, spatial_dims, block_neg1_input
+        )
+
+        # Compute overlap for Gaussian blurring for all blocks
+        is_vector = previous_image.dims[-1] == "c"
+
+        # pixel units
+        # Compute metadata for region splitting
+        shrink_factors = [dim_factors[sd] for sd in spatial_dims]
+        sigma_values = _compute_sigma(shrink_factors)
+        kernel_radius = gaussian_kernel_radius(size=block_0_size, sigma=sigma_values)
+
+        dtype = block_0_input.dtype
+
+        output_chunks = list(previous_image.data.chunks)
+        output_chunks_start = 0
+        while previous_image.dims[output_chunks_start] not in _spatial_dims:
+            output_chunks_start += 1
+        output_chunks = output_chunks[output_chunks_start:]
+        next_block_0_shape = next_block_0_shape[output_chunks_start:]
+        for i, c in enumerate(output_chunks):
+            output_chunks[i] = [
+                next_block_0_shape[i],
+            ] * len(c)
+
+        next_block_neg1_shape = next_block_neg1_shape[output_chunks_start:]
+        for i in range(len(output_chunks)):
+            output_chunks[i][-1] = next_block_neg1_shape[i]
+            output_chunks[i] = tuple(output_chunks[i])
+        output_chunks = tuple(output_chunks)
+
+        non_spatial_dims = [d for d in dims if d not in _spatial_dims]
+        if "c" in non_spatial_dims and previous_image.dims[-1] == "c":
+            non_spatial_dims.remove("c")
+
+        if output_chunks_start > 0:
+            # We'll iterate over each index for the non-spatial dimensions, run the desired
+            # map_overlap, and aggregate the outputs into a final result.
+
+            # Determine the size for each non-spatial dimension
+            non_spatial_shapes = previous_image.data.shape[:output_chunks_start]
+
+            # Collect results for each sub-block
+            aggregated_blocks = []
+            for idx in product(*(range(s) for s in non_spatial_shapes)):
+                # Build the slice object for indexing
+                slice_obj = []
+                non_spatial_index = 0
+                for dim in previous_image.dims:
+                    if dim in non_spatial_dims:
+                        # Take a single index (like "t=0,1,...") for the non-spatial dimension
+                        slice_obj.append(idx[non_spatial_index])
+                        non_spatial_index += 1
+                    else:
+                        # Keep full slice for spatial/channel dims
+                        slice_obj.append(slice(None))
+
+                slice_obj = tuple(slice_obj)
+                # Extract the sub-block data for the chosen index from the non-spatial dims
+                sub_block_data = previous_image.data[slice_obj]
+
+                if smoothing == "bin_shrink":
+                    downscaled_sub_block = map_blocks(
+                        _itkwasm_chunk_bin_shrink,
+                        sub_block_data,
+                        shrink_factors=shrink_factors,
+                        is_vector=is_vector,
+                        dtype=dtype,
+                        chunks=output_chunks,
+                    )
+                else:
+                    downscaled_sub_block = map_overlap(
+                        _itkwasm_blur_and_downsample,
+                        sub_block_data,
+                        shrink_factors=shrink_factors,
+                        kernel_radius=kernel_radius,
+                        smoothing=smoothing,
+                        is_vector=is_vector,
+                        dtype=dtype,
+                        depth=dict(
+                            enumerate(np.flip(kernel_radius))
+                        ),  # overlap is in tzyx
+                        boundary="nearest",
+                        trim=False,  # Overlapped region is trimmed in blur_and_downsample to output size
+                        chunks=output_chunks,
+                    )
+                aggregated_blocks.append(downscaled_sub_block)
+            downscaled_array_shape = non_spatial_shapes + downscaled_sub_block.shape
+            downscaled_array = dask.array.empty(downscaled_array_shape, dtype=dtype)
+            for sub_block_idx, idx in enumerate(
+                product(*(range(s) for s in non_spatial_shapes))
+            ):
+                # Build the slice object for indexing
+                slice_obj = []
+                non_spatial_index = 0
+                for dim in previous_image.dims:
+                    if dim in non_spatial_dims:
+                        # Take a single index (like "t=0,1,...") for the non-spatial dimension
+                        slice_obj.append(idx[non_spatial_index])
+                        non_spatial_index += 1
+                    else:
+                        # Keep full slice for spatial/channel dims
+                        slice_obj.append(slice(None))
+
+                slice_obj = tuple(slice_obj)
+                downscaled_array[slice_obj] = aggregated_blocks[sub_block_idx]
+        else:
+            data = previous_image.data
+            if smoothing == "bin_shrink":
+                downscaled_array = map_blocks(
+                    _itkwasm_chunk_bin_shrink,
+                    data,
+                    shrink_factors=shrink_factors,
+                    is_vector=is_vector,
+                    dtype=dtype,
+                    chunks=output_chunks,
+                )
+            else:
+                downscaled_array = map_overlap(
+                    _itkwasm_blur_and_downsample,
+                    data,
+                    shrink_factors=shrink_factors,
+                    kernel_radius=kernel_radius,
+                    smoothing=smoothing,
+                    is_vector=is_vector,
+                    dtype=dtype,
+                    depth=dict(enumerate(np.flip(kernel_radius))),  # overlap is in tzyx
+                    boundary="nearest",
+                    trim=False,  # Overlapped region is trimmed in blur_and_downsample to output size
+                    chunks=output_chunks,
+                )
+
+        out_chunks_list = []
+        for dim in previous_image.dims:
+            if dim in out_chunks:
+                out_chunks_list.append(out_chunks[dim])
+            else:
+                out_chunks_list.append(1)
+        downscaled_array = downscaled_array.rechunk(tuple(out_chunks_list))
+
+        # transpose back to original order if needed (_spatial_dims_zyx transposed the order)
+        if transposed_dims:
+            downscaled_array = downscaled_array.transpose(reorder)
+
+        previous_image = NgffImage(downscaled_array, dims, scale, translation)
+        multiscales.append(previous_image)
+
+    return multiscales
---
+++ packages/ngff-zarr/ngff_zarr-0.19.1-py3-none-any.whl/ngff_zarr/methods/_support.py
@@ -0,0 +1,230 @@
+# SPDX-FileCopyrightText: Copyright (c) Fideus Labs LLC
+# SPDX-License-Identifier: MIT
+from typing import List
+import copy
+
+import numpy as np
+from numpy.typing import ArrayLike
+
+from ..ngff_image import NgffImage
+
+_spatial_dims = {"x", "y", "z"}
+
+
+def _spatial_dims_last(ngff_image: NgffImage) -> NgffImage:
+    dims = list(ngff_image.dims)
+    spatial_dims = [dim for dim in dims if dim in _spatial_dims]
+
+    dims_spatial_channel = len(spatial_dims)
+    if dims[-1] == "c":
+        dims_spatial_channel += 1
+
+    # If spatial dimensions are already last (and 'c' can be last), return the original image
+    if all(dim in dims[-dims_spatial_channel:] for dim in spatial_dims + ["c"]):
+        return ngff_image
+
+    # Move spatial dimensions to the end, keeping 'c' as the last pre-spatial dimension if present
+    non_spatial_dims = [dim for dim in dims if dim not in _spatial_dims]
+    if "c" in non_spatial_dims:
+        non_spatial_dims.remove("c")
+        new_dims = non_spatial_dims + ["c"] + spatial_dims
+    else:
+        new_dims = non_spatial_dims + spatial_dims
+
+    new_order = [dims.index(dim) for dim in new_dims]
+
+    if tuple(new_dims) == tuple(ngff_image.dims):
+        return ngff_image
+
+    # Reorder the data array
+    reordered_data = ngff_image.data.transpose(new_order)
+
+    result = copy.copy(ngff_image)
+    result.data = reordered_data
+    result.dims = tuple(new_dims)
+
+    return result
+
+
+def _spatial_dims_last_zyx(ngff_image: NgffImage) -> NgffImage:
+    dims = list(ngff_image.dims)
+    spatial_dims = [dim for dim in dims if dim in _spatial_dims]
+
+    # If spatial dimensions are already zyx, return the original image
+    if spatial_dims == ["z", "y", "x"] or spatial_dims == ["y", "x"]:
+        dims_spatial_channel = len(spatial_dims)
+        if dims[-1] == "c":
+            dims_spatial_channel += 1
+
+        # If spatial dimensions are already last (and 'c' can be last), return the original image
+        if all(dim in dims[-dims_spatial_channel:] for dim in spatial_dims + ["c"]):
+            return ngff_image
+
+    # Move spatial dimensions to the end, keeping 'c' as the last pre-spatial dimension if present
+    non_spatial_dims = [dim for dim in dims if dim not in _spatial_dims]
+    new_spatial_dims = ["z", "y", "x"][-len(spatial_dims) :]
+    if "c" in non_spatial_dims:
+        non_spatial_dims.remove("c")
+        new_dims = non_spatial_dims + ["c"] + new_spatial_dims
+    else:
+        new_dims = non_spatial_dims + new_spatial_dims
+
+    new_order = [dims.index(dim) for dim in new_dims]
+
+    if tuple(new_dims) == tuple(ngff_image.dims):
+        return ngff_image
+
+    # Reorder the data array
+    reordered_data = ngff_image.data.transpose(new_order)
+
+    result = copy.copy(ngff_image)
+    result.data = reordered_data
+    result.dims = tuple(new_dims)
+
+    return result
+
+
+def _channel_dim_last(ngff_image: NgffImage) -> NgffImage:
+    if "c" not in ngff_image.dims or ngff_image.dims[-1] == "c":
+        return ngff_image
+
+    dims = list(ngff_image.dims)
+    # Move 'c' dimension to the end
+    dims.remove("c")
+    dims.append("c")
+
+    # Reorder the data array
+    new_order = [ngff_image.dims.index(dim) for dim in dims]
+    reordered_data = ngff_image.data.transpose(new_order)
+
+    result = copy.copy(ngff_image)
+    result.data = reordered_data
+    result.dims = tuple(dims)
+
+    return result
+
+
+def _dim_scale_factors(dims, scale_factor, previous_dim_factors):
+    if isinstance(scale_factor, int):
+        result_scale_factors = {
+            dim: int(scale_factor / previous_dim_factors[dim])
+            for dim in dims
+            if dim in _spatial_dims
+        }
+    else:
+        result_scale_factors = {
+            d: int(scale_factor[d] / previous_dim_factors[d]) for d in scale_factor
+        }
+        # if a dim is not in the scale_factors, add it with a scale factor of 1
+        for d in dims:
+            if d not in result_scale_factors:
+                result_scale_factors[d] = 1
+
+    return result_scale_factors
+
+
+def _update_previous_dim_factors(scale_factor, spatial_dims, previous_dim_factors):
+    previous_dim_factors = copy.copy(previous_dim_factors)
+    if isinstance(scale_factor, int):
+        for d in spatial_dims:
+            previous_dim_factors[d] = scale_factor
+    else:
+        for d in scale_factor:
+            previous_dim_factors[d] = scale_factor[d]
+    return previous_dim_factors
+
+
+def _align_chunks(previous_image, default_chunks, dim_factors):
+    block_0_shape = [c[0] for c in previous_image.data.chunks]
+
+    rechunk = False
+    aligned_chunks = {}
+    for dim, factor in dim_factors.items():
+        dim_index = previous_image.dims.index(dim)
+        if block_0_shape[dim_index] % factor:
+            aligned_chunks[dim] = block_0_shape[dim_index] * factor
+            rechunk = True
+        else:
+            aligned_chunks[dim] = default_chunks[dim]
+    if rechunk:
+        dask_aligned_chunks = {
+            previous_image.dims.index(dim): aligned_chunks[dim]
+            for dim in aligned_chunks
+        }
+        previous_image.data = previous_image.data.rechunk(dask_aligned_chunks)
+
+    return previous_image
+
+
+def _compute_sigma(shrink_factors: List[int]) -> List[float]:
+    """Compute Gaussian kernel sigma values in pixel units for downsampling.
+    sigma = sqrt((k^2 - 1^2)/(2*sqrt(2*ln(2)))^2)
+    Ref https://discourse.itk.org/t/resampling-to-isotropic-signal-processing-theory/1403/16
+    https://doi.org/10.1007/978-3-319-24571-3_81
+    http://discovery.ucl.ac.uk/1469251/1/scale-factor-point-5.pdf
+
+    Note: If input spacing / output sigma in physical units, the function would be
+        sigma = sqrt((input_spacing^2*(k^2 - 1^2))/(2*sqrt(2*ln(2)))^2)
+
+    input spacings: List
+        Input image physical spacings in xyzt order
+
+    shrink_factors: List
+        Shrink ratio along each axis in xyzt order
+
+    result: List
+        Standard deviation of Gaussian kernel along each axis in xyzt order
+    """
+    import math
+
+    denominator = (2 * ((2 * math.log(2)) ** 0.5)) ** 2
+    return [((factor**2 - 1**2) / denominator) ** 0.5 for factor in shrink_factors]
+
+
+def _get_block(previous_image: NgffImage, block_index: int):
+    """Helper method for accessing an enumerated chunk from input"""
+    block_shape = [c[block_index] for c in previous_image.data.chunks]
+    block = previous_image.data[tuple([slice(0, s) for s in block_shape])]
+    return block
+
+
+def _next_scale_metadata(
+    previous_image: NgffImage, dim_factors: dict, spatial_dims: tuple
+) -> tuple:
+    """Compute the next scale metadata based on the previous image and scale factor."""
+
+    scale = {}
+    for dim in previous_image.dims:
+        if dim in spatial_dims:
+            scale[dim] = previous_image.scale[dim] * dim_factors[dim]
+        elif dim in previous_image.scale:
+            scale[dim] = previous_image.scale[dim]
+
+    translation = {}
+    for dim in previous_image.dims:
+        if dim in spatial_dims:
+            translation[dim] = previous_image.translation[dim] + (
+                0.5 * (dim_factors[dim] - 1) * previous_image.scale[dim]
+            )
+        elif dim in previous_image.translation:
+            translation[dim] = previous_image.translation[dim]
+
+    return translation, scale
+
+
+def _next_block_shape(
+    previous_image: NgffImage,
+    dim_factors: dict,
+    spatial_dims: tuple,
+    block_input: ArrayLike,
+) -> tuple:
+    """Compute the next block shape based on the previous image and scale factor."""
+
+    shape = []
+    for i, dim in enumerate(previous_image.dims):
+        if dim in spatial_dims:
+            shape.append(int(np.floor(block_input.shape[i] / dim_factors[dim])))
+        else:
+            shape.append(block_input.shape[i])
+
+    return tuple(shape)
