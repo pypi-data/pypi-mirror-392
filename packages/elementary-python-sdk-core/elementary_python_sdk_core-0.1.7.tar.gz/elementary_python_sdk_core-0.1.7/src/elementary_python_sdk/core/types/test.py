14ced24eea4d76b12b1741357c4a37f0e8f6d960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 19:05:12 +0000

    Add package elementary-python-sdk-core/0.1.7/elementary_python_sdk_core-0.1.7.tar.gz

---
+++ packages/elementary-python-sdk-core/elementary_python_sdk_core-0.1.7.tar.gz/elementary_python_sdk_core-0.1.7/src/elementary_python_sdk/core/types/test.py
@@ -0,0 +1,83 @@
+import uuid
+from datetime import datetime
+from enum import Enum
+from typing import Literal
+
+from elementary_python_sdk.core.types.base import ElementaryBaseModel
+
+
+class QualityDimension(str, Enum):
+    COMPLETENESS = "completeness"
+    UNIQUENESS = "uniqueness"
+    FRESHNESS = "freshness"
+    VALIDITY = "validity"
+    ACCURACY = "accuracy"
+    CONSISTENCY = "consistency"
+
+
+class TestType(str, Enum):
+    __test__ = False
+
+    DQX = "dqx"
+
+
+class TestSeverity(str, Enum):
+    __test__ = False
+
+    ERROR = "ERROR"
+    WARNING = "WARNING"
+
+
+class TestExecutionStatus(str, Enum):
+    __test__ = False
+
+    PASS = "PASS"
+    WARN = "WARN"
+    FAIL = "FAIL"
+    ERROR = "ERROR"
+    SKIPPED = "SKIPPED"
+    NO_DATA = "NO_DATA"
+
+
+class Test(ElementaryBaseModel):
+    kind: Literal["test"] = "test"
+    name: str
+    test_type: TestType
+    asset_id: str
+    column_name: str | None = None
+    severity: TestSeverity
+    config: dict | None = None
+    meta: dict | None = None
+
+    @property
+    def id(self) -> str:
+        return f"test.[{self.asset_id}].{self.column_name}.{self.name}"
+
+
+class PartialTestResult(ElementaryBaseModel):
+    kind: Literal["partial_test_result"] = "partial_test_result"
+    test_id: str
+    sub_type: str
+    status: TestExecutionStatus
+    column_name: str
+
+
+class TestExecution(ElementaryBaseModel):
+    kind: Literal["test_execution"] = "test_execution"
+    test_id: str
+    test_sub_unique_id: str
+    column_name: str | None = None
+    sub_type: str | None = None
+    quality_dimension: QualityDimension | None = None
+    start_time: datetime
+    status: TestExecutionStatus
+    failure_count: int
+    description: str
+    code: str | None = None
+    duration_seconds: float
+    exception: str | None = None
+    traceback: str | None = None
+
+    @property
+    def id(self) -> str:
+        return str(uuid.uuid5(uuid.NAMESPACE_DNS, f"{self.test_id}.{self.start_time}"))
