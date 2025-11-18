d4ea87f32e651c1a106d94adda69dc8e13e18bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:15 +0000

    Add package thothctl/0.7.1/thothctl-0.7.1-py3-none-any.whl

---
+++ packages/thothctl/thothctl-0.7.1-py3-none-any.whl/thothctl/core/version_tools.py
@@ -0,0 +1,83 @@
+"""Set version tools."""
+version_tools = """[
+  {
+    "name": "terraform",
+    "version": "1.13.1"
+  },
+  {
+    "name": "tofu",
+    "version": "1.10.6"
+  },
+  {
+    "name": "terragrunt",
+    "version": "0.87.0"
+  },
+  {
+    "name": "thothctl",
+    "version": "0.4.6"
+  },
+  {
+    "name": "pre-commit",
+    "version": "3.7.1"
+  },
+  {
+    "name": "tflint",
+    "version": "0.51.1"
+  },
+  {
+    "name": "tfsec",
+    "version": "1.28.1"
+  },
+  {
+    "name": "checkov",
+    "version": "3.2.451"
+  },
+  {
+    "name": "terraform-docs",
+    "version": "0.19.0"
+  },
+  {
+    "name": "terraform-compliance",
+    "version": "1.12.3"
+  },
+  {
+    "name": "terramate",
+    "version": "0.14.5"
+  },
+  {
+    "name": "tfswitch",
+    "version": "0.13.1308"
+  },
+  {
+    "name": "commitizen",
+    "version": "3.27.0",
+    "command": "cz version"
+  },
+  {
+    "name": "pipx",
+    "version": "1.4.3"
+    },
+  {
+    "name": "uv",
+    "version": "0.8.15"
+  },
+  {
+    "name": "trivy",
+    "version": "0.59.1"
+  },
+  {
+    "name": "snyk",
+    "version": "1.1266.0",
+    "command": "snyk --version"
+  },
+  {
+    "name": "open-tofu",
+    "version": "1.10.6",
+    "command": "tofu version"
+  },
+  {
+    "name": "amazon-q",
+    "version": "1.15.0",
+    "command": "q --version"
+  }
+]"""
---
+++ packages/thothctl/thothctl-0.7.1-py3-none-any.whl/thothctl/version.py
@@ -0,0 +1,2 @@
+"""Version module."""
+__version__ = "0.7.1"
