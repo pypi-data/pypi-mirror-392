914fb6444f284c2365e7eeeb215f3df34b8ead90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 07:05:19 +0000

    Add package PyDNI/0.3.0/pydni-0.3.0.tar.gz

---
+++ packages/PyDNI/pydni-0.3.0.tar.gz/pydni-0.3.0/PyDNI/__init__.py
@@ -0,0 +1,14 @@
+from .dni import verificar_dni
+from .cif import verificar_cif
+from .nie import verificar_nie
+from .nif import verificar_nif
+from .utils import verificar_identificador
+
+__all__ = [
+    "verificar_dni",
+    "verificar_cif",
+    "verificar_nie",
+    "verificar_nif",
+    "verificar_identificador",
+]
+__version__ = "0.3.0"
