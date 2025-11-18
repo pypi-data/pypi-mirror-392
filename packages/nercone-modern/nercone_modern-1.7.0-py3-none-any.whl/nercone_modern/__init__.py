f0c64aaaf613029317d62c8c99ad5ac23e84d084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:04:54 +0000

    Add package nercone-modern/1.7.0/nercone_modern-1.7.0-py3-none-any.whl

---
+++ packages/nercone-modern/nercone_modern-1.7.0-py3-none-any.whl/nercone_modern/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# -- nercone-modern --------------------------------------------- #
+# __init__.py on nercone-modern                                   #
+# Made by DiamondGotCat, Licensed under MIT License               #
+# Copyright (c) 2025 DiamondGotCat                                #
+# ---------------------------------------------- DiamondGotCat -- #
+
+from .logging import ModernLogging
+from .progressbar import ModernProgressBar
