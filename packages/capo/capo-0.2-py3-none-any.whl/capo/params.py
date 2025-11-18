3bb5b1554fc044770b631b6e2887a3dcc2adea89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:05:12 +0000

    Add package capo/0.2/capo-0.2-py3-none-any.whl

---
+++ packages/capo/capo-0.2-py3-none-any.whl/capo/params.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""capo params."""
+CAPO_VERSION = "0.2"
+
+NOTES_SHARP = ["C", "C#", "D", "D#", "E", "F",
+               "F#", "G", "G#", "A", "A#", "B"]
+
+NOTES_FLAT = ["C", "Db", "D", "Eb", "E", "F",
+              "Gb", "G", "Ab", "A", "Bb", "B"]
+
+ENHARMONIC_EQUIVALENTS = {
+    "Db": "C#",
+    "Eb": "D#",
+    "Gb": "F#",
+    "Ab": "G#",
+    "Bb": "A#"
+}
+
+CHORDS_TYPE_ERROR_MESSAGE = "`chords` must be a list of strings."
+CAPO_POSITION_ERROR_MESSAGE = "capo position must be a non-negative integer."
+SEMITONES_TYPE_ERROR_MESSAGE = "`semitones` must be an integer."
+CHORD_FORMAT_ERROR_MESSAGE = "invalid chord format or unknown note: `{chord}`"
