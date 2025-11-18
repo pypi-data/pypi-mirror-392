c346fa34570f9fec0a4823b8217f4ac1f164d9ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 18:06:08 +0000

    Add package starco-dj-utils/1.0.6/starco_dj_utils-1.0.6.tar.gz

---
+++ packages/starco-dj-utils/starco_dj_utils-1.0.6.tar.gz/starco_dj_utils-1.0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='starco-dj-utils',
+    version='1.0.6',
+    packages=['dj_utils'],
+    include_package_data=True,
+    license='MIT',
+    description='A Django pluggable app for utilities and database management',
+    author='Mojtaba',
+    author_email='m.tahmasbi0111@yahoo.com',
+    install_requires=[
+        'Django>=4.0',
+        'python-telegram-bot==22.5',
+        'celery',
+        'redis',
+        'django-redis',
+        'django-celery-beat',
+        'colorama',
+        'python-dotenv',
+        'colorlog',
+        'psycopg2-binary',
+        'djangorestframework',
+        'gunicorn',
+        'uvicorn'
+
+    ],
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+    ],
+)
