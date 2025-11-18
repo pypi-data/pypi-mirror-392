bc54d36d65df04cea9b0aaca4bf4f8c530abf7c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 17:07:14 +0000

    Add package suou/0.9.0/suou-0.9.0-py3-none-any.whl

---
+++ packages/suou/suou-0.9.0-py3-none-any.whl/suou/flask_sqlalchemy.py
@@ -0,0 +1,83 @@
+"""
+Utilities for Flask-SQLAlchemy binding.
+
+---
+
+Copyright (c) 2025 Sakuragasaki46.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+See LICENSE for the specific language governing permissions and
+limitations under the License.
+
+This software is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+"""
+
+from functools import partial
+from typing import Any, Callable, Never
+
+from flask import abort, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Session
+from .functools import deprecated
+
+from .codecs import want_bytes
+from .sqlalchemy import AuthSrc, require_auth_base
+
+@deprecated('inherits from deprecated and unused class')
+class FlaskAuthSrc(AuthSrc):
+    '''
+    
+    '''
+    db: SQLAlchemy
+    def __init__(self, db: SQLAlchemy):
+        super().__init__()
+        self.db = db
+    def get_session(self) -> Session:
+        return self.db.session
+    def get_token(self):
+        if request.authorization:
+            return request.authorization.token
+    def get_signature(self) -> bytes:
+        sig = request.headers.get('authorization-signature', None)
+        return want_bytes(sig) if sig else None
+    def invalid_exc(self, msg: str = 'validation failed') -> Never:
+        abort(400, msg)
+    def required_exc(self):
+        abort(401, 'Login required')
+
+@deprecated('not intuitive to use')
+def require_auth(cls: type[DeclarativeBase], db: SQLAlchemy) -> Callable[Any, Callable]:
+    """
+    Make an auth_required() decorator for Flask views.
+
+    This looks for a token in the Authorization header, validates it, loads the
+    appropriate object, and injects it as the user= parameter.
+
+    NOTE: the actual decorator to be used on routes is **auth_required()**,
+    NOT require_auth() which is the **constructor** for it.
+
+    cls is a SQLAlchemy table.
+    db is a flask_sqlalchemy.SQLAlchemy() binding.
+
+    Usage:
+
+    auth_required = require_auth(User, db)
+
+    @route('/admin')
+    @auth_required(validators=[lambda x: x.is_administrator])
+    def super_secret_stuff(user):
+        pass
+
+    NOTE: require_auth() DOES NOT work with flask_restx.
+    """
+    def auth_required(**kwargs):
+        return require_auth_base(cls=cls, src=FlaskAuthSrc(db), **kwargs)
+
+    auth_required.__doc__ = require_auth_base.__doc__
+
+    return auth_required
+
+# Optional dependency: do not import into __init__.py
+__all__ = ()
---
+++ packages/suou/suou-0.9.0-py3-none-any.whl/suou/legal.py
@@ -0,0 +1,49 @@
+"""
+TOS / policy building blocks for the lazy, in English language.
+
+XXX DANGER! This is not replacement for legal advice. Contact your lawyer.
+
+---
+
+Copyright (c) 2025 Sakuragasaki46.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+See LICENSE for the specific language governing permissions and
+limitations under the License.
+
+This software is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+"""
+
+# TODO more snippets
+
+INDEMNIFY = """
+You agree to indemnify and hold harmless {0} from any and all claims, damages, liabilities, costs and expenses, including reasonable and unreasonable counsel and attorney’s fees, arising out of any breach of this agreement.
+"""
+
+NO_WARRANTY = """
+Except as represented in this agreement, the {0} is provided “AS IS”. Other than as provided in this agreement, {1} makes no other warranties, express or implied, and hereby disclaims all implied warranties, including any warranty of merchantability and warranty of fitness for a particular purpose.
+"""
+
+GOVERNING_LAW = """
+These terms of services are governed by, and shall be interpreted in accordance with, the laws of {0}. You consent to the sole jurisdiction of {1} for all disputes between You and , and You consent to the sole application of {2} law for all such disputes.
+"""
+
+ENGLISH_FIRST = """
+In case there is any inconsistency between these Terms and any translation into other languages, the English language version takes precedence.
+"""
+
+EXPECT_UPDATES = """
+{0} may periodically update these Terms of Service. Every time this happens, {0} will make its best efforts to notify You of such changes.
+
+Whenever {0} updates these Terms of Service, Your continued use of the {0} platform constitutes Your agreement to the updated Terms of Service.
+"""
+
+SEVERABILITY = """
+If one clause of these Terms of Service or any policy incorporated here by reference is determined by a court to be unenforceable, the remainder of the Terms and Content Policy shall remain in force.
+"""
+
+COMPLETENESS = """
+These Terms, together with the other policies incorporated into them by reference, contain all the terms and conditions agreed upon by You and {{ app_name }} regarding Your use of the {{ app_name }} service. No other agreement, oral or otherwise, will be deemed to exist or to bind either of the parties to this Agreement.
+"""
