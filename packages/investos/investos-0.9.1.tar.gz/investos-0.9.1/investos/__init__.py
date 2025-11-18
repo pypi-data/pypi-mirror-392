4c232c792c1b9e32a23e9e911ae208b14399642e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 20:06:42 +0000

    Add package investos/0.9.1/investos-0.9.1.tar.gz

---
+++ packages/investos/investos-0.9.1.tar.gz/investos-0.9.1/investos/__init__.py
@@ -0,0 +1,10 @@
+import investos.portfolio
+
+__version__ = "0.9.1"
+
+import os
+
+api_key = os.environ.get("FORECASTOS_API_KEY", "")
+api_endpoint = os.environ.get(
+    "FORECASTOS_API_ENDPOINT", "https://app.forecastos.com/api/v1"
+)
