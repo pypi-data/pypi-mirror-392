5ff2a1e5e2301370f347dd158b81d2c5863cb71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:52 +0000

    Add package cylindra/1.0.2/cylindra-1.0.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cylindra/cylindra-1.0.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cylindra/cli/config.py
@@ -0,0 +1,159 @@
+from __future__ import annotations
+
+import argparse
+import json
+from glob import glob
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from cylindra.cli._base import ParserBase
+from cylindra.core import collect_projects
+
+if TYPE_CHECKING:
+    from cylindra.project import CylindraProject
+
+
+class InitAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        from cylindra._config import init_config
+
+        init_config(force=True)
+        parser.exit()
+
+
+def list_all_configs():
+    import rich
+    from rich.panel import Panel
+
+    from cylindra._config import get_config
+
+    for path in get_config().list_config_paths():
+        with open(path) as f:
+            js = json.load(f)
+        assert isinstance(js, dict)
+
+        vals = list[str]()
+        for k, v in js.items():
+            vals.append(f" {k} = {v!r}")
+        title = f"[bold green]{path.stem}[/bold green]"
+        rich.print(Panel("\n".join(vals), title=title, border_style="green"))
+
+
+class ListAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        list_all_configs()
+        parser.exit()
+
+
+class ImportAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        path = Path(namespace.path)
+        if not path.exists():
+            raise FileNotFoundError(path)
+        from cylindra._config import get_config
+
+        save_path = get_config().spline_config_path(path.stem)
+        if save_path.exists():
+            raise FileExistsError(f"{save_path.as_posix()} already exists.")
+        with open(path) as f:
+            js = json.load(f)
+        assert isinstance(js, dict)
+        from cylindra.components import SplineConfig
+
+        cfg = SplineConfig().updated(**js)
+        with open(save_path, mode="w") as f:
+            json.dump(cfg.asdict(), f, indent=4, separators=(", ", ": "))
+        print(f"Config file imported: {save_path.as_posix()}")
+        parser.exit()
+
+
+class ParserConfig(ParserBase):
+    """
+    cylindra config [bold green]path[/bold green] [bold cyan]options[/bold cyan]
+
+    [u bold green]path[/u bold green]
+        Path to the project/image file.
+
+    [u bold cyan]options[/u bold cyan]
+        [bold]--remove, -r[/bold]
+            Remove the configuration specific keyword arguments. If a project path is given, also remove the default configuration file.
+
+        [bold]--init, -i[/bold]
+            Initialize the default configuration directory. This operation will not remove the user-defined files
+
+        [bold]--list, -l[/bold]
+            List up all the available configurations.
+
+        [bold]--import[/bold]
+            Import a configuration from a file.
+    """
+
+    def __init__(self):
+        super().__init__(prog="cylindra config", description="Configure cylindra.")
+        self.add_argument("path")
+        self.add_argument("--remove", "-r", action="store_true")
+        self.add_argument("--init", "-i", action=InitAction, nargs=0)
+        self.add_argument("--list", "-l", action=ListAction, nargs=0)
+        self.add_argument("--import", action=ImportAction, nargs=0)
+
+    def run_action(self, path: str, remove: bool = False, **kwargs):
+        _path = Path(path)
+        if path == "list" and not _path.exists():
+            return list_all_configs()
+        if remove:
+            if _path.suffix == ".py":
+                if "*" in str(_path):
+                    for py_path in glob(_path.as_posix()):
+                        self.remove_project_config_kwargs(Path(py_path))
+                else:
+                    self.remove_project_config_kwargs(_path)
+            else:
+                for prj in collect_projects(path):
+                    assert prj.project_path is not None
+                    with prj.open_project() as dir:
+                        py_path = prj._script_py_path(dir)
+                        self.remove_project_config_kwargs(Path(py_path))
+                        cfg_path = prj._default_spline_config_path(dir)
+                        if cfg_path.exists():
+                            cfg_path.unlink()
+                        prj.rewrite(dir)
+        else:
+            for prj in collect_projects(path):
+                self.show_project_default_config(prj)
+
+    def show_project_default_config(self, prj: CylindraProject):
+        import rich
+
+        with prj.open_project() as dir:
+            if project_path := prj.project_path:
+                _p = Path(project_path).as_posix()
+                rich.print(f"[bold green]{_p}[/bold green]:")
+            with open(prj._default_spline_config_path(dir)) as f:
+                js = json.load(f)
+            assert isinstance(js, dict)
+            for k, v in js.items():
+                print(f"    {k} = {v!r}")
+
+    def remove_project_config_kwargs(self, py_path: Path):
+        """Remove the config={...} kwargs from the given script.py file."""
+        import re
+
+        ptn = re.compile(r", config=\{.*\}")
+        _edit_prefix = (
+            "ui.register_path(",
+            "ui.protofilaments_to_spline(",
+            "ui.molecules_to_spline(",
+        )
+        original = py_path.read_text()
+        lines = original.splitlines()
+        for i, line in enumerate(lines):
+            line_stripped = line.strip()
+            if not line_stripped.startswith(_edit_prefix):
+                continue
+            lines[i] = ptn.sub("", line)
+
+        edited = "\n".join(lines)
+        if original != edited:
+            py_path.write_text(edited)
+
+            print(f"Processed: {py_path.as_posix()}")
---
+++ packages/cylindra/cylindra-1.0.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cylindra/const.py
@@ -0,0 +1,257 @@
+from __future__ import annotations
+
+from enum import Enum
+from types import SimpleNamespace
+
+import polars as pl
+
+nm = float  # type alias for nanometer
+pf = float  # type alias for protofilament numbering
+
+# Constant strings for layer names and metadata keys
+WORKING_LAYER_NAME = "Drawing Layer"
+SELECTION_LAYER_NAME = "Splines"
+ALN_SUFFIX = "ALN"
+PREVIEW_LAYER_NAME = "<Preview>"
+LANDSCAPE_PREFIX = "Landscape_"
+SPLINE_ID = "spline-id"
+SEAM_SEARCH_RESULT = "seam-search-result"
+ANNEALING_RESULT = "annealing-result"
+INTERPOLATION_CHOICES = (("0 (nearest)", 0), ("1 (linear)", 1), ("3 (cubic)", 3))
+
+
+class strEnum(Enum):
+    """Enum with string values"""
+
+    def __str__(self):
+        return self.value
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.value == other
+        return super().__eq__(other)
+
+
+class Ori(strEnum):
+    """Orientations"""
+
+    none = "none"
+    PlusToMinus = "PlusToMinus"
+    MinusToPlus = "MinusToPlus"
+
+    @classmethod
+    def invert(cls, ori: Ori, allow_none: bool = True) -> Ori:
+        """
+        Invert orientation.
+
+        Parameters
+        ----------
+        ori : Ori
+            Ori object to be inverted.
+        allow_none : bool, default True
+            If true, convert ``Ori.none`` into ``Ori.none``. Raise an error
+            otherwise.
+
+        Returns
+        -------
+        Ori
+            Inverted ``Ori`` object.
+        """
+        if ori == Ori.PlusToMinus:
+            out = Ori.MinusToPlus
+        elif ori == Ori.MinusToPlus:
+            out = Ori.PlusToMinus
+        else:
+            if allow_none:
+                out = Ori.none
+            else:
+                raise ValueError(f"{ori} cannot be inverted.")
+        return out
+
+
+class ExtrapolationMode(strEnum):
+    """Extrapolation mode for splines."""
+
+    default = "default"
+    linear = "linear"
+
+
+class PropertyNames(SimpleNamespace):
+    """Header names for result table of local properties."""
+
+    spline_id = "spline-id"
+    pos_id = "pos-id"
+    spl_dist = "spl_dist"
+    spl_pos = "spl_pos"
+    rise = "rise_angle"
+    rise_length = "rise_length"
+    pitch = "pitch"
+    spacing = "spacing"
+    twist = "twist"
+    skew = "skew_angle"
+    npf = "npf"
+    radius = "radius"
+    orientation = "orientation"  # global only
+    offset_radial = "offset_radial"  # global only
+    offset_axial = "offset_axial"  # global only
+    offset_angular = "offset_angular"  # global only
+    start = "start"
+
+
+class FileFilter(SimpleNamespace):
+    """File dialog filter strings"""
+
+    IMAGE = "Tomograms (*.mrc *.rec *.tif *.tiff *.map);;All files (*)"
+    PNG = "PNG (*.png *.jpg);;All files (*)"
+    JSON = "JSON(*.json *.txt);;All files (*)"
+    PROJECT = "Project file (project.json *.tar *.zip);;All files (*)"
+    CSV = "CSV (*.csv *.txt *.dat);;All files (*)"
+    DIRECTORY = "New directory (*)"
+    MOLE = "Molecules (*.csv *.txt *.dat *.parquet);;All files (*)"
+    PY = "Python (*.py);;All files (*)"
+    MOD = "Model files (*.mod);;All files (*)"
+    STAR = "STAR files (*.star);;All files (*)"
+    STAR_JOB = "RELION job files (job.star);;All files (*)"
+    PDB = "PDB files (*.pdb);;All files (*)"
+    EDF = "IMOD project file (*.edf);;All files (*)"
+
+
+class Mode(SimpleNamespace):
+    """Padding mode used in scipy.ndimage."""
+
+    grid_wrap = "grid-wrap"
+    wrap = "wrap"
+    reflect = "reflect"
+    mirror = "mirror"
+    constant = "constant"
+    nearest = "nearest"
+
+
+class MoleculesHeader(SimpleNamespace):
+    """Feature header names for Molecules."""
+
+    nth = "nth"  # n-th molecule of a protofilament
+    pf = "pf-id"  # protofilament ID
+    isotype = "isotype-id"
+    template = "template-id"
+    score = "score"  # alignment score
+    spacing = PropertyNames.spacing  # interval between two molecules
+    twist = PropertyNames.twist  # twist between two molecules
+    skew = PropertyNames.skew  # skew tilt angle between two molecules
+    rise = PropertyNames.rise  # rise angle between two molecules
+    lateral_interval = "lateral-interval"  # lateral interval between two molecules
+    radius = "radius"  # distance between the molecule and the spline
+    curve_index = "curve-index"  # curve index (1 is the inside of curve)
+    lateral_angle = "lateral-angle"  # lateral angle between molecules
+    elev_angle = "elevation-angle"  # elevation angle between molecules
+    position = "position-nm"  # position of the molecule along the spline
+    id = "molecules-id"
+    image = "image-id"
+    z = "z"
+    y = "y"
+    x = "x"
+    zvec = "zvec"
+    yvec = "yvec"
+    xvec = "xvec"
+
+
+class EulerAxes(strEnum):
+    """Sequence of Euler angles."""
+
+    xyz = "xyz"
+    yzx = "yzx"
+    zxy = "zxy"
+    xzy = "xzy"
+    yxz = "yxz"
+    zyx = "zyx"
+    xyx = "xyx"
+    xzx = "xzx"
+    yxy = "yxy"
+    yzy = "yzy"
+    zxz = "zxz"
+    zyz = "zyz"
+    XYZ = "XYZ"
+    YZX = "YZX"
+    ZXY = "ZXY"
+    XZY = "XZY"
+    YXZ = "YXZ"
+    ZYX = "ZYX"
+    XYX = "XYX"
+    XZX = "XZX"
+    YXY = "YXY"
+    YZY = "YZY"
+    ZXZ = "ZXZ"
+    ZYZ = "ZYZ"
+
+
+def get_versions() -> dict[str, str]:
+    """Return version info of relevant libraries."""
+    import dask
+    import impy as ip
+    import magicclass as mcls
+    import magicgui
+    import napari
+    import numpy as np
+
+    from cylindra import __version__
+
+    return {
+        "cylindra": __version__,
+        "numpy": np.__version__,
+        "impy": ip.__version__,
+        "magicgui": magicgui.__version__,
+        "magicclass": mcls.__version__,
+        "napari": napari.__version__,
+        "dask": dask.__version__,
+    }
+
+
+class SplineColor(SimpleNamespace):
+    """Color of spline used in the 2D/3D canvas."""
+
+    DEFAULT = [0.0, 0.0, 1.0, 1.0]
+    SELECTED = [0.8, 0.0, 0.5, 1.0]
+
+
+class ImageFilter(strEnum):
+    """Available filters for the reference image."""
+
+    Lowpass = "Lowpass"
+    Gaussian = "Gaussian"
+    DoG = "DoG"
+    LoG = "LoG"
+
+
+_POLARS_DTYPES = {
+    PropertyNames.npf: pl.UInt8,
+    PropertyNames.start: pl.Int8,
+    PropertyNames.orientation: pl.Utf8,
+    PropertyNames.spline_id: pl.UInt16,
+    MoleculesHeader.image: pl.UInt16,
+}
+
+
+POLARS_INTEGER_DTYPES = (
+    pl.Int8,
+    pl.Int16,
+    pl.Int32,
+    pl.Int64,
+    pl.UInt8,
+    pl.UInt16,
+    pl.UInt32,
+    pl.UInt64,
+)
+
+POLARS_FLOAT_DTYPES = (pl.Float32, pl.Float64)
+POLARS_NUMERIC_DTYPES = (pl.Decimal,) + POLARS_INTEGER_DTYPES + POLARS_FLOAT_DTYPES
+
+
+def cast_dataframe(df: pl.DataFrame) -> pl.DataFrame:
+    """Cast the dataframe to the appropriate dtype based on the columns."""
+    out = list[pl.Expr]()
+    for cname in df.columns:
+        if dtype := _POLARS_DTYPES.get(cname, None):
+            out.append(pl.col(cname).cast(dtype))
+        elif df[cname].dtype in POLARS_FLOAT_DTYPES:
+            out.append(pl.col(cname).cast(pl.Float32))
+    return df.with_columns(out)
