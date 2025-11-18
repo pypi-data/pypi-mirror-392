2d01ac11e2d6be232a1a3c75bca8c75c14202273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:05:11 +0000

    Add package langchat/0.0.3/langchat-0.0.3-py3-none-any.whl

---
+++ packages/langchat/langchat-0.0.3-py3-none-any.whl/langchat/__init__.py
@@ -0,0 +1,13 @@
+"""
+LangChat - A conversational AI library with vector search capabilities.
+"""
+
+__version__ = "0.0.3"
+
+from langchat.core.engine import LangChatEngine
+from langchat.core.session import UserSession
+from langchat.config import LangChatConfig
+from langchat.main import LangChat
+from langchat.utils.document_indexer import DocumentIndexer
+
+__all__ = ["LangChat", "LangChatEngine", "UserSession", "LangChatConfig", "DocumentIndexer"]
