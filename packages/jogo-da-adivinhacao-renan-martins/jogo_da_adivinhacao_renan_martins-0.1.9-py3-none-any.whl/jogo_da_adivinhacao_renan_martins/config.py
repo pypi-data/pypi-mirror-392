7cca904b87ab1c8625e97abe6b9bedecaad41f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 20:06:30 +0000

    Add package jogo-da-adivinhacao-renan-martins/0.1.9/jogo_da_adivinhacao_renan_martins-0.1.9-py3-none-any.whl

---
+++ packages/jogo-da-adivinhacao-renan-martins/jogo_da_adivinhacao_renan_martins-0.1.9-py3-none-any.whl/jogo_da_adivinhacao_renan_martins/config.py
@@ -0,0 +1,5 @@
+"""Configurações e constantes do jogo"""
+
+VERSION = "0.1.9"
+LARGURA_TELA = 63
+TEMPO_ESPERA_FINAL = 3
---
+++ packages/jogo-da-adivinhacao-renan-martins/jogo_da_adivinhacao_renan_martins-0.1.9-py3-none-any.whl/jogo_da_adivinhacao_renan_martins/main.py
@@ -0,0 +1,41 @@
+import time
+from src.jogo_da_adivinhacao_renan_martins.cartelas import geraCartelas, exibeCartela
+from src.jogo_da_adivinhacao_renan_martins.validacao import (
+    validaRespostaUsuario,
+    verificaContinuacao,
+)
+from src.jogo_da_adivinhacao_renan_martins.exibicao import (
+    exibeMensagemFinal,
+    exibeMensagemInicial,
+    exibeValorEscolhido,
+)
+
+
+def main():
+
+    while True:
+        exibeMensagemInicial()
+
+        cartelas = geraCartelas()
+
+        valorEscolhido = 0
+
+        for pos, cartela in enumerate(cartelas):
+
+            exibeCartela(cartela, pos)
+
+            if validaRespostaUsuario():
+                valorEscolhido += cartela[0]
+
+        exibeValorEscolhido(valorEscolhido)
+
+        if not verificaContinuacao():
+            break
+
+    time.sleep(3)
+    print()
+    exibeMensagemFinal()
+
+
+if __name__ == "__main__":
+    main()
