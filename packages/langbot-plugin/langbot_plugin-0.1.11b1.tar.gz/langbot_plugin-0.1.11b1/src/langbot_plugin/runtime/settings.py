ff163ed08b6f74c228cf3e3936b2cbdd62f9cfbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 17:06:40 +0000

    Add package langbot-plugin/0.1.11b1/langbot_plugin-0.1.11b1.tar.gz

---
+++ packages/langbot-plugin/langbot_plugin-0.1.11b1.tar.gz/langbot_plugin-0.1.11b1/src/langbot_plugin/runtime/settings.py
@@ -0,0 +1,21 @@
+from typing import Literal
+
+from pydantic import Field, model_validator
+from pydantic_settings import BaseSettings
+
+
+class RuntimeSettings(BaseSettings):
+    """Runtime settings using Pydantic Settings"""
+
+    cloud_service_url: str = Field(default="https://space.langbot.app")
+
+    model_config = {
+        "env_file": "data/.env",
+        "env_file_encoding": "utf-8",
+        "case_sensitive": False,
+        "extra": "ignore",
+    }
+
+
+# Global settings instance
+settings: RuntimeSettings = RuntimeSettings()
