eaa8749bcab6a5fd52e34faae0a17c788c0308a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 15:05:20 +0000

    Add package nlp-sentiment-pipeline/0.1.2/nlp_sentiment_pipeline-0.1.2.tar.gz

---
+++ packages/nlp-sentiment-pipeline/nlp_sentiment_pipeline-0.1.2.tar.gz/nlp_sentiment_pipeline-0.1.2/setup.py
@@ -0,0 +1,7 @@
+"""Setup script for the NLP Tweets Sentiment Analysis Pipeline package."""
+
+from setuptools import setup
+
+# All configuration is now in pyproject.toml
+# This file is kept for backwards compatibility
+setup()
