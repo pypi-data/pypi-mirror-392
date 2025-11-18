bbe853ae137438476e7d87a490ab95f9f59c38fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 13:14:34 +0000

    Add package tree-sitter-vie/0.2.3/tree_sitter_vie-0.2.3-cp310-abi3-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/tree-sitter-vie/tree_sitter_vie-0.2.3-cp310-abi3-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/tree_sitter_vie/queries/highlights.scm
@@ -0,0 +1,53 @@
+[
+  "{{"
+  "}}"
+  "{%"
+  "%}"
+  "("
+  ")"
+] @punctuation.bracket
+
+"," @punctuation.delimiter
+
+(comment) @comment
+
+[
+  "if"
+  "else"
+  "switch"
+  "case"
+  "end"
+  "is"
+  "not"
+  "is not"
+  "or"
+  "and"
+] @keyword
+
+[
+  "||"
+  "&&"
+  "<="
+  ">="
+  ">"
+  "<"
+  "!="
+  "=="
+  "!"
+  "~"
+  "|"
+] @operator
+
+(identifier) @variable
+
+(pipe_expression
+  function: (identifier) @function.call)
+
+(call_expression
+  function: (identifier) @function.call)
+
+(boolean_literal) @boolean
+
+(string_literal) @string
+
+(escape_sequence) @string.escape
