374c3a0c582676ca39863d3c8aeaf8ab35b32079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 09:05:03 +0000

    Add package unxt/1.7.3/unxt-1.7.3-py3-none-any.whl

---
+++ packages/unxt/unxt-1.7.3-py3-none-any.whl/unxt/_src/dimensions/__init__.py
@@ -0,0 +1,13 @@
+"""Dimensions module.
+
+This is the private implementation of the dimensions module.
+
+"""
+
+__all__ = ("AbstractDimension", "dimension", "dimension_of")
+
+from .api import AbstractDimension, dimension, dimension_of
+
+# Register the dispatches
+# isort: split
+from . import core  # noqa: F401
