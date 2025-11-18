1e43c1cffe8cb75b98ef829ab79c27c026af84df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 12:05:27 +0000

    Add package vs-server-colab/1.0.9/vs_server_colab-1.0.9.tar.gz

---
+++ packages/vs-server-colab/vs_server_colab-1.0.9.tar.gz/vs_server_colab-1.0.9/vs_server_colab/src/vs_server.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+VERSION=4.105.1
+DOWNLOAD_DIR=/usr/downloads
+INSTALL_DIR=/usr/lib/code-server
+
+# Create directories
+mkdir -p "$DOWNLOAD_DIR"
+mkdir -p "$INSTALL_DIR"
+
+# Download tar.gz
+curl -fLo "$DOWNLOAD_DIR/code-server.tar.gz" \
+  "https://github.com/coder/code-server/releases/download/v$VERSION/code-server-$VERSION-linux-amd64.tar.gz"
+
+# Extract inside downloads
+tar -xzf "$DOWNLOAD_DIR/code-server.tar.gz" -C "$DOWNLOAD_DIR"
+
+# Move extracted folder to install location
+mv "$DOWNLOAD_DIR/code-server-$VERSION-linux-amd64"/* "$INSTALL_DIR"
+
+# Cleanup
+rm "$DOWNLOAD_DIR/code-server.tar.gz"
+rm -rf "$DOWNLOAD_DIR/code-server-$VERSION-linux-amd64"
+
+# Remove old symlink if exists
+rm -f /usr/local/bin/code-server
+
+# Create new symlink
+ln -s "$INSTALL_DIR/bin/code-server" /usr/local/bin/code-server
+
+mkdir -p ~/.local/share/code-server/User
+cat << 'EOF' > ~/.local/share/code-server/User/settings.json
+{
+  "workbench.colorTheme": "Default Dark Modern"
+}
+EOF
+
+echo "Code-Server installed successfully!"
