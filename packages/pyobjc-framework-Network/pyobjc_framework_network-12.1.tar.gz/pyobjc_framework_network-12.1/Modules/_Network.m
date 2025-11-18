fac8055e037d0dfc72ef6e3e5c997961a812d498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 03:23:49 +0000

    Add package pyobjc-framework-Network/12.1/pyobjc_framework_network-12.1.tar.gz

---
+++ packages/pyobjc-framework-Network/pyobjc_framework_network-12.1.tar.gz/pyobjc_framework_network-12.1/Modules/_Network.m
@@ -0,0 +1,123 @@
+#define PY_SSIZE_T_CLEAN
+#include "Python.h"
+#include "pyobjc-api.h"
+
+#include <Network/Network.h>
+
+static PyMethodDef mod_methods[] = {
+    {0, 0, 0, 0} /* sentinel */
+};
+
+static int
+add_constant(PyObject* m, const char* name, char* typestr, const void* value)
+{
+    PyObject* v;
+    int       r;
+
+    v = PyObjC_ObjCToPython(typestr, (void*)value);
+    if (v == NULL) {
+        return -1;
+    }
+
+    r = PyModule_AddObject(m, name, v);
+
+    return r;
+}
+
+static int
+mod_exec_module(PyObject* m)
+{
+    if (PyObjC_ImportAPI(m) == -1)
+        return -1;
+
+    if (@available(macos 10.14, *)) {
+        nw_connection_send_completion_t t = NW_CONNECTION_SEND_IDEMPOTENT_CONTENT;
+        if (add_constant(m, "NW_CONNECTION_SEND_IDEMPOTENT_CONTENT",
+                         @encode(nw_connection_send_completion_t), &t)
+            != 0)
+            goto error;
+        nw_content_context_t t2 = NW_CONNECTION_DEFAULT_MESSAGE_CONTEXT;
+        if (add_constant(m, "NW_CONNECTION_DEFAULT_MESSAGE_CONTEXT",
+                         @encode(nw_content_context_t), &t2)
+            != 0)
+            goto error;
+        t2 = NW_CONNECTION_FINAL_MESSAGE_CONTEXT;
+        if (add_constant(m, "NW_CONNECTION_FINAL_MESSAGE_CONTEXT",
+                         @encode(nw_content_context_t), &t2)
+            != 0)
+            goto error;
+        t2 = NW_CONNECTION_DEFAULT_STREAM_CONTEXT;
+        if (add_constant(m, "NW_CONNECTION_DEFAULT_STREAM_CONTEXT",
+                         @encode(nw_content_context_t), &t2)
+            != 0)
+            goto error;
+        nw_parameters_configure_protocol_block_t p = NW_PARAMETERS_DEFAULT_CONFIGURATION;
+        if (add_constant(m, "NW_PARAMETERS_DEFAULT_CONFIGURATION",
+                         @encode(nw_parameters_configure_protocol_block_t), &p)
+            != 0)
+            goto error;
+        p = NW_PARAMETERS_DISABLE_PROTOCOL;
+        if (add_constant(m, "NW_PARAMETERS_DISABLE_PROTOCOL",
+                         @encode(nw_parameters_configure_protocol_block_t), &p)
+            != 0)
+            goto error;
+    }
+
+#if PyObjC_BUILD_RELEASE >= 1016
+    if (@available(macos 10.16, *)) {
+        nw_privacy_context_t c = NW_DEFAULT_PRIVACY_CONTEXT;
+        if (add_constant(m, "NW_DEFAULT_PRIVACY_CONTEXT", @encode(nw_privacy_context_t),
+                         &c)
+            != 0)
+            goto error;
+    }
+#endif
+
+    return 0;
+
+error:
+    return -1;
+}
+
+static struct PyModuleDef_Slot mod_slots[] = {
+    {.slot = Py_mod_exec, .value = (void*)mod_exec_module},
+#if PY_VERSION_HEX >= 0x030c0000
+    {
+        /* This extension does not use the CPython API other than initializing
+         * the module, hence is safe with subinterpreters and per-interpreter
+         * GILs
+         */
+        .slot  = Py_mod_multiple_interpreters,
+        .value = Py_MOD_MULTIPLE_INTERPRETERS_NOT_SUPPORTED,
+    },
+#endif
+#if PY_VERSION_HEX >= 0x030d0000
+    {
+        /* The code in this extension should be safe to use without the GIL */
+        .slot  = Py_mod_gil,
+        .value = Py_MOD_GIL_NOT_USED,
+    },
+#endif
+    {/* Sentinel */
+     .slot  = 0,
+     .value = 0}};
+
+static struct PyModuleDef mod_module = {
+    .m_base     = PyModuleDef_HEAD_INIT,
+    .m_name     = "_Network",
+    .m_doc      = NULL,
+    .m_size     = 0,
+    .m_methods  = mod_methods,
+    .m_slots    = mod_slots,
+    .m_traverse = NULL,
+    .m_clear    = NULL,
+    .m_free     = NULL,
+};
+
+PyObject* PyInit__Network(void);
+
+PyObject* __attribute__((__visibility__("default")))
+PyInit__Network(void)
+{
+    return PyModuleDef_Init(&mod_module);
+}
---
+++ packages/pyobjc-framework-Network/pyobjc_framework_network-12.1.tar.gz/pyobjc_framework_network-12.1/PyObjCTest/test_path.py
@@ -0,0 +1,65 @@
+from PyObjCTools.TestSupport import TestCase, min_os_level
+
+import Network
+
+nw_path_enumerate_interfaces_block_t = b"B@"
+nw_path_enumerate_gateways_block_t = b"B@"
+
+
+class TestPath(TestCase):
+    def test_constants(self):
+        self.assertIsEnumType(Network.nw_path_status_t)
+        self.assertEqual(Network.nw_path_status_invalid, 0)
+        self.assertEqual(Network.nw_path_status_satisfied, 1)
+        self.assertEqual(Network.nw_path_status_unsatisfied, 2)
+        self.assertEqual(Network.nw_path_status_satisfiable, 3)
+
+        self.assertIsEnumType(Network.nw_path_unsatisfied_reason_t)
+        self.assertEqual(Network.nw_path_unsatisfied_reason_not_available, 0)
+        self.assertEqual(Network.nw_path_unsatisfied_reason_cellular_denied, 1)
+        self.assertEqual(Network.nw_path_unsatisfied_reason_wifi_denied, 2)
+        self.assertEqual(Network.nw_path_unsatisfied_reason_local_network_denied, 3)
+        self.assertEqual(Network.nw_path_unsatisfied_reason_vpn_inactive, 4)
+
+        self.assertIsEnumType(Network.nw_link_quality_t)
+        self.assertEqual(Network.nw_link_quality_unknown, 0)
+        self.assertEqual(Network.nw_link_quality_minimal, 10)
+        self.assertEqual(Network.nw_link_quality_moderate, 20)
+        self.assertEqual(Network.nw_link_quality_good, 30)
+
+    def test_functions(self):
+        Network.nw_path_get_status
+
+        self.assertArgIsBlock(
+            Network.nw_path_enumerate_interfaces,
+            1,
+            nw_path_enumerate_interfaces_block_t,
+        )
+
+        Network.nw_path_is_equal
+        Network.nw_path_is_expensive
+        Network.nw_path_has_ipv4
+        Network.nw_path_has_ipv6
+        Network.nw_path_has_dns
+        Network.nw_path_uses_interface_type
+
+        self.assertResultIsRetained(Network.nw_path_copy_effective_local_endpoint)
+
+        self.assertResultIsRetained(Network.nw_path_copy_effective_remote_endpoint)
+
+    @min_os_level("10.15")
+    def test_functions10_15(self):
+        Network.nw_path_is_constrained
+
+        self.assertArgIsBlock(
+            Network.nw_path_enumerate_gateways, 1, nw_path_enumerate_gateways_block_t
+        )
+
+    @min_os_level("11.0")
+    def test_functions11_0(self):
+        Network.nw_path_get_unsatisfied_reason
+
+    @min_os_level("26.0")
+    def test_functions26_0(self):
+        Network.nw_path_is_ultra_constrained
+        Network.nw_path_get_link_quality
