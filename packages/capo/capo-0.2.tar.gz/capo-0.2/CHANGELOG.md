d9da4594b321c94771fd679b711e7196e49e95eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 11:05:12 +0000

    Add package capo/0.2/capo-0.2.tar.gz

---
+++ packages/capo/capo-0.2.tar.gz/capo-0.2/CHANGELOG.md
@@ -0,0 +1,27 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+## [0.2] - 2025-11-15
+### Added
+- `transpose` function
+- Logo
+### Changed
+- `README.md` updated
+- `capo_map` function modified
+- Test system modified
+## [0.1] - 2025-11-05
+### Added
+- `capo_map` function
+- Flat notes
+- Sharp notes
+
+[Unreleased]: https://github.com/sepandhaghighi/capo/compare/v0.2...dev
+[0.2]: https://github.com/sepandhaghighi/capo/compare/v0.1...v0.2
+[0.1]: https://github.com/sepandhaghighi/capo/compare/3f293b1...v0.1
+
+
+
---
+++ packages/capo/capo-0.2.tar.gz/capo-0.2/capo/functions.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+"""capo functions."""
+from typing import List, Any
+from .errors import CapoValidationError
+from .params import NOTES_SHARP, NOTES_FLAT
+from .params import ENHARMONIC_EQUIVALENTS
+from .params import CHORDS_TYPE_ERROR_MESSAGE, CAPO_POSITION_ERROR_MESSAGE
+from .params import CHORD_FORMAT_ERROR_MESSAGE, SEMITONES_TYPE_ERROR_MESSAGE
+
+
+def _is_int(number: Any) -> bool:
+    """
+    Check that input number is integer or not.
+
+    :param number: input number
+    """
+    try:
+        if int(number) == number:
+            return True
+        return False
+    except Exception:
+        return False
+
+
+def _validate_chords(chords: Any) -> bool:
+    """
+    Validate chords.
+
+    :param chords: chords list
+    """
+    if not isinstance(chords, list):
+        raise CapoValidationError(CHORDS_TYPE_ERROR_MESSAGE)
+    if not all(isinstance(chord, str) for chord in chords):
+        raise CapoValidationError(CHORDS_TYPE_ERROR_MESSAGE)
+    return True
+
+
+def _validate_capo_position(target_capo: Any, current_capo: Any) -> bool:
+    """
+    Validate capo position.
+
+    :param target_capo: target capo position
+    :param current_capo: current capo position
+    """
+    if not _is_int(current_capo) or not _is_int(target_capo):
+        raise CapoValidationError(CAPO_POSITION_ERROR_MESSAGE)
+    if current_capo < 0 or target_capo < 0:
+        raise CapoValidationError(CAPO_POSITION_ERROR_MESSAGE)
+    return True
+
+
+def _validate_semitones(semitones: Any) -> bool:
+    """
+    Validate semitones.
+
+    :param semitones: semitones
+    """
+    if not _is_int(semitones):
+        raise CapoValidationError(SEMITONES_TYPE_ERROR_MESSAGE)
+    return True
+
+
+def _normalize_note(note: str) -> str:
+    """
+    Normalize a note to its sharp representation.
+
+    :param note: input note
+    """
+    return ENHARMONIC_EQUIVALENTS.get(note, note)
+
+
+def _extract_parts(chord: str) -> tuple:
+    """
+    Split a chord into root, suffix, and optional bass note (for slash chords).
+
+    :param chord: input chord
+    """
+    if "/" in chord:
+        main, bass = chord.split("/", 1)
+    else:
+        main, bass = chord, None
+
+    if len(main) > 1 and main[1] in ['#', 'b']:
+        root = main[:2]
+        suffix = main[2:]
+    else:
+        root = main[:1]
+        suffix = main[1:]
+
+    root = _normalize_note(root)
+    if bass:
+        if len(bass) > 1 and bass[1] in ['#', 'b']:
+            bass_root = _normalize_note(bass[:2])
+        else:
+            bass_root = _normalize_note(bass[:1])
+    else:
+        bass_root = None
+
+    return root, suffix, bass_root
+
+
+def _transpose_note(note: str, semitones: int, flat_mode: bool = False) -> str:
+    """
+    Transpose a note by given semitones.
+
+    :param note: input note
+    :param semitones: semitones
+    :param flat_mode: flat mode flag
+    """
+    note_sharp = _normalize_note(note)
+    notes_system = NOTES_FLAT if flat_mode else NOTES_SHARP
+    old_index = NOTES_SHARP.index(note_sharp)
+    new_index = (old_index + semitones) % 12
+    return notes_system[new_index]
+
+
+def _transpose_chord(chord: str, semitones: int, flat_mode: bool = False) -> str:
+    """
+    Transpose a full chord string by semitones.
+
+    :param chord: input chord
+    :param semitones: semitones
+    :param flat_mode: flat mode flag
+    """
+    root, suffix, bass = _extract_parts(chord)
+    new_root = _transpose_note(root, semitones, flat_mode)
+    new_bass = _transpose_note(bass, semitones, flat_mode) if bass else None
+
+    return "{new_root}{suffix}/{new_bass}".format(
+        new_root=new_root,
+        suffix=suffix,
+        new_bass=new_bass) if new_bass else "{new_root}{suffix}".format(
+        new_root=new_root,
+        suffix=suffix)
+
+
+def transpose(chords: List[str], semitones: int, flat_mode: bool = False) -> List[str]:
+    """
+    Transpose chords by semitones.
+
+    :param chords: chords list
+    :param semitones: semitones
+    :param flat_mode: flat mode flag
+    """
+    _validate_chords(chords)
+    _validate_semitones(semitones)
+    result = []
+    for chord in chords:
+        try:
+            result.append(_transpose_chord(chord, semitones, flat_mode))
+        except Exception:
+            raise CapoValidationError(CHORD_FORMAT_ERROR_MESSAGE.format(chord=chord))
+    return result
+
+
+def capo_map(chords: List[str], target_capo: int, current_capo: int = 0, flat_mode: bool = False) -> List[str]:
+    """
+    Map a list of chords from current capo to target capo position.
+
+    :param chords: chords list
+    :param target_capo: target capo position
+    :param current_capo: current capo position
+    :param flat_mode: flat mode flag
+    """
+    _validate_chords(chords)
+    _validate_capo_position(target_capo, current_capo)
+
+    semitones = target_capo - current_capo
+    return transpose(chords=chords, semitones=-semitones, flat_mode=flat_mode)
