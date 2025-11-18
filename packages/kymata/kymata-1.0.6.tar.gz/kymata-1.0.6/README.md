fae381d3b5ee2f052f64ab87d2c3af69f1b23df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 05:04:13 +0000

    Add package kymata/1.0.6/kymata-1.0.6.tar.gz

---
+++ packages/kymata/kymata-1.0.6.tar.gz/kymata-1.0.6/README.md
@@ -0,0 +1,139 @@
+[![Unit Tests Passed](https://github.com/kymata-atlas/kymata-core/actions/workflows/perform-unit-tests.yml/badge.svg)](https://github.com/kymata-atlas/kymata-core/actions/workflows/perform-unit-tests.yml)
+[![Linting Checks Passed](https://github.com/kymata-atlas/kymata-core/actions/workflows/lint-and-check-formatting.yml/badge.svg)](https://github.com/kymata-atlas/kymata-core/actions/workflows/lint-and-check-formatting.yml)
+[![CodeFactor](https://www.codefactor.io/repository/github/kymata-atlas/kymata-core/badge)](https://www.codefactor.io/repository/github/kymata-atlas/kymata-core)
+[![codecov](https://codecov.io/gh/kymata-atlas/kymata-core/branch/main/graph/badge.svg)](https://codecov.io/gh/kymata-atlas/kymata-core)
+<br />
+<br />
+<div>
+      <a href="https://github.com/kymata-atlas/kymata-core">
+        <img src="https://kymata.org/assets/img/toolbox_logo.png" alt="Logo" height="112" align="left">
+      </a>
+    <div>
+        <div>
+            <h3>Kymata Core</h3>
+            <p>Core codebase for the Kymata Atlas
+            <br>
+            <a href="https://kymata-atlas.github.io/kymata-core"><strong>Explore the docs »</strong></a>
+            </p>
+        </div>
+    </div>
+
+</div>
+<br>
+<p align="center">
+        <a href="#Overview">Overview</a>
+        ·
+        <a href="#Getting Started">Setup</a>
+        ·
+        <a href="#Citing the Codebase">Citing</a>
+        ·
+        <a href="#Licence">Licence</a>
+</p>
+
+## Overview
+
+_Kymata Core_ is the central codebase underlying the [Kymata Atlas](https://kymata.org).[^1] It is maintained by the
+[Kymata Research Group](https://kymata.org).
+
+The central pipeline includes:
+* Standard preprocessing and source localisation steps for neural sensor data (MEG, EEG, ECoG);
+* Gridsearch approaches for transform mapping;
+* Information Processing Pathway Map[^2] generation (both offline generation and evaluation);
+* Plotting functionality
+
+The codebase is released under an MIT license to ensure the transparency of the results in the Kymata Atlas. While
+comments and issues are welcomed, we are unable to prioritise installation or usage support (please see our code of
+conduct).
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## Getting Started
+
+This provides an overview of how to set Kymata Core locally.
+
+### Prerequisites
+
+* **Python**
+
+   Confirm you have the correct version of Python installed. Type
+   ```sh
+   $ pyenv versions
+   ```
+   This should confirm that python 3.11 or above is installed. If it isn't already there,
+   install it using `pyenv install`. You should be able to confirm
+   you are using the correct version using
+   ```sh
+   $ python -V
+   ```
+* **Poetry**
+
+  This package uses [Poetry](https://python-poetry.org/) to manage packages. See [python-poetry.org](https://python-poetry.org/docs/#installing-with-the-official-installer) for installation instructions.
+
+### Installation
+
+1. Clone this repository:
+   ```sh
+   $ git clone https://github.com/kymata-atlas/kymata-core.git
+   ```
+3. To install the python packages you will need to use Poetry. Assuming you have installed [Poetry](https://python-poetry.org/docs/#installing-with-the-official-installer), 
+   type:
+   ```sh
+   $ poetry install
+   ```
+   to load the pakages needed.
+
+4. At this point, you should be able to either run the xx from the terminal
+   ```sh
+   $ poetry run invokers/run_gridsearch.py
+   ```
+   or activate in this environment in an IDE such as PyCharm.
+
+### Running tests, linting, and generating documentation
+
+This will be done automatically via Github actions.
+
+To run the tests manually, run:
+```
+$ poetry run pytest
+```
+To run linting manually, run:
+```
+$ poetry run ruff check
+```
+To serve the documentation locally, run:
+```
+$ poetry run mkdocs serve check
+```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+Please refer to the [documentation](https://kymata-atlas.github.io/kymata-core), or see the `demos/` folder for example code, including test
+data.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## Citing the codebase
+
+Please use the following reference when citing the codebase or the `kymata` package: 
+
+> TBC
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- LICENSE -->
+## Licence
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+<!-- REFERENCES -->
+## References
+
+[^1]: Thwaites, Wieser, Soltan, Wingfield, Parish, Yang, Lakra, Woolgar, Zhang (in prep) The Kymata Atlas.
+[^2]: Thwaites, Zhang and Woolgar (2024) Information Processing Pathway Maps: A New Frontier in Cortical Processing (2025) _NeuroImage_ 217:121345. doi:[10.1016/j.neuroimage.2025.121345](https://doi.org/10.1016/j.neuroimage.2025.121345).
+
---
+++ packages/kymata/kymata-1.0.6.tar.gz/kymata-1.0.6/kymata/datasets/sample.py
@@ -0,0 +1,154 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Optional
+from urllib import request
+
+from kymata.datasets.data_root import data_root_path
+from kymata.entities.expression import HexelExpressionSet, SensorExpressionSet
+from kymata.io.file import PathType
+from kymata.io.nkg import load_expression_set
+
+_SAMPLE_DATA_DIR_NAME = "tutorial_nkg_data"
+
+
+class SampleDataset(ABC):
+    """
+    Info required to retrieve a dataset stored locally.
+
+    Names in `.filenames` refer to local files, which (if `remote_root` is specified) are paired with identically
+    named remote files.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        filenames: list[str],
+        data_root: Optional[PathType],
+        remote_root: Optional[str],
+        download: bool,
+    ):
+        self.name: str = name
+        self.filenames: list[str] = filenames
+        self.data_root: Path = Path(data_root_path(data_root), _SAMPLE_DATA_DIR_NAME)
+        self.remote_root: str = remote_root
+
+        # Create the default location, if it's not being overridden
+        if data_root is None:
+            self.data_root.mkdir(exist_ok=True)
+
+        if download:
+            self.download()
+
+    @property
+    def path(self) -> Path:
+        return Path(self.data_root, self.name)
+
+    def download(self):
+        print(f"Downloading dataset: {self.name}")
+        if self.remote_root is None:
+            raise ValueError("No remote root provided")
+        self.path.mkdir(exist_ok=True)
+        for filename in self.filenames:
+            remote = self.remote_root + "/" + filename
+            local = Path(self.path, filename)
+            if local.exists():
+                print(f"Local file already exists: {local}")
+            else:
+                print(f"Downloading {remote} to {local}")
+                request.urlretrieve(remote, local)
+
+
+class SampleExpressionDataset(SampleDataset, ABC):
+    @abstractmethod
+    def to_expressionset(self) -> HexelExpressionSet:
+        raise NotImplementedError()
+
+
+class KymataMirror2023Q3Dataset(SampleExpressionDataset):
+    def __init__(self, data_root: Optional[PathType] = None, download: bool = True):
+        name = "kymata_mirror_Q3_2023"
+        super().__init__(
+            name=name,
+            filenames=[
+                "kymata_mirror_Q3_2023_expression_endtable.nkg",
+            ],
+            data_root=data_root,
+            remote_root="https://kymata.org/assets_kymata_toolbox_tutorial_data/gridsearch-result-data/",
+            download=download,
+        )
+
+    def to_expressionset(self) -> HexelExpressionSet:
+        es = load_expression_set(from_path_or_file=Path(self.path, self.filenames[0]))
+        assert isinstance(es, HexelExpressionSet)
+        return es
+
+
+class TVLInsLoudnessOnlyDataset(SampleExpressionDataset):
+    def __init__(self, data_root: Optional[PathType] = None, download: bool = True):
+        name = "TVL_2020_ins_loudness_only"
+        super().__init__(
+            name=name,
+            filenames=[
+                "TVL_2020_ins_loudness_only.nkg",
+            ],
+            data_root=data_root,
+            remote_root="https://kymata.org/assets_kymata_toolbox_tutorial_data/gridsearch-result-data/",
+            download=download,
+        )
+
+    def to_expressionset(self) -> HexelExpressionSet:
+        es = load_expression_set(from_path_or_file=Path(self.path, self.filenames[0]))
+        assert isinstance(es, HexelExpressionSet)
+        return es
+
+
+class TVLDeltaInsTC1LoudnessOnlyDataset(SampleExpressionDataset):
+    def __init__(self, data_root: Optional[PathType] = None, download: bool = True):
+        name = "TVL_2020_delta_ins_tontop_chan1_loudness_only"
+        super().__init__(
+            name=name,
+            filenames=["TVL_2020_delta_ins_tontop_chan1_loudness_only.nkg"],
+            data_root=data_root,
+            remote_root="https://kymata.org/assets_kymata_toolbox_tutorial_data/gridsearch-result-data/",
+            download=download,
+        )
+
+    def to_expressionset(self) -> HexelExpressionSet:
+        es = load_expression_set(from_path_or_file=Path(self.path, self.filenames[0]))
+        assert isinstance(es, HexelExpressionSet)
+        return es
+
+
+class TVLDeltaInsTC1LoudnessOnlySensorsDataset(SampleExpressionDataset):
+    def __init__(self, data_root: Optional[PathType] = None, download: bool = True):
+        name = "TVL_2020_delta_ins_tontop_chan1_loudness_only_sensors"
+        super().__init__(
+            name=name,
+            filenames=["TVL_2020_delta_ins_tontop_chan1_loudness_only_sensors.nkg"],
+            data_root=data_root,
+            remote_root="https://kymata.org/assets_kymata_toolbox_tutorial_data/gridsearch-result-data/",
+            download=download,
+        )
+
+    def to_expressionset(self) -> SensorExpressionSet:
+        es = load_expression_set(from_path_or_file=Path(self.path, self.filenames[0]))
+        assert isinstance(es, SensorExpressionSet)
+        return es
+
+
+def delete_dataset(local_dataset: SampleDataset):
+    from shutil import rmtree
+
+    # Make sure it's not silent
+    print(f"Deleting dataset {local_dataset.name}")
+    # Only allow deletion if the specified url is within the data dir
+    assert (
+        data_root_path() in local_dataset.path.parents
+    ), "Cannot delete dataset outside of data root directory"
+    if not local_dataset.path.exists():
+        # Nothing to delete
+        print(f"{str(local_dataset.path)} doesn't exist")
+        return
+
+    # We passed the checks, do the delete
+    rmtree(local_dataset.path)
