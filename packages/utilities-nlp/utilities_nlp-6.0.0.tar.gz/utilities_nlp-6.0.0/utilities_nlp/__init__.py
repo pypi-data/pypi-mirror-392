7515c90649bb96dad5607173770603e344c2aa09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 08:04:15 +0000

    Add package utilities-nlp/6.0.0/utilities_nlp-6.0.0.tar.gz

---
+++ packages/utilities-nlp/utilities_nlp-6.0.0.tar.gz/utilities_nlp-6.0.0/utilities_nlp/__init__.py
@@ -0,0 +1,7 @@
+# This is a library of utility codes with features to facilitate the development and programming of language model algorithms from Sapiens Technology®.
+# All code here is the intellectual property of Sapiens Technology®, and any public mention, distribution, modification, customization, or unauthorized sharing of this or other codes from Sapiens Technology® will result in the author being legally punished by our legal team.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from .utilities_nlp import *
+# This is a library of utility codes with features to facilitate the development and programming of language model algorithms from Sapiens Technology®.
+# All code here is the intellectual property of Sapiens Technology®, and any public mention, distribution, modification, customization, or unauthorized sharing of this or other codes from Sapiens Technology® will result in the author being legally punished by our legal team.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
