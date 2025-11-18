5a21ecde3bf5df4431fef409722daaecae9e4e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 07:05:15 +0000

    Add package vision-agents-plugins-heygen/0.2.0/vision_agents_plugins_heygen-0.2.0.tar.gz

---
+++ packages/vision-agents-plugins-heygen/vision_agents_plugins_heygen-0.2.0.tar.gz/vision_agents_plugins_heygen-0.2.0/vision_agents/plugins/heygen/heygen_types.py
@@ -0,0 +1,12 @@
+"""Type definitions for HeyGen plugin."""
+
+from enum import Enum
+
+
+class VideoQuality(str, Enum):
+    """Video quality options for HeyGen avatar streaming."""
+    
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+
