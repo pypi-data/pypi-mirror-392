4b42fce8fe845ae7f8ac2a836004a10ff1ab628d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 14:05:39 +0000

    Add package san-cli/1.0.17/san_cli-1.0.17.tar.gz

---
+++ packages/san-cli/san_cli-1.0.17.tar.gz/san_cli-1.0.17/neuron_cli/plugins/__init__.py
@@ -0,0 +1,6 @@
+"""Plugin system for Neuron CLI"""
+
+from .base import NeuronPlugin
+
+# Loader, Registry, Manager will be implemented in Phase 2
+__all__ = ['NeuronPlugin']
---
+++ packages/san-cli/san_cli-1.0.17.tar.gz/san_cli-1.0.17/neuron_cli/version.py
@@ -0,0 +1,5 @@
+"""Version information for Neuron CLI"""
+
+__version__ = "1.0.16"  # SAN CLI (SPACE Agent Neuron CLI) - Dynamic Service Discovery
+__author__ = "Kai Gartner (https://linkedin.com/in/kaigartner)"
+__license__ = "Commercial - All Rights Reserved"
