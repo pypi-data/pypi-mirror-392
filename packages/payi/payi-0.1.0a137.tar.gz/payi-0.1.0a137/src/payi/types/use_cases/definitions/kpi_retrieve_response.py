c57098a1266d13baec2c7144fe4e3ed99b053276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-18 22:06:39 +0000

    Add package payi/0.1.0a137/payi-0.1.0a137.tar.gz

---
+++ packages/payi/payi-0.1.0a137.tar.gz/payi-0.1.0a137/src/payi/types/use_cases/definitions/kpi_retrieve_response.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from typing_extensions import Literal
+
+from ...._models import BaseModel
+
+__all__ = ["KpiRetrieveResponse"]
+
+
+class KpiRetrieveResponse(BaseModel):
+    description: str
+
+    name: str
+
+    request_id: str
+
+    goal: Optional[float] = None
+
+    kpi_type: Optional[Literal["boolean", "number", "percentage", "likert5", "likert7", "likert10"]] = None
