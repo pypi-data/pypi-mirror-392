47fea9ce44a22bc16b2e85b7892e3c1b858d4343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 06:06:01 +0000

    Add package rapidfireai/0.12.6rc2/rapidfireai-0.12.6rc2-py3-none-any.whl

---
+++ packages/rapidfireai/rapidfireai-0.12.6rc2-py3-none-any.whl/rapidfireai/evals/utils/__init__.py
@@ -0,0 +1,9 @@
+"""Utility functions and helpers."""
+
+from .constants import get_colab_auth_token, get_dispatcher_headers, get_dispatcher_url
+
+__all__ = [
+    "get_dispatcher_url",
+    "get_colab_auth_token",
+    "get_dispatcher_headers",
+]
