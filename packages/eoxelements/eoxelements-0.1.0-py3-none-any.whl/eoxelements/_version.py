ee54a2c4e960117ee3a5a35e17cde7cb6e927618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:25:20 +0000

    Add package eoxelements/0.1.0/eoxelements-0.1.0-py3-none-any.whl

---
+++ packages/eoxelements/eoxelements-0.1.0-py3-none-any.whl/eoxelements/_version.py
@@ -0,0 +1,4 @@
+# Version pinning for JS components and Python package
+__eox_map_version__ = "1.26.2"  # Update via CI/CD
+__eox_layercontrol_version__ = "1.3.1"  # Update via CI/CD
+__version__ = "0.1.0"  # Python package version
