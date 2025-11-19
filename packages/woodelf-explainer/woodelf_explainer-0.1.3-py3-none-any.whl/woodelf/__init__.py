a7ef22ead940b2db07c5b5c60e2c10ecab801156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 06:05:45 +0000

    Add package woodelf-explainer/0.1.3/woodelf_explainer-0.1.3-py3-none-any.whl

---
+++ packages/woodelf-explainer/woodelf_explainer-0.1.3-py3-none-any.whl/woodelf/__init__.py
@@ -0,0 +1,7 @@
+import woodelf.cube_metric
+import woodelf.decision_trees_ensemble
+import woodelf.parse_models
+import woodelf.path_to_matrices
+import woodelf.simple_woodelf
+
+__version__ = "0.1.3"
