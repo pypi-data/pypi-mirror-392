ed0496e3aaff8ec0d0c637dcf615a7a3a2abe7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 05:08:27 +0000

    Add package django-email-learning/0.1.5/django_email_learning-0.1.5.tar.gz

---
+++ packages/django-email-learning/django_email_learning-0.1.5.tar.gz/django_email_learning-0.1.5/django_email_learning/static/assets/render-hCAsEABD.js
@@ -0,0 +1,2 @@
+import{c as t,b as o,j as e,r as a,T as s}from"./Base-rYbAJ89M.js";const n=t({palette:{primary:{main:"#00d5be"},secondary:{main:"#7c86ff"}},components:{MuiDrawer:{styleOverrides:{backdrop:{backgroundColor:"#fff"}}}}});function d({children:r}){o.createRoot(document.getElementById("root")).render(e.jsx(a.StrictMode,{children:e.jsx(s,{theme:n,children:r})}))}export{d as r};
+//# sourceMappingURL=render-hCAsEABD.js.map
