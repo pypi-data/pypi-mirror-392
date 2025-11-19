eee80f3d1a14532b21e2ffe4ac3510892f85cf1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:25:27 +0000

    Add package argo-metadata-validator/0.0.5/argo_metadata_validator-0.0.5.tar.gz

---
+++ packages/argo-metadata-validator/argo_metadata_validator-0.0.5.tar.gz/argo_metadata_validator-0.0.5/argo_metadata_validator/models/sensor.py
@@ -0,0 +1,47 @@
+"""Models based for parsed sensor metadata."""
+
+from pydantic import BaseModel, Field
+
+
+class SensorInfo(BaseModel):
+    """Model for the sensor_info property."""
+
+    created_by: str
+    date_creation: str
+    link: str
+    format_version: str
+    contents: str
+    sensor_described: str
+
+
+class SensorInstance(BaseModel):
+    """Model for the SENSOR property."""
+
+    SENSOR: str  # URI
+    SENSOR_MAKER: str  # URI
+    SENSOR_MODEL: str  # URI
+    SENSOR_MODEL_FIRMWARE: str
+    SENSOR_SERIAL_NO: str
+
+
+class SensorParameter(BaseModel):
+    """Model for the PARAMETER property."""
+
+    PARAMETER: str  # URI
+    PARAMETER_SENSOR: str  # URI
+    PARAMETER_UNITS: str
+    PARAMETER_ACCURACY: str
+    PARAMETER_RESOLUTION: str
+    PREDEPLOYMENT_CALIB_EQUATION: str
+    PREDEPLOYMENT_CALIB_COEFFICIENT_LIST: dict[str, str]
+    PREDEPLOYMENT_CALIB_COMMENT: str
+    PREDEPLOYMENT_CALIB_DATE: str
+
+
+class Sensor(BaseModel):
+    """Model to hold sensor metadata."""
+
+    sensor_info: SensorInfo
+    SENSORS: list[SensorInstance]
+    PARAMETERS: list[SensorParameter]
+    context: dict[str, str] = Field(alias="@context")
