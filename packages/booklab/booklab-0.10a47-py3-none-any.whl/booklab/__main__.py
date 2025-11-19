18e14d4ef2818992b65c28c9e780f14bae349450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:25:39 +0000

    Add package booklab/0.10a47/booklab-0.10a47-py3-none-any.whl

---
+++ packages/booklab/booklab-0.10a47-py3-none-any.whl/booklab/__main__.py
@@ -0,0 +1,14 @@
+"""
+... wip ...
+modules to be launched at package call with `-m` option
+
+"""
+
+from . import __version__ as sysver
+
+# this is just a tesr
+print("I'm in main module executing what is here...")
+print(f"System version is: {sysver=}")
+
+
+
