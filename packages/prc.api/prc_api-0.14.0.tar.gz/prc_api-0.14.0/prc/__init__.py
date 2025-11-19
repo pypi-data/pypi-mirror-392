f8d4206c262f295c19f1a1907e88afbeeb56f077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 11:07:16 +0000

    Add package prc.api/0.14.0/prc_api-0.14.0.tar.gz

---
+++ packages/prc.api/prc_api-0.14.0.tar.gz/prc_api-0.14.0/prc/__init__.py
@@ -0,0 +1,18 @@
+"""
+
+prc.api
+~~~~~~~~~~~~~~~~~~~
+
+An asynchronous Python wrapper for the PRC/ERLC API.
+
+Copyright 2025-present Tycho
+License: MIT, see LICENSE
+
+"""
+
+from prc import exceptions
+from prc.models import *
+
+from prc.client import PRC
+from prc.server import Server
+from prc.webhooks import Webhooks
