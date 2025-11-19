23a46d6db0669408bb626f1e2c6fc58843695fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:25:33 +0000

    Add package sustainability-measurement-agent/0.1.5/sustainability_measurement_agent-0.1.5-py3-none-any.whl

---
+++ packages/sustainability-measurement-agent/sustainability_measurement_agent-0.1.5-py3-none-any.whl/sma/deploy.py
@@ -0,0 +1,19 @@
+# wrapper for helm 
+
+class Deployer():
+    pass
+
+class HelmDeployer(Deployer):
+    pass
+
+class KustomizeDeployer(Deployer):
+    pass
+
+class KubectlDeployer(Deployer):
+    pass
+
+class AnsibleDeployer(Deployer):
+    pass
+
+class KubernetesJobDeployer(Deployer):
+    pass
