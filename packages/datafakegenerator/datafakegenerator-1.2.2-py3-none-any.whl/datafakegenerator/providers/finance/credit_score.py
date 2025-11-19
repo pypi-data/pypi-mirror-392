8506ffed2e2e25e499cdf0385f18636ae0e31839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 08:06:34 +0000

    Add package datafakegenerator/1.2.2/datafakegenerator-1.2.2-py3-none-any.whl

---
+++ packages/datafakegenerator/datafakegenerator-1.2.2-py3-none-any.whl/datafakegenerator/providers/finance/credit_score.py
@@ -0,0 +1,10 @@
+from datafakegenerator.providers.base_provider import BaseProvider
+import random
+
+
+class CreditScoreProvider(BaseProvider):
+    def __init__(self, blank_percentage: float = 0.0, **kwargs):
+        super().__init__(blank_percentage=blank_percentage, **kwargs)
+
+    def generate_non_blank(self, row_data=None):
+        return random.randint(300, 850)
