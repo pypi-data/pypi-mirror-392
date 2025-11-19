34bd7a43467ae5322a5d846fa2b62f29f0d6911e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 05:07:27 +0000

    Add package coSMicQC/1.0.1/cosmicqc-1.0.1.tar.gz

---
+++ packages/coSMicQC/cosmicqc-1.0.1.tar.gz/cosmicqc-1.0.1/src/cosmicqc/__init__.py
@@ -0,0 +1,10 @@
+"""
+Initialization for cosmicqc package
+"""
+
+from .analyze import find_outliers, identify_outliers, label_outliers
+from .detection import ContaminationDetector
+
+# note: version placeholder is updated during build
+# by poetry-dynamic-versioning.
+__version__ = "1.0.1"
