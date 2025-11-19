84cf054faf57d41757eee8d33b9d343cfbb744c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 07:07:00 +0000

    Add package hcs-cli/0.1.307/hcs_cli-0.1.307-py3-none-any.whl

---
+++ packages/hcs-cli/hcs_cli-0.1.307-py3-none-any.whl/hcs_cli/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "0.1.307"
+
+from . import service
---
+++ packages/hcs-cli/hcs_cli-0.1.307-py3-none-any.whl/hcs_cli/support/constant.py
@@ -0,0 +1,2 @@
+provider_labels = ["azure", "vsphere", "nutanix", "akka", "aws", "openstack"]
+provider_label_lcm = provider_labels + ["aliyun", "gcp", "zerocloud", "edgeproxy"]
