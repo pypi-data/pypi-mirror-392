b3761c343fa8d76457d54c43ad7b0e2d49f09a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 08:06:16 +0000

    Add package gypsum-dl/1.3.1.dev0/gypsum_dl-1.3.1.dev0.tar.gz

---
+++ packages/gypsum-dl/gypsum_dl-1.3.1.dev0.tar.gz/gypsum_dl-1.3.1.dev0/CONTRIBUTORS.md
@@ -0,0 +1,9 @@
+# Contributors in Alphabetical Order
+
+-   Jacob Durrant
+-   Erich Hellemann
+-   Katherine Milliken
+-   John Ringe
+-   Patrick Ropp
+-   Jacob Spigel
+-   Jennifer Walker
---
+++ packages/gypsum-dl/gypsum_dl-1.3.1.dev0.tar.gz/gypsum_dl-1.3.1.dev0/gypsum_dl/steps/smiles/PrepareSmiles.py
@@ -0,0 +1,169 @@
+"""
+Runs the smile preparation process. Generates alternate ionization,
+tautomeric, chiral forms, etc.
+"""
+
+import __future__
+
+from gypsum_dl import utils
+from gypsum_dl.steps.smiles.AddHydrogens import add_hydrogens
+from gypsum_dl.steps.smiles.DeSaltOrigSmiles import desalt_orig_smi
+from gypsum_dl.steps.smiles.DurrantLabFilter import (
+    durrant_lab_contains_bad_substr,
+    durrant_lab_filters,
+)
+from gypsum_dl.steps.smiles.EnumerateChiralMols import enumerate_chiral_molecules
+from gypsum_dl.steps.smiles.EnumerateDoubleBonds import enumerate_double_bonds
+from gypsum_dl.steps.smiles.MakeTautomers import make_tauts
+
+
+def prepare_smiles(contnrs, params):
+    """Runs the appropriate steps for processing the SMILES strings.
+
+    :param contnrs: A list of containers (MolContainer.MolContainer).
+    :type contnrs: list
+    :param params: The user parameters.
+    :type params: dict
+    """
+
+    # Unpack some of the parameter values.
+    min_ph = params["min_ph"]
+    max_ph = params["max_ph"]
+    std_dev = params["pka_precision"]
+    max_variants_per_compound = params["max_variants_per_compound"]
+    thoroughness = params["thoroughness"]
+    num_procs = params["num_processors"]
+    job_manager = params["job_manager"]
+    let_tautomers_change_chirality = params["let_tautomers_change_chirality"]
+    parallelizer_obj = params["Parallelizer"]
+
+    debug = True
+
+    # Desalt the molecules. Note that the program always desalts (can't turn it
+    # off).
+    # utils.log("Begin Desaltings")
+    desalt_orig_smi(contnrs, num_procs, job_manager, parallelizer_obj)
+    # utils.log("Done with Desalting")
+
+    # Filter the containers to remove ones that have bad substrings (metal,
+    # etc.) in the desalted smiles, assuming durrant lab filter turned on. Note
+    # that some compounds aren't filtered until later.
+    if params["use_durrant_lab_filters"] == True:
+        contnrs = [
+            c for c in contnrs if not durrant_lab_contains_bad_substr(c.orig_smi_deslt)
+        ]
+
+    if debug:
+        utils.print_current_smiles(contnrs)
+
+    # Add hydrogens for user-specified pH, if requested.
+    if not params["skip_adding_hydrogen"]:
+        # utils.log("Ionizing Molecules")
+        add_hydrogens(
+            contnrs,
+            min_ph,
+            max_ph,
+            std_dev,
+            max_variants_per_compound,
+            thoroughness,
+            num_procs,
+            job_manager,
+            parallelizer_obj,
+        )
+        # utils.log("Done with Ionization")
+    else:
+        utils.log("Skipping ionization")
+        wrap_molecules(contnrs)
+
+    if debug:
+        utils.print_current_smiles(contnrs)
+
+    # Make alternate tautomeric forms, if requested.
+    if not params["skip_making_tautomers"]:
+        # utils.log("Tautomerizing Molecules")
+        make_tauts(
+            contnrs,
+            max_variants_per_compound,
+            thoroughness,
+            num_procs,
+            job_manager,
+            let_tautomers_change_chirality,
+            parallelizer_obj,
+        )
+        # utils.log("Done with Tautomerization")
+    else:
+        utils.log("Skipping tautomerization")
+
+    if debug:
+        utils.print_current_smiles(contnrs)
+
+    # Apply Durrant-lab filters if requested
+    if params["use_durrant_lab_filters"]:
+        # utils.log("Applying Durrant-Lab Filters")
+        durrant_lab_filters(contnrs, num_procs, job_manager, parallelizer_obj)
+        # utils.log("Done Applying Durrant-Lab Filters")
+    else:
+        utils.log("Not applying Durrant-lab filters")
+
+    if debug:
+        utils.print_current_smiles(contnrs)
+
+    # Make alternate chiral forms, if requested.
+    if not params["skip_enumerate_chiral_mol"]:
+        # utils.log("Enumerating Chirality")
+        enumerate_chiral_molecules(
+            contnrs,
+            max_variants_per_compound,
+            thoroughness,
+            num_procs,
+            job_manager,
+            parallelizer_obj,
+        )
+        # utils.log("Done with Chirality Enumeration")
+    else:
+        utils.log("Skipping chirality enumeration")
+
+    if debug:
+        utils.print_current_smiles(contnrs)
+
+    # Make alternate double-bond isomers, if requested.
+    if not params["skip_enumerate_double_bonds"]:
+        # utils.log("Enumerating Double Bonds")
+        enumerate_double_bonds(
+            contnrs,
+            max_variants_per_compound,
+            thoroughness,
+            num_procs,
+            job_manager,
+            parallelizer_obj,
+        )
+        # utils.log("Done with Double Bond Enumeration")
+    else:
+        utils.log("Skipping double bond enumeration")
+
+    if debug:
+        utils.print_current_smiles(contnrs)
+
+
+def wrap_molecules(contnrs):
+    """Each molecule container holds only one SMILES string
+    (corresponding to the input structure). Dimorphite-DL can potentially
+    produce multiple SMILES strings with different ionization states. There is
+    no way to store muliple smiles in a molecule container.
+
+    But those containers are designed to store multiple RDKit molecule
+    objects. Gypsum-DL stores Dimorphite-DL output molecules as RdKit molecule
+    objects, in the container's mol list.
+
+    But Gypsum-DL gives the user the option of skipping the ionization step.
+    In this case, the one SMILES needs to be converted to a RDKit mol object
+    for subsequent steps to work. Let's do that here.
+
+    :param contnrs: A list of containers (MolContainer.MolContainer).
+    :type contnrs: list
+    """
+
+    for mol_cont in contnrs:
+        if len(mol_cont.mols) == 0:
+            smi = mol_cont.orig_smi_canonical
+            mol_cont.add_smiles(smi)
