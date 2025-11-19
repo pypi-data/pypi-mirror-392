2bba4b7b26c30dd5633bf583042cd7c54cfacc68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:25:54 +0000

    Add package bluer-sbc/9.216.1/bluer_sbc-9.216.1-py3-none-any.whl

---
+++ packages/bluer-sbc/bluer_sbc-9.216.1-py3-none-any.whl/bluer_sbc/README/designs/swallow_head/history.py
@@ -0,0 +1,22 @@
+from bluer_objects.README.items import ImageItems
+
+from bluer_sbc.README.design import design_doc
+from bluer_sbc.README.designs.swallow_head import image_template, latest_version
+from bluer_sbc.README.designs.swallow_head.parts import parts
+
+docs = [
+    design_doc(
+        f"swallow-head/v{version}.md",
+        ImageItems(
+            {
+                image_template(version).format(
+                    f"{index+1:02}.jpg",
+                ): ""
+                for index in range(6)
+            }
+        ),
+        parts,
+        own_folder=True,
+    )
+    for version in range(1, latest_version)
+]
