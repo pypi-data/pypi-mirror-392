9a55dfb20eb6b0359b785d97dca247fe488ef9f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 11:07:08 +0000

    Add package bach-google-search74/1.0.0/bach_google_search74-1.0.0-py3-none-any.whl

---
+++ packages/bach-google-search74/bach_google_search74-1.0.0-py3-none-any.whl/setup.py
@@ -0,0 +1,27 @@
+"""Setup script for google_search74"""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="google_search74",
+    version="1.0.0",
+    author="BACH Studio",
+    author_email="contact@bachstudio.com",
+    description="A short description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BACH-AI-Tools/google_search74",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        # 添加你的依赖
+    ],
+)
