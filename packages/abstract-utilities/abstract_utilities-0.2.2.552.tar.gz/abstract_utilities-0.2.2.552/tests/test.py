250f6f9caab58949677c71baec8712eb3b6bdbde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:21 +0000

    Add package abstract-utilities/0.2.2.552/abstract_utilities-0.2.2.552.tar.gz

---
+++ packages/abstract-utilities/abstract_utilities-0.2.2.552.tar.gz/abstract_utilities-0.2.2.552/tests/test.py
@@ -0,0 +1,12 @@
+from find_content import *
+
+file_path = "/home/flerb/Pictures/gts/gts2/73w"
+sysroot="/home/flerb/Downloads"
+
+###get_dot_fro_line(line,dirname=None,file_path=None,get_info=False)
+
+file_path="/home/flerb/Documents/pythonTools/modules/src/modules/abstract_paths/src/abstract_paths/finderTab"
+result = get_file_filters(file_path,exclude_exts='pyc')
+input(result)
+
+input(defaults)
