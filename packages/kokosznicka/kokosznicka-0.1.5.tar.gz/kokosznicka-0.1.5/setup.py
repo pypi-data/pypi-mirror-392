c666ff599f936bc513cc362739a4b100ca5cc93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 12:05:47 +0000

    Add package kokosznicka/0.1.5/kokosznicka-0.1.5.tar.gz

---
+++ packages/kokosznicka/kokosznicka-0.1.5.tar.gz/kokosznicka-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'kokosznicka',
+  packages = ['kokosznicka'],
+  version = '0.1.5',
+  license='GPL-3.0',
+  description = 'A simple and effective syllabificator for the Polish language.',
+  author = 'Tytus Dunin',
+  author_email = 'tm.dunin@student.uw.edu.pl',
+  url = 'https://github.com/tytusdunin/kokosznicka',
+  download_url = 'https://github.com/tytusdunin/kokosznicka/archive/refs/tags/v.0.1.5.tar.gz',
+  keywords = ['nlp', 'syllabification', 'hyphenation', 'polish'],
+  install_requires=[],
+
+  classifiers=[
+    'Development Status :: 4 - Beta',
+
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+    'Programming Language :: Python :: 3',  
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
