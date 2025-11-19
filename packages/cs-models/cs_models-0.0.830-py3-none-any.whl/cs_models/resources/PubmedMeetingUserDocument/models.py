0db98f6118b49eae352dec706b5665995260e12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:17 +0000

    Add package cs-models/0.0.830/cs_models-0.0.830-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.830-py3-none-any.whl/cs_models/resources/PubmedMeetingUserDocument/models.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    String,
+    Text,
+    Boolean,
+    Integer,
+    DateTime,
+    ForeignKey,
+)
+
+from ...database import Base
+
+
+class PubmedMeetingUserDocumentModel(Base):
+    __tablename__ = "pubmed_meeting_user_documents"
+
+    id = Column(Integer, primary_key=True)
+    pubmed_id = Column(
+        Integer,
+        ForeignKey('pubmed.id'),
+        nullable=False,
+    )
+    meeting_id = Column(
+        Integer,
+        ForeignKey('meetings.id'),
+        nullable=False,
+    )
+    user_document_id = Column(
+        Integer,
+        ForeignKey('user_documents.id'),
+        nullable=False,
+    )
+    status = Column(
+        String(50),
+        nullable=False,
+    )
+    is_active = Column(Boolean, nullable=True)
+    details = Column(Text, nullable=True)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
