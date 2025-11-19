c9cf05e6e71935e54f2f65f08b25a2f56af1f1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:35 +0000

    Add package kimi-cli/0.55/kimi_cli-0.55-py3-none-any.whl

---
+++ packages/kimi-cli/kimi_cli-0.55-py3-none-any.whl/kimi_cli/soul/toolset.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from contextvars import ContextVar
+from typing import override
+
+from kosong.message import ToolCall
+from kosong.tooling import HandleResult
+from kosong.tooling.simple import SimpleToolset
+
+current_tool_call = ContextVar[ToolCall | None]("current_tool_call", default=None)
+
+
+def get_current_tool_call_or_none() -> ToolCall | None:
+    """
+    Get the current tool call or None.
+    Expect to be not None when called from a `__call__` method of a tool.
+    """
+    return current_tool_call.get()
+
+
+class CustomToolset(SimpleToolset):
+    @override
+    def handle(self, tool_call: ToolCall) -> HandleResult:
+        token = current_tool_call.set(tool_call)
+        try:
+            return super().handle(tool_call)
+        finally:
+            current_tool_call.reset(token)
