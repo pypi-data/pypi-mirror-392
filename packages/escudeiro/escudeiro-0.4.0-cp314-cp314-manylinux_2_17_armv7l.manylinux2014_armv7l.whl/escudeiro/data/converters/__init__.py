36f2fd1e464e664e8f689c89d041c75fd9fa515c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 08:06:18 +0000

    Add package escudeiro/0.4.0/escudeiro-0.4.0-cp314-cp314-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/escudeiro/escudeiro-0.4.0-cp314-cp314-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/escudeiro/data/converters/__init__.py
@@ -0,0 +1,12 @@
+from escudeiro.escudeiro_pyrs import squire
+
+from .json import (
+    asjson,
+    fromjson,
+)
+from .utils import (
+    asdict,
+    fromdict,
+)
+
+__all__ = ["squire", "fromdict", "asdict", "fromjson", "asjson"]
