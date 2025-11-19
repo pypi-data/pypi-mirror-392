4289ae86f4ce97ba3beedbf6ae0eba07639aa8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:26:06 +0000

    Add package playwright-stealth-plugin/0.1.5/playwright_stealth_plugin-0.1.5-py3-none-any.whl

---
+++ packages/playwright-stealth-plugin/playwright_stealth_plugin-0.1.5-py3-none-any.whl/playwright_stealth_plugin/evasions/python/web_gl_vendor.py
@@ -0,0 +1,7 @@
+import playwright_stealth_plugin.evasions.python._utils as _utils
+
+
+def run():
+    script = _utils.read_script(f"../javascript/{__name__.split(".")[-1]}.js")
+    _utils.scripts.append(script)
+    _utils.logger.info(f"RUN: {__name__}")
