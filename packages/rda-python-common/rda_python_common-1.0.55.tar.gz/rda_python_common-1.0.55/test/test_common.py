844784c5a14a78a71b354f9f788ea286235dde4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 04:07:17 +0000

    Add package rda-python-common/1.0.55/rda_python_common-1.0.55.tar.gz

---
+++ packages/rda-python-common/rda_python_common-1.0.55.tar.gz/rda_python_common-1.0.55/test/test_common.py
@@ -0,0 +1,16 @@
+# test_hello_world.py
+
+import pytest
+
+
+
+def test_common():
+    import rda_python_common.PgCMD
+    import rda_python_common.PgDBI
+    import rda_python_common.PgFile
+    import rda_python_common.PgLock
+    import rda_python_common.PgLOG
+    import rda_python_common.PgOPT
+    import rda_python_common.PgSIG
+    import rda_python_common.PgSplit
+    import rda_python_common.PgUtil
