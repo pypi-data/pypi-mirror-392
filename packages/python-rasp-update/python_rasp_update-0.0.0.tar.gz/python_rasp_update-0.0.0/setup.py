82cf101c44b4af4d41259f36ed90ab6a77791e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 08:06:01 +0000

    Add package python-rasp-update/0.0.0/python_rasp_update-0.0.0.tar.gz

---
+++ packages/python-rasp-update/python_rasp_update-0.0.0.tar.gz/python_rasp_update-0.0.0/setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+def get_version():
+    return os.getenv("PYRASP_VERSION")
+
+
+setup(
+    name="python-rasp-update",
+    version=get_version(),
+    description="Python RASP Auto-Update Package",
+    long_description="Python RASP Auto-Update Package",
+    long_description_content_type="text/markdown",
+    author="Alibaba Cloud",
+    author_email="security@alibaba-inc.com",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    zip_safe=False,
+)
