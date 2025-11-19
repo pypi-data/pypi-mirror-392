b77c4995355e1b3ce544368739f41355979bf431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 06:06:16 +0000

    Add package opengris-scaler/1.12.31/opengris_scaler-1.12.31.tar.gz

---
+++ packages/opengris-scaler/opengris_scaler-1.12.31.tar.gz/opengris_scaler-1.12.31/src/cpp/scaler/ymq/tcp_client.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <memory>
+
+// First-party
+#include "scaler/logging/logging.h"
+#include "scaler/ymq/configuration.h"
+#include "scaler/ymq/internal/raw_client_tcp_fd.h"
+
+namespace scaler {
+namespace ymq {
+
+class EventLoopThread;
+class EventManager;
+
+class TcpClient {
+public:
+    using ConnectReturnCallback = Configuration::ConnectReturnCallback;
+
+    TcpClient(
+        EventLoopThread* eventLoopThread,
+        std::string localIOSocketIdentity,
+        sockaddr remoteAddr,
+        ConnectReturnCallback onConnectReturn,
+        size_t maxRetryTimes) noexcept;
+    TcpClient(const TcpClient&)            = delete;
+    TcpClient& operator=(const TcpClient&) = delete;
+    ~TcpClient() noexcept;
+
+    void onCreated();
+    void retry();
+    void disconnect();
+
+    EventLoopThread* _eventLoopThread; /* shared ownership */
+    bool _connected;
+
+private:
+    // Implementation defined method. connect(3) should happen here.
+    // This function will call user defined onConnectReturn()
+    // It will handle error it can handle. If it is unreasonable to
+    // handle the error here, pass it to onConnectReturn()
+    void onRead();
+    void onWrite();
+    void onClose() {}
+    void onError() {}
+
+    ConnectReturnCallback _onConnectReturn;
+    std::string _localIOSocketIdentity;
+    int _retryIdentifier;
+
+    Logger _logger;
+
+    std::unique_ptr<EventManager> _eventManager;
+    size_t _retryTimes;
+
+    const size_t _maxRetryTimes;
+
+    RawClientTCPFD _rawClient;
+};
+
+}  // namespace ymq
+}  // namespace scaler
---
+++ packages/opengris-scaler/opengris_scaler-1.12.31.tar.gz/opengris_scaler-1.12.31/src/cpp/scaler/ymq/tcp_server.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <memory>
+
+#include "scaler/ymq/internal/raw_server_tcp_fd.h"
+
+// First-party
+#include "scaler/logging/logging.h"
+#include "scaler/ymq/configuration.h"
+
+namespace scaler {
+namespace ymq {
+
+class EventLoopThread;
+class EventManager;
+
+class TcpServer {
+public:
+    using BindReturnCallback = Configuration::BindReturnCallback;
+
+    TcpServer(
+        EventLoopThread* eventLoop,
+        std::string localIOSocketIdentity,
+        sockaddr addr,
+        BindReturnCallback onBindReturn) noexcept;
+    TcpServer(const TcpServer&)            = delete;
+    TcpServer& operator=(const TcpServer&) = delete;
+    ~TcpServer() noexcept;
+
+    void disconnect();
+    void onCreated();
+    EventLoopThread* _eventLoopThread;
+
+private:
+    // Implementation defined method. accept(3) should happen here.
+    // This function will call user defined onAcceptReturn()
+    // It will handle error it can handle. If it is unreasonable to
+    // handle the error here, pass it to onAcceptReturn()
+    void onRead();
+    void onWrite() {}
+    void onClose() { printf("%s\n", __PRETTY_FUNCTION__); }
+    void onError() { printf("%s\n", __PRETTY_FUNCTION__); }
+
+    bool createAndBindSocket();
+
+    BindReturnCallback _onBindReturn;
+    sockaddr _addr;
+    std::string _localIOSocketIdentity;
+
+    std::unique_ptr<EventManager> _eventManager;  // will copy the `onRead()` to itself
+
+    Logger _logger;
+
+    RawServerTCPFD _rawServer;
+};
+
+}  // namespace ymq
+}  // namespace scaler
