a1181788279a095c374a525240fc2a9da9309a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:25:54 +0000

    Add package INFINITE-CONTEXT-WINDOW/2.0.6/infinite_context_window-2.0.6.tar.gz

---
+++ packages/INFINITE-CONTEXT-WINDOW/infinite_context_window-2.0.6.tar.gz/infinite_context_window-2.0.6/setup.py
@@ -0,0 +1,24 @@
+# Module for managing infinite context window in language models, developed by the Sapiens Technology® team.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from setuptools import setup, find_packages
+package_name = 'INFINITE_CONTEXT_WINDOW'
+version = '2.0.6'
+setup(
+    name=package_name,
+    version=version,
+    author='SAPIENS TECHNOLOGY',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'TTS==0.22.0',
+        'paddlepaddle',
+        'utilities-nlp',
+        'perpetual-context-window',
+        'infinite-context',
+        'perpetual-context'
+    ],
+    url='https://github.com/sapiens-technology/INFINITE_CONTEXT_WINDOW',
+    license='Proprietary Software'
+)
+# Module for managing infinite context window in language models, developed by the Sapiens Technology® team.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
