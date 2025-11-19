76ac2a7a91c9bbc2c1d4e164e7e68bd42fa188cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 06:05:48 +0000

    Add package crappytext/0.1.0/crappytext-0.1.0.tar.gz

---
+++ packages/crappytext/crappytext-0.1.0.tar.gz/crappytext-0.1.0/src/crappytext/__main__.py
@@ -0,0 +1,7 @@
+def main():
+    print("Welcome to CrappyText! This is the world's crappiest text editor.")
+
+if __name__ == "__main__":
+    main()
+else:
+    raise RuntimeError("CrappyText is not meant to be imported, try running it as a script.")
