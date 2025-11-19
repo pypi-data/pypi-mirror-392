e1479536387f684eb21431f674eeb88210480b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 12:06:18 +0000

    Add package rankvectors/1.3.1/rankvectors-1.3.1.tar.gz

---
+++ packages/rankvectors/rankvectors-1.3.1.tar.gz/rankvectors-1.3.1/rankvectors/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from rankvectors.api.api_keys_api import APIKeysApi
+from rankvectors.api.analytics_api import AnalyticsApi
+from rankvectors.api.content_verification_api import ContentVerificationApi
+from rankvectors.api.crawling_api import CrawlingApi
+from rankvectors.api.implementations_api import ImplementationsApi
+from rankvectors.api.pages_api import PagesApi
+from rankvectors.api.projects_api import ProjectsApi
+from rankvectors.api.suggestions_api import SuggestionsApi
+from rankvectors.api.user_api import UserApi
+from rankvectors.api.webhooks_api import WebhooksApi
+
---
+++ packages/rankvectors/rankvectors-1.3.1.tar.gz/rankvectors-1.3.1/rankvectors/com/rankvectors/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from com.rankvectors.api.api_keys_api import APIKeysApi
+from com.rankvectors.api.analytics_api import AnalyticsApi
+from com.rankvectors.api.content_verification_api import ContentVerificationApi
+from com.rankvectors.api.crawling_api import CrawlingApi
+from com.rankvectors.api.implementations_api import ImplementationsApi
+from com.rankvectors.api.pages_api import PagesApi
+from com.rankvectors.api.projects_api import ProjectsApi
+from com.rankvectors.api.suggestions_api import SuggestionsApi
+from com.rankvectors.api.user_api import UserApi
+from com.rankvectors.api.webhooks_api import WebhooksApi
+
