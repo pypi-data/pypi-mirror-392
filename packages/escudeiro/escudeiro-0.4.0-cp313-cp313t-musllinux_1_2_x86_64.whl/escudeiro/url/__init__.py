ecb808f23c738d1550184eb8596f19ef8fdfc755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 08:06:21 +0000

    Add package escudeiro/0.4.0/escudeiro-0.4.0-cp313-cp313t-musllinux_1_2_x86_64.whl

---
+++ packages/escudeiro/escudeiro-0.4.0-cp313-cp313t-musllinux_1_2_x86_64.whl/escudeiro/url/__init__.py
@@ -0,0 +1,7 @@
+from .fragment import Fragment
+from .netloc import Netloc
+from .path import Path
+from .query import Query
+from .url import URL
+
+__all__ = ["Query", "Path", "Fragment", "Netloc", "URL"]
