56b3d6c67db5d93e2db69f94001f7fe0975fa28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 12:05:41 +0000

    Add package flync-sdk/0.0.1/flync_sdk-0.0.1.tar.gz

---
+++ packages/flync-sdk/flync_sdk-0.0.1.tar.gz/flync_sdk-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flync-sdk",
+    version="0.0.1",
+    packages=find_packages(),
+    description="SDK package for flync",
+    author="Technica Engineering",
+    license="MIT",
+)
