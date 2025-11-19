79c2f016f41c4812c54a90db8ad9a11ca9ae48ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 04:07:16 +0000

    Add package fromager/0.72.0/fromager-0.72.0.tar.gz

---
+++ packages/fromager/fromager-0.72.0.tar.gz/fromager-0.72.0/tests/test_external_commands.py
@@ -0,0 +1,87 @@
+import os
+import pathlib
+import subprocess
+import typing
+from unittest import mock
+
+import pytest
+
+from fromager import external_commands
+
+
+def test_external_commands_environ() -> None:
+    env = {"BLAH": "test"}
+    output = external_commands.run(["sh", "-c", "echo $BLAH"], extra_environ=env)
+    assert "test\n" == output
+
+
+def test_external_commands_log_file(tmp_path: pathlib.Path) -> None:
+    log_filename = pathlib.Path(tmp_path) / "test.log"
+    env = {"BLAH": "test"}
+    output = external_commands.run(
+        ["sh", "-c", "echo $BLAH"],
+        extra_environ=env,
+        log_filename=str(log_filename),
+    )
+    assert "test\n" == output
+    assert log_filename.exists()
+    file_contents = log_filename.read_text()
+    assert "test\n" == file_contents
+
+
+@mock.patch("subprocess.run", return_value=mock.Mock(returncode=0))
+@mock.patch(
+    "fromager.external_commands.network_isolation_cmd",
+    return_value=["/bin/unshare", "--net", "--map-current-user"],
+)
+@mock.patch.dict(os.environ)
+def test_external_commands_network_isolation(
+    m_network_isolation_cmd: mock.Mock,
+    m_run: mock.Mock,
+) -> None:
+    os.environ.clear()
+    external_commands.run(
+        ["host", "github.com"],
+        extra_environ={},
+        network_isolation=True,
+    )
+    m_network_isolation_cmd.assert_called()
+    m_run.assert_called_with(
+        [
+            "/bin/unshare",
+            "--net",
+            "--map-current-user",
+            "host",
+            "github.com",
+        ],
+        cwd=None,
+        env={},
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        stdin=None,
+    )
+
+
+NETWORK_ISOLATION_ERROR: Exception | None = None
+try:
+    external_commands.detect_network_isolation()
+except Exception as err:
+    NETWORK_ISOLATION_ERROR = err
+    SUPPORTS_NETWORK_ISOLATION: bool = False
+else:
+    SUPPORTS_NETWORK_ISOLATION = True
+
+
+@pytest.mark.skipif(
+    not SUPPORTS_NETWORK_ISOLATION,
+    reason=f"network isolation is not supported: {NETWORK_ISOLATION_ERROR}",
+)
+def test_external_commands_network_isolation_real() -> None:
+    with pytest.raises(external_commands.NetworkIsolationError) as e:
+        external_commands.run(
+            ["host", "github.com"],
+            network_isolation=True,
+            extra_environ={"LC_ALL": "C"},
+        )
+    exc = typing.cast(subprocess.CalledProcessError, e.value)
+    assert exc.returncode == 1
