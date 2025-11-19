781c97f46ecf39cf31442584a0ef5ade488b2378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:46 +0000

    Add package wbcrm/1.57.1/wbcrm-1.57.1-py2.py3-none-any.whl

---
+++ packages/wbcrm/wbcrm-1.57.1-py2.py3-none-any.whl/wbcrm/synchronization/activity/tasks.py
@@ -0,0 +1,21 @@
+from celery import shared_task
+
+from .shortcuts import get_backend
+
+
+@shared_task(queue="synchronization")
+def periodic_notify_admins_of_webhook_inconsistencies_task(emails: list | None = None):
+    """
+    Periodic tasks to notify webhook inconsistencies
+    """
+    if emails and (controller := get_backend()):
+        controller.backend.notify_admins_of_webhook_inconsistencies(emails)
+
+
+@shared_task(queue="synchronization")
+def periodic_renew_web_hooks_task():
+    """
+    Periodic tasks to renew active webhooks
+    """
+    if controller := get_backend():
+        controller.backend.renew_web_hooks()
