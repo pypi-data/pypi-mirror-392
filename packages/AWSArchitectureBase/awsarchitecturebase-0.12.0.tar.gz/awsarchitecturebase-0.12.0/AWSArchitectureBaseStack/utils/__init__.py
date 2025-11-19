7d3dede89e822ac2e9046f45da580f9f754c6461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:25:54 +0000

    Add package AWSArchitectureBase/0.12.0/awsarchitecturebase-0.12.0.tar.gz

---
+++ packages/AWSArchitectureBase/awsarchitecturebase-0.12.0.tar.gz/awsarchitecturebase-0.12.0/AWSArchitectureBaseStack/utils/__init__.py
@@ -0,0 +1,15 @@
+"""
+Utility modules for AWS Architecture Base.
+
+This package contains helper functions and utilities that are used
+throughout the AWS Architecture Base stack.
+"""
+
+from .naming import properize_string, clean_hyphens
+from .secrets import parse_secrets_from_env
+
+__all__ = [
+    "properize_string",
+    "clean_hyphens",
+    "parse_secrets_from_env",
+]
