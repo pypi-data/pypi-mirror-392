bdbfce5ba25afcbde810d954edb673108d21a6fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:16 +0000

    Add package OpenGeode-IO/7.4.6/opengeode_io-7.4.6-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/OpenGeode-IO/opengeode_io-7.4.6-cp39-cp39-manylinux_2_28_x86_64.whl/opengeode_io/mesh_io.pyi
@@ -0,0 +1,4 @@
+from __future__ import annotations
+import opengeode as opengeode
+from opengeode_io.lib64.opengeode_io_py_mesh import IOMeshLibrary
+__all__: list[str] = ['IOMeshLibrary', 'opengeode']
