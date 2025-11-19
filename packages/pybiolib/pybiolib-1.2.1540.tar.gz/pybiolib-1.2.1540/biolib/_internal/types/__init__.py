c8974256ecb16af24e19696625384120d8adf35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 11:06:24 +0000

    Add package pybiolib/1.2.1540/pybiolib-1.2.1540.tar.gz

---
+++ packages/pybiolib/pybiolib-1.2.1540.tar.gz/pybiolib-1.2.1540/biolib/_internal/types/__init__.py
@@ -0,0 +1,6 @@
+from .app import *  # noqa: F403
+from .data_record import *  # noqa: F403
+from .experiment import *  # noqa: F403
+from .resource import *  # noqa: F403
+from .resource_permission import *  # noqa: F403
+from .resource_version import *  # noqa: F403
