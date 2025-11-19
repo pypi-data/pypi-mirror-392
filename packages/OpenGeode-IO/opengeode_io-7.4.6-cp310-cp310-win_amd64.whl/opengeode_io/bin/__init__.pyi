a0246fb04c1ddd63efbed9c0239600fc0bcdab92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:16 +0000

    Add package OpenGeode-IO/7.4.6/opengeode_io-7.4.6-cp310-cp310-win_amd64.whl

---
+++ packages/OpenGeode-IO/opengeode_io-7.4.6-cp310-cp310-win_amd64.whl/opengeode_io/bin/__init__.pyi
@@ -0,0 +1,5 @@
+from __future__ import annotations
+from . import opengeode_io_py_image
+from . import opengeode_io_py_mesh
+from . import opengeode_io_py_model
+__all__: list[str] = ['opengeode_io_py_image', 'opengeode_io_py_mesh', 'opengeode_io_py_model']
