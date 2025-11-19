2d02dd56f5538bb394dd9e584550225c71caf595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 07:07:37 +0000

    Add package cantera/3.2.0/cantera-3.2.0-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/cantera/cantera-3.2.0-cp312-cp312-manylinux_2_28_x86_64.whl/cantera/__init__.py
@@ -0,0 +1,20 @@
+# This file is part of Cantera. See License.txt in the top-level directory or
+# at https://cantera.org/license.txt for license and copyright information.
+
+from ._cantera import *
+from ._utils import __version__, __sundials_version__, __git_commit__
+from .composite import *
+from .liquidvapor import *
+from .onedim import *
+from .utils import *
+from .data import *
+import cantera.interrupts  # Helps with standalone packaging (PyInstaller etc.)
+
+from pathlib import Path
+import warnings
+
+warnings.filterwarnings('default', module='cantera')
+add_data_directory(Path(__file__).parent / "data")
+add_data_directory('.')  # Move current working directory to the front of the path
+
+del warnings, Path, np, os
