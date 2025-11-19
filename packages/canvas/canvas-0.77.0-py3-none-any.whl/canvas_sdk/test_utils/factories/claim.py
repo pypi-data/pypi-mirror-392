29062ea754874ac29d7599ba3f9366874b213291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 00:07:58 +0000

    Add package canvas/0.77.0/canvas-0.77.0-py3-none-any.whl

---
+++ packages/canvas/canvas-0.77.0-py3-none-any.whl/canvas_sdk/test_utils/factories/claim.py
@@ -0,0 +1,53 @@
+import factory
+
+from canvas_sdk.v1.data import Claim, ClaimComment, ClaimLabel, ClaimProvider, ClaimQueue
+
+
+class ClaimQueueFactory(factory.django.DjangoModelFactory[ClaimQueue]):
+    """Factory for creating ClaimQueue."""
+
+    class Meta:
+        model = ClaimQueue
+        django_get_or_create = ("queue_sort_ordering",)
+
+    queue_sort_ordering = 5
+    name = "Claim: Filed"
+
+
+class ClaimFactory(factory.django.DjangoModelFactory[Claim]):
+    """Factory for creating Claim."""
+
+    class Meta:
+        model = Claim
+
+    note = factory.SubFactory("canvas_sdk.test_utils.factories.NoteFactory")
+    current_queue = factory.SubFactory(ClaimQueueFactory)
+
+
+class ClaimCommentFactory(factory.django.DjangoModelFactory[ClaimComment]):
+    """Factory for creating ClaimComment."""
+
+    class Meta:
+        model = ClaimComment
+
+    claim = factory.SubFactory(ClaimFactory)
+    comment = "Need to message Dr. House"
+
+
+class ClaimProviderFactory(factory.django.DjangoModelFactory[ClaimProvider]):
+    """Factory for creating ClaimProvider."""
+
+    class Meta:
+        model = ClaimProvider
+
+    claim = factory.SubFactory(ClaimFactory)
+
+
+class ClaimLabelFactory(factory.django.DjangoModelFactory[ClaimLabel]):
+    """Factory for creating ClaimLabel."""
+
+    class Meta:
+        model = ClaimLabel
+
+    claim = factory.SubFactory(ClaimFactory)
+    label = factory.SubFactory("canvas_sdk.test_utils.factories.TaskLabelFactory")
