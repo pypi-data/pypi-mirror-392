a6d70c4c347a228ae068ce45ba1601f2dc55a34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 04:07:20 +0000

    Add package eoxelements/0.2.7/eoxelements-0.2.7-py3-none-any.whl

---
+++ packages/eoxelements/eoxelements-0.2.7-py3-none-any.whl/eoxelements/layouts.py
@@ -0,0 +1,41 @@
+import uuid
+from ipywidgets import HBox, Layout
+from .map import EOxMap
+from .layercontrol import EOxLayerControl
+
+class EOxMapWithControl(HBox):
+    """
+    A convenience widget that displays an EOxMap and
+    an EOxLayerControl side-by-side.
+    """
+    def __init__(self, layers=None, config=None, map_layout=None, control_layout=None, **kwargs):
+        map_id = f"eox-map-{uuid.uuid4()}"
+        if map_layout is None:
+            map_layout = Layout(flex='1 1 70%', height='400px')
+        if control_layout is None:
+            control_layout = Layout(flex='1 1 30%', height='400px')
+        map_layers = layers if layers is not None else []
+        map_config = config if config is not None else {}
+        self.map = EOxMap(
+            element_id=map_id,
+            layers=map_layers,
+            config=map_config,
+            layout=map_layout
+        )
+        self.control = EOxLayerControl(
+            for_id=f"#{map_id}",
+            layout=control_layout
+        )
+        super().__init__(children=[self.map, self.control], **kwargs)
+    @property
+    def layers(self):
+        return self.map.layers
+    @layers.setter
+    def layers(self, value):
+        self.map.layers = value
+    @property
+    def config(self):
+        return self.map.config
+    @config.setter
+    def config(self, value):
+        self.map.config = value
