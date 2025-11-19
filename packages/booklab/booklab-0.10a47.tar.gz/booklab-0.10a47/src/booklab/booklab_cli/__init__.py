0161257bc0a4c0efc579c02363b9ef26ec1b2cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:25:39 +0000

    Add package booklab/0.10a47/booklab-0.10a47.tar.gz

---
+++ packages/booklab/booklab-0.10a47.tar.gz/booklab-0.10a47/src/booklab/booklab_cli/__init__.py
@@ -0,0 +1,38 @@
+"""
+Booklab CLI module to assure system operatins as command line.
+
+Usage:
+
+    booklab -v
+    booklab setup <options>
+    booklab init <options>
+    booklab server (run|stop|restart)
+    booklab catalog
+    booklab status <my-book>
+    booklab new
+    booklab build <my-book>
+    booklab preview <my-book>
+    booklab deploy <my-book>
+    booklab page-edit <my-book>
+    booklab page-upload <my-book>
+    booklab page-upload <my-book>
+    vooklab edit-nav[igation] <my-book>
+    booklab pack <my-book>
+
+Options:
+
+    -l, --list    List books catalog
+    -s, --status  Display detailes about a book
+    -v, --version Display booklab application version
+
+
+
+
+Architecture; Linux standard (POSIX) CLI
+
+Author; Petre Iordanescu (petre.iordanescu@gmail.com)
+"""
+
+
+
+
