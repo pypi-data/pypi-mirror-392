7be32931cad0cc8916af295593f942eca7e5855b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 12:05:41 +0000

    Add package flync-model/0.0.1/flync_model-0.0.1.tar.gz

---
+++ packages/flync-model/flync_model-0.0.1.tar.gz/flync_model-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flync-model",
+    version="0.0.1",
+    packages=find_packages(),
+    description="Model package for flync",
+    author="Technica Engineering",
+    license="MIT",
+)
