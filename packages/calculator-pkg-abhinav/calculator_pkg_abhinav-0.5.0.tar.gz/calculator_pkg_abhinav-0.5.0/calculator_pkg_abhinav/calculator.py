6edc22d92fd756816a6fac678cc2532ff4344ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 07:07:01 +0000

    Add package calculator-pkg-abhinav/0.5.0/calculator_pkg_abhinav-0.5.0.tar.gz

---
+++ packages/calculator-pkg-abhinav/calculator_pkg_abhinav-0.5.0.tar.gz/calculator_pkg_abhinav-0.5.0/calculator_pkg_abhinav/calculator.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def __init__(self):
+        print("hello!")
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
