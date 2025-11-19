7e9b27e61d6c426f780cad7603ab66f0d8b6608d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:45 +0000

    Add package rabbitmq-amqp-python-client/0.3.0/rabbitmq_amqp_python_client-0.3.0-py3-none-any.whl

---
+++ packages/rabbitmq-amqp-python-client/rabbitmq_amqp_python_client-0.3.0-py3-none-any.whl/rabbitmq_amqp_python_client/consumer.py
@@ -0,0 +1,171 @@
+import logging
+from typing import Literal, Optional, Union, cast
+
+from .amqp_consumer_handler import AMQPMessagingHandler
+from .entities import ConsumerOptions
+from .options import (
+    ReceiverOptionUnsettled,
+    ReceiverOptionUnsettledWithFilters,
+)
+from .qpid.proton._message import Message
+from .qpid.proton.utils import (
+    BlockingConnection,
+    BlockingReceiver,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class Consumer:
+    """
+    A consumer class for receiving messages from RabbitMQ via AMQP 1.0 protocol.
+
+    This class handles the consumption of messages from a specified address in RabbitMQ.
+    It supports both standard message consumption and stream-based consumption with
+    optional filtering capabilities.
+
+    Attributes:
+        _receiver (Optional[BlockingReceiver]): The receiver for consuming messages
+        _conn (BlockingConnection): The underlying connection to RabbitMQ
+        _addr (str): The address to consume from
+        _handler (Optional[MessagingHandler]): Optional message handling callback
+        _stream_options (Optional[StreamConsumerOptions]): Configuration for stream consumption
+        _credit (Optional[int]): Flow control credit value
+    """
+
+    def __init__(
+        self,
+        conn: BlockingConnection,
+        addr: str,
+        handler: Optional[AMQPMessagingHandler] = None,
+        stream_options: Optional[ConsumerOptions] = None,
+        credit: Optional[int] = None,
+    ):
+        """
+        Initialize a new Consumer instance.
+
+        Args:
+            conn: The blocking connection to use for consuming
+            addr: The address to consume from
+            handler: Optional message handler for processing received messages
+            stream_options: Optional configuration for stream-based consumption
+            credit: Optional credit value for flow control
+        """
+        self._receiver: Optional[BlockingReceiver] = None
+        self._conn = conn
+        self._addr = addr
+        self._handler = handler
+        self._stream_options = stream_options
+        self._credit = credit
+        self._consumers: list[Consumer] = []
+        self._open()
+
+    def _open(self) -> None:
+        if self._receiver is None:
+            logger.debug("Creating Receiver")
+            self._receiver = self._create_receiver(self._addr)
+
+    def _update_connection(self, conn: BlockingConnection) -> None:
+        self._conn = conn
+        if self._stream_options is None:
+            logger.debug("creating new receiver without stream")
+            self._receiver = self._conn.create_receiver(
+                self._addr,
+                options=ReceiverOptionUnsettled(self._addr),
+                handler=self._handler,
+            )
+        else:
+            logger.debug("creating new stream receiver")
+            self._stream_options.offset(self._handler.offset - 1)  # type: ignore
+            self._receiver = self._conn.create_receiver(
+                self._addr,
+                options=ReceiverOptionUnsettledWithFilters(
+                    self._addr, self._stream_options
+                ),
+                handler=self._handler,
+            )
+
+    def _set_consumers_list(self, consumers: []) -> None:  # type: ignore
+        self._consumers = consumers
+
+    def consume(self, timeout: Union[None, Literal[False], float] = False) -> Message:
+        """
+        Consume a message from the queue.
+
+        Args:
+            timeout: The time to wait for a message.
+                    None: Defaults to 60s
+                    float: Wait for specified number of seconds
+
+        Returns:
+            Message: The received message
+
+        Note:
+            The return type might be None if no message is available and timeout occurs,
+            but this is handled by the cast to Message.
+        """
+        if self._receiver is not None:
+            message = self._receiver.receive(timeout=timeout)
+            return cast(Message, message)
+
+    def close(self) -> None:
+        """
+        Close the consumer connection.
+
+        Closes the receiver if it exists and cleans up resources.
+        """
+        logger.debug("Closing the receiver")
+        if self._receiver is not None:
+            self._receiver.close()
+            if self in self._consumers:
+                self._consumers.remove(self)
+
+    def run(self) -> None:
+        """
+        Run the consumer in continuous mode.
+
+        Starts the consumer's container to process messages continuously.
+        """
+        logger.debug("Running the consumer: starting to consume")
+        if self._receiver is not None:
+            self._receiver.container.run()
+
+    def stop(self) -> None:
+        """
+        Stop the consumer's continuous processing.
+
+        Stops the consumer's container, halting message processing.
+        This should be called to cleanly stop a consumer that was started with run().
+        """
+        logger.debug("Stopping the consumer: starting to consume")
+        if self._receiver is not None:
+            self._receiver.container.stop_events()
+            self._receiver.container.stop()
+
+    def _create_receiver(self, addr: str) -> BlockingReceiver:
+        logger.debug("Creating the receiver")
+        if self._stream_options is None:
+            receiver = self._conn.create_receiver(
+                addr, options=ReceiverOptionUnsettled(addr), handler=self._handler
+            )
+
+        else:
+            receiver = self._conn.create_receiver(
+                addr,
+                options=ReceiverOptionUnsettledWithFilters(addr, self._stream_options),
+                handler=self._handler,
+            )
+
+        if self._credit is not None:
+            receiver.credit = self._credit
+
+        return receiver
+
+    @property
+    def address(self) -> str:
+        """Get the current publisher address."""
+        return self._addr
+
+    @property
+    def handler(self) -> Optional[AMQPMessagingHandler]:
+        return self._handler
---
+++ packages/rabbitmq-amqp-python-client/rabbitmq_amqp_python_client-0.3.0-py3-none-any.whl/rabbitmq_amqp_python_client/options.py
@@ -0,0 +1,84 @@
+from .entities import ConsumerOptions
+from .qpid.proton._data import (  # noqa: E402
+    PropertyDict,
+    symbol,
+)
+from .qpid.proton._endpoints import Link  # noqa: E402
+from .qpid.proton.reactor import (  # noqa: E402
+    Filter,
+    LinkOption,
+)
+
+
+class SenderOption(LinkOption):  # type: ignore
+    def __init__(self, addr: str):
+        self._addr = addr
+
+    def apply(self, link: Link) -> None:
+        link.source.address = self._addr
+        link.snd_settle_mode = Link.SND_SETTLED
+        link.rcv_settle_mode = Link.RCV_FIRST
+        link.properties = PropertyDict({symbol("paired"): True})
+        link.source.dynamic = False
+
+    def test(self, link: Link) -> bool:
+        return bool(link.is_sender)
+
+
+class SenderOptionUnseattle(LinkOption):  # type: ignore
+    def __init__(self, addr: str):
+        self._addr = addr
+
+    def apply(self, link: Link) -> None:
+        link.source.address = self._addr
+        link.snd_settle_mode = Link.SND_UNSETTLED
+        link.rcv_settle_mode = Link.RCV_FIRST
+        link.properties = PropertyDict({symbol("paired"): True})
+        link.source.dynamic = False
+
+    def test(self, link: Link) -> bool:
+        return bool(link.is_sender)
+
+
+class ReceiverOption(LinkOption):  # type: ignore
+    def __init__(self, addr: str):
+        self._addr = addr
+
+    def apply(self, link: Link) -> None:
+        link.target.address = self._addr
+        link.snd_settle_mode = Link.SND_SETTLED
+        link.rcv_settle_mode = Link.RCV_FIRST
+        link.properties = PropertyDict({symbol("paired"): True})
+        link.source.dynamic = False
+
+
+class ReceiverOptionUnsettled(LinkOption):  # type: ignore
+    def __init__(self, addr: str):
+        self._addr = addr
+
+    def apply(self, link: Link) -> None:
+        link.target.address = self._addr
+        link.snd_settle_mode = Link.SND_UNSETTLED
+        link.rcv_settle_mode = Link.RCV_FIRST
+        link.properties = PropertyDict({symbol("paired"): True})
+        link.source.dynamic = False
+
+    def test(self, link: Link) -> bool:
+        return bool(link.is_receiver)
+
+
+class ReceiverOptionUnsettledWithFilters(Filter):  # type: ignore
+    def __init__(self, addr: str, consumer_options: ConsumerOptions):
+        super().__init__(consumer_options.filter_set())
+        self._addr = addr
+
+    def apply(self, link: Link) -> None:
+        link.target.address = self._addr
+        link.snd_settle_mode = Link.SND_UNSETTLED
+        link.rcv_settle_mode = Link.RCV_FIRST
+        link.properties = PropertyDict({symbol("paired"): True})
+        link.source.dynamic = False
+        link.source.filter.put_dict(self.filter_set)
+
+    def test(self, link: Link) -> bool:
+        return bool(link.is_receiver)
