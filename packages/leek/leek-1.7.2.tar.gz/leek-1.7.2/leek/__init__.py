a83571b3d0b235736f7e1455ba3b4047ba73c2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 00:08:01 +0000

    Add package leek/1.7.2/leek-1.7.2.tar.gz

---
+++ packages/leek/leek-1.7.2.tar.gz/leek-1.7.2/leek/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 19/10/12 下午12:13
+# @Author  : cc
+# @FileName: __init__.py.py
+from .middleware_eum import *
+from .task_publisher import *
+from .task_consumer import *
+from .init_config_file import use_config_form_distributed_frame_config_module
+from multiprocessing import set_start_method
+import platform
+if platform.system() == 'Darwin' or platform.system() == 'Linux':
+    set_start_method("fork")
+
+use_config_form_distributed_frame_config_module()
