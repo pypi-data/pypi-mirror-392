80d7a2d02701ef53c2ab78ba76fd059010eb5683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:25:23 +0000

    Add package cityseer/4.22.2b5/cityseer-4.22.2b5-cp313-cp313-win_amd64.whl

---
+++ packages/cityseer/cityseer-4.22.2b5-cp313-cp313-win_amd64.whl/cityseer/config.py
@@ -0,0 +1,127 @@
+from __future__ import annotations
+
+import logging
+import os
+import threading
+import time
+from collections.abc import Callable
+from queue import Queue
+
+import numpy as np
+from tqdm import tqdm
+
+from . import rustalgos
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+np.seterr(invalid="ignore")
+
+
+def prep_gdf_key(key: str, dist: int, angular: bool = False, weighted: bool | None = None) -> str:
+    """Format a column label for GeoPandas."""
+    key = key.replace(".0", "")
+    key = key.replace(".0_", "_")
+    key = f"cc_{key}_{dist}"
+    if angular is True:
+        key += "_ang"
+    if weighted is True:
+        key += "_wt"
+    elif weighted is False:
+        key += "_nw"
+    return key
+
+
+def check_quiet() -> bool:
+    """Check whether to enable quiet mode."""
+    if "GCP_PROJECT" in os.environ:
+        return True
+    return "CITYSEER_QUIET_MODE" in os.environ and os.environ["CITYSEER_QUIET_MODE"].lower() in [
+        "true",
+        "1",
+    ]
+
+
+QUIET_MODE = check_quiet()
+
+
+def check_debug() -> bool:
+    """Check whether to enable debug mode."""
+    return "CITYSEER_DEBUG_MODE" in os.environ and os.environ["CITYSEER_DEBUG_MODE"].lower() in [
+        "true",
+        "1",
+    ]
+
+
+DEBUG_MODE: bool = check_debug()
+# for turning off validation
+SKIP_VALIDATION: bool = False
+# for calculating default betas vs. distances
+MIN_THRESH_WT: float = 0.01831563888873418
+SPEED_M_S = 1.33333
+# for all_close equality checks
+ATOL: float = 0.01
+RTOL: float = 0.0001
+
+
+def log_thresholds(
+    distances: list[int] | None = None,
+    betas: list[float] | None = None,
+    minutes: list[float] | None = None,
+    min_threshold_wt: float = MIN_THRESH_WT,
+    speed_m_s: float = SPEED_M_S,
+):
+    # pair distances, betas, and time for logging - DO AFTER PARTIAL FUNC
+    distances, betas, seconds = rustalgos.pair_distances_betas_time(
+        speed_m_s, distances, betas, minutes, min_threshold_wt=min_threshold_wt
+    )
+    # log distances, betas, minutes
+    logger.info("Metrics computed for:")
+    for distance, beta, walking_time in zip(distances, betas, seconds, strict=True):
+        logger.info(f"Distance: {distance}m, Beta: {round(beta, 5)}, Walking Time: {walking_time / 60} minutes.")
+    return distances
+
+
+RustResults = (
+    rustalgos.centrality.CentralityShortestResult
+    | rustalgos.centrality.CentralitySimplestResult
+    | rustalgos.centrality.CentralitySegmentResult
+    | rustalgos.data.AccessibilityResult
+    | rustalgos.data.MixedUsesResult
+    | rustalgos.data.StatsResult
+)
+
+
+def wrap_progress(
+    total: int,
+    rust_struct: rustalgos.graph.NetworkStructure | rustalgos.data.DataMap | rustalgos.viewshed.Viewshed,
+    partial_func: Callable,  # type: ignore
+) -> RustResults:
+    """Wraps long running parallelised rust functions with a progress counter."""
+
+    def wrapper(queue: Queue[RustResults | Exception]):
+        try:
+            result: RustResults = partial_func()  # type: ignore
+            queue.put(result)  # type: ignore
+        except Exception as e:
+            queue.put(e)
+
+    result_queue: Queue[RustResults | Exception] = Queue()
+    thread = threading.Thread(target=wrapper, args=(result_queue,))
+    pbar = tqdm(
+        total=total,
+        disable=QUIET_MODE,
+    )
+    thread.start()
+    while thread.is_alive():
+        time.sleep(1)
+        pbar.update(rust_struct.progress() - pbar.n)  # type: ignore
+    pbar.update(total - pbar.n)
+    pbar.close()
+    result = result_queue.get()
+    thread.join()
+
+    if isinstance(result, Exception):
+        raise result
+
+    return result
