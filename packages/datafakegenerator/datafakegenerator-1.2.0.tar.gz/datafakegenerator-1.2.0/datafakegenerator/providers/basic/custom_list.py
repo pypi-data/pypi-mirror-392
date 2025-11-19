e07dd353856f4b920a8828d31736ae5c65b60585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 08:06:11 +0000

    Add package datafakegenerator/1.2.0/datafakegenerator-1.2.0.tar.gz

---
+++ packages/datafakegenerator/datafakegenerator-1.2.0.tar.gz/datafakegenerator-1.2.0/datafakegenerator/providers/basic/custom_list.py
@@ -0,0 +1,10 @@
+from datafakegenerator.providers.base_provider import BaseProvider
+
+
+class CustomListProvider(BaseProvider):
+    def __init__(self, blank_percentage: float = 0.0, custom_format: str = '', **kwargs):
+        super().__init__(blank_percentage=blank_percentage, **kwargs)
+        self.custom_format = custom_format.split(',')
+
+    def generate_non_blank(self, row_data=None):
+        return self.get_random_data_by_list(self.custom_format)
