8503c5bca5aef702754616e3b3ae867536d811a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:25:23 +0000

    Add package pykx/4.0.0b4/pykx-4.0.0b4-cp310-cp310-win_amd64.whl

---
+++ packages/pykx/pykx-4.0.0b4-cp310-cp310-win_amd64.whl/pykx/license.py
@@ -0,0 +1,226 @@
+import base64
+import os
+import shutil
+from pathlib import Path
+from typing import Optional
+
+from . import licensed
+from .config import lic_type, qlic
+
+
+__all__ = [
+    'check',
+    'expires',
+    'install',
+]
+
+
+def __dir__():
+    return sorted(__all__)
+
+
+def _init(_q):
+    global q
+    q = _q
+
+
+def check(license: str,
+          *,
+          format: Optional[str] = 'FILE',
+          license_type: Optional[str] = lic_type
+) -> bool:
+    """
+    Validate the license key information you provided matches the license
+        saved to disk which is read by PyKX
+
+    Parameters:
+        license: If using "FILE" format this is the location of the file being used for comparison.
+            If "STRING" this is the base64 encoded string provided in your license email
+        format: Is the license check being completed using a downloaded file or base64
+            encoded string. Accepted inputs are "FILE"(default) or "STRING".
+        license_type: The license file type/name which is to be checked, by default this
+            is 'kc.lic' which is the version provided with personal and commercial
+            evaluation licenses but can be changed to 'k4.lic' or 'kx.lic' if appropriate
+
+    Returns:
+        A boolean indicating if the license is correct or not and a printed message describing
+            the issue
+
+    Examples:
+
+    Validate that a provided license matches an existing persisted license
+
+    ```python
+    >>> import pykx as kx
+    >>> kx.license.check('/usr/location/kc.lic')
+    True
+    ```
+
+    Attempt to check a new license against an existing installed license
+
+    ```python
+    >>> import pykx as kx
+    >>> check = kx.license.check('/usr/location/kc.lic')
+    Supplied license information does not match.
+    Please consider reinstalling your license using pykx.license.install
+
+    Installed license representation:
+    b'iIXSiEWzCNTkkCWK5Gggy..'
+    User expected license representation:
+    b'IyEvdXNyL2Jpbi9lbngDf..'
+    >>> check
+    False
+    ```
+
+    Attempt to check a license in the case no license is currently installed
+
+    ```python
+    >>> import pykx as kx
+    >>> check = kx.license.check('setup.py', license_type='kc.lic')
+    Unable to find an installed license: kc.lic at location: /usr/local/anaconda3/envs/qenv/q.
+    Please consider installing your license again using pykx.license.install
+    >>> check
+    False
+    ```
+    """
+    format = format.lower()
+    if format not in ('file', 'string'):
+        raise Exception('Unsupported option provided for format parameter')
+
+    if license_type not in ('kx.lic', 'kc.lic', 'k4.lic'):
+        raise Exception(f'License type {license_type} not supported.')
+
+    license_located = False
+    installed_lic = qlic/license_type
+    if os.path.exists(installed_lic):
+        license_located = True
+
+    if not license_located:
+        print(f'Unable to find an installed license: {license_type} at location: {str(qlic)}.\n'
+              'Please consider installing your license again using pykx.license.install')
+        return False
+
+    with open(installed_lic, 'rb') as f:
+        license_content = base64.encodebytes(f.read()).decode('utf-8')
+        license_content = license_content.replace('\n', '')
+        license_content = bytes(license_content, 'utf-8')
+
+    if format == 'file':
+        license_path = Path(os.path.expanduser(license))
+        if os.path.exists(license_path):
+            with open(str(license_path), 'rb') as f:
+                license = base64.encodebytes(f.read()).decode('utf-8')
+                license = license.replace('\n', '')
+        else:
+            print(f'Unable to locate license {license_path} for comparison')
+            return False
+
+    if isinstance(license, str):
+        license = license.replace('\n', '')
+        license = bytes(license, 'utf-8')
+
+    if license_content != license:
+        print('Supplied license information does not match.\n'
+              'Please consider reinstalling your license using pykx.license.install\n\n'
+              f'Installed license representation:\n{license_content}\n\n'
+              f'User expected license representation:\n{license}')
+        return False
+
+    return True
+
+
+def expires() -> int:
+    """
+    The number of days until the license is set to expire.
+
+    Returns:
+        The number of days until the license is set to expire
+
+    Example:
+
+    The number of days until your license expires:
+
+    ```python
+    >>> import pykx as kx
+    >>> kx.license.expires()
+    265
+    ```
+    """
+    if not licensed:
+        raise Exception('Unlicensed user, unable to determine license expiry')
+    return (q('"D"$', q.z.l[1]) - q.z.D).py()
+
+
+def install(license: str,
+            *,
+            format: Optional[str] = 'FILE',
+            license_type: Optional[str] = 'kc.lic',
+            force: Optional[bool] = False
+) -> bool:
+    """
+    (Re)install a KX license key, optionally overwriting the currently installed license.
+
+    Parameters:
+        license: If using "FILE" this is the location of the file being used for comparison.
+            If "STRING" this is the base64 encoded string provided in your license email
+        format: Is the license check being completed using a downloaded file or base64
+            encoded string. Accepted inputs are "FILE"(default) or "STRING".
+        license_type: The license file type/name which is to be checked, by default this
+            is 'kc.lic' which is the version provided with personal and commercial
+            evaluation licenses but can be changed to 'k4.lic' or 'kx.lic' if appropriate
+        force: Enforce overwrite without opt-in message for overwrite
+
+    Returns:
+        A boolean indicating if the license has been correctly overwritten
+
+    Examples:
+
+    Install a license using a supplied file location
+
+    ```python
+    >>> import pykx as kx
+    >>> kx.license.install('/path/to/license')
+    True
+    ```
+
+    Install a k4.lic base64 encoded string representation of the license
+
+    ```python
+    >>> import pykx as kx
+    >>> b64_string = 'IdyannfDangfa4FasdjD9fcda' # Example
+    >>> kx.license.install(b64_string, format = "STRING", license_type = "k4.lic")
+    True
+    ```
+    """
+    format = format.lower()
+    if format not in ('file', 'string'):
+        raise Exception('Unsupported option provided for format parameter')
+
+    license_located = False
+    installed_lic = qlic/license_type
+    if os.path.exists(installed_lic):
+        license_located = True
+
+    if license_located and not force:
+        raise Exception(f'Installed license: {license_type} at location: {str(qlic)} '
+                        'detected. to overwrite currently installed license use parameter '
+                        'force=True')
+
+    if format == 'file':
+        download_location = os.path.expanduser(Path(license))
+
+        if not os.path.exists(download_location):
+            raise Exception(f'Download location provided {download_location} does not exist.')
+
+        shutil.copy(download_location, qlic)
+    else:
+        try:
+            lic = base64.b64decode(license)
+        except base64.binascii.Error:
+            raise Exception('Invalid license copy provided, '
+                            'please ensure you have copied the license information correctly')
+
+        with open(qlic/license_type, 'wb') as binary_file:
+            binary_file.write(lic)
+    print("KDB-X Python license successfully installed!")
+    return True
---
+++ packages/pykx/pykx-4.0.0b4-cp310-cp310-win_amd64.whl/pykx/streamlit.py
@@ -0,0 +1,237 @@
+from warnings import warn
+
+from .config import pykx_threading, suppress_warnings, system
+from .exceptions import QError
+from .ipc import SyncQConnection
+
+
+# This class is required to ensure that in the absence
+# of the streamlit dependency KDB-X Python can be imported
+class _dummy_class(object):
+    def __getattr__(self, item):
+        return self
+
+    def __call__(self, *args, **kwargs):
+        return self
+
+
+try:
+    from streamlit.connections import BaseConnection
+    _streamlit_unavailable = False
+except ImportError:
+    # This base connection object is to ensure the streamlit
+    # class can be initialized correctly
+    BaseConnection = {SyncQConnection: _dummy_class}
+    _streamlit_unavailable = True
+
+
+def _check_streamlit():
+    if _streamlit_unavailable:
+        raise QError('Use of streamlit functionality requires access to '
+                     'of streamlit as a dependency, this can be installed '
+                     ' using:\n\npip install pykx[streamlit]')
+
+
+class PyKXConnection(BaseConnection[SyncQConnection]):
+    """
+    A connection to a q server from Streamlit. Initialise using:
+
+    ```python
+    st.connection("<name>", type = pykx.streamlit.PyKXConnection, *args)
+    ```
+
+    !!! Warning
+            Streamlit integration is not supported for Windows.
+            Full utilization requires `#!bash PYKX_THREADING` which is not supported on windows.
+
+    Parameters:
+        host: Server host name.
+        port: Server port number.
+        username: Username for q connection.
+        password: Password for q connection.
+        timeout: The number of seconds before a blocking operation times out. A value of 0 creates
+            a non-blocking connection.
+        large_messages: Boolean flag to enable/disable support for messages >2GB.
+        tls: Boolean flag to enable/disable TLS.
+        unix: Boolean flag to enable Unix domain socket connection. Host parameter is ignored if
+            `#!python True`. Does not work on Windows.
+        wait: Boolean to enable/disable waiting for the q server to complete executing the query
+            and return the result. `#!python False` emulates async queries, causing the q server
+            to respond immediately with the generic null `#!q ::` and perform calculations at
+            another time.
+        reconnection_attempts: The number of maximum attempts to reconnect when a connection is
+            lost. A negative number prevents any attempts to reconnect. A value of 0 will cause
+            continuous reconnect attempts until a connection is established. Positive values are
+            the number of times to attempt. Successive reconnect attempts are run at exponentially
+            increasing backoff times. Hitting the maximum number of limits with unsuccessful
+            attempts will throw an error.
+
+    Note: The `username` and `password` parameters are not required.
+        The `username` and `password` parameters are only required if the q server requires
+        authorization. Refer to [ssl documentation](https://code.kx.com/q/kb/ssl/) for more
+        information.
+
+    Note: The `timeout` argument may not always be enforced when making successive queries.
+        When making successive queries if one query times out the next query will wait until a
+        response has been received from the previous query before starting the timer for its own
+        timeout. This can be avoided by using a separate `SyncQConnection` instance for each
+        query.
+
+    Examples:
+
+    Open a streamlit connection to a locally running q process on port 5050.
+
+    ```python
+    >>> import streamlit as st
+    >>> import pykx as kx
+    >>> conn = st.connection('pykx', type=kx.streamlit.PyKXConnection,
+    ...                      host = 'localhost', port = 5050)
+    >>>
+    ```
+    """
+    _connection = None
+    _connection_kwargs = {}
+
+    def _connect(self, **kwargs) -> None:
+        _check_streamlit()
+        if system == 'Windows':
+            raise QError('Streamlit integration currently unsupported for Windows')
+        if (not pykx_threading) and (not suppress_warnings):
+            warn("Streamlit caching requires execution on secondary threads, "
+                 "to utilize this fully please consider setting PYKX_THREADING "
+                 "= 'True'. To suppress this warning please consider setting "
+                 "PYKX_SUPPRESS_WARNINGS = 'True'")
+        self._connection = SyncQConnection(no_ctx=True, **kwargs)
+        self._connection_kwargs = kwargs
+
+    def reset(self, **kwargs) -> None:
+        """
+        Close and reopen an existing Streamlit connection.
+
+        Example:
+
+        Open a connection to a locally running process on port 5050 and check if it is a healthy
+        connection. If it is not, reset the connection.
+
+        ```python
+        >>> import streamlit as st
+        >>> import pykx as kx
+        >>> conn = st.connection('pykx', type=kx.streamlit.PyKXConnection,
+        ...                      host = 'localhost', port = 5050)
+        >>> if not conn.is_healthy():
+        ...     conn.reset()
+        >>>
+        ```
+        """
+        _check_streamlit()
+        if not isinstance(self._connection, SyncQConnection):
+            raise QError('Unable to reset uninitialized connection')
+        self._connection.close()
+        self._connect(**self._connection_kwargs)
+
+    def is_healthy(self) -> bool:
+        """
+        Check if an existing streamlit connection is 'healthy' and available for query.
+
+        Returns:
+            A boolean indicating if the connection being used is in a 'healthy' state
+
+        Example:
+
+        ```python
+        >>> import streamlit as st
+        >>> import pykx as kx
+        >>> conn = st.connection('pykx', type=kx.streamlit.PyKXConnection,
+        ...                      host = 'localhost', port = 5050)
+        >>> conn.is_healthy()
+        True
+        ```
+        """
+        _check_streamlit()
+        if not isinstance(self._connection, SyncQConnection):
+            raise QError('Unable to validate uninitialized connection')
+        if self._connection.closed:
+            warn('Connection closed')
+            return False
+        try:
+            self.query('::')
+            return True
+        except BaseException as err:
+            warn('Unhealthy connection detected with error: ' + str(err))
+            return False
+
+    def query(self, query: str, *args, format='q', **kwargs):
+        """
+        Query the connected q process over IPC.
+
+        Parameters:
+            query: A q expression to be evaluated. This must be valid q, qSQL or SQL in the KX
+                Insights style.
+            *args: Arguments to the query. Each argument will be converted into a `#!python pykx.K`
+                object. Up to 8 arguments can be provided (maximum supported by q functions).
+            format: Description of query format for internal pre-processing before the query is sent
+                to the server. This must be one of 'q', 'qsql' or 'sql'.
+
+        Raises:
+            RuntimeError: A closed IPC connection was used.
+            QError: Query timed out, may be raised if the time taken to make or receive a query
+                goes over the timeout limit.
+            TypeError: Too many arguments were provided - q queries cannot have more than 8
+                parameters.
+            ValueError: Attempted to send a Python function over IPC.
+
+        Examples:
+
+        Open a connection to a locally running q process on port 5050 and query using 'q' format.
+
+        ```python
+        >>> import streamlit as st
+        >>> import pykx as kx
+        >>> conn = st.connection('pykx', type=kx.streamlit.PyKXConnection,
+        ...                      host = 'localhost', port = 5050)
+        >>> df = conn.query('select from tab').pd()
+        >>> st.dataframe(df)
+        ```
+
+        Open a connection to a locally running q process on port 5050 and query using 'qsql'
+        format.
+
+        ```python
+        >>> import streamlit as st
+        >>> import pykx as kx
+        >>> conn = st.connection('pykx', type=kx.streamlit.PyKXConnection,
+        ...                      host = 'localhost', port = 5050)
+        >>> df = conn.query('tab', where=kx.Column('x')>0.5, format='qsql').pd()
+        >>> st.dataframe(df)
+        ```
+
+        Connect to a locally running q process on port 5050 and query using 'sql'
+        format.
+
+        ```python
+        >>> import streamlit as st
+        >>> import pykx as kx
+        >>> conn = st.connection('pykx', type=kx.streamlit.PyKXConnection,
+        ...                      host = 'localhost', port = 5050)
+        >>> df = conn.query('select * from tab where x>0.5', format='sql').pd()
+        >>> st.dataframe(df)
+        ```
+        """
+        _check_streamlit()
+
+        def _query(query: str, format, args, kwargs):
+            if format == 'sql':
+                try:
+                    res = self._connection.sql(query, *args)
+                except QError as err:
+                    if '.s.sp' in str(err):
+                        raise QError('SQL functionality not loaded on connected server, error: ' + str(err)) # noqa: E501
+                    raise QError(err)
+                return res
+            elif format == 'q':
+                return self._connection(query, *args, **kwargs)
+            if format == 'qsql':
+                return self._connection.qsql.select(query, *args, **kwargs)
+            else:
+                raise QError("Unsupported format provided for query, must be one of 'q', 'qsql' or 'sql'") # noqa: E501
+        return _query(query, format, args, kwargs)
