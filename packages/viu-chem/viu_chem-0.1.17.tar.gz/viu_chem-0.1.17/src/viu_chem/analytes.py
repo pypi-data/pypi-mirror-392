6e9985773783ef63c9a13a254591113acc004357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 07:06:41 +0000

    Add package viu-chem/0.1.17/viu_chem-0.1.17.tar.gz

---
+++ packages/viu-chem/viu_chem-0.1.17.tar.gz/viu_chem-0.1.17/src/viu_chem/analytes.py
@@ -0,0 +1,20 @@
+
+
+analytes = {
+    "1-MNA":137.0709,
+    "Methionine":150.0583,
+    "Adenosine":256.24023,
+    "Kynurenine":209.09262,
+    "Arginine":175.1195,
+    "N1-methyl-2and4-pyridone-5-carboxamide":153.06640,
+    "SAM":399.1451,
+    "Glutathione":330.0736,
+    "5-Methylthioadenosine":298.09739,
+    "Spermine":203.2236,
+    "Spermidine":146.1657,
+    "Putrescine":89.1079,
+    "SAH": 385.1294,
+    "Homocysteine": 136.0432,
+    "Nicotinamide": 123.0552,
+    "Asparagine": 133.0613,
+}
