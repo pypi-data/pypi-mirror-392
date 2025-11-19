c84186f2cbb8d6612fb8083121bd68876a9e8c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 11:06:16 +0000

    Add package cardealerdp/0.3.2/cardealerdp-0.3.2.tar.gz

---
+++ packages/cardealerdp/cardealerdp-0.3.2.tar.gz/cardealerdp-0.3.2/README.md
@@ -0,0 +1,3 @@
+# Cardealer DP
+
+Cardealer DP (Car Dealer Data Package) is a data exchange format for car dealerships. It is developed on top of the Data Package standard.
---
+++ packages/cardealerdp/cardealerdp-0.3.2.tar.gz/cardealerdp-0.3.2/cardealerdp/profile.py
@@ -0,0 +1,56 @@
+# ruff: noqa -- DO NOT UPDATE this @generated file
+
+from __future__ import annotations
+
+from typing import Any, Literal, Mapping, Sequence, TypedDict, Union
+
+
+class CarResource(TypedDict):
+    name: Literal['car']
+    data: Sequence[Mapping[str, Any]]
+    """
+    Data items have to conform to the Car table schema
+    """
+    schema: Literal[
+        'https://raw.githubusercontent.com/datisthq/cardealerdp/v0.3.2/extension/schemas/car.json'
+    ]
+
+
+class DealerResource(TypedDict):
+    name: Literal['dealer']
+    data: Sequence[Mapping[str, Any]]
+    """
+    Data items have to conform to the Dealer table schema
+    """
+    schema: Literal[
+        'https://raw.githubusercontent.com/datisthq/cardealerdp/v0.3.2/extension/schemas/dealer.json'
+    ]
+
+
+class ShowroomResource(TypedDict):
+    name: Literal['showroom']
+    data: Sequence[Mapping[str, Any]]
+    """
+    Data items have to conform to the Showroom table schema
+    """
+    schema: Literal[
+        'https://raw.githubusercontent.com/datisthq/cardealerdp/v0.3.2/extension/schemas/showroom.json'
+    ]
+
+
+Resource = Union[CarResource, DealerResource, ShowroomResource]
+
+
+Package = TypedDict(
+    'Package',
+    {
+        '$schema': Literal[
+            'https://raw.githubusercontent.com/datisthq/cardealerdp/v0.3.2/extension/profile.json'
+        ],
+        'resources': Sequence[Resource],
+    },
+)
+
+
+class CarDealerDataPackageProfile(Package):
+    pass
