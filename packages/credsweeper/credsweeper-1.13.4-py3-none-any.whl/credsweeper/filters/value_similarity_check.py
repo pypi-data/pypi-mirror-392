1f07395e2716bd2fd105a755a84c776fcb19e9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:25:54 +0000

    Add package credsweeper/1.13.4/credsweeper-1.13.4-py3-none-any.whl

---
+++ packages/credsweeper/credsweeper-1.13.4-py3-none-any.whl/credsweeper/filters/value_similarity_check.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+from credsweeper.config.config import Config
+from credsweeper.credentials.line_data import LineData
+from credsweeper.file_handler.analysis_target import AnalysisTarget
+from credsweeper.filters.filter import Filter
+
+
+class ValueSimilarityCheck(Filter):
+    """Check if candidate value is at least 70% same as candidate keyword. Like: `secret = "mysecret"`."""
+
+    def __init__(self, config: Optional[Config] = None) -> None:
+        pass
+
+    def run(self, line_data: LineData, target: AnalysisTarget) -> bool:
+        """Run filter checks on received credential candidate data 'line_data'.
+
+        Args:
+            line_data: credential candidate data
+            target: multiline target from which line data was obtained
+
+        Return:
+            True, if need to filter candidate and False if left
+
+        """
+        # Cannot evaluate if key is None
+        if line_data.key is None:
+            return False
+        if line_data.key.lower() in line_data.value.lower() and \
+                len(line_data.key) / len(line_data.value) >= 0.7:
+            return True
+        if line_data.variable is not None and line_data.value in line_data.variable:
+            return True
+        return False
