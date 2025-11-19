fdf10f14b158d59f35341f189797dd3978ee05e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 06:05:50 +0000

    Add package sapiens-file-interpreter/1.0.2/sapiens_file_interpreter-1.0.2.tar.gz

---
+++ packages/sapiens-file-interpreter/sapiens_file_interpreter-1.0.2.tar.gz/sapiens_file_interpreter-1.0.2/setup.py
@@ -0,0 +1,17 @@
+# Module for managing infinite context window in language models, developed by the Sapiens Technology® team.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from setuptools import setup, find_packages
+package_name = 'sapiens_file_interpreter'
+version = '1.0.2'
+setup(
+    name=package_name,
+    version=version,
+    author='SAPIENS TECHNOLOGY',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['INFINITE-CONTEXT-WINDOW==2.0.5'],
+    url='https://github.com/sapiens-technology/sapiens_file_interpreter',
+    license='Proprietary Software'
+)
+# Module for managing infinite context window in language models, developed by the Sapiens Technology® team.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
