242291742714c68c0d6f4475a0a91c206ca6053f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 06:06:01 +0000

    Add package deepchopper-cli/1.2.8/deepchopper_cli-1.2.8.tar.gz

---
+++ packages/deepchopper-cli/deepchopper_cli-1.2.8.tar.gz/deepchopper_cli-1.2.8/deepchopper/__init__.py
@@ -0,0 +1,9 @@
+"""DeepChopper package."""
+
+from . import cli, data, eval, models, train, ui, utils
+from .deepchopper import *  # noqa: F403
+from .models import DeepChopper
+
+__version__ = "1.2.8"
+
+__all__ = ["DeepChopper", "__version__", "cli", "data", "eval", "models", "train", "ui", "utils"]
