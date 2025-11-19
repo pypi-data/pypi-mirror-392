5ddcbdabeecab0a0664ddc47eb33f7729f5c3fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:25:44 +0000

    Add package pygbm-yhuo/1.0.0.post2/pygbm_yhuo-1.0.0.post2.tar.gz

---
+++ packages/pygbm-yhuo/pygbm_yhuo-1.0.0.post2.tar.gz/pygbm_yhuo-1.0.0.post2/README.md
@@ -0,0 +1,2 @@
+# GBM
+A Python package for Geometric Brownian motion simulations. 
---
+++ packages/pygbm-yhuo/pygbm_yhuo-1.0.0.post2.tar.gz/pygbm_yhuo-1.0.0.post2/pygbm/base.py
@@ -0,0 +1,6 @@
+
+class GBM_setup:
+    def __init__(self, y0, mu, sigma):
+        self.y0 = y0 #initial value
+        self.mu = mu #drift
+        self.sigma = sigma #diffusion coefficient
