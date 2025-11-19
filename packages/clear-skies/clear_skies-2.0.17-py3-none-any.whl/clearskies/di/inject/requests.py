27739c94f2d604131eb86e304498c892009e62f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 00:07:25 +0000

    Add package clear-skies/2.0.17/clear_skies-2.0.17-py3-none-any.whl

---
+++ packages/clear-skies/clear_skies-2.0.17-py3-none-any.whl/clearskies/di/inject/requests.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import requests
+
+from clearskies.di.injectable import Injectable
+
+
+class Requests(Injectable):
+    def __init__(self, cache: bool = True):
+        self.cache = cache
+
+    def __get__(self, instance, parent) -> requests.Session:
+        if instance is None:
+            return self  # type: ignore
+        return self._di.build_from_name("requests", cache=self.cache)
