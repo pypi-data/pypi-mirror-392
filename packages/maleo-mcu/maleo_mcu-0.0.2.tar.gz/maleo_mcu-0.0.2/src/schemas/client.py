d6199e03ea4e4a42a512c56f72d0860af8fd64f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 07:07:10 +0000

    Add package maleo-mcu/0.0.2/maleo_mcu-0.0.2.tar.gz

---
+++ packages/maleo-mcu/maleo_mcu-0.0.2.tar.gz/maleo_mcu-0.0.2/src/schemas/client.py
@@ -0,0 +1,299 @@
+from pydantic import BaseModel, Field
+from typing import Annotated, Generic, Literal, Self, TypeVar, overload
+from uuid import UUID as PythonUUID
+from maleo.enums.status import (
+    DataStatus,
+    ListOfDataStatuses,
+    SimpleDataStatusMixin,
+    FULL_DATA_STATUSES,
+)
+from maleo.schemas.mixins.filter import ListOfDateFilters, convert as convert_filter
+from maleo.schemas.mixins.identity import (
+    UUID,
+    IdentifierMixin,
+    DataIdentifier,
+    UUIDOrganizationId,
+    Ids,
+    UUIDs,
+    Names,
+)
+from maleo.schemas.mixins.sort import ListOfSortColumns, convert as convert_sort
+from maleo.schemas.mixins.timestamp import DataTimestamp
+from maleo.schemas.operation.enums import ResourceOperationStatusUpdateType
+from maleo.schemas.pagination import Limit
+from maleo.schemas.parameter import (
+    ReadSingleParameter as BaseReadSingleParameter,
+    ReadPaginatedMultipleParameter,
+    StatusUpdateParameter as BaseStatusUpdateParameter,
+    DeleteSingleParameter as BaseDeleteSingleParameter,
+)
+from maleo.types.dict import StrToAnyDict
+from maleo.types.integer import OptListOfInts
+from maleo.types.string import OptListOfStrs, OptStr
+from maleo.types.uuid import OptUUID, OptListOfUUIDs
+from ..enums.client import IdentifierType
+from ..mixins.client import Name, ClientIdentifier
+from ..types.client import IdentifierValueType
+
+
+class CreateParameter(Name[str]):
+    pass
+
+
+class BaseInsertData(UUID[OptUUID]):
+    uuid: Annotated[OptUUID, Field(None, description="UUID")]
+    key: Annotated[str, Field(..., description="Key")]
+
+
+class InsertData(Name, BaseInsertData):
+    pass
+
+
+class ReadMultipleParameter(
+    ReadPaginatedMultipleParameter,
+    Names[OptListOfStrs],
+    UUIDs[OptListOfUUIDs],
+    Ids[OptListOfInts],
+):
+    ids: Annotated[OptListOfInts, Field(None, description="Ids")] = None
+    uuids: Annotated[OptListOfUUIDs, Field(None, description="UUIDs")] = None
+    names: Annotated[OptListOfStrs, Field(None, description="Names")] = None
+
+    @classmethod
+    def new(
+        cls,
+        ids: OptListOfInts = None,
+        uuids: OptListOfUUIDs = None,
+        names: OptListOfStrs = None,
+        date_filters: ListOfDateFilters = ListOfDateFilters(),
+        statuses: ListOfDataStatuses = FULL_DATA_STATUSES,
+        search: OptStr = None,
+        sort_columns: ListOfSortColumns = ListOfSortColumns(),
+        page: int = 1,
+        limit: Limit = Limit.LIM_10,
+        use_cache: bool = True,
+    ) -> Self:
+        return cls(
+            ids=ids,
+            uuids=uuids,
+            names=names,
+            date_filters=date_filters,
+            statuses=statuses,
+            search=search,
+            sort_columns=sort_columns,
+            page=page,
+            limit=limit,
+            use_cache=use_cache,
+        )
+
+    @property
+    def _query_param_fields(self) -> set[str]:
+        return {
+            "ids",
+            "uuids",
+            "statuses",
+            "keys",
+            "names",
+            "search",
+            "page",
+            "limit",
+            "use_cache",
+        }
+
+    def to_query_params(self) -> StrToAnyDict:
+        params = self.model_dump(
+            mode="json", include=self._query_param_fields, exclude_none=True
+        )
+        params["filters"] = convert_filter(self.date_filters)
+        params["sorts"] = convert_sort(self.sort_columns)
+        params = {k: v for k, v in params.items()}
+        return params
+
+
+class ReadSingleParameter(BaseReadSingleParameter[ClientIdentifier]):
+    @classmethod
+    def from_identifier(
+        cls,
+        identifier: ClientIdentifier,
+        statuses: ListOfDataStatuses = FULL_DATA_STATUSES,
+        use_cache: bool = True,
+    ) -> "ReadSingleParameter":
+        return cls(identifier=identifier, statuses=statuses, use_cache=use_cache)
+
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier_type: Literal[IdentifierType.ID],
+        identifier_value: int,
+        statuses: ListOfDataStatuses = FULL_DATA_STATUSES,
+        use_cache: bool = True,
+    ) -> "ReadSingleParameter": ...
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier_type: Literal[IdentifierType.UUID],
+        identifier_value: PythonUUID,
+        statuses: ListOfDataStatuses = FULL_DATA_STATUSES,
+        use_cache: bool = True,
+    ) -> "ReadSingleParameter": ...
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier_type: IdentifierType,
+        identifier_value: IdentifierValueType,
+        statuses: ListOfDataStatuses = FULL_DATA_STATUSES,
+        use_cache: bool = True,
+    ) -> "ReadSingleParameter": ...
+    @classmethod
+    def new(
+        cls,
+        identifier_type: IdentifierType,
+        identifier_value: IdentifierValueType,
+        statuses: ListOfDataStatuses = FULL_DATA_STATUSES,
+        use_cache: bool = True,
+    ) -> "ReadSingleParameter":
+        return cls(
+            identifier=ClientIdentifier(type=identifier_type, value=identifier_value),
+            statuses=statuses,
+            use_cache=use_cache,
+        )
+
+    def to_query_params(self) -> StrToAnyDict:
+        return self.model_dump(
+            mode="json", include={"statuses", "use_cache"}, exclude_none=True
+        )
+
+
+class FullUpdateData(Name[str]):
+    pass
+
+
+class PartialUpdateData(Name[OptStr]):
+    pass
+
+
+UpdateDataT = TypeVar("UpdateDataT", FullUpdateData, PartialUpdateData)
+
+
+class UpdateDataMixin(BaseModel, Generic[UpdateDataT]):
+    data: UpdateDataT = Field(..., description="Update data")
+
+
+class UpdateParameter(
+    UpdateDataMixin[UpdateDataT],
+    IdentifierMixin[ClientIdentifier],
+    Generic[UpdateDataT],
+):
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier_type: Literal[IdentifierType.ID],
+        identifier_value: int,
+        data: UpdateDataT,
+    ) -> "UpdateParameter": ...
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier_type: Literal[IdentifierType.UUID],
+        identifier_value: PythonUUID,
+        data: UpdateDataT,
+    ) -> "UpdateParameter": ...
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier_type: IdentifierType,
+        identifier_value: IdentifierValueType,
+        data: UpdateDataT,
+    ) -> "UpdateParameter": ...
+    @classmethod
+    def new(
+        cls,
+        identifier_type: IdentifierType,
+        identifier_value: IdentifierValueType,
+        data: UpdateDataT,
+    ) -> "UpdateParameter":
+        return cls(
+            identifier=ClientIdentifier(type=identifier_type, value=identifier_value),
+            data=data,
+        )
+
+
+class StatusUpdateParameter(
+    BaseStatusUpdateParameter[ClientIdentifier],
+):
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier_type: Literal[IdentifierType.ID],
+        identifier_value: int,
+        type: ResourceOperationStatusUpdateType,
+    ) -> "StatusUpdateParameter": ...
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier_type: Literal[IdentifierType.UUID],
+        identifier_value: PythonUUID,
+        type: ResourceOperationStatusUpdateType,
+    ) -> "StatusUpdateParameter": ...
+    @overload
+    @classmethod
+    def new(
+        cls,
+        identifier_type: IdentifierType,
+        identifier_value: IdentifierValueType,
+        type: ResourceOperationStatusUpdateType,
+    ) -> "StatusUpdateParameter": ...
+    @classmethod
+    def new(
+        cls,
+        identifier_type: IdentifierType,
+        identifier_value: IdentifierValueType,
+        type: ResourceOperationStatusUpdateType,
+    ) -> "StatusUpdateParameter":
+        return cls(
+            identifier=ClientIdentifier(type=identifier_type, value=identifier_value),
+            type=type,
+        )
+
+
+class DeleteSingleParameter(BaseDeleteSingleParameter[ClientIdentifier]):
+    @overload
+    @classmethod
+    def new(
+        cls, identifier_type: Literal[IdentifierType.ID], identifier_value: int
+    ) -> "DeleteSingleParameter": ...
+    @overload
+    @classmethod
+    def new(
+        cls, identifier_type: Literal[IdentifierType.UUID], identifier_value: PythonUUID
+    ) -> "DeleteSingleParameter": ...
+    @overload
+    @classmethod
+    def new(
+        cls, identifier_type: IdentifierType, identifier_value: IdentifierValueType
+    ) -> "DeleteSingleParameter": ...
+    @classmethod
+    def new(
+        cls, identifier_type: IdentifierType, identifier_value: IdentifierValueType
+    ) -> "DeleteSingleParameter":
+        return cls(
+            identifier=ClientIdentifier(type=identifier_type, value=identifier_value)
+        )
+
+
+class ClientSchema(
+    Name,
+    UUIDOrganizationId[PythonUUID],
+    SimpleDataStatusMixin[DataStatus],
+    DataTimestamp,
+    DataIdentifier,
+):
+    pass
