63a881dc2ce527444a5f69adf0e7dcbcf4f73074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:40 +0000

    Add package jupyter-rsession-proxy/2.4.0/jupyter_rsession_proxy-2.4.0.tar.gz

---
+++ packages/jupyter-rsession-proxy/jupyter_rsession_proxy-2.4.0.tar.gz/jupyter_rsession_proxy-2.4.0/README.md
@@ -0,0 +1,53 @@
+# jupyter-rsession-proxy
+
+[![TravisCI build status](https://img.shields.io/travis/com/jupyterhub/jupyter-rsession-proxy?logo=travis)](https://travis-ci.com/jupyterhub/jupyter-rsession-proxy)
+
+**jupyter-rsession-proxy** provides Jupyter server and notebook extensions to proxy RStudio.
+
+![Screenshot](screenshot.png)
+
+If you have a JupyterHub deployment, jupyter-rsession-proxy can take advantage of JupyterHub's existing authenticator and spawner to launch RStudio in users' Jupyter environments. You can also run this from within Jupyter.
+Note that [RStudio Server Pro](https://rstudio.com/products/rstudio-server-pro) has more featureful authentication and spawning than the standard version, in the event that you do not want to use Jupyter's.
+
+This extension used to proxy Shiny server as well, however that functionality has been [separated](https://github.com/ryanlovett/jupyter-shiny-proxy).
+
+## Installation
+
+### Pre-reqs
+
+#### Install rstudio
+Use conda `conda install rstudio` or [download](https://www.rstudio.com/products/rstudio/download-server/) the corresponding package for your platform
+
+Note that rstudio server is needed to work with this extension.
+
+### Install jupyter-rsession-proxy
+
+Install the library via `pip`:
+```
+pip install jupyter-rsession-proxy
+```
+Or via `conda`:
+```
+conda install -c conda-forge jupyter-rsession-proxy
+```
+
+## Example
+
+[rocker/binder](https://hub.docker.com/r/rocker/binder) contains an example installation which you can run on binder.
+
+[![Launch binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/rocker-org/binder/master?urlpath=rstudio)
+
+### Multiuser Considerations
+
+This extension launches an RStudio server process from the Jupyter notebook server. This is fine in JupyterHub deployments where user servers are containerized since other users cannot connect to the RStudio server port. In non-containerized JupyterHub deployments, for example on multiuser systems running LocalSpawner or BatchSpawner, this not secure. Any user may connect to Rstudio server and run arbitrary code.
+
+## Configuration with Environment Variables
+The following behavior can be configured with environment variables:
+
+| Environment Variable                      | Effect                                                                | Default Value       | Notes                                                                     |
+|-------------------------------------------|-----------------------------------------------------------------------|---------------------|---------------------------------------------------------------------------|
+| `JUPYTER_RSESSION_PROXY_USE_SOCKET`       | Whether to use unix socket                                            | `true`                | By default a unix socket is used. If set to case-insensitive `no` or `false` it will switch to using a TCP socket |
+| `JUPYTER_RSESSION_PROXY_WWW_FRAME_ORIGIN` | The value of the `www-frame-origin` flag to rserver                   | `same`              |                                                                           |
+| `RSERVER_TIMEOUT`                         | Idle timeout flag to rserver in minutes                               | `15`                | Must be numeric and positive                                              |
+| `RSESSION_TIMEOUT`                        | Idle timeout flag to rsession in minutes                              | `15`                | Must be numeric and positive                                              |
+| `NB_USER`                                 | Fallback name of the Notebook user, if password database lookup fails | `getuser.getpass()` |                                                                           |
---
+++ packages/jupyter-rsession-proxy/jupyter_rsession_proxy-2.4.0.tar.gz/jupyter_rsession_proxy-2.4.0/jupyter_rsession_proxy/__init__.py
@@ -0,0 +1,215 @@
+import getpass
+import os
+import shutil
+import subprocess
+import tempfile
+import pwd
+from textwrap import dedent
+from urllib.parse import urlparse, urlunparse, urljoin
+
+
+def get_rstudio_executable(prog):
+    # Find prog in known locations
+    other_paths = [
+        # When rstudio-server deb is installed
+        os.path.join('/usr/lib/rstudio-server/bin', prog),
+        # When just rstudio deb is installed
+        os.path.join('/usr/lib/rstudio/bin', prog),
+    ]
+    if shutil.which(prog):
+        return prog
+
+    for op in other_paths:
+        if os.path.exists(op):
+            return op
+
+    raise FileNotFoundError(f'Could not find {prog} in PATH')
+
+def get_icon_path():
+    return os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'icons', 'rstudio.svg'
+    )
+
+def rewrite_netloc(response, request):
+    '''
+       In some circumstances, rstudio-server appends a port to the URL while
+       setting Location in the header. We rewrite the response to use the host
+       in the request.
+    '''
+    for header, v in response.headers.get_all():
+        if header == "Location":
+            u = urlparse(v)
+            redirect_path = u.path
+            if u.path.startswith("../"):
+                # R Help server sometimes responds with relative locations which
+                # need to be handled if changing the host part of the header.
+                redirect_path = urljoin(request.path, u.path)
+            if u.netloc != request.host:
+                response.headers[header] = urlunparse(u._replace(netloc=request.host, path=redirect_path))
+
+def get_system_user():
+    try:
+        user = pwd.getpwuid(os.getuid())[0]
+    except:
+        user = os.getenv('NB_USER', getpass.getuser())
+    return(user)
+
+def setup_rserver():
+    def _get_env(port, unix_socket):
+        return dict(USER=get_system_user())
+
+    def db_config(db_dir):
+        '''
+        Create a temporary directory to hold rserver's database, and create
+        the configuration file rserver uses to find the database.
+
+        https://docs.rstudio.com/ide/server-pro/latest/database.html
+        https://github.com/rstudio/rstudio/tree/v1.4.1103/src/cpp/server/db
+        '''
+        # create the rserver database config
+        db_conf = dedent("""
+            provider=sqlite
+            directory={directory}
+        """).format(directory=db_dir)
+        f = tempfile.NamedTemporaryFile(mode='w', delete=False, dir=db_dir)
+        db_config_name = f.name
+        f.write(db_conf)
+        f.close()
+        return db_config_name
+
+    def _support_args(args):
+        ret = subprocess.check_output([get_rstudio_executable('rserver'), '--help'])
+        help_output = ret.decode()
+        return {arg: (help_output.find(f"--{arg}") != -1) for arg in args}
+
+    def _get_www_frame_origin(default="same"):
+        try:
+            return os.getenv('JUPYTER_RSESSION_PROXY_WWW_FRAME_ORIGIN', default)
+        except Exception:
+            return default
+
+    def _get_cmd(port, unix_socket):
+        ntf = tempfile.NamedTemporaryFile()
+
+        # use mkdtemp() so the directory and its contents don't vanish when
+        # we're out of scope
+        server_data_dir = tempfile.mkdtemp()
+        database_config_file = db_config(server_data_dir)
+
+        cmd = [
+            get_rstudio_executable('rserver'),
+            '--auth-none=1',
+            '--www-frame-origin=' + _get_www_frame_origin(),
+            '--www-verify-user-agent=0',
+            '--secure-cookie-key-file=' + ntf.name,
+            '--server-user=' + get_system_user(),
+        ]
+        # Support at least v1.2.1335 and up
+
+        supported_args = _support_args([
+            'www-root-path',
+            'server-data-dir',
+            'database-config-file',
+            'www-thread-pool-size',
+            'www-socket',
+        ])
+        if supported_args['www-root-path']:
+            cmd.append('--www-root-path={base_url}rstudio/')
+        if supported_args['server-data-dir']:
+            cmd.append(f'--server-data-dir={server_data_dir}')
+        if supported_args['database-config-file']:
+            cmd.append(f'--database-config-file={database_config_file}')
+
+        if supported_args['www-thread-pool-size']:
+            thread_pool_size_env = os.getenv('JUPYTER_RSESSION_PROXY_THREAD_POOL_SIZE', None)
+            try:
+                if thread_pool_size_env is not None:
+                    thread_pool_size = int(thread_pool_size_env)
+                    if thread_pool_size > 0:
+                        cmd.append('--www-thread-pool-size=' + str(thread_pool_size))
+            except ValueError:
+                print("Invalid value for JUPYTER_RSESSION_PROXY_THREAD_POOL_SIZE. Must be an integer.")
+                pass
+
+        if unix_socket and supported_args['www-socket']:
+            cmd.append('--www-socket={unix_socket}')
+        else:
+            cmd.append('--www-port={port}')
+
+        return cmd
+
+    def _get_timeout(default=15):
+        try:
+            return float(os.getenv('RSERVER_TIMEOUT', default))
+        except Exception:
+            return default
+
+    server_process = {
+        'command': _get_cmd,
+        'timeout': _get_timeout(),
+        'environment': _get_env,
+        'rewrite_response': rewrite_netloc,
+        'launcher_entry': {
+            'title': 'RStudio',
+            'icon_path': get_icon_path()
+        }
+    }
+
+    # The environment variable serves to explicitly disable Unix sockets, which are the default
+    use_unix_socket = True
+    disable_socket = os.getenv('JUPYTER_RSESSION_PROXY_USE_SOCKET')
+    if disable_socket is not None:
+        if disable_socket.casefold() in ('no', 'false'):
+            use_unix_socket = False
+
+    if use_unix_socket:
+        server_process['unix_socket'] = True
+
+    return server_process
+
+def setup_rsession():
+    def _get_env(port):
+        # Detect various environment variables rsession requires to run
+        # Via rstudio's src/cpp/core/r_util/REnvironmentPosix.cpp
+        cmd = ['R', '--slave', '--vanilla', '-e',
+                'cat(paste(R.home("home"),R.home("share"),R.home("include"),R.home("doc"),getRversion(),sep=":"))']
+
+        r_output = subprocess.check_output(cmd)
+        R_HOME, R_SHARE_DIR, R_INCLUDE_DIR, R_DOC_DIR, version = \
+            r_output.decode().split(':')
+
+        return {
+            'R_DOC_DIR': R_DOC_DIR,
+            'R_HOME': R_HOME,
+            'R_INCLUDE_DIR': R_INCLUDE_DIR,
+            'R_SHARE_DIR': R_SHARE_DIR,
+            'RSTUDIO_DEFAULT_R_VERSION_HOME': R_HOME,
+            'RSTUDIO_DEFAULT_R_VERSION': version,
+        }
+
+    def _get_cmd(port):
+        return [
+            get_rstudio_executable('rsession'),
+            '--standalone=1',
+            '--program-mode=server',
+            '--log-stderr=1',
+            '--session-timeout-minutes=0',
+            '--user-identity=' + get_system_user(),
+            '--www-port=' + str(port)
+        ]
+
+    def _get_timeout(default=15):
+        try:
+            return float(os.getenv('RSESSION_TIMEOUT', default))
+        except Exception:
+            return default
+
+    return {
+        'command': _get_cmd,
+        'timeout': _get_timeout(),
+        'environment': _get_env,
+        'launcher_entry': {
+            'title': 'RStudio',
+            'icon_path': get_icon_path()
+        }
+    }
