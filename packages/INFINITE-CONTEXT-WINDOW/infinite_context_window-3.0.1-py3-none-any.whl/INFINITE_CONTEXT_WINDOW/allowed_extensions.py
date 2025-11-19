b3353118bf814be40b048b727a6cc5a384c1054b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:26:25 +0000

    Add package INFINITE-CONTEXT-WINDOW/3.0.1/infinite_context_window-3.0.1-py3-none-any.whl

---
+++ packages/INFINITE-CONTEXT-WINDOW/infinite_context_window-3.0.1-py3-none-any.whl/INFINITE_CONTEXT_WINDOW/allowed_extensions.py
@@ -0,0 +1,19 @@
+# Module for managing infinite context window in language models, developed by the Sapiens Technology® team.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+class SapiensAllowedExtensions:
+	def get_allowed_extensions(self):
+		allowed_extensions = (
+			'.py', '.js', '.ts', '.java', '.c', '.cpp', '.h', '.cs', '.go', '.rb', '.php', '.rs', '.swift', '.kt', '.m', '.scala', '.lua', '.r', '.sql', '.sh',
+			'.bash', '.pl', '.clj', '.hs', '.erl', '.ex', '.exs', '.dart', '.vb', '.f90', '.fs', '.asm', '.html', '.htm', '.xhtml', '.css', '.scss', '.sass', '.less',
+			'.txt', '.pdf', '.docx', '.pptx', '.ppsx', '.pptm', '.xlsx', '.csv', '.webp', '.jpg', '.jpeg', '.png', '.gif', '.bmp', '.dng', '.mpo', '.tif', '.tiff', '.pfm',
+			'.mp3', '.wav', '.mpeg', '.m4a', '.aac', '.ogg', '.flac', '.aiff', '.wma', '.ac3', '.amr', '.mp4', '.avi', '.mkv', '.mov', '.webm', '.flv', '.3gp', '.wmv', '.ogv', '.md'
+		)
+		return allowed_extensions
+	def get_programming_extensions(self):
+		programming_extensions = (
+			'.py', '.js', '.ts', '.java', '.c', '.cpp', '.h', '.cs', '.go', '.rb', '.php', '.rs', '.swift', '.kt', '.m', '.scala', '.lua', '.r', '.sql', '.sh',
+			'.bash', '.pl', '.clj', '.hs', '.erl', '.ex', '.exs', '.dart', '.vb', '.f90', '.fs', '.asm', '.html', '.htm', '.xhtml', '.css', '.scss', '.sass', '.less'
+		)
+		return programming_extensions
+# Module for managing infinite context window in language models, developed by the Sapiens Technology® team.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
