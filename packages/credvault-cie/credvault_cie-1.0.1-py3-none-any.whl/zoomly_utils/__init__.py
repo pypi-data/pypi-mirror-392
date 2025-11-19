c2189876b2c73787721ed930d81c832bd96bf927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 12:06:17 +0000

    Add package credvault-cie/1.0.1/credvault_cie-1.0.1-py3-none-any.whl

---
+++ packages/credvault-cie/credvault_cie-1.0.1-py3-none-any.whl/zoomly_utils/__init__.py
@@ -0,0 +1,15 @@
+"""
+Zoomly Utilities - Python utilities and helpers for development
+"""
+
+__version__ = "1.0.0"
+__author__ = "Samuel Mbugua"
+__email__ = "mrsamuelmbugua908@gmail.com"
+
+__all__ = [
+    "get_version",
+]
+
+def get_version():
+    """Get the version of zoomly-utils"""
+    return __version__
