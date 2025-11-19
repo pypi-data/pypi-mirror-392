9f9d9aa3526ac3372e4362de937ca4c2c2d80d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 12:05:41 +0000

    Add package flync-core/0.0.1/flync_core-0.0.1.tar.gz

---
+++ packages/flync-core/flync_core-0.0.1.tar.gz/flync_core-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flync-core",
+    version="0.0.1",
+    packages=find_packages(),
+    description="Core package for flync",
+    author="Technica Engineering",
+    license="MIT",
+)
