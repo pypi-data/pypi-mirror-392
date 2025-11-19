0f263d048a601eab0c838bec847e558527b5d263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 05:07:03 +0000

    Add package cvcuda-cu12/0.16.0/cvcuda_cu12-0.16.0-py3-none-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/cvcuda-cu12/cvcuda_cu12-0.16.0-py3-none-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/cvcuda/__init__.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Import the C++ extension module directly
+# Python automatically selects the correct .so file based on ABI (SOABI suffix)
+# Imports from _cvcuda.cpython-3xx-....so
+
+# Explicitly export C API capsule (not included in 'import *' since it starts with _)
+from ._cvcuda import _C_API  # noqa: F401
+from ._cvcuda import *  # noqa: F401, F403
+from ._cvcuda import __version__  # noqa: F401
