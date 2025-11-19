5be7de59d8de7905fa6a73e54aeec9ac34f1bdad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 08:06:00 +0000

    Add package abstract-utilities/0.2.2.546/abstract_utilities-0.2.2.546.tar.gz

---
+++ packages/abstract-utilities/abstract_utilities-0.2.2.546.tar.gz/abstract_utilities-0.2.2.546/tests/test.py
@@ -0,0 +1,9 @@
+from imports.src.abstract_utilities.file_utils.src.file_filters import *
+
+file_path = "/home/flerb/Pictures/gts/gts2/73w"
+sysroot="/home/flerb/Downloads"
+
+###get_dot_fro_line(line,dirname=None,file_path=None,get_info=False)
+
+defaults = define_defaults()
+input(defaults)
