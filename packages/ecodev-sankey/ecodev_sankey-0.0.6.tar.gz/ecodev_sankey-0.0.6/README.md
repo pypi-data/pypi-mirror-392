3e231bcea1e43d691f265b3b7ca4fe4f9ecb48d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:56 +0000

    Add package ecodev-sankey/0.0.6/ecodev_sankey-0.0.6.tar.gz

---
+++ packages/ecodev-sankey/ecodev_sankey-0.0.6.tar.gz/ecodev_sankey-0.0.6/README.md
@@ -0,0 +1,4 @@
+
+# EcoDev Sankey
+
+**This is work in progress** This library easily allows to create (DataPoint) objects having associated (TreeNode) hierarchies, and manipulate these objects: filtering on them... The final aim is usually to show the filtered information in a Sankey diagram, hence the library name
---
+++ packages/ecodev-sankey/ecodev_sankey-0.0.6.tar.gz/ecodev_sankey-0.0.6/ecodev_sankey/constants.py
@@ -0,0 +1,16 @@
+"""
+Module listing all sankey related constants
+"""
+LABEL = 'label'
+NODE_ID = 'node-id'
+COLOR = 'color'
+ROOT = 'root'
+VALUE = 'value'
+NUM_DATAPOINTS = 'num_datapoints'
+NUM_CHILDREN = 'num-children'
+ID = 'id'
+DESCRIPTION = 'description'
+HIERARCHIES_SHEET = 'Hierarchies'
+HIERARCHY = 'hierarchy'
+DEPTH = 'depth'
+PROP = 'proportion'
