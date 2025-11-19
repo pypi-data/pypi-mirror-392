76b2d47a98e0eaa8c9a363fd585355eb72c3149d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 12:06:19 +0000

    Add package django-bulk-hooks/0.3.7/django_bulk_hooks-0.3.7-py3-none-any.whl

---
+++ packages/django-bulk-hooks/django_bulk_hooks-0.3.7-py3-none-any.whl/django_bulk_hooks/operations/value_prep.py
@@ -0,0 +1,4 @@
+from django_bulk_hooks.operations.field_utils import _get_field_or_none
+from django_bulk_hooks.operations.field_utils import get_field_value_for_db
+
+__all__ = ["get_field_value_for_db", "_get_field_or_none"]
