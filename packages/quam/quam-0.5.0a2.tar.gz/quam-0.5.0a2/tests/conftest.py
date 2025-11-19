5f866c62025765da606b9388ef591b4ebcefa561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 05:06:21 +0000

    Add package quam/0.5.0a2/quam-0.5.0a2.tar.gz

---
+++ packages/quam/quam-0.5.0a2.tar.gz/quam-0.5.0a2/tests/conftest.py
@@ -0,0 +1,34 @@
+from copy import deepcopy
+import pytest
+
+from quam.core import *
+
+
+@pytest.fixture
+def BareQuamRoot():
+    @quam_dataclass
+    class BareQuamRoot(QuamRoot): ...
+
+    return BareQuamRoot
+
+
+@pytest.fixture
+def BareQuamComponent():
+    @quam_dataclass
+    class BareQuamComponent(QuamComponent): ...
+
+    return BareQuamComponent
+
+
+@pytest.fixture(scope="function", autouse=True)
+def remove_quam_root():
+    from quam.core import QuamBase
+
+    QuamBase._last_instantiated_root = None
+
+
+@pytest.fixture
+def qua_config():
+    from quam.core.qua_config_template import qua_config_template
+
+    return deepcopy(qua_config_template)
