628f56785eb8fb6ed7e016d4f5c15945d09f6c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 12:05:41 +0000

    Add package flync-toolkit/0.0.1/flync_toolkit-0.0.1.tar.gz

---
+++ packages/flync-toolkit/flync_toolkit-0.0.1.tar.gz/flync_toolkit-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flync-toolkit",
+    version="0.0.1",
+    packages=find_packages(),
+    description="Toolkit package for flync",
+    author="Technica Engineering",
+    license="MIT",
+)
