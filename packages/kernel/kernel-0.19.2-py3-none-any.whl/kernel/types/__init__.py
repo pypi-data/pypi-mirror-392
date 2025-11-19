fbeb2dc64d017b6832070222ae2471bafad95855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 00:08:05 +0000

    Add package kernel/0.19.2/kernel-0.19.2-py3-none-any.whl

---
+++ packages/kernel/kernel-0.19.2-py3-none-any.whl/kernel/types/__init__.py
@@ -0,0 +1,53 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .shared import (
+    LogEvent as LogEvent,
+    AppAction as AppAction,
+    ErrorEvent as ErrorEvent,
+    ErrorModel as ErrorModel,
+    ErrorDetail as ErrorDetail,
+    HeartbeatEvent as HeartbeatEvent,
+)
+from .profile import Profile as Profile
+from .app_list_params import AppListParams as AppListParams
+from .app_list_response import AppListResponse as AppListResponse
+from .browser_persistence import BrowserPersistence as BrowserPersistence
+from .proxy_create_params import ProxyCreateParams as ProxyCreateParams
+from .proxy_list_response import ProxyListResponse as ProxyListResponse
+from .browser_create_params import BrowserCreateParams as BrowserCreateParams
+from .browser_delete_params import BrowserDeleteParams as BrowserDeleteParams
+from .browser_list_response import BrowserListResponse as BrowserListResponse
+from .profile_create_params import ProfileCreateParams as ProfileCreateParams
+from .profile_list_response import ProfileListResponse as ProfileListResponse
+from .proxy_create_response import ProxyCreateResponse as ProxyCreateResponse
+from .deployment_list_params import DeploymentListParams as DeploymentListParams
+from .deployment_state_event import DeploymentStateEvent as DeploymentStateEvent
+from .invocation_list_params import InvocationListParams as InvocationListParams
+from .invocation_state_event import InvocationStateEvent as InvocationStateEvent
+from .browser_create_response import BrowserCreateResponse as BrowserCreateResponse
+from .extension_list_response import ExtensionListResponse as ExtensionListResponse
+from .extension_upload_params import ExtensionUploadParams as ExtensionUploadParams
+from .proxy_retrieve_response import ProxyRetrieveResponse as ProxyRetrieveResponse
+from .deployment_create_params import DeploymentCreateParams as DeploymentCreateParams
+from .deployment_follow_params import DeploymentFollowParams as DeploymentFollowParams
+from .deployment_list_response import DeploymentListResponse as DeploymentListResponse
+from .invocation_create_params import InvocationCreateParams as InvocationCreateParams
+from .invocation_follow_params import InvocationFollowParams as InvocationFollowParams
+from .invocation_list_response import InvocationListResponse as InvocationListResponse
+from .invocation_update_params import InvocationUpdateParams as InvocationUpdateParams
+from .browser_persistence_param import BrowserPersistenceParam as BrowserPersistenceParam
+from .browser_retrieve_response import BrowserRetrieveResponse as BrowserRetrieveResponse
+from .extension_upload_response import ExtensionUploadResponse as ExtensionUploadResponse
+from .deployment_create_response import DeploymentCreateResponse as DeploymentCreateResponse
+from .deployment_follow_response import DeploymentFollowResponse as DeploymentFollowResponse
+from .invocation_create_response import InvocationCreateResponse as InvocationCreateResponse
+from .invocation_follow_response import InvocationFollowResponse as InvocationFollowResponse
+from .invocation_update_response import InvocationUpdateResponse as InvocationUpdateResponse
+from .deployment_retrieve_response import DeploymentRetrieveResponse as DeploymentRetrieveResponse
+from .invocation_retrieve_response import InvocationRetrieveResponse as InvocationRetrieveResponse
+from .browser_load_extensions_params import BrowserLoadExtensionsParams as BrowserLoadExtensionsParams
+from .extension_download_from_chrome_store_params import (
+    ExtensionDownloadFromChromeStoreParams as ExtensionDownloadFromChromeStoreParams,
+)
