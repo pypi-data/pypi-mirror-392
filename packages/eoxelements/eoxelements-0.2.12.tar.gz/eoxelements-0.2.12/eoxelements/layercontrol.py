87111e4486155c04410235fc4706aed3f8b06f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 04:07:15 +0000

    Add package eoxelements/0.2.12/eoxelements-0.2.12.tar.gz

---
+++ packages/eoxelements/eoxelements-0.2.12.tar.gz/eoxelements-0.2.12/eoxelements/layercontrol.py
@@ -0,0 +1,22 @@
+import anywidget
+import traitlets
+from ipywidgets import DOMWidget
+from ._version import __eox_layercontrol_version__
+
+layercontrol_esm = f"""
+import \"https://cdn.jsdelivr.net/npm/@eox/layercontrol@{__eox_layercontrol_version__}/dist/eox-layercontrol.js\";
+
+function render(view) {{
+  const control = document.createElement(\"eox-layercontrol\");
+  control.style.width = view.model.get(\"layout\").width;
+  control.style.height = view.model.get(\"layout\").height;
+  control.for = view.model.get(\"for_id\");
+  view.model.on(\"change:for_id\", () => {{ control.for = view.model.get(\"for_id\"); }});
+  view.el.appendChild(control);
+}}
+export default {{ render }}
+"""
+
+class EOxLayerControl(anywidget.AnyWidget, DOMWidget):
+    _esm = layercontrol_esm
+    for_id = traitlets.Unicode("").tag(sync=True)
