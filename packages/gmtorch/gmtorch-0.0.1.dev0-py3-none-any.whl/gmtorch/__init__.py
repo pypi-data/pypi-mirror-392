c2adae7b589333bd92e98d199257219b1664e1d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 00:08:10 +0000

    Add package gmtorch/0.0.1.dev0/gmtorch-0.0.1.dev0-py3-none-any.whl

---
+++ packages/gmtorch/gmtorch-0.0.1.dev0-py3-none-any.whl/gmtorch/__init__.py
@@ -0,0 +1,8 @@
+"""Gaussian Mixture."""
+
+# Authors: Ali Darijani
+# License: GNU General Public License, version 2 (GPL-2.0)
+
+from gmtorch._gaussian_mixture import GaussianMixture
+
+__all__ = ["GaussianMixture"]
