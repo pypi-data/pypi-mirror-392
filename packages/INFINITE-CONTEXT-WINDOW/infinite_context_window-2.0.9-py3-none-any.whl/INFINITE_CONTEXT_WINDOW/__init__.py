eec4d4abbcbdf372288ad49b9fc669762e53d896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:26:23 +0000

    Add package INFINITE-CONTEXT-WINDOW/2.0.9/infinite_context_window-2.0.9-py3-none-any.whl

---
+++ packages/INFINITE-CONTEXT-WINDOW/infinite_context_window-2.0.9-py3-none-any.whl/INFINITE_CONTEXT_WINDOW/__init__.py
@@ -0,0 +1,5 @@
+# Module for managing infinite context window in language models, developed by the Sapiens Technology® team.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from .INFINITE_CONTEXT_WINDOW import *
+# Module for managing infinite context window in language models, developed by the Sapiens Technology® team.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
