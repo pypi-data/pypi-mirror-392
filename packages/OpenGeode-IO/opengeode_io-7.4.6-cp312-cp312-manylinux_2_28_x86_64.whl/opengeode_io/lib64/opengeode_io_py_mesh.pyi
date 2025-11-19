e01ebc081e9f18210e234eb483213ac2a2444af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:16 +0000

    Add package OpenGeode-IO/7.4.6/opengeode_io-7.4.6-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/OpenGeode-IO/opengeode_io-7.4.6-cp312-cp312-manylinux_2_28_x86_64.whl/opengeode_io/lib64/opengeode_io_py_mesh.pyi
@@ -0,0 +1,9 @@
+"""
+OpenGeode-IO Python binding for mesh
+"""
+from __future__ import annotations
+__all__: list[str] = ['IOMeshLibrary']
+class IOMeshLibrary:
+    @staticmethod
+    def initialize() -> None:
+        ...
