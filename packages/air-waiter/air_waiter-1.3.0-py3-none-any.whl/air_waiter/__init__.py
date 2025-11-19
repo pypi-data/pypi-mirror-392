7c6aa93d7750ddcc40445f6430a04cbd9dc3288b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 08:06:12 +0000

    Add package air-waiter/1.3.0/air_waiter-1.3.0-py3-none-any.whl

---
+++ packages/air-waiter/air_waiter-1.3.0-py3-none-any.whl/air_waiter/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from .wait import UnlimitedWaiterError, UnusedMaxIntervalError, Wait, WaiterTimeoutError
+
+__all__ = [
+    "UnlimitedWaiterError",
+    "UnusedMaxIntervalError",
+    "Wait",
+    "WaiterTimeoutError",
+]
