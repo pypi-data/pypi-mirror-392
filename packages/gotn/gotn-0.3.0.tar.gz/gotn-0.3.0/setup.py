4edf20982dab8833c454bf644891452d9374e34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 04:07:16 +0000

    Add package gotn/0.3.0/gotn-0.3.0.tar.gz

---
+++ packages/gotn/gotn-0.3.0.tar.gz/gotn-0.3.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="gotn",
+    version="0.3.0",
+    packages=find_packages(),
+     install_requires=[
+        "requests",
+        "argparse"
+     ],
+    entry_points={
+        "console_scripts": [
+            "gotn=gotn.cli:main",
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
