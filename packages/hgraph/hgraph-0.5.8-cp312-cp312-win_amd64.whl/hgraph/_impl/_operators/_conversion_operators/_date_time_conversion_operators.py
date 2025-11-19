b5c98601a77e4c66b44cb8f608b1b32f666df973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 12:06:07 +0000

    Add package hgraph/0.5.8/hgraph-0.5.8-cp312-cp312-win_amd64.whl

---
+++ packages/hgraph/hgraph-0.5.8-cp312-cp312-win_amd64.whl/hgraph/_impl/_operators/_conversion_operators/_date_time_conversion_operators.py
@@ -0,0 +1,56 @@
+from datetime import date, timedelta, datetime
+
+from hgraph import compute_node, TS, OUT, WiringPort, combine, convert
+
+
+@compute_node(overloads=convert, requires=lambda m, s: m[OUT].py_type == TS[date])
+def convert_str_to_date(ts: TS[str]) -> TS[date]:
+    return date.fromisoformat(ts.value)
+
+
+@compute_node(overloads=convert, requires=lambda m, s: m[OUT].py_type == TS[datetime])
+def convert_str_to_datetime(ts: TS[str]) -> TS[datetime]:
+    return datetime.fromisoformat(ts.value)
+
+
+@compute_node(overloads=convert, requires=lambda m, s: m[OUT].py_type == TS[datetime])
+def convert_date_to_datetime(ts: TS[date]) -> TS[datetime]:
+    return datetime(ts.value.year, ts.value.month, ts.value.day)
+
+
+@compute_node(overloads=convert, requires=lambda m, s: m[OUT].py_type == TS[date])
+def convert_datetime_to_date(ts: TS[datetime]) -> TS[date]:
+    v: datetime = ts.value
+    return v.date()
+
+
+@compute_node(overloads=combine, requires=lambda m, s: m[OUT].py_type == TS[date])
+def combine_date(year: TS[int], month: TS[int], day: TS[int]) -> TS[date]:
+    return date(year.value, month.value, day.value)
+
+
+@compute_node(
+    overloads=combine,
+    requires=lambda m, s: m[OUT].py_type == TS[timedelta],
+    valid=lambda m, s: (k for k, v in s.items() if isinstance(v, WiringPort)) if s["__strict__"] else (),
+)
+def combine_timedelta(
+    *,
+    days: TS[int] = None,
+    seconds: TS[int] = None,
+    microseconds: TS[int] = None,
+    milliseconds: TS[int] = None,
+    minutes: TS[int] = None,
+    hours: TS[int] = None,
+    weeks: TS[int] = None,
+    __strict__: bool = True,
+) -> TS[timedelta]:
+    return timedelta(
+        days=days.value if days.valid else 0,
+        seconds=seconds.value if seconds.valid else 0,
+        microseconds=microseconds.value if microseconds.valid else 0,
+        milliseconds=milliseconds.value if milliseconds.valid else 0,
+        minutes=minutes.value if minutes.valid else 0,
+        hours=hours.value if hours.valid else 0,
+        weeks=weeks.value if weeks.valid else 0,
+    )
---
+++ packages/hgraph/hgraph-0.5.8-cp312-cp312-win_amd64.whl/hgraph/debug/_inspector_state.py
@@ -0,0 +1,48 @@
+import os
+from collections import defaultdict
+from dataclasses import dataclass, field
+from datetime import datetime
+
+import psutil
+from perspective import Table
+
+from hgraph import CompoundScalar
+from hgraph._impl._runtime._node import _SenderReceiverState
+from hgraph.adaptors.perspective import PerspectiveTablesManager
+from hgraph.debug._inspector_observer import InspectionObserver
+
+
+@dataclass
+class InspectorState(CompoundScalar):
+    observer: InspectionObserver = None
+    manager: PerspectiveTablesManager = None
+    table: Table = None
+    total_cycle_table: Table = None
+
+    process: psutil.Process = field(default_factory=lambda: psutil.Process(os.getpid()))
+
+    requests: _SenderReceiverState = field(default_factory=_SenderReceiverState)
+    last_request_process_time: datetime = None
+
+    graph_subscriptions: dict = field(default_factory=lambda: dict())  # graph_id -> item_id
+    node_subscriptions: dict = field(default_factory=lambda: defaultdict(set))  # node_id -> item_id
+    node_item_subscriptions: dict = field(default_factory=lambda: defaultdict(set))  # node_id -> {item_ids}
+    found_items: set = field(default_factory=set)
+
+    value_removals: set = field(default_factory=set)
+    tick_node_ids = set()
+
+    value_data: dict = field(default_factory=dict)
+    tick_data: dict = field(default_factory=dict)
+    perf_data: dict = field(default_factory=dict)
+    
+    track_detailed_performance: bool = True
+    detailed_perf_data: dict = field(default_factory=lambda: defaultdict(list))
+    detailed_perf_data_time: datetime = datetime.utcnow()
+    detailed_perf_data_node_times = dict()
+    detailed_perf_data_graph_times = dict()
+    
+    total_data_prev: list = field(default_factory=dict)
+    total_data: list = field(default_factory=lambda: defaultdict(list))
+    last_publish_time: datetime = None
+    inspector_time: float = 0.0
