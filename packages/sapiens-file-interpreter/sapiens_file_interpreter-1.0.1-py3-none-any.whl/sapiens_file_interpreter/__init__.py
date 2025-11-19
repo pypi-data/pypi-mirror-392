39bdf23aa594d99ff0cb3183f01729340c66f433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 05:07:45 +0000

    Add package sapiens-file-interpreter/1.0.1/sapiens_file_interpreter-1.0.1-py3-none-any.whl

---
+++ packages/sapiens-file-interpreter/sapiens_file_interpreter-1.0.1-py3-none-any.whl/sapiens_file_interpreter/__init__.py
@@ -0,0 +1,5 @@
+# Module specialized in reading and interpreting local or internet files, web pages, YouTube videos, zip packages, tar.gz packages, or local directories.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from .sapiens_file_interpreter import *
+# Module specialized in reading and interpreting local or internet files, web pages, YouTube videos, zip packages, tar.gz packages, or local directories.
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
