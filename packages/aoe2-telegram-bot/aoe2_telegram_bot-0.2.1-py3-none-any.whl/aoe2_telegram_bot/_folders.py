f20542f488d7e02e12208b5d2936f284ca8d1e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 00:07:36 +0000

    Add package aoe2-telegram-bot/0.2.1/aoe2_telegram_bot-0.2.1-py3-none-any.whl

---
+++ packages/aoe2-telegram-bot/aoe2_telegram_bot-0.2.1-py3-none-any.whl/aoe2_telegram_bot/_folders.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+current_module_folder = Path(__file__).parent
+images_folder = current_module_folder / "images"
+audio_caption = images_folder / "aok-ico.png"
+audio_folder = current_module_folder / "audio"
+bootstrap_file = audio_folder / "installation_complete"
+
+audio_url = "https://media.githubusercontent.com/media/PercevalSA/aoe2-bot/main/audio/"
+audio_archives = [
+    "civilization.zip",
+    "sound.zip",
+    "taunt.zip",
+]
+
+# systemd
+service_file = current_module_folder / "distro" / "aoe2-telegram-bot.service"
+systemd_folder = Path("/etc/systemd/system/")
+config_folder = Path.home() / ".config/aoe2-telegram-bot"
+env_file = config_folder / "env"
