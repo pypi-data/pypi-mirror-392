d420a2e02a09b78cdc6ffefd1f46dc0ffe6b5a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 12:05:54 +0000

    Add package pytorch-lightning-enterprise/0.0.1.dev8/pytorch_lightning_enterprise-0.0.1dev8-py314-none-any.whl

---
+++ packages/pytorch-lightning-enterprise/pytorch_lightning_enterprise-0.0.1dev8-py314-none-any.whl/pytorch_lightning_enterprise/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2025 Lightning AI, Inc.
+# Licensed under the Lightning.ai Enterprise Add-on EULA (see LICENSE file).
+# Contact: support@lightning.ai for commercial licensing.
+
+__version__ = "0.0.1dev8"
