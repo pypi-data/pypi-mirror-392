eb998aabb18019a61c7edfb1b0e762f37e47fabe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 11:06:54 +0000

    Add package langmiddle/0.1.5b2/langmiddle-0.1.5b2.tar.gz

---
+++ packages/langmiddle/langmiddle-0.1.5b2.tar.gz/langmiddle-0.1.5b2/langmiddle/__init__.py
@@ -0,0 +1,10 @@
+"""Middlewares for LangChain / LangGraph
+
+This package provides middleware components for LangChain and LangGraph applications,
+enabling enhanced functionality and streamlined development workflows.
+"""
+
+__version__ = "0.1.5b2"
+__author__ = "Alpha x1"
+__email__ = "alpha.xone@outlook.com"
+__x__ = "alpha_xone_"
