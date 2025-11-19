6c1a2d1de6f32d0d01d43ac16497e2a03db1c7bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:28 +0000

    Add package aliyun-akless-idp-python-sdk/0.0.0/aliyun_akless_idp_python_sdk-0.0.0.tar.gz

---
+++ packages/aliyun-akless-idp-python-sdk/aliyun_akless_idp_python_sdk-0.0.0.tar.gz/aliyun_akless_idp_python_sdk-0.0.0/setup.py
@@ -0,0 +1,21 @@
+import os
+from distutils.core import setup
+setup(
+  name='aliyun-akless-idp-python-sdk',         # How you named your package folder (MyLib)
+  packages=['aliyun-akless-idp-python-sdk'],   # Chose the same as "name"
+  version='0.0.0',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description='aliyun-akless-idp-python-sdk',   # Give a short description about your library
+  author='',                   # Type in your name
+  author_email='',      # Type in your E-Mail
+  url='',   # Provide either the link to your github or to your website
+  download_url='',    # I explain this later on
+  keywords=['aliyun-akless-idp-python-sdk'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+      ],
+  classifiers=[
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 3',
+  ],
+)
