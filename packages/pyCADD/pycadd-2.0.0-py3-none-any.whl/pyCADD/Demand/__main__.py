9657334124ea495ba4f466a6448b2fe4060e31ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 08:06:02 +0000

    Add package pyCADD/2.0.0/pycadd-2.0.0-py3-none-any.whl

---
+++ packages/pyCADD/pycadd-2.0.0-py3-none-any.whl/pyCADD/Demand/__main__.py
@@ -0,0 +1,4 @@
+from pyCADD.Demand.cli import main
+
+if __name__ == '__main__':
+    main()
---
+++ packages/pyCADD/pycadd-2.0.0-py3-none-any.whl/pyCADD/__init__.py
@@ -0,0 +1,7 @@
+__version__= '2.0.0'
+__update_date__ = '2025-10-28'
+__author__ = 'YH. W'
+__institution__ = 'Xiamen University, China'
+
+from pyCADD.utils.log import _init_log
+_init_log('pyCADD')
