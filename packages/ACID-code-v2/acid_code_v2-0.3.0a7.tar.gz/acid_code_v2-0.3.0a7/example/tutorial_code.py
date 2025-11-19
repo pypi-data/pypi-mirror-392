32f91c24c2210e22ac080f2f69d8fcaa8810fe46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 04:07:42 +0000

    Add package ACID-code-v2/0.3.0a7/acid_code_v2-0.3.0a7.tar.gz

---
+++ packages/ACID-code-v2/acid_code_v2-0.3.0a7.tar.gz/acid_code_v2-0.3.0a7/example/tutorial_code.py
@@ -0,0 +1,44 @@
+#%%
+from astropy.io import fits
+import importlib, os, sys
+import numpy as np
+import matplotlib.pyplot as plt
+os.chdir(os.path.dirname(__file__))
+os.chdir("..")  # ensures we are in the main directory
+try:
+    import ACID_code_v2 as acid
+except:
+    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+    PROJECT_ROOT = os.path.dirname(SCRIPT_DIR)
+    sys.path.append(PROJECT_ROOT)
+    from src import ACID_code_v2 as acid
+    print("pip module failed to import, imported from local instead")
+acid._reload_all()
+
+spec_file = fits.open('example/sample_spec_1.fits')
+
+wavelength = spec_file[0].data[::5]   # Wavelengths in Angstroms
+spectrum = spec_file[1].data[::5]     # Spectral Flux
+error = spec_file[2].data[::5]        # Spectral Flux Errors
+sn = spec_file[3].data[::5]           # SN of Spectrum
+
+linelist = 'example/example_linelist.txt' # Insert path to line list
+
+# choose a velocity grid for the final profile(s)
+deltav = 0.82   # velocity pixel size must not be smaller than the spectral pixel size
+velocities = np.arange(-25, 25, deltav)
+
+# run ACID function
+result = acid.run_ACID([wavelength], [spectrum], [error], [sn], linelist, velocities,
+                   parallel=True, cores=None, nsteps=4000, verbose=True)
+
+# extract profile and errors
+profile = result[0, 0, 0]
+profile_error = result[0, 0, 1]
+
+# plot results
+plt.figure()
+plt.errorbar(velocities, profile, profile_error)
+plt.xlabel('Velocities (km/s)')
+plt.ylabel('Flux')
+plt.show()
---
+++ packages/ACID-code-v2/acid_code_v2-0.3.0a7.tar.gz/acid_code_v2-0.3.0a7/tests/tests.py
@@ -0,0 +1,46 @@
+#%%
+from astropy.io import fits
+import os, glob, importlib, sys
+import numpy as np
+import matplotlib.pyplot as plt
+os.chdir(os.path.dirname(__file__))
+os.chdir("..")  # ensures we are in the main directory
+try:
+    import ACID_code_v2 as acid
+except:
+    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+    PROJECT_ROOT = os.path.dirname(SCRIPT_DIR)
+    sys.path.append(PROJECT_ROOT)
+    from src import ACID_code_v2 as acid
+    print("pip module failed to import, imported from local instead")
+acid._reload_all()
+
+def test_run_e2ds():
+
+    e2ds_files = glob.glob('tests/data/*e2ds_A*.fits')
+    linelist = 'example/example_linelist.txt'
+    save_path = 'no save'
+
+    velocities = np.arange(-25, 25, 0.82)
+
+    # run ACID on e2ds files
+    ACID_results_e2ds = acid.run_ACID_HARPS(e2ds_files, linelist, velocities=velocities, save_path=save_path,
+                                            order_range=np.arange(41, 43), nsteps=2000)
+    return ACID_results_e2ds
+
+def test_run_s1d():
+
+    s1d_files = glob.glob('tests/data/*s1d_A*.fits')
+    linelist = 'example/example_linelist.txt'
+    save_path = 'no save'
+
+    velocities = np.arange(-25, 25, 0.82)
+
+    # run ACID on s1d files
+    ACID_results_s1d = acid.run_ACID_HARPS(s1d_files, linelist, velocities=velocities, save_path=save_path,
+                                       order_range = np.arange(41, 43), file_type = 's1d', nsteps=2000)
+    return ACID_results_s1d
+
+res_e2ds = test_run_e2ds()
+res_s1d = test_run_s1d()
+print("All tests passed!")
