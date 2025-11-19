58f2c9d3f7e5641c341c792560a62aed88d5fac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 00:07:46 +0000

    Add package vcsp/0.0.1/vcsp-0.0.1-py3-none-any.whl

---
+++ packages/vcsp/vcsp-0.0.1-py3-none-any.whl/settings.py
@@ -0,0 +1,11 @@
+from colorama import init, Fore, Back, Style
+
+#Foreground colour
+green = Fore.GREEN 
+black = Fore.BLACK
+red = Fore.RED
+yellow = Fore.YELLOW
+blue = Fore.BLUE
+magenta = Fore.MAGENTA
+cyan = Fore.CYAN
+white = Fore.WHITE
