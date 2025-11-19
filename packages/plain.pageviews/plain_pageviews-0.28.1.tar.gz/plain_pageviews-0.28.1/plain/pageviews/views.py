d9fae1d4568765a6dffb1a73d804fffd30e5afea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 06:06:10 +0000

    Add package plain.pageviews/0.28.1/plain_pageviews-0.28.1.tar.gz

---
+++ packages/plain.pageviews/plain_pageviews-0.28.1.tar.gz/plain_pageviews-0.28.1/plain/pageviews/views.py
@@ -0,0 +1,29 @@
+import json
+
+from plain.views import View
+
+from .models import Pageview
+
+
+class TrackView(View):
+    def post(self) -> int:
+        try:
+            data = self.request.data
+        except json.JSONDecodeError:
+            return 400
+
+        try:
+            pageview = Pageview.create_from_request(
+                self.request,
+                url=data["url"],
+                title=data["title"],
+                referrer=data["referrer"],
+                timestamp=data["timestamp"],
+            )
+        except KeyError:
+            return 400
+
+        if pageview is None:
+            return 200
+
+        return 201
