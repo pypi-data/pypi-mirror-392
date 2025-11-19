367f53b1b6cfc7247c40277a10313d249380b158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 03:25:23 +0000

    Add package swaggyjenkins/3.2.0/swaggyjenkins-3.2.0.tar.gz

---
+++ packages/swaggyjenkins/swaggyjenkins-3.2.0.tar.gz/swaggyjenkins-3.2.0/swaggyjenkins/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from swaggyjenkins.api.base_api import BaseApi
+from swaggyjenkins.api.blue_ocean_api import BlueOceanApi
+from swaggyjenkins.api.remote_access_api import RemoteAccessApi
+
