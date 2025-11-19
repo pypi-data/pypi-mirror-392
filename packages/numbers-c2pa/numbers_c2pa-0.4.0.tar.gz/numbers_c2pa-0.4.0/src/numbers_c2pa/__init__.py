87c9bc5a3649d75431cfa0681d3be3c789ba8c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:32 +0000

    Add package numbers-c2pa/0.4.0/numbers_c2pa-0.4.0.tar.gz

---
+++ packages/numbers-c2pa/numbers_c2pa-0.4.0.tar.gz/numbers_c2pa-0.4.0/src/numbers_c2pa/__init__.py
@@ -0,0 +1,18 @@
+from .core import (create_c2pa_manifest, inject, inject_file, read_c2pa,
+                   read_c2pa_file)
+from .exceptions import NoClaimFound, UnknownError
+from .utils import (create_es256_private_key_file,
+                    create_self_signed_certificate, generate_es256_private_key)
+
+__all__ = [
+    'create_c2pa_manifest',
+    'inject',
+    'inject_file',
+    'read_c2pa',
+    'read_c2pa_file',
+    'create_es256_private_key_file',
+    'create_self_signed_certificate',
+    'generate_es256_private_key',
+    'NoClaimFound',
+    'UnknownError',
+]
