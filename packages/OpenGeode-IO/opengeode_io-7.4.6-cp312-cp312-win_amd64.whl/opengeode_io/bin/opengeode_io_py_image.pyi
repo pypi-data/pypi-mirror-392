dfe40ef63c651eeab3f425749c32bad3f29be230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:17 +0000

    Add package OpenGeode-IO/7.4.6/opengeode_io-7.4.6-cp312-cp312-win_amd64.whl

---
+++ packages/OpenGeode-IO/opengeode_io-7.4.6-cp312-cp312-win_amd64.whl/opengeode_io/bin/opengeode_io_py_image.pyi
@@ -0,0 +1,9 @@
+"""
+OpenGeode-IO Python binding for image
+"""
+from __future__ import annotations
+__all__: list[str] = ['IOImageLibrary']
+class IOImageLibrary:
+    @staticmethod
+    def initialize() -> None:
+        ...
---
+++ packages/OpenGeode-IO/opengeode_io-7.4.6-cp312-cp312-win_amd64.whl/opengeode_io/model_io.pyi
@@ -0,0 +1,4 @@
+from __future__ import annotations
+import opengeode as opengeode
+from opengeode_io.bin.opengeode_io_py_model import IOModelLibrary
+__all__: list[str] = ['IOModelLibrary', 'opengeode']
