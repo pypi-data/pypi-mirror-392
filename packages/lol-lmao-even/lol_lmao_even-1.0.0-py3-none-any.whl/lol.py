ac886483efcf138952b9dbfaa2394acb8184d6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 09:06:42 +0000

    Add package lol-lmao-even/1.0.0/lol_lmao_even-1.0.0-py3-none-any.whl

---
+++ packages/lol-lmao-even/lol_lmao_even-1.0.0-py3-none-any.whl/lol.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# /// script
+# requires-python = ">=3.0"
+# ///
+__version__ = '1.0.0'
+__author__  = 'Slackow'
+__license__ = 'MIT'
+
+# # # # # # # # # # # # # # # # # # # # # #
+# Please set this to your username if you are modifying this script
+modified_by = ''
+# # # # # # # # # # # # # # # # # # # # # #
+
+def main():
+    print("lmao even")
+
+if __name__ == '__main__':
+    main()
