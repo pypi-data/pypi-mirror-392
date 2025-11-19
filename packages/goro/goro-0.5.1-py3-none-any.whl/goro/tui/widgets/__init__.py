7b386d8e6a083433b5e89c8acaae08ad3707093c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-19 05:08:14 +0000

    Add package goro/0.5.1/goro-0.5.1-py3-none-any.whl

---
+++ packages/goro/goro-0.5.1-py3-none-any.whl/goro/tui/widgets/__init__.py
@@ -0,0 +1,7 @@
+"""Widgets for the GORO TUI."""
+
+from .repo_list import RepoList
+from .repo_details import RepoDetails
+from .status_bar import StatusBar
+
+__all__ = ["RepoList", "RepoDetails", "StatusBar"]
