279c4648981330a37a29ecd41c7c126e96026044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 07:06:21 +0000

    Add package eea.kitkat/8.2/eea_kitkat-8.2.tar.gz

---
+++ packages/eea.kitkat/eea_kitkat-8.2.tar.gz/eea_kitkat-8.2/setup.py
@@ -0,0 +1,66 @@
+""" eea.kitkat Installer
+"""
+import os
+from os.path import join
+from setuptools import setup, find_packages
+
+NAME = 'eea.kitkat'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(join(*PATH)).read().strip()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="An add-on for Plone",
+    long_description_content_type="text/x-rst",
+    long_description=(
+        open("README.rst").read() + "\n" +
+        open(os.path.join("docs", "HISTORY.txt")).read()
+    ),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: Addon",
+        "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: 6.0",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+    keywords='EEA Add-ons Plone Zope',
+    author='European Environment Agency: IDM2 A-Team',
+    author_email='eea-edw-a-team-alerts@googlegroups.com',
+    url='https://github.com/eea/eea.kitkat',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['eea'],
+    include_package_data=True,
+    zip_safe=False,
+        install_requires=[
+            'eea.banner',
+            'eea.zotero',
+            'eea.api.controlpanel',
+            'eea.api.layout',
+            'eea.geolocation',
+            'eea.coremetadata',
+            'eea.schema.slate',
+            'eea.sentry',
+            'eea.volto.policy',
+            'collective.taxonomy',
+            'plone.volto',
+            'plone.restapi',
+            'setuptools',
+        ],
+    extras_require={
+        'test': [
+            'plone.app.testing[test]',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """
+)
