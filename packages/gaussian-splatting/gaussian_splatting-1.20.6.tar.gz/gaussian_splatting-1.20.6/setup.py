1fc4368d51c692dd6bb440bfc4726fd7f2e12b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 04:07:08 +0000

    Add package gaussian-splatting/1.20.6/gaussian_splatting-1.20.6.tar.gz

---
+++ packages/gaussian-splatting/gaussian_splatting-1.20.6.tar.gz/gaussian_splatting-1.20.6/setup.py
@@ -0,0 +1,88 @@
+#
+# Copyright (C) 2023, Inria
+# GRAPHDECO research group, https://team.inria.fr/graphdeco
+# All rights reserved.
+#
+# This software is free for non-commercial, research and evaluation use
+# under the terms of the LICENSE.md file.
+#
+# For inquiries contact  george.drettakis@inria.fr
+#
+
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import CUDAExtension, BuildExtension
+import os
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+rasterizor_root = "submodules/diff-gaussian-rasterization"
+rasterizor_sources = [
+    "cuda_rasterizer/rasterizer_impl.cu",
+    "cuda_rasterizer/forward.cu",
+    "cuda_rasterizer/backward.cu",
+    "rasterize_points.cu",
+    "ext.cpp"]
+simpleknn_root = "submodules/simple-knn"
+simpleknn_sources = [
+    "spatial.cu",
+    "simple_knn.cu",
+    "ext.cpp"]
+
+packages = ['gaussian_splatting'] + ["gaussian_splatting." + package for package in find_packages(where="gaussian_splatting")]
+rasterizor_packages = {
+    'gaussian_splatting.diff_gaussian_rasterization': 'submodules/diff-gaussian-rasterization/diff_gaussian_rasterization',
+    'gaussian_splatting.simple_knn': 'submodules/simple-knn/simple_knn',
+}
+
+cxx_compiler_flags = []
+nvcc_compiler_flags = []
+
+if os.name == 'nt':
+    cxx_compiler_flags.append("/wd4624")
+    nvcc_compiler_flags.append("-allow-unsupported-compiler")
+
+setup(
+    name="gaussian_splatting",
+    version='1.20.6',
+    author='yindaheng98',
+    author_email='yindaheng98@gmail.com',
+    url='https://github.com/yindaheng98/gaussian-splatting',
+    description=u'Refactored python training and inference code for 3D Gaussian Splatting',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    packages=packages + list(rasterizor_packages.keys()),
+    package_dir={
+        'gaussian_splatting': 'gaussian_splatting',
+        **rasterizor_packages
+    },
+    ext_modules=[
+        CUDAExtension(
+            name="gaussian_splatting.diff_gaussian_rasterization._C",
+            sources=[os.path.join(rasterizor_root, source) for source in rasterizor_sources],
+            extra_compile_args={"nvcc": nvcc_compiler_flags + ["-I" + os.path.join(os.path.abspath(rasterizor_root), "third_party/glm/")]}
+        ),
+        CUDAExtension(
+            name="gaussian_splatting.simple_knn._C",
+            sources=[os.path.join(simpleknn_root, source) for source in simpleknn_sources],
+            extra_compile_args={"nvcc": nvcc_compiler_flags, "cxx": cxx_compiler_flags}
+        )
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    },
+    install_requires=[
+        'torch',
+        'torchvision',
+        'tqdm',
+        'plyfile',
+        'tifffile',
+        'numpy',
+        'opencv-python',
+        'pillow',
+        'open3d',
+    ]
+)
