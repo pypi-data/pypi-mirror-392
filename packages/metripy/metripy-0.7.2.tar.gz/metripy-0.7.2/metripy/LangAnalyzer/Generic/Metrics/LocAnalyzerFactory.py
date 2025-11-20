1dfcd64aa875fd9c0e5925a366106c781f02ca68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 07:06:25 +0000

    Add package metripy/0.7.2/metripy-0.7.2.tar.gz

---
+++ packages/metripy/metripy-0.7.2.tar.gz/metripy-0.7.2/metripy/LangAnalyzer/Generic/Metrics/LocAnalyzerFactory.py
@@ -0,0 +1,18 @@
+from metripy.LangAnalyzer.Generic.Metrics.GenericLocAnalyzer import GenericLocAnalyzer
+from metripy.LangAnalyzer.Php.Metrics.PhpLocAnalyzer import PhpLocAnalyzer
+from metripy.LangAnalyzer.Python.Metrics.PythonLocAnalyzer import PythonLocAnalyzer
+
+
+class LocAnalyzerFactory:
+    _ANALYZERS = {
+        "Python": PythonLocAnalyzer(),
+        "PHP": PhpLocAnalyzer(),
+        "Typescript": None,
+    }
+
+    @staticmethod
+    def get_loc_analyzer(language: str) -> GenericLocAnalyzer:
+        try:
+            return LocAnalyzerFactory._ANALYZERS[language]
+        except KeyError:
+            raise ValueError(f"No loc analyzer found for language: {language}")
