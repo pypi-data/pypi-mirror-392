896faae8695219273c55f3a325f024e0d628c39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 05:06:51 +0000

    Add package jettask/1.0.16/jettask-1.0.16.tar.gz

---
+++ packages/jettask/jettask-1.0.16.tar.gz/jettask-1.0.16/jettask/db/models/__init__.py
@@ -0,0 +1,25 @@
+"""
+数据库模型定义
+
+所有表的 SQLAlchemy 模型定义
+"""
+
+from .task import Task
+from .task_run import TaskRun
+from .scheduled_task import ScheduledTask
+from .task_metrics_minute import TaskMetricsMinute
+from .task_runs_metrics_minute import TaskRunsMetricsMinute
+from .sql_history import SQLHistory
+
+from jettask.core.enums import TaskType, TaskStatus
+
+__all__ = [
+    'Task',
+    'TaskRun',
+    'ScheduledTask',
+    'TaskMetricsMinute',
+    'TaskRunsMetricsMinute',
+    'SQLHistory',
+    'TaskType',
+    'TaskStatus',
+]
---
+++ packages/jettask/jettask-1.0.16.tar.gz/jettask-1.0.16/jettask/schemas/__init__.py
@@ -0,0 +1,205 @@
+"""
+数据模型定义
+所有的Pydantic模型集中管理
+"""
+
+from .task import (
+    TasksRequest,
+    TaskDetailResponse,
+    TaskInfo,
+    TaskActionRequest,
+    TaskListResponse,
+    TaskMessageRequest,
+    SendTasksRequest,
+    SendTasksResponse
+)
+
+from .queue import (
+    TimeRangeQuery,
+    QueueStatsResponse,
+    QueueTimelineResponse,
+    TrimQueueRequest,
+    QueueInfo,
+    QueueStats,
+    QueueActionRequest,
+    TaskRunsQueryRequest
+)
+
+from .scheduled_task import (
+    ScheduledTaskRequest,
+    ScheduledTaskResponse,
+    ScheduledTaskInfo,
+    ScheduleConfig,
+    ScheduledTaskCreate,
+    ScheduledTaskUpdate,
+    ScheduledTaskCreateRequest
+)
+
+from .alert import (
+    AlertRuleRequest,
+    AlertRuleCreate,
+    AlertRuleUpdate,
+    AlertRule,
+    AlertInstance,
+    AlertSummary
+)
+
+from .namespace import (
+    ConfigMode,
+    NamespaceCreate,
+    NamespaceUpdate,
+    NamespaceResponse,
+    NamespaceInfo,
+    NamespaceCreateRequest,
+    NamespaceUpdateRequest
+)
+
+from .backlog import (
+    BacklogLatestRequest,
+    BacklogTrendRequest,
+    BacklogSnapshot,
+    BacklogStatistics,
+    BacklogTrendResponse,
+    BacklogAlert
+)
+
+from .common import (
+    ErrorResponse,
+    FilterCondition,
+    BaseListRequest,
+    TimeRangeRequest,
+    BatchOperationRequest,
+    BatchOperationResponse,
+    PaginationResponse,
+    ListResponse,
+    HealthCheck,
+    SystemConfigUpdateRequest,
+    ApiResponse
+)
+
+from .monitoring import (
+    MetricPoint,
+    TimeSeries,
+    MonitoringMetrics,
+    AnalyticsData,
+    SystemHealth,
+    DashboardOverviewRequest,
+    DashboardOverview,
+    WorkerMetrics
+)
+
+from .responses import (
+    SuccessResponse,
+    DataResponse,
+    PaginatedDataResponse,
+    SystemStatsData,
+    SystemStatsResponse,
+    DashboardStatsData,
+    DashboardStatsResponse,
+    QueueRankingItem,
+    TopQueuesResponse,
+    NamespaceStatistics,
+    NamespaceStatisticsResponse,
+    WorkerHeartbeatInfo,
+    WorkersResponse,
+    WorkerSummary,
+    WorkerSummaryResponse,
+    WorkerOfflineRecord,
+    WorkerOfflineHistoryResponse,
+    DatabaseStatus,
+    SystemSettings,
+    SystemSettingsResponse,
+    DatabaseStatusResponse
+)
+
+__all__ = [
+    'TasksRequest',
+    'TaskDetailResponse',
+    'TaskInfo',
+    'TaskActionRequest',
+    'TaskListResponse',
+    'TaskMessageRequest',
+    'SendTasksRequest',
+    'SendTasksResponse',
+    
+    'TimeRangeQuery',
+    'QueueStatsResponse',
+    'QueueTimelineResponse',
+    'TrimQueueRequest',
+    'QueueInfo',
+    'QueueStats',
+    'QueueActionRequest',
+    'TaskRunsQueryRequest',
+
+    'ScheduledTaskRequest',
+    'ScheduledTaskResponse',
+    'ScheduledTaskInfo',
+    'ScheduleConfig',
+    'ScheduledTaskCreate',
+    'ScheduledTaskUpdate',
+    'ScheduledTaskCreateRequest',
+    
+    'AlertRuleRequest',
+    'AlertRuleCreate',
+    'AlertRuleUpdate',
+    'AlertRule',
+    'AlertInstance',
+    'AlertSummary',
+    
+    'ConfigMode',
+    'NamespaceCreate',
+    'NamespaceUpdate',
+    'NamespaceResponse',
+    'NamespaceInfo',
+    'NamespaceCreateRequest',
+    'NamespaceUpdateRequest',
+    
+    'BacklogTrendRequest',
+    'BacklogSnapshot',
+    'BacklogStatistics',
+    'BacklogTrendResponse',
+    'BacklogAlert',
+    
+    'ErrorResponse',
+    'FilterCondition',
+    'BaseListRequest',
+    'TimeRangeRequest',
+    'BatchOperationRequest',
+    'BatchOperationResponse',
+    'PaginationResponse',
+    'ListResponse',
+    'HealthCheck',
+    'SystemConfigUpdateRequest',
+    'ApiResponse',
+    
+    'MetricPoint',
+    'TimeSeries',
+    'MonitoringMetrics',
+    'AnalyticsData',
+    'SystemHealth',
+    'DashboardOverviewRequest',
+    'DashboardOverview',
+    'WorkerMetrics',
+
+    'SuccessResponse',
+    'DataResponse',
+    'PaginatedDataResponse',
+    'SystemStatsData',
+    'SystemStatsResponse',
+    'DashboardStatsData',
+    'DashboardStatsResponse',
+    'QueueRankingItem',
+    'TopQueuesResponse',
+    'NamespaceStatistics',
+    'NamespaceStatisticsResponse',
+    'WorkerHeartbeatInfo',
+    'WorkersResponse',
+    'WorkerSummary',
+    'WorkerSummaryResponse',
+    'WorkerOfflineRecord',
+    'WorkerOfflineHistoryResponse',
+    'DatabaseStatus',
+    'SystemSettings',
+    'SystemSettingsResponse',
+    'DatabaseStatusResponse'
+]
