de79213d7066198debb1207745213217818b0ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 06:06:35 +0000

    Add package dq-validation/0.2.0.dev0/dq_validation-0.2.0.dev0-cp313-cp313-macosx_10_12_x86_64.macosx_11_0_arm64.macosx_10_12_universal2.whl

---
+++ packages/dq-validation/dq_validation-0.2.0.dev0-cp313-cp313-macosx_10_12_x86_64.macosx_11_0_arm64.macosx_10_12_universal2.whl/dq_validation/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+
+logger = logging.getLogger("dq_validation")
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
