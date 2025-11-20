436b35343b2e9f2b378523d562ff4a6ff16f3962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 07:06:14 +0000

    Add package kosong/0.26.1/kosong-0.26.1.tar.gz

---
+++ packages/kosong/kosong-0.26.1.tar.gz/kosong-0.26.1/src/kosong/tooling/empty.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+from kosong.message import ToolCall
+from kosong.tooling import HandleResult, Tool, ToolResult, Toolset
+from kosong.tooling.error import ToolNotFoundError
+
+if TYPE_CHECKING:
+
+    def type_check(empty: "EmptyToolset"):
+        _: Toolset = empty
+
+
+class EmptyToolset:
+    """A toolset implementation that always contains no tools."""
+
+    @property
+    def tools(self) -> list[Tool]:
+        return []
+
+    def handle(self, tool_call: ToolCall) -> HandleResult:
+        return ToolResult(tool_call.id, ToolNotFoundError(tool_call.function.name))
