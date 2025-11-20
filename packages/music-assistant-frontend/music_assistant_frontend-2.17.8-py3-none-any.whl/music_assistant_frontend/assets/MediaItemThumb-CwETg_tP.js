babc3db03225c51e6fa0b75aa6d7eec7f55d65c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 04:06:37 +0000

    Add package music-assistant-frontend/2.17.8/music_assistant_frontend-2.17.8-py3-none-any.whl

---
+++ packages/music-assistant-frontend/music_assistant_frontend-2.17.8-py3-none-any.whl/music_assistant_frontend/assets/MediaItemThumb-CwETg_tP.js
@@ -0,0 +1,1 @@
+import{cb as o}from"./index-Bi2RCUhr.js";import{_ as a}from"./_plugin-vue_export-helper-DlAUqK2U.js";const t=a(o,[["__scopeId","data-v-565bc716"]]);export{t as M};
---
+++ packages/music-assistant-frontend/music_assistant_frontend-2.17.8-py3-none-any.whl/music_assistant_frontend/assets/VSpacer-B-_qj5CM.js
@@ -0,0 +1,1 @@
+/* empty css              */import{cO as r}from"./index-Bi2RCUhr.js";const e=r("v-spacer","div","VSpacer");export{e as V};
