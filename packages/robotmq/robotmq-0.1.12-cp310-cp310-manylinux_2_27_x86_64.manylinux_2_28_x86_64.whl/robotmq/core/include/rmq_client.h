218f0d7b6328f15651af7e7a091a7760073073d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 04:07:00 +0000

    Add package robotmq/0.1.12/robotmq-0.1.12-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/robotmq/robotmq-0.1.12-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/robotmq/core/include/rmq_client.h
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2024 Yihuai Gao
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+#pragma once
+
+#include <zmq.hpp>
+
+#include "common.h"
+#include "rmq_message.h"
+#include <map>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+#include <string>
+#include <vector>
+
+class RMQClient
+{
+  public:
+    RMQClient(const std::string &client_name, const std::string &server_endpoint, spdlog::level::level_enum log_level);
+    RMQClient(const std::string &client_name, const std::string &server_endpoint);
+    ~RMQClient();
+
+    int get_topic_status(const std::string &topic, double timeout_s);
+    // -1 if the server or topic does not exist: get no reply after timeout_s seconds
+    // 0 if the topic exists but has no data
+    // positive number means the number of data in the topic
+    pybind11::tuple peek_data(const std::string &topic, int32_t n, double timeout_s);
+    pybind11::tuple peek_data(const std::string &topic, int32_t n);
+    pybind11::tuple pop_data(const std::string &topic, int32_t n, double timeout_s);
+    pybind11::tuple pop_data(const std::string &topic, int32_t n);
+    void put_data(const std::string &topic, const pybind11::bytes &data, double timeout_s);
+    void put_data(const std::string &topic, const pybind11::bytes &data);
+    pybind11::tuple get_last_retrieved_data();
+    pybind11::bytes request_with_data(const std::string &topic, const pybind11::bytes &data, double timeout_s);
+    pybind11::bytes request_with_data(const std::string &topic, const pybind11::bytes &data);
+
+    double get_timestamp();
+    void reset_start_time(int64_t system_time_us);
+
+  private:
+    const int MAX_RETRIES_ = 800;
+    int retries_ = 0;
+    double default_timeout_s_ = 1.0;
+    std::map<std::string, bool> topic_using_shared_memory_;
+    std::vector<TimedPtr> deserialize_multiple_data_(const std::string &data);
+    std::vector<TimedPtr> send_request_(RMQMessage &message);
+    std::vector<TimedPtr> send_request_(RMQMessage &message, double timeout_s);
+    pybind11::tuple ptrs_to_tuple_(const std::vector<TimedPtr> &ptrs);
+    std::string client_name_;
+    std::shared_ptr<spdlog::logger> logger_;
+    zmq::context_t context_;
+    zmq::socket_t socket_;
+    std::vector<TimedPtr> last_retrieved_ptrs_;
+    int64_t steady_clock_start_time_us_;
+};
---
+++ packages/robotmq/robotmq-0.1.12-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/robotmq/core/include/rmq_server.h
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2024 Yihuai Gao
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+#pragma once
+
+#include <zmq.hpp>
+
+#include <deque>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+#include "common.h"
+#include "data_topic.h"
+#include "rmq_message.h"
+#include "spdlog/spdlog.h"
+class RMQServer
+{
+  public:
+    RMQServer(const std::string &server_name, const std::string &server_endpoint); // Default log level is info
+    RMQServer(const std::string &server_name, const std::string &server_endpoint, spdlog::level::level_enum log_level);
+    ~RMQServer();
+    void add_topic(const std::string &topic, double message_remaining_time_s);
+    void add_shared_memory_topic(const std::string &topic, double message_remaining_time_s,
+                                 double shared_memory_size_gb);
+    void put_data(const std::string &topic, const pybind11::bytes &data);
+    pybind11::tuple peek_data(const std::string &topic, int n);
+    pybind11::tuple pop_data(const std::string &topic, int n);
+    pybind11::tuple wait_for_request(double timeout_s);
+    void reply_request(const std::string &topic, const pybind11::bytes &data);
+    double get_timestamp();
+    void reset_start_time(int64_t system_time_us);
+
+    std::unordered_map<std::string, int> get_all_topic_status();
+
+  private:
+    const std::string server_name_;
+    bool running_;
+    int64_t steady_clock_start_time_us_;
+    zmq::context_t context_;
+    zmq::socket_t socket_;
+    zmq::pollitem_t poller_item_;
+    const std::chrono::milliseconds poller_timeout_ms_;
+    std::thread background_thread_;
+    std::mutex data_topic_mutex_;
+    std::string get_new_request_ = "";
+    std::mutex get_new_request_mutex_;
+    std::string reply_topic_ = "";
+    std::mutex reply_mutex_;
+
+    std::unordered_map<std::string, DataTopic> data_topics_;
+    std::shared_ptr<spdlog::logger> logger_;
+
+    void process_request_(RMQMessage &message);
+
+    std::vector<TimedPtr> peek_data_ptrs_(const std::string &topic, int32_t n);
+    std::vector<TimedPtr> pop_data_ptrs_(const std::string &topic, int32_t n);
+    void add_data_ptrs_(const std::string &topic, const std::vector<TimedPtr> &data_ptrs);
+    bool exists_topic_(const std::string &topic);
+    std::function<TimedPtr(const TimedPtr)> request_with_data_handler_;
+
+    void background_loop_();
+};
