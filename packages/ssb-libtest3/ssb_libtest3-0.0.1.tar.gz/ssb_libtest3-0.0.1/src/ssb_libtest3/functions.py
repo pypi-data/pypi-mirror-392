7a5313905f25a91e7a0b9b7ac8355d7153fe5adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 06:06:21 +0000

    Add package ssb-libtest3/0.0.1/ssb_libtest3-0.0.1.tar.gz

---
+++ packages/ssb-libtest3/ssb_libtest3-0.0.1.tar.gz/ssb_libtest3-0.0.1/src/ssb_libtest3/functions.py
@@ -0,0 +1,51 @@
+"""A collection of useful functions.
+
+The template and this example uses Google style docstrings as described at:
+https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+
+"""
+
+from math import sqrt
+
+
+def is_prime(number: int) -> bool:
+    """Check if the given number is a prime number.
+
+    Args:
+        number: The number to check.
+
+    Returns:
+        True if the number is a prime number. False otherwise.
+    """
+    if number <= 1:
+        return False
+    return all(number % i != 0 for i in range(2, int(sqrt(number)) + 1))
+
+
+def example_function(number1: int, number2: int) -> str:
+    """Compare two integers.
+
+    This is merely an example function can be deleted. It is used to show and test generating
+    documentation from code, type hinting, testing, and testing examples
+    in the code.
+
+
+    Args:
+        number1: The first number.
+        number2: The second number, which will be compared to number1.
+
+    Returns:
+        A string describing which number is the greatest.
+
+    Examples:
+        Examples should be written in doctest format, and should illustrate how
+        to use the function.
+
+        >>> example_function(1, 2)
+        1 is less than 2
+
+    """
+    if number1 < number2:
+        return f"{number1} is less than {number2}"
+
+    return f"{number1} is greater than or equal to {number2}"
