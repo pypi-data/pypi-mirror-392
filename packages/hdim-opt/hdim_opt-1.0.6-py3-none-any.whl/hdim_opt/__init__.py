cfadbcbc316487c771ff3068e1b6530b0e958062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 04:06:14 +0000

    Add package hdim-opt/1.0.6/hdim_opt-1.0.6-py3-none-any.whl

---
+++ packages/hdim-opt/hdim_opt-1.0.6-py3-none-any.whl/hdim_opt/__init__.py
@@ -0,0 +1,11 @@
+# hdim_opt/__init__.py
+
+# package version
+__version__ = "1.0.6"
+__all__ = ['quasar', 'hds', 'sobol', 'sensitivity'] # available for star imports
+
+# import core components
+from .quasar_optimization import optimize as quasar
+from .hyperellipsoid_sampling import sample as hds
+from .sobol_sampling import sobol_sample as sobol
+from .sobol_sensitivity import sens_analysis as sensitivity
