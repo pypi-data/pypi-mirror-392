a585ff74ce86594972beb3a7ba063a93498d2ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 05:07:00 +0000

    Add package ansys-units/0.9.1/ansys_units-0.9.1-py3-none-any.whl

---
+++ packages/ansys-units/ansys_units-0.9.1-py3-none-any.whl/ansys/units/unit_registry.py
@@ -0,0 +1,106 @@
+# Copyright (C) 2023 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""Provides the ``UnitRegistry`` class."""
+
+from collections.abc import Generator, Mapping
+import os
+from typing import TYPE_CHECKING, Any
+
+import yaml
+
+from ansys.units._constants import _BaseUnitInfo, _DerivedUnitInfo
+from ansys.units.unit import Unit
+
+
+class UnitRegistry:
+    """
+    A representation of valid ``Unit`` instances.
+
+    All base and derived units loaded from the configuration file, `cfg.yaml`,
+    on package initialization are provided by default.
+
+    Parameters
+    ----------
+    config: str, optional
+        Path of a ``YAML`` configuration file, which can be a custom file, and
+        defaults to the provided file, ``cfg.yaml``. Custom configuration files
+        must match the format of the default configuration file.
+    other: dict, optional
+        Dictionary for additional units.
+
+    Examples
+    --------
+    >>> from ansys.units import UnitRegistry, Unit
+    >>> ureg = UnitRegistry()
+    >>> assert ureg.kg == Unit(units="kg")
+    >>> fps = Unit("ft s^-1")
+    >>> ureg.foot_per_sec = fps
+    """
+
+    def __init__(
+        self,
+        config: str = "cfg.yaml",
+        other: Mapping[
+            str, Mapping[str, Any]
+        ] = {},  # pyright: ignore[reportCallInDefaultInitializer]
+    ):
+        unitdict = dict(other)
+
+        if config:
+            file_dir = os.path.dirname(__file__)
+            qc_path = os.path.join(file_dir, config)
+
+            with open(qc_path, "r") as qc_yaml:
+                qc_data = yaml.safe_load(qc_yaml)
+                _base_units: dict[str, _BaseUnitInfo] = qc_data["base_units"]
+                _derived_units: dict[str, _DerivedUnitInfo] = qc_data["derived_units"]
+
+            unitdict |= _base_units | _derived_units
+
+        for unit in unitdict:
+            setattr(self, unit, Unit(unit, unitdict[unit]))
+
+    def __str__(self):
+        returned_string = ""
+        attrs = self.__dict__
+        for key in attrs:
+            returned_string += f"{key}, "
+        return returned_string
+
+    if TYPE_CHECKING:
+
+        def __getattr__(self, name: str) -> Unit: ...
+
+    def __setattr__(self, name: str, unit: Any) -> None:
+        if hasattr(self, name):
+            raise UnitAlreadyRegistered(name)
+        self.__dict__[name] = unit
+
+    def __iter__(self) -> Generator[str]:
+        yield from self.__dict__
+
+
+class UnitAlreadyRegistered(ValueError):
+    """Raised when a unit has previously been registered."""
+
+    def __init__(self, name: str):
+        super().__init__(f"Unable to override `{name}` it has already been registered.")
