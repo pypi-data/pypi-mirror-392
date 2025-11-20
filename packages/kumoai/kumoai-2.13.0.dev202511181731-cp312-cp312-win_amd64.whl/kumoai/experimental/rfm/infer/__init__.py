7724d9d8c5c0d6b884049d7f72c4f8ac8bbe7a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 03:26:28 +0000

    Add package kumoai/2.13.0.dev202511181731/kumoai-2.13.0.dev202511181731-cp312-cp312-win_amd64.whl

---
+++ packages/kumoai/kumoai-2.13.0.dev202511181731-cp312-cp312-win_amd64.whl/kumoai/experimental/rfm/infer/__init__.py
@@ -0,0 +1,11 @@
+from .id import contains_id
+from .timestamp import contains_timestamp
+from .categorical import contains_categorical
+from .multicategorical import contains_multicategorical
+
+__all__ = [
+    'contains_id',
+    'contains_timestamp',
+    'contains_categorical',
+    'contains_multicategorical',
+]
