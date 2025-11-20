4d70f1eb3fa01fd325d2f9c88c78224ead631a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 06:06:46 +0000

    Add package esuls/0.1.2/esuls-0.1.2.tar.gz

---
+++ packages/esuls/esuls-0.1.2.tar.gz/esuls-0.1.2/src/esuls/__init__.py
@@ -0,0 +1,21 @@
+"""
+esuls - Utility library for async database operations, HTTP requests, and parallel execution
+"""
+
+__version__ = "0.1.0"
+
+# Import all utilities
+from .utils import run_parallel
+from .db_cli import AsyncDB, BaseModel
+from .request_cli import AsyncRequest, make_request, make_request_cffi, Response
+
+__all__ = [
+    '__version__',
+    'run_parallel',
+    'AsyncDB',
+    'BaseModel',
+    'AsyncRequest',
+    'make_request',
+    'make_request_cffi',
+    'Response',
+]
