fcc578ebb716fc5d5418ef7fb16b295af486357e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 03:26:23 +0000

    Add package video-reader-rs/0.3.0/video_reader_rs-0.3.0-cp312-cp312-win_amd64.whl

---
+++ packages/video-reader-rs/video_reader_rs-0.3.0-cp312-cp312-win_amd64.whl/video_reader/__init__.py
@@ -0,0 +1,16 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'video_reader_rs.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+from .video_reader import *
+
+__doc__ = video_reader.__doc__
+if hasattr(video_reader, "__all__"):
+    __all__ = video_reader.__all__
