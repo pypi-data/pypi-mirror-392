7f055191384545b03c1a5f6e6ef1257ed5c222d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 04:06:38 +0000

    Add package maialib/1.10.0/maialib-1.10.0-cp39-cp39-win_amd64.whl

---
+++ packages/maialib/maialib-1.10.0-cp39-cp39-win_amd64.whl/maialib/maiacore/__init__.pyi
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from maialib.maiacore.Release.maiacore import Barline
+from maialib.maiacore.Release.maiacore import Chord
+from maialib.maiacore.Release.maiacore import Clef
+from maialib.maiacore.Release.maiacore import ClefSign
+from maialib.maiacore.Release.maiacore import HeapData
+from maialib.maiacore.Release.maiacore import Helper
+from maialib.maiacore.Release.maiacore import Interval
+from maialib.maiacore.Release.maiacore import Key
+from maialib.maiacore.Release.maiacore import Measure
+from maialib.maiacore.Release.maiacore import Note
+from maialib.maiacore.Release.maiacore import NoteData
+from maialib.maiacore.Release.maiacore import NoteDataHeap
+from maialib.maiacore.Release.maiacore import Part
+from maialib.maiacore.Release.maiacore import RhythmFigure
+from maialib.maiacore.Release.maiacore import Score
+from maialib.maiacore.Release.maiacore import ScoreCollection
+from maialib.maiacore.Release.maiacore import TimeSignature
+from . import Release
+__all__ = ['Barline', 'C', 'Chord', 'Clef', 'ClefSign', 'F', 'G', 'HeapData', 'Helper', 'Interval', 'Key', 'Measure', 'Note', 'NoteData', 'NoteDataHeap', 'P', 'Part', 'Release', 'RhythmFigure', 'Score', 'ScoreCollection', 'TimeSignature']
+C: Release.maiacore.ClefSign  # value = <ClefSign.C: 2>
+F: Release.maiacore.ClefSign  # value = <ClefSign.F: 1>
+G: Release.maiacore.ClefSign  # value = <ClefSign.G: 0>
+P: Release.maiacore.ClefSign  # value = <ClefSign.P: 3>
