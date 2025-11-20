728e0eedcf9c7baa908075b18ab395950b1b847d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 03:25:11 +0000

    Add package sbom-compliance-tool/0.0.7/sbom_compliance_tool-0.0.7.tar.gz

---
+++ packages/sbom-compliance-tool/sbom_compliance_tool-0.0.7.tar.gz/sbom_compliance_tool-0.0.7/sbom_compliance_tool/config.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+sbom_compliance_tool_version = '0.0.7'
+description = 'Tool to assists complioance work with SBOMs'
+module_name = 'sbom_compliance_tool'
