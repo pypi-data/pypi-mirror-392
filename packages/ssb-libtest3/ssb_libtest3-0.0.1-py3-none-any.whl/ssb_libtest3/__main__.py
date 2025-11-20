a8b5297836ca82e97a126547a937cbb7c1ea5a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 06:06:21 +0000

    Add package ssb-libtest3/0.0.1/ssb_libtest3-0.0.1-py3-none-any.whl

---
+++ packages/ssb-libtest3/ssb_libtest3-0.0.1-py3-none-any.whl/ssb_libtest3/__main__.py
@@ -0,0 +1,13 @@
+"""Command-line interface."""
+
+import click
+
+
+@click.command()
+@click.version_option()
+def main() -> None:
+    """SSB Libtest3."""
+
+
+if __name__ == "__main__":
+    main(prog_name="ssb-libtest3")  # pragma: no cover
