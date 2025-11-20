473dc0def8a6577d719d4d9759bb7aeed19d6bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 03:26:11 +0000

    Add package dad-tool/3.0.0/dad_tool-3.0.0-py3-none-any.whl

---
+++ packages/dad-tool/dad_tool-3.0.0-py3-none-any.whl/dad_tool/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+from dad_tool.data_router import (
+    list_addresses,
+    list_iso_country_codes,
+    random_address,
+)
+
+
+__all__ = [
+    "list_addresses",
+    "list_iso_country_codes",
+    "random_address",
+]
