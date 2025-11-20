6d48f32b78148eef465fc091f76efa591629d50d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 05:08:02 +0000

    Add package microsoft-agents-hosting-teams/0.7.0.dev0/microsoft_agents_hosting_teams-0.7.0.dev0.tar.gz

---
+++ packages/microsoft-agents-hosting-teams/microsoft_agents_hosting_teams-0.7.0.dev0.tar.gz/microsoft_agents_hosting_teams-0.7.0.dev0/setup.py
@@ -0,0 +1,12 @@
+from os import environ
+from setuptools import setup
+
+package_version = environ.get("PackageVersion", "0.0.0")
+
+setup(
+    version=package_version,
+    install_requires=[
+        f"microsoft-agents-hosting-core=={package_version}",
+        "aiohttp>=3.11.11",
+    ],
+)
