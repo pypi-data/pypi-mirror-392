38524c2f7dd69d318e0e119346fbbab5c2ef00bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 05:06:50 +0000

    Add package syndesi/0.4.4/syndesi-0.4.4-py3-none-any.whl

---
+++ packages/syndesi/syndesi-0.4.4-py3-none-any.whl/syndesi/__init__.py
@@ -0,0 +1,25 @@
+"""
+Syndesi module
+"""
+
+from .adapters.ip import IP
+from .adapters.serialport import SerialPort
+from .adapters.timeout import Timeout
+from .adapters.visa import Visa
+from .protocols.delimited import Delimited
+from .protocols.modbus import Modbus
+from .protocols.raw import Raw
+from .protocols.scpi import SCPI
+from .tools.log import log
+
+__all__ = [
+    "IP",
+    "SerialPort",
+    "Visa",
+    "Delimited",
+    "Modbus",
+    "Raw",
+    "SCPI",
+    "log",
+    "Timeout",
+]
