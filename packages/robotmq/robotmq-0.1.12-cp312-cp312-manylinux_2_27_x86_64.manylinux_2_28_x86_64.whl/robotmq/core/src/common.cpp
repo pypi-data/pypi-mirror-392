9aaa635e3d0fea06024f8af2ed7737306757f229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 04:07:00 +0000

    Add package robotmq/0.1.12/robotmq-0.1.12-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/robotmq/robotmq-0.1.12-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/robotmq/core/src/common.cpp
@@ -0,0 +1,322 @@
+/**
+ * Copyright (c) 2024 Yihuai Gao
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+#include "common.h"
+#include <cstring>
+#include <fcntl.h>
+#include <pybind11/functional.h>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/pytypes.h>
+#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
+#include <sstream>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <sys/types.h>
+
+namespace py = pybind11;
+
+void interruptible_sleep(double seconds)
+{
+    py::gil_scoped_release release; // Release the GIL for C++ execution
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_seconds;
+
+    // Sleep in small increments and check for interrupts
+    while (true)
+    {
+        elapsed_seconds = std::chrono::high_resolution_clock::now() - start_time;
+        if (elapsed_seconds.count() >= seconds)
+        {
+            break; // Target sleep duration reached
+        }
+
+        // Calculate remaining time or a small chunk
+        double remaining_seconds = seconds - elapsed_seconds.count();
+        double sleep_chunk = std::min(remaining_seconds, 0.001); // Sleep for 1ms at a time
+
+        std::this_thread::sleep_for(std::chrono::duration<double>(sleep_chunk));
+
+        // Reacquire GIL temporarily to check for Python signals
+        py::gil_scoped_acquire acquire;
+        if (PyErr_CheckSignals() != 0)
+        {
+            // A Python error (like KeyboardInterrupt) is pending
+            throw py::error_already_set(); // Re-raise the Python exception
+        }
+    }
+}
+
+int64_t steady_clock_us()
+{
+    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch())
+        .count();
+}
+int64_t system_clock_us()
+{
+    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch())
+        .count();
+}
+
+std::string uint32_to_bytes(uint32_t value)
+{
+    return std::string(reinterpret_cast<const char *>(&value), sizeof(uint32_t));
+}
+
+uint32_t bytes_to_uint32(const std::string &bytes)
+{
+    if (bytes.size() != sizeof(uint32_t))
+    {
+        throw std::invalid_argument("Input bytes must have the same size as a 32-bit unsigned integer, but got " +
+                                    std::to_string(bytes.size()));
+    }
+    return *reinterpret_cast<const uint32_t *>(bytes.data());
+}
+
+std::string int32_to_bytes(int32_t value)
+{
+    return std::string(reinterpret_cast<const char *>(&value), sizeof(int32_t));
+}
+
+int32_t bytes_to_int32(const std::string &bytes)
+{
+    if (bytes.size() != sizeof(int32_t))
+    {
+        throw std::invalid_argument("Input bytes must have the same size as a 32-bit integer, but got " +
+                                    std::to_string(bytes.size()));
+    }
+    return *reinterpret_cast<const int32_t *>(bytes.data());
+}
+
+std::string uint64_to_bytes(uint64_t value)
+{
+    return std::string(reinterpret_cast<const char *>(&value), sizeof(uint64_t));
+}
+
+uint64_t bytes_to_uint64(const std::string &bytes)
+{
+    if (bytes.size() != sizeof(uint64_t))
+    {
+        throw std::invalid_argument("Input bytes must have the same size as a 64-bit unsigned integer, but got " +
+                                    std::to_string(bytes.size()));
+    }
+    return *reinterpret_cast<const uint64_t *>(bytes.data());
+}
+std::string double_to_bytes(double value)
+{
+    return std::string(reinterpret_cast<const char *>(&value), sizeof(double));
+}
+
+double bytes_to_double(const std::string &bytes)
+{
+    if (bytes.size() != sizeof(double))
+    {
+        throw std::invalid_argument("Input bytes must have the same size as a double, but got " +
+                                    std::to_string(bytes.size()));
+    }
+    return *reinterpret_cast<const double *>(bytes.data());
+}
+
+std::string bytes_to_hex(const std::string &bytes)
+{
+    std::ostringstream hex_stream;
+    hex_stream << std::hex << std::setfill('0');
+    for (unsigned char byte : bytes)
+    {
+        hex_stream << std::setw(2) << static_cast<int>(byte);
+    }
+    return hex_stream.str();
+}
+
+std::string get_user_name()
+{
+    char *user_name = getlogin();
+    if (user_name == nullptr)
+    {
+        uid_t uid = getuid();
+        struct passwd *pw = getpwuid(uid);
+        if (pw == nullptr)
+        {
+            printf("Failed to get user_name. Using uid: %d\n", uid);
+            return std::to_string(uid);
+        }
+        return std::string(pw->pw_name);
+    }
+    return std::string(user_name);
+}
+
+std::string get_pid()
+{
+    pid_t pid = getpid();
+    return std::to_string(pid);
+}
+
+SharedMemoryDataInfo::SharedMemoryDataInfo(const std::string &shm_name, uint64_t shm_size_bytes, uint64_t shm_start_idx,
+                                           uint64_t data_size_bytes)
+    : shm_name_(shm_name), shm_size_bytes_(shm_size_bytes), shm_start_idx_(shm_start_idx),
+      data_size_bytes_(data_size_bytes)
+{
+}
+
+SharedMemoryDataInfo::SharedMemoryDataInfo(const std::string &serialized_data_info)
+{
+    uint64_t current_byte_idx = 0;
+    if (serialized_data_info.substr(0, HEADER.size()) != HEADER)
+    {
+        printf("serialized_data_info: first 10 bytes: %s, total size: %zu, HEADER: %s\n",
+               bytes_to_hex(serialized_data_info.substr(0, 10)).c_str(), serialized_data_info.size(),
+               bytes_to_hex(HEADER).c_str());
+        throw std::invalid_argument("Invalid serialized data info (beginning doesn't match with HEADER)");
+    }
+    current_byte_idx += HEADER.size();
+
+    uint64_t shm_name_size = bytes_to_uint64(serialized_data_info.substr(current_byte_idx, sizeof(uint64_t)));
+    current_byte_idx += sizeof(uint64_t);
+
+    shm_name_ = serialized_data_info.substr(current_byte_idx, shm_name_size);
+    current_byte_idx += shm_name_size;
+
+    shm_size_bytes_ = bytes_to_uint64(serialized_data_info.substr(current_byte_idx, sizeof(uint64_t)));
+    current_byte_idx += sizeof(uint64_t);
+
+    shm_start_idx_ = bytes_to_uint64(serialized_data_info.substr(current_byte_idx, sizeof(uint64_t)));
+    current_byte_idx += sizeof(uint64_t);
+
+    data_size_bytes_ = bytes_to_uint64(serialized_data_info.substr(current_byte_idx, sizeof(uint64_t)));
+    current_byte_idx += sizeof(uint64_t);
+
+    if (current_byte_idx != serialized_data_info.size())
+    {
+        throw std::runtime_error("Shared memory data info is not complete");
+    }
+}
+
+const std::string SharedMemoryDataInfo::HEADER = "\x0d\x0a\x0d\x0b";
+
+bool SharedMemoryDataInfo::is_shm_data_info(const std::string &serialized_data_info)
+{
+    return serialized_data_info.substr(0, HEADER.size()) == HEADER;
+}
+
+std::string SharedMemoryDataInfo::serialize() const
+{
+    std::string serialized;
+    serialized.append(HEADER);
+    serialized.append(uint64_to_bytes(shm_name_.size()));
+    serialized.append(shm_name_);
+    serialized.append(uint64_to_bytes(shm_size_bytes_));
+    serialized.append(uint64_to_bytes(shm_start_idx_));
+    serialized.append(uint64_to_bytes(data_size_bytes_));
+    return serialized;
+}
+
+std::string SharedMemoryDataInfo::shm_name() const
+{
+    return shm_name_;
+}
+
+std::string SharedMemoryDataInfo::shm_mutex_name() const
+{
+    return shm_name_ + "_mutex";
+}
+
+uint64_t SharedMemoryDataInfo::shm_size_bytes() const
+{
+    return shm_size_bytes_;
+}
+
+uint64_t SharedMemoryDataInfo::shm_start_idx() const
+{
+    return shm_start_idx_;
+}
+
+uint64_t SharedMemoryDataInfo::data_size_bytes() const
+{
+    return data_size_bytes_;
+}
+
+pybind11::bytes concat_to_pybytes(const char *a, size_t a_len, const char *b, size_t b_len)
+{
+    size_t total_len = a_len + b_len;
+
+    // Allocate Python bytes object with uninitialized buffer
+    PyObject *py_bytes = PyBytes_FromStringAndSize(nullptr, total_len);
+    if (!py_bytes)
+        throw std::runtime_error("Failed to allocate Python bytes");
+
+    // Get pointer to internal buffer
+    char *buffer = PyBytes_AS_STRING(py_bytes);
+
+    // Copy both arrays into the buffer
+    std::memcpy(buffer, a, a_len);
+    std::memcpy(buffer + a_len, b, b_len);
+
+    // Return py::bytes without extra copy
+    return pybind11::reinterpret_steal<pybind11::bytes>(py_bytes);
+}
+
+pybind11::bytes SharedMemoryDataInfo::get_shm_data() const
+{
+
+    int shm_fd = shm_open(shm_name_.c_str(), O_RDONLY, 0666);
+    if (shm_fd == -1)
+    {
+        throw std::runtime_error("Failed to open shared memory: " + shm_name_ + " " + std::string(strerror(errno)));
+    }
+
+    void *shm_ptr = mmap(0, shm_size_bytes_, PROT_READ, MAP_SHARED, shm_fd, 0);
+    if (shm_ptr == MAP_FAILED)
+    {
+        throw std::runtime_error("Failed to map shared memory: " + shm_name_ + " " + std::string(strerror(errno)));
+    }
+    pybind11::bytes data;
+    if (shm_start_idx_ + data_size_bytes_ < shm_size_bytes_)
+    {
+        data = pybind11::bytes(static_cast<char *>(shm_ptr) + shm_start_idx_, data_size_bytes_);
+    }
+    else
+    {
+        char *a = static_cast<char *>(shm_ptr) + shm_start_idx_;
+        size_t a_len = shm_size_bytes_ - shm_start_idx_;
+        char *b = static_cast<char *>(shm_ptr);
+        size_t b_len = data_size_bytes_ - a_len;
+        data = concat_to_pybytes(a, a_len, b, b_len);
+    }
+    munmap(shm_ptr, shm_size_bytes_);
+    close(shm_fd);
+
+    return data;
+}
+
+pybind11::bytes SharedMemoryDataInfo::get_shm_data_with_mutex() const
+{
+
+    // Lock the mutex
+    int shm_mutex_fd = shm_open((shm_name_ + "_mutex").c_str(), O_RDWR, 0666);
+    if (shm_mutex_fd == -1)
+    {
+        throw std::runtime_error("Failed to open shared memory mutex: " + shm_name_ + "_mutex " +
+                                 std::string(strerror(errno)));
+    }
+    pthread_mutex_t *shm_mutex_ptr =
+        (pthread_mutex_t *)mmap(0, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE, MAP_SHARED, shm_mutex_fd, 0);
+    pthread_mutex_lock(shm_mutex_ptr);
+
+    // Get data
+    pybind11::bytes data = get_shm_data();
+
+    // Unlock the mutex
+    pthread_mutex_unlock(shm_mutex_ptr);
+    munmap(shm_mutex_ptr, sizeof(pthread_mutex_t));
+    close(shm_mutex_fd);
+
+    return data;
+}
---
+++ packages/robotmq/robotmq-0.1.12-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/robotmq/core/src/pybind.cpp
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2024 Yihuai Gao
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+#include "common.h"
+#include "data_topic.h"
+#include "rmq_client.h"
+#include "rmq_message.h"
+#include "rmq_server.h"
+#include <pybind11/functional.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(robotmq_core, m)
+{
+
+    m.def("steady_clock_us", &steady_clock_us);
+    m.def("system_clock_us", &system_clock_us);
+
+    py::enum_<spdlog::level::level_enum>(m, "RMQLogLevel")
+        .value("TRACE", spdlog::level::level_enum::trace)
+        .value("DEBUG", spdlog::level::level_enum::debug)
+        .value("INFO", spdlog::level::level_enum::info)
+        .value("WARNING", spdlog::level::level_enum::warn)
+        .value("ERROR", spdlog::level::level_enum::err)
+        .value("CRITICAL", spdlog::level::level_enum::critical)
+        .value("OFF", spdlog::level::level_enum::off)
+        .export_values();
+
+    py::class_<RMQClient>(m, "RMQClient")
+        .def(py::init<const std::string &, const std::string &>(), py::arg("client_name"), py::arg("server_endpoint"))
+        .def(py::init<const std::string &, const std::string &, spdlog::level::level_enum>(), py::arg("client_name"), py::arg("server_endpoint"), py::arg("log_level"))
+        .def("get_topic_status", &RMQClient::get_topic_status, py::arg("topic"), py::arg("timeout_s"))
+        .def("peek_data", py::overload_cast<const std::string &, int32_t>(&RMQClient::peek_data), py::arg("topic"), py::arg("n"))
+        .def("peek_data", py::overload_cast<const std::string &, int32_t, double>(&RMQClient::peek_data), py::arg("topic"), py::arg("n"), py::arg("timeout_s"))
+        .def("pop_data", py::overload_cast<const std::string &, int32_t>(&RMQClient::pop_data), py::arg("topic"), py::arg("n"))
+        .def("pop_data", py::overload_cast<const std::string &, int32_t, double>(&RMQClient::pop_data), py::arg("topic"), py::arg("n"), py::arg("timeout_s"))
+        .def("put_data", py::overload_cast<const std::string &, const pybind11::bytes &>(&RMQClient::put_data), py::arg("topic"), py::arg("data"))
+        .def("put_data", py::overload_cast<const std::string &, const pybind11::bytes &, double>(&RMQClient::put_data), py::arg("topic"), py::arg("data"), py::arg("timeout_s"))
+        .def("get_last_retrieved_data", &RMQClient::get_last_retrieved_data)
+        .def("reset_start_time", &RMQClient::reset_start_time, py::arg("system_time_us"))
+        .def("get_timestamp", &RMQClient::get_timestamp)
+        .def("request_with_data", py::overload_cast<const std::string &, const pybind11::bytes &>(&RMQClient::request_with_data), py::arg("topic"), py::arg("data"))
+        .def("request_with_data", py::overload_cast<const std::string &, const pybind11::bytes &, double>(&RMQClient::request_with_data), py::arg("topic"), py::arg("data"), py::arg("timeout_s"));
+
+    py::class_<RMQServer>(m, "RMQServer")
+        .def(py::init<const std::string &, const std::string &>(), py::arg("server_name"), py::arg("server_endpoint"))
+        .def(py::init<const std::string &, const std::string &, spdlog::level::level_enum>(), py::arg("server_name"), py::arg("server_endpoint"), py::arg("log_level"))
+        .def("add_topic", &RMQServer::add_topic, py::arg("topic"), py::arg("message_remaining_time_s"))
+        .def("add_shared_memory_topic", &RMQServer::add_shared_memory_topic, py::arg("topic"),
+             py::arg("message_remaining_time_s"), py::arg("shared_memory_size_gb"))
+        .def("put_data", &RMQServer::put_data, py::arg("topic"), py::arg("data"))
+        .def("peek_data", &RMQServer::peek_data, py::arg("topic"), py::arg("n"))
+        .def("pop_data", &RMQServer::pop_data, py::arg("topic"), py::arg("n"))
+        .def("get_all_topic_status", &RMQServer::get_all_topic_status)
+        .def("reset_start_time", &RMQServer::reset_start_time, py::arg("system_time_us"))
+        .def("wait_for_request", &RMQServer::wait_for_request, py::arg("timeout_s"))
+        .def("reply_request", &RMQServer::reply_request, py::arg("topic"), py::arg("data"))
+        .def("get_timestamp", &RMQServer::get_timestamp);
+}
