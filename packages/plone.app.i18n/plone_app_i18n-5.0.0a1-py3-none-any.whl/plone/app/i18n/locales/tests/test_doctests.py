a191d6bc5c2f3f5ea7e572bce2bc58b57e456e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 06:06:49 +0000

    Add package plone.app.i18n/5.0.0a1/plone_app_i18n-5.0.0a1-py3-none-any.whl

---
+++ packages/plone.app.i18n/plone_app_i18n-5.0.0a1-py3-none-any.whl/plone/app/i18n/locales/tests/test_doctests.py
@@ -0,0 +1,35 @@
+from plone.app.testing import PLONE_INTEGRATION_TESTING
+from plone.testing import layered
+
+import doctest
+import unittest
+
+
+OPTIONFLAGS = (
+    doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
+)
+
+
+def test_suite():
+    return unittest.TestSuite(
+        (
+            doctest.DocTestSuite("plone.app.i18n.locales.countries"),
+            doctest.DocTestSuite("plone.app.i18n.locales.languages"),
+            layered(
+                doctest.DocFileSuite(
+                    "countries.txt",
+                    optionflags=OPTIONFLAGS,
+                    package="plone.app.i18n.locales.tests",
+                ),
+                layer=PLONE_INTEGRATION_TESTING,
+            ),
+            layered(
+                doctest.DocFileSuite(
+                    "languages.txt",
+                    optionflags=OPTIONFLAGS,
+                    package="plone.app.i18n.locales.tests",
+                ),
+                layer=PLONE_INTEGRATION_TESTING,
+            ),
+        )
+    )
