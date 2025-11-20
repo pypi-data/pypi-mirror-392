04bcbf2e83d523a086f5c7b487c057f05ce49f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 04:06:36 +0000

    Add package user-scanner/1.0.2.1/user_scanner-1.0.2.1.tar.gz

---
+++ packages/user-scanner/user_scanner-1.0.2.1.tar.gz/user_scanner-1.0.2.1/user_scanner/dev/codeberg.py
@@ -0,0 +1,37 @@
+import httpx
+from httpx import ConnectError, TimeoutException
+
+def validate_codeberg(user):
+    url = f"https://codeberg.org/{user}"
+
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Mobile Safari/537.36",
+        'Accept': "text/html",
+    }
+
+    try:
+        response = httpx.head(url, headers=headers, timeout=3.0, follow_redirects=True)
+        status = response.status_code
+
+        if status == 200:
+           return 0
+        elif status == 404:
+           return 1
+        else:
+           return 2
+
+    except (ConnectError, TimeoutException):
+        return 2
+    except Exception:
+        return 2
+
+if __name__ == "__main__":
+   user = input ("Username?: ").strip()
+   result = validate_codeberg(user)
+
+   if result == 1:
+      print("Available!")
+   elif result == 0:
+      print("Unavailable!")
+   else:
+      print("Error occurred!")
