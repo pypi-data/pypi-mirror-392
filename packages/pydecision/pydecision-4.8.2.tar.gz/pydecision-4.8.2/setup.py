b3fe8b8672750d59f465510171667398a583276c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 07:06:01 +0000

    Add package pydecision/4.8.2/pydecision-4.8.2.tar.gz

---
+++ packages/pydecision/pydecision-4.8.2.tar.gz/pydecision-4.8.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name='pydecision',
+    version='4.8.2',
+    license='GNU',
+    author='Valdecy Pereira',
+    author_email='valdecy.pereira@gmail.com',
+    url='https://github.com/Valdecy/pyDecisions',
+    packages=find_packages(),
+    install_requires=[
+        'llmx',
+        'matplotlib',
+        'numpy',
+        'openai',
+        'pandas',
+        'scikit-learn',
+        'scipy'
+    ],
+    description='A MCDA Library Incorporating a Large Language Model to Enhance Decision Analysis',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
