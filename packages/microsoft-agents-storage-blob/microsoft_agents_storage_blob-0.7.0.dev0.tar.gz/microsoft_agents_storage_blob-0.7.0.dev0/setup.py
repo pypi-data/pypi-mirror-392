2c477e581bddc73aa34aaa42c69f34133edb70f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 05:08:02 +0000

    Add package microsoft-agents-storage-blob/0.7.0.dev0/microsoft_agents_storage_blob-0.7.0.dev0.tar.gz

---
+++ packages/microsoft-agents-storage-blob/microsoft_agents_storage_blob-0.7.0.dev0.tar.gz/microsoft_agents_storage_blob-0.7.0.dev0/setup.py
@@ -0,0 +1,13 @@
+from os import environ
+from setuptools import setup
+
+package_version = environ.get("PackageVersion", "0.0.0")
+
+setup(
+    version=package_version,
+    install_requires=[
+        f"microsoft-agents-hosting-core=={package_version}",
+        "azure-core",
+        "azure-storage-blob",
+    ],
+)
