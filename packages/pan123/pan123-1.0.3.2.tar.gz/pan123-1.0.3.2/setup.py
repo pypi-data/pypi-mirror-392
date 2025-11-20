d56d8813e169376e97facd86c6cc0025d8fa6414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 05:07:04 +0000

    Add package pan123/1.0.3.2/pan123-1.0.3.2.tar.gz

---
+++ packages/pan123/pan123-1.0.3.2.tar.gz/pan123-1.0.3.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pan123",
+    version="1.0.3.2",
+    author="SodaCodeSave&lixuehua",
+    author_email="soda_code@outlook.com",
+    description="非官方的123云盘Python包",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SodaCodeSave/Pan123",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
