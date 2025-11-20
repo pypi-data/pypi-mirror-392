4f46da8adbc43c5c0e0738d8e753e04273e91f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 05:06:47 +0000

    Add package playwright-stealth-plugin/1.3.0/playwright_stealth_plugin-1.3.0-py3-none-any.whl

---
+++ packages/playwright-stealth-plugin/playwright_stealth_plugin-1.3.0-py3-none-any.whl/playwright_stealth_plugin/_python/config.py
@@ -0,0 +1,35 @@
+import typing
+
+script_folder = "javascript"
+script_names: typing.List[str] = [
+    "utils.js",
+    "chrome.app.js",
+    "chrome.csi.js",
+    "chrome.hairline.js",
+    "chrome.load.times.js",
+    "chrome.runtime.js",
+    "generate.magic.arrays.js",
+    "iframe.contentWindow.js",
+    "media.codecs.js",
+    "navigator.hardwareConcurrency.js",
+    "navigator.languages.js",
+    "navigator.permissions.js",
+    "navigator.platform.js",
+    "navigator.plugins.js",
+    "navigator.userAgent.js",
+    "navigator.vendor.js",
+    "navigator.webdriver.js",
+    "webgl.vendor.js",
+    "window.outerdimensions.js",
+]
+
+browser_options: typing.Dict[str, typing.Any] = {}
+context_options: typing.Dict[str, typing.Any] = {}
+
+import os
+import sys
+import playwright_stealth_plugin
+
+sys.__file__ = os.path.abspath(playwright_stealth_plugin.__file__)
+
+# [line-length: 150]
