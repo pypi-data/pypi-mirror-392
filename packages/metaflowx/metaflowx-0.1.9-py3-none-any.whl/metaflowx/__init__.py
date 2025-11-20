e6ed1f0b7789b8a409562c1748405da0a638c2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 04:06:37 +0000

    Add package metaflowx/0.1.9/metaflowx-0.1.9-py3-none-any.whl

---
+++ packages/metaflowx/metaflowx-0.1.9-py3-none-any.whl/metaflowx/__init__.py
@@ -0,0 +1,13 @@
+# Core public modules
+from . import cli_components
+from . import decomposition
+from . import ensemble
+from . import feature_selection
+from . import frozen
+from . import optimiser
+
+__all__ = [
+    "cli_components",
+    "frozen",
+    "optimiser"
+]
