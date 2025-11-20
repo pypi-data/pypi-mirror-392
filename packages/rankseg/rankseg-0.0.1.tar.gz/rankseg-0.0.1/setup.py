0fa41848056050cad6983cfcce552a1ce384ff4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 05:07:56 +0000

    Add package rankseg/0.0.1/rankseg-0.0.1.tar.gz

---
+++ packages/rankseg/rankseg-0.0.1.tar.gz/rankseg-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+__version__ = "0.0.1"
+
+setup(
+    name="rankseg",
+    version=__version__,
+    author=["Ben Dai", "Zixun Wang"],
+    author_email="bendai@cuhk.edu.hk",
+    url="https://rankseg.readthedocs.io/en/latest/",
+    description="RankSEG: A Statistically Consistent Segmentation Prediction Solver for Dice and IoU Metrics Optimization",
+    packages=["rankseg"],
+    install_requires=["torch", "scipy", "numpy"],
+    zip_safe=False,
+    python_requires=">= 3.9",
+)
