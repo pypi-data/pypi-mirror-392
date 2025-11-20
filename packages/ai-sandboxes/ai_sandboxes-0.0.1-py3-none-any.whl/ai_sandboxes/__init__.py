554ad227387eddeee1ffdd48cb92676e6d4c58ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 06:06:12 +0000

    Add package ai-sandboxes/0.0.1/ai_sandboxes-0.0.1-py3-none-any.whl

---
+++ packages/ai-sandboxes/ai_sandboxes-0.0.1-py3-none-any.whl/ai_sandboxes/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2023-2024 Datalayer, Inc.
+#
+# BSD 3-Clause License
+
+"""AI Sandboxes."""
+
+__all__ = []
---
+++ packages/ai-sandboxes/ai_sandboxes-0.0.1-py3-none-any.whl/ai_sandboxes/__version__.py
@@ -0,0 +1,6 @@
+#
+# BSD 3-Clause License
+
+"""AI Sandboxes."""
+
+__version__ = "0.0.1"
