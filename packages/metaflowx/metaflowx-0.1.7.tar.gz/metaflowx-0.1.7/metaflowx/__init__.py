8fc5b3968600f88673418bffa83fcdadda29b521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 04:06:35 +0000

    Add package metaflowx/0.1.7/metaflowx-0.1.7.tar.gz

---
+++ packages/metaflowx/metaflowx-0.1.7.tar.gz/metaflowx-0.1.7/metaflowx/__init__.py
@@ -0,0 +1,16 @@
+# Core public modules
+from . import cli_components
+from . import decomposition
+from . import ensemble
+from . import feature_selection
+from . import frozen
+from . import optimiser
+
+__all__ = [
+    "cli_components",
+    "decomposition",
+    "ensemble",
+    "feature_selection",
+    "frozen",
+    "optimiser"
+]
