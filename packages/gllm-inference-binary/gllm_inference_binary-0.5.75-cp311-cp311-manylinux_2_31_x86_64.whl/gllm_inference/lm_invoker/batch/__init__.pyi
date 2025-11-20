4689a75f466cd85c1324acc5e799d7f38196c7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 05:07:38 +0000

    Add package gllm-inference-binary/0.5.75/gllm_inference_binary-0.5.75-cp311-cp311-manylinux_2_31_x86_64.whl

---
+++ packages/gllm-inference-binary/gllm_inference_binary-0.5.75-cp311-cp311-manylinux_2_31_x86_64.whl/gllm_inference/lm_invoker/batch/__init__.pyi
@@ -0,0 +1,3 @@
+from gllm_inference.lm_invoker.batch.batch_operations import BatchOperations as BatchOperations
+
+__all__ = ['BatchOperations']
---
+++ packages/gllm-inference-binary/gllm_inference_binary-0.5.75-cp311-cp311-manylinux_2_31_x86_64.whl/gllm_inference/lm_invoker/schema/datasaur.pyi
@@ -0,0 +1,14 @@
+class Key:
+    """Defines valid keys in Datasaur."""
+    API_KEY: str
+    BASE_URL: str
+    CONTEXTS: str
+    MAX_RETRIES: str
+    NAME: str
+    TIMEOUT: str
+    TYPE: str
+    URL: str
+
+class InputType:
+    """Defines valid input types in Datasaur."""
+    URL: str
