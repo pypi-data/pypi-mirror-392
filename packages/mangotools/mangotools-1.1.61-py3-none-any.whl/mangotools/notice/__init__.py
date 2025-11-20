4534b43e9be6e4417d5ed25aa3e5387bf2e0359e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 05:07:09 +0000

    Add package mangotools/1.1.61/mangotools-1.1.61-py3-none-any.whl

---
+++ packages/mangotools/mangotools-1.1.61-py3-none-any.whl/mangotools/notice/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description:
+# @Time   : 2022-11-04 22:05
+# @Author : 毛鹏
+
+from ..notice._mail_send import EmailSend
+from ..notice._wechat_send import WeChatSend
+__all__ = [
+    'WeChatSend',
+    'EmailSend',
+]
