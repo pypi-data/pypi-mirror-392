79b9df9784fefdfdd99c20bd12e40e0ab70b5f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 07:06:14 +0000

    Add package wah/1.18.7/wah-1.18.7.tar.gz

---
+++ packages/wah/wah-1.18.7.tar.gz/wah-1.18.7/setup.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="wah",
+    version="1.18.7",
+    description="a library so simple you will learn Within An Hour",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Juyeop Kim",
+    author_email="juyeopkim@yonsei.ac.kr",
+    url="https://github.com/yupeeee/WAH",
+    license="MIT",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    entry_points={
+        "console_scripts": [
+            "wah=wah.cli:main",
+        ],
+    },
+    install_requires=[
+        "setuptools>=61.0",
+        "accelerate",
+        "diffusers",
+        "timm",
+        "torch",
+        "torchvision",
+        "transformers",
+    ],
+)
