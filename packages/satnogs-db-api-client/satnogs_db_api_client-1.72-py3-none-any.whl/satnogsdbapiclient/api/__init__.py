4e276789d3813b4207edf56338e97576f39ebd65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 07:06:11 +0000

    Add package satnogs-db-api-client/1.72/satnogs_db_api_client-1.72-py3-none-any.whl

---
+++ packages/satnogs-db-api-client/satnogs_db_api_client-1.72-py3-none-any.whl/satnogsdbapiclient/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from satnogsdbapiclient.api.artifacts_api import ArtifactsApi
+from satnogsdbapiclient.api.decoded_frame_api import DecodedFrameApi
+from satnogsdbapiclient.api.modes_api import ModesApi
+from satnogsdbapiclient.api.optical_observations_api import OpticalObservationsApi
+from satnogsdbapiclient.api.satellites_api import SatellitesApi
+from satnogsdbapiclient.api.telemetry_api import TelemetryApi
+from satnogsdbapiclient.api.tle_api import TleApi
+from satnogsdbapiclient.api.transmitters_api import TransmittersApi
+
