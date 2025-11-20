b4e44599e9d1e3c19f10aaaabf32aab6837d0844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 03:26:21 +0000

    Add package boto3-stubs-full/1.40.76/boto3_stubs_full-1.40.76.tar.gz

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.40.76.tar.gz/boto3_stubs_full-1.40.76/mypy_boto3_bedrock_data_automation_runtime/type_defs.py
@@ -0,0 +1,163 @@
+"""
+Type annotations for bedrock-data-automation-runtime service type definitions.
+
+[Documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock_data_automation_runtime/type_defs/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from mypy_boto3_bedrock_data_automation_runtime.type_defs import BlueprintTypeDef
+
+    data: BlueprintTypeDef = ...
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from collections.abc import Mapping, Sequence
+
+from .literals import AutomationJobStatusType, BlueprintStageType, DataAutomationStageType
+
+if sys.version_info >= (3, 12):
+    from typing import NotRequired, TypedDict
+else:
+    from typing_extensions import NotRequired, TypedDict
+
+
+__all__ = (
+    "AssetProcessingConfigurationTypeDef",
+    "BlueprintTypeDef",
+    "DataAutomationConfigurationTypeDef",
+    "EncryptionConfigurationTypeDef",
+    "EventBridgeConfigurationTypeDef",
+    "GetDataAutomationStatusRequestTypeDef",
+    "GetDataAutomationStatusResponseTypeDef",
+    "InputConfigurationTypeDef",
+    "InvokeDataAutomationAsyncRequestTypeDef",
+    "InvokeDataAutomationAsyncResponseTypeDef",
+    "ListTagsForResourceRequestTypeDef",
+    "ListTagsForResourceResponseTypeDef",
+    "NotificationConfigurationTypeDef",
+    "OutputConfigurationTypeDef",
+    "ResponseMetadataTypeDef",
+    "TagResourceRequestTypeDef",
+    "TagTypeDef",
+    "TimestampSegmentTypeDef",
+    "UntagResourceRequestTypeDef",
+    "VideoAssetProcessingConfigurationTypeDef",
+    "VideoSegmentConfigurationTypeDef",
+)
+
+
+class BlueprintTypeDef(TypedDict):
+    blueprintArn: str
+    version: NotRequired[str]
+    stage: NotRequired[BlueprintStageType]
+
+
+class DataAutomationConfigurationTypeDef(TypedDict):
+    dataAutomationProjectArn: str
+    stage: NotRequired[DataAutomationStageType]
+
+
+class EncryptionConfigurationTypeDef(TypedDict):
+    kmsKeyId: str
+    kmsEncryptionContext: NotRequired[Mapping[str, str]]
+
+
+class EventBridgeConfigurationTypeDef(TypedDict):
+    eventBridgeEnabled: bool
+
+
+class GetDataAutomationStatusRequestTypeDef(TypedDict):
+    invocationArn: str
+
+
+class OutputConfigurationTypeDef(TypedDict):
+    s3Uri: str
+
+
+class ResponseMetadataTypeDef(TypedDict):
+    RequestId: str
+    HTTPStatusCode: int
+    HTTPHeaders: dict[str, str]
+    RetryAttempts: int
+    HostId: NotRequired[str]
+
+
+class TagTypeDef(TypedDict):
+    key: str
+    value: str
+
+
+class ListTagsForResourceRequestTypeDef(TypedDict):
+    resourceARN: str
+
+
+class TimestampSegmentTypeDef(TypedDict):
+    startTimeMillis: int
+    endTimeMillis: int
+
+
+class UntagResourceRequestTypeDef(TypedDict):
+    resourceARN: str
+    tagKeys: Sequence[str]
+
+
+class NotificationConfigurationTypeDef(TypedDict):
+    eventBridgeConfiguration: EventBridgeConfigurationTypeDef
+
+
+class GetDataAutomationStatusResponseTypeDef(TypedDict):
+    status: AutomationJobStatusType
+    errorType: str
+    errorMessage: str
+    outputConfiguration: OutputConfigurationTypeDef
+    ResponseMetadata: ResponseMetadataTypeDef
+
+
+class InvokeDataAutomationAsyncResponseTypeDef(TypedDict):
+    invocationArn: str
+    ResponseMetadata: ResponseMetadataTypeDef
+
+
+class ListTagsForResourceResponseTypeDef(TypedDict):
+    tags: list[TagTypeDef]
+    ResponseMetadata: ResponseMetadataTypeDef
+
+
+class TagResourceRequestTypeDef(TypedDict):
+    resourceARN: str
+    tags: Sequence[TagTypeDef]
+
+
+class VideoSegmentConfigurationTypeDef(TypedDict):
+    timestampSegment: NotRequired[TimestampSegmentTypeDef]
+
+
+class VideoAssetProcessingConfigurationTypeDef(TypedDict):
+    segmentConfiguration: NotRequired[VideoSegmentConfigurationTypeDef]
+
+
+class AssetProcessingConfigurationTypeDef(TypedDict):
+    video: NotRequired[VideoAssetProcessingConfigurationTypeDef]
+
+
+class InputConfigurationTypeDef(TypedDict):
+    s3Uri: str
+    assetProcessingConfiguration: NotRequired[AssetProcessingConfigurationTypeDef]
+
+
+class InvokeDataAutomationAsyncRequestTypeDef(TypedDict):
+    inputConfiguration: InputConfigurationTypeDef
+    outputConfiguration: OutputConfigurationTypeDef
+    dataAutomationProfileArn: str
+    clientToken: NotRequired[str]
+    dataAutomationConfiguration: NotRequired[DataAutomationConfigurationTypeDef]
+    encryptionConfiguration: NotRequired[EncryptionConfigurationTypeDef]
+    notificationConfiguration: NotRequired[NotificationConfigurationTypeDef]
+    blueprints: NotRequired[Sequence[BlueprintTypeDef]]
+    tags: NotRequired[Sequence[TagTypeDef]]
---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.40.76.tar.gz/boto3_stubs_full-1.40.76/mypy_boto3_mediaconnect/waiter.py
@@ -0,0 +1,92 @@
+"""
+Type annotations for mediaconnect service client waiters.
+
+[Documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mediaconnect/waiters/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_mediaconnect.client import MediaConnectClient
+    from mypy_boto3_mediaconnect.waiter import (
+        FlowActiveWaiter,
+        FlowDeletedWaiter,
+        FlowStandbyWaiter,
+    )
+
+    session = Session()
+    client: MediaConnectClient = session.client("mediaconnect")
+
+    flow_active_waiter: FlowActiveWaiter = client.get_waiter("flow_active")
+    flow_deleted_waiter: FlowDeletedWaiter = client.get_waiter("flow_deleted")
+    flow_standby_waiter: FlowStandbyWaiter = client.get_waiter("flow_standby")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+
+from botocore.waiter import Waiter
+
+from .type_defs import (
+    DescribeFlowRequestWaitExtraExtraTypeDef,
+    DescribeFlowRequestWaitExtraTypeDef,
+    DescribeFlowRequestWaitTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("FlowActiveWaiter", "FlowDeletedWaiter", "FlowStandbyWaiter")
+
+
+class FlowActiveWaiter(Waiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediaconnect/waiter/FlowActive.html#MediaConnect.Waiter.FlowActive)
+    [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mediaconnect/waiters/#flowactivewaiter)
+    """
+
+    def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeFlowRequestWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediaconnect/waiter/FlowActive.html#MediaConnect.Waiter.FlowActive.wait)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mediaconnect/waiters/#flowactivewaiter)
+        """
+
+
+class FlowDeletedWaiter(Waiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediaconnect/waiter/FlowDeleted.html#MediaConnect.Waiter.FlowDeleted)
+    [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mediaconnect/waiters/#flowdeletedwaiter)
+    """
+
+    def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeFlowRequestWaitExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediaconnect/waiter/FlowDeleted.html#MediaConnect.Waiter.FlowDeleted.wait)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mediaconnect/waiters/#flowdeletedwaiter)
+        """
+
+
+class FlowStandbyWaiter(Waiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediaconnect/waiter/FlowStandby.html#MediaConnect.Waiter.FlowStandby)
+    [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mediaconnect/waiters/#flowstandbywaiter)
+    """
+
+    def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeFlowRequestWaitExtraExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediaconnect/waiter/FlowStandby.html#MediaConnect.Waiter.FlowStandby.wait)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mediaconnect/waiters/#flowstandbywaiter)
+        """
