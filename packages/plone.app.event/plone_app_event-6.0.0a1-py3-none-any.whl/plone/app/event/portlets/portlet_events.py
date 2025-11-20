0b080522a342207ee037bae5d1538ffd68d6d151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 07:06:32 +0000

    Add package plone.app.event/6.0.0a1/plone_app_event-6.0.0a1-py3-none-any.whl

---
+++ packages/plone.app.event/plone_app_event-6.0.0a1-py3-none-any.whl/plone/app/event/portlets/portlet_events.py
@@ -0,0 +1,296 @@
+from Acquisition import aq_inner
+from ComputedAttribute import ComputedAttribute
+from plone.app.event import _
+from plone.app.event.base import _prepare_range
+from plone.app.event.base import expand_events
+from plone.app.event.base import get_events
+from plone.app.event.base import localized_now
+from plone.app.event.base import RET_MODE_ACCESSORS
+from plone.app.event.base import start_end_query
+from plone.app.event.portlets import get_calendar_url
+from plone.app.event.portlets.portlet_calendar import ISyndicatableCollection
+from plone.app.event.portlets.portlet_calendar import search_base_uid_source
+from plone.app.portlets.portlets import base
+from plone.app.querystring import queryparser
+from plone.app.uuid.utils import uuidToObject
+from plone.base.interfaces.controlpanel import ISiteSchema
+from plone.event.interfaces import IEvent
+from plone.memoize import ram
+from plone.memoize.compress import xhtml_compress
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.registry.interfaces import IRegistry
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zExceptions import NotFound
+from zope import schema
+from zope.component import getMultiAdapter
+from zope.component import getUtility
+from zope.component.hooks import getSite
+from zope.contentprovider.interfaces import IContentProvider
+from zope.interface import implementer
+
+
+def _render_events_cachekey(method, self):
+    # Since the events portlet shows the upcoming events, we simply get the
+    # most recently modified event and return its uuid and last modified date
+    # as key. If there is a change, then the most expensive computation can be
+    # performed.
+
+    site = getSite()
+    cat = getToolByName(site, "portal_catalog")
+
+    query = {}
+    data = self.data
+    if data.state:
+        query["review_state"] = data.state
+
+    start = localized_now(site)
+
+    query.update(self.request.get("contentFilter", {}))
+    if ISyndicatableCollection and ISyndicatableCollection.providedBy(self.search_base):
+        # Whatever sorting is defined, we're overriding it.
+        query = queryparser.parseFormquery(
+            self.search_base, self.search_base.query, sort_on="start", sort_order=None
+        )
+        if "start" in query:
+            start = query["start"]
+    elif self.search_base_path:
+        query["path"] = {"query": self.search_base_path}
+
+    # Any object with an IEvent interface.
+    query["object_provides"] = IEvent.__identifier__
+    # Only upcoming events.
+    query.update(start_end_query(start=start, end=None))
+    # We want to get the next data.count events, just as the portlet would.
+    query["sort_on"] = "start"
+    query["sort_order"] = "asc"
+    query["sort_limit"] = data.count
+
+    events = cat(**query)
+    uuid = ""
+    modified = 0
+    for event in events:
+        uuid += event.UID
+        modified += event.modified.asdatetime().timestamp()
+
+    return (uuid, modified)
+
+
+class IEventsPortlet(IPortletDataProvider):
+    count = schema.Int(
+        title=_("Number of items to display"),
+        description=_("How many items to list."),
+        required=True,
+        default=5,
+        min=1,
+    )
+
+    state = schema.Tuple(
+        title=_("Workflow state"),
+        description=_("Items in which workflow state to show."),
+        default=None,
+        required=False,
+        value_type=schema.Choice(vocabulary="plone.app.vocabularies.WorkflowStates"),
+    )
+
+    search_base_uid = schema.Choice(
+        title=_("portlet_label_search_base", default="Search base"),
+        description=_(
+            "portlet_help_search_base",
+            default="Select search base Folder or Collection to search for "
+            "events. The URL to to this item will also be used to "
+            "link to in calendar searches. If empty, the whole site "
+            "will be searched and the event listing view will be "
+            "called on the site root.",
+        ),
+        required=False,
+        source=search_base_uid_source,
+    )
+    thumb_scale = schema.TextLine(
+        title=_("Override thumb scale"),
+        description=_(
+            "Enter a valid scale name"
+            " (see 'Image Handling' control panel) to override"
+            " (e.g. icon, tile, thumb, mini, preview, ... )."
+            " Leave empty to use default (see 'Site' control panel)."
+        ),
+        required=False,
+        default="",
+    )
+
+    no_thumbs = schema.Bool(
+        title=_("Suppress thumbs"),
+        description=_("If enabled, the portlet will not show thumbs."),
+        required=False,
+        default=False,
+    )
+
+
+@implementer(IEventsPortlet)
+class Assignment(base.Assignment):
+    # reduce upgrade pain
+    thumb_scale = None
+    no_thumbs = False
+
+    def __init__(
+        self,
+        count=5,
+        state=None,
+        search_base_uid=None,
+        thumb_scale=None,
+        no_thumbs=False,
+    ):
+        self.count = count
+        self.state = state
+        self.search_base_uid = search_base_uid
+        self.thumb_scale = thumb_scale
+        self.no_thumbs = no_thumbs
+
+    @property
+    def title(self):
+        return _("Events")
+
+    def _uid(self):
+        # This is only called if the instance doesn't have a search_base_uid
+        # attribute, which is probably because it has an old
+        # 'search_base' attribute that needs to be converted.
+        path = self.search_base
+        try:
+            search_base = getSite().unrestrictedTraverse(path.lstrip("/"))
+        except (AttributeError, KeyError, TypeError, NotFound):
+            return
+        return search_base.UID()
+
+    search_base_uid = ComputedAttribute(_uid, 1)
+
+
+class Renderer(base.Renderer):
+    _template = ViewPageTemplateFile("portlet_events.pt")
+    _search_base = None
+
+    @property
+    def search_base(self):
+        if not self._search_base and self.data.search_base_uid:
+            self._search_base = uuidToObject(self.data.search_base_uid)
+        return aq_inner(self._search_base) if self._search_base else None
+
+    @property
+    def search_base_path(self):
+        return (
+            "/".join(self.search_base.getPhysicalPath()) if self.search_base else None
+        )  # noqa
+
+    def update(self):
+        context = aq_inner(self.context)
+        calendar_url = get_calendar_url(context, self.search_base_path)
+        self.next_url = "%s?mode=future" % calendar_url
+        self.prev_url = "%s?mode=past" % calendar_url
+
+    def render(self):
+        return xhtml_compress(self._template())
+
+    @property
+    def available(self):
+        return self.data.count > 0 and len(self.events)
+
+    @property
+    @ram.cache(_render_events_cachekey)
+    def events(self):
+        context = aq_inner(self.context)
+        data = self.data
+
+        query = {}
+        if data.state:
+            query["review_state"] = data.state
+
+        events = []
+        query.update(self.request.get("contentFilter", {}))
+        if ISyndicatableCollection.providedBy(self.search_base):
+            # Whatever sorting is defined, we're overriding it.
+            query = queryparser.parseFormquery(
+                self.search_base,
+                self.search_base.query,
+                sort_on="start",
+                sort_order=None,
+            )
+
+            start = None
+            if "start" in query:
+                start = query["start"]
+            else:
+                start = localized_now(context)
+
+            end = None
+            if "end" in query:
+                end = query["end"]
+
+            start, end = _prepare_range(self.search_base, start, end)
+            query.update(start_end_query(start, end))
+            events = self.search_base.results(
+                batch=False, brains=True, custom_query=query, limit=data.count
+            )
+            events = expand_events(
+                events,
+                ret_mode=RET_MODE_ACCESSORS,
+                start=start,
+                end=end,
+                sort="start",
+                sort_reverse=False,
+            )
+            events = events[: data.count]  # limit expanded
+        else:
+            if self.search_base_path:
+                query["path"] = {"query": self.search_base_path}
+            events = get_events(
+                context,
+                start=localized_now(context),
+                ret_mode=RET_MODE_ACCESSORS,
+                expand=True,
+                limit=data.count,
+                **query,
+            )
+
+        return events
+
+    def formatted_date(self, event):
+        provider = getMultiAdapter(
+            (self.context, self.request, self), IContentProvider, name="formatted_date"
+        )
+        return provider(event)
+
+    def thumb_scale(self):
+        """Use override value or read thumb_scale from registry.
+        Image sizes must fit to value in allowed image sizes.
+        None will suppress thumb.
+        """
+        if getattr(self.data, "no_thumbs", False):
+            # Individual setting overrides ...
+            return None
+        thsize = getattr(self.data, "thumb_scale", None)
+        if thsize:
+            return thsize
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(ISiteSchema, prefix="plone", check=False)
+        if settings.no_thumbs_portlet:
+            return None
+        thumb_scale_portlet = settings.thumb_scale_portlet
+        return thumb_scale_portlet
+
+
+class AddForm(base.AddForm):
+    schema = IEventsPortlet
+    label = _("Add Events Portlet")
+    description = _("This portlet lists upcoming Events.")
+
+    def create(self, data):
+        return Assignment(
+            count=data.get("count", 5),
+            state=data.get("state", None),
+            search_base_uid=data.get("search_base_uid", None),
+        )
+
+
+class EditForm(base.EditForm):
+    schema = IEventsPortlet
+    label = _("Edit Events Portlet")
+    description = _("This portlet lists upcoming Events.")
---
+++ packages/plone.app.event/plone_app_event-6.0.0a1-py3-none-any.whl/plone/app/event/tests/test_portlet_calendar.py
@@ -0,0 +1,387 @@
+from calendar import monthrange
+from datetime import datetime
+from datetime import timedelta
+from plone.app.event.base import localized_today
+from plone.app.event.portlets import portlet_calendar
+from plone.app.event.testing import PAEvent_INTEGRATION_TESTING
+from plone.app.event.testing import PAEventDX_INTEGRATION_TESTING
+from plone.app.event.testing import set_env_timezone
+from plone.app.event.testing import set_timezone
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.dexterity.utils import createContentInContainer
+from plone.portlets.interfaces import IPortletAssignment
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletRenderer
+from plone.portlets.interfaces import IPortletType
+from Products.CMFCore.utils import getToolByName
+from Products.GenericSetup.utils import _getDottedName
+from zope.component import getMultiAdapter
+from zope.component import getUtility
+
+import pytz
+import unittest
+
+
+TZNAME = "Europe/Vienna"
+PTYPE = "plone.app.event.dx.event"
+
+
+class PortletTest(unittest.TestCase):
+    layer = PAEvent_INTEGRATION_TESTING
+
+    def setUp(self):
+        portal = self.layer["portal"]
+        self.portal = portal
+        self.request = self.layer["request"]
+        setRoles(portal, TEST_USER_ID, ["Manager"])
+
+    def testPortletTypeRegistered(self):
+        portlet = getUtility(IPortletType, name="portlets.Calendar")
+        self.assertEqual(portlet.addview, "portlets.Calendar")
+
+    def testRegisteredInterfaces(self):
+        portlet = getUtility(IPortletType, name="portlets.Calendar")
+        registered_interfaces = [_getDottedName(i) for i in portlet.for_]
+        registered_interfaces.sort()
+        self.assertEqual(
+            [
+                "plone.app.portlets.interfaces.IColumn",
+                "plone.app.portlets.interfaces.IDashboard",
+            ],
+            registered_interfaces,
+        )
+
+    def testInterfaces(self):
+        portlet = portlet_calendar.Assignment()
+        self.assertTrue(IPortletAssignment.providedBy(portlet))
+        self.assertTrue(IPortletDataProvider.providedBy(portlet.data))
+
+    def testInvokeAddview(self):
+        portlet = getUtility(IPortletType, name="portlets.Calendar")
+        mapping = self.portal.restrictedTraverse("++contextportlets++plone.leftcolumn")
+        for m in mapping.keys():
+            del mapping[m]
+        addview = mapping.restrictedTraverse("+/" + portlet.addview)
+
+        addview.createAndAdd(data={})
+
+        self.assertEqual(len(mapping), 1)
+        self.assertTrue(
+            isinstance(list(mapping.values())[0], portlet_calendar.Assignment)
+        )
+
+    def testRenderer(self):
+        context = self.portal
+        view = context.restrictedTraverse("@@plone")
+        manager = getUtility(
+            IPortletManager, name="plone.rightcolumn", context=self.portal
+        )
+        assignment = portlet_calendar.Assignment()
+
+        renderer = getMultiAdapter(
+            (context, self.request, view, manager, assignment), IPortletRenderer
+        )
+        self.assertTrue(isinstance(renderer, portlet_calendar.Renderer))
+
+
+class RendererTest(unittest.TestCase):
+    layer = PAEventDX_INTEGRATION_TESTING
+
+    def setUp(self):
+        portal = self.layer["portal"]
+        self.portal = portal
+        self.request = self.layer["request"]
+        self.wft = getToolByName(self.portal, "portal_workflow")
+        self.wft.setDefaultChain("simple_publication_workflow")
+        setRoles(portal, TEST_USER_ID, ["Manager"])
+
+        set_env_timezone(TZNAME)
+        set_timezone(TZNAME)
+
+    def renderer(
+        self, context=None, request=None, view=None, manager=None, assignment=None
+    ):
+        context = context or self.portal
+        request = request or self.request
+        view = view or context.restrictedTraverse("@@plone")
+        manager = manager or getUtility(
+            IPortletManager, name="plone.rightcolumn", context=self.portal
+        )
+        assignment = assignment or portlet_calendar.Assignment()
+
+        return getMultiAdapter(
+            (context, request, view, manager, assignment), IPortletRenderer
+        )
+
+    def test_portlet_config(self):
+        tz = pytz.timezone(TZNAME)
+        start = tz.localize(datetime.now())
+        end = start + timedelta(hours=1)
+
+        e1 = createContentInContainer(
+            self.portal, PTYPE, title="e1", start=start, end=end
+        )
+        self.portal.invokeFactory("Folder", "eventfolder")
+        createContentInContainer(
+            self.portal.eventfolder, PTYPE, title="e2", start=start, end=end
+        )
+        self.portal.portal_workflow.doActionFor(e1, "publish")
+
+        r = self.renderer(assignment=portlet_calendar.Assignment(state=("draft",)))
+        r.update()
+        rd = r.render()
+        self.assertTrue("e1" not in rd and "e2" not in rd)
+
+        r = self.renderer(assignment=portlet_calendar.Assignment(state=("published",)))
+        r.update()
+        rd = r.render()
+        self.assertTrue("e1" in rd and "e2" not in rd)
+
+        r = self.renderer(
+            assignment=portlet_calendar.Assignment(
+                state=(
+                    "published",
+                    "private",
+                )
+            )
+        )
+        r.update()
+        rd = r.render()
+        self.assertTrue("e1" in rd and "e2" in rd)
+
+        r = self.renderer(assignment=portlet_calendar.Assignment())
+        r.update()
+        rd = r.render()
+        self.assertTrue("e1" in rd and "e2" in rd)
+
+        # No search base gives calendar urls with event_listing part
+        self.assertTrue("event_listing?mode=day" in rd)
+
+        r = self.renderer(
+            assignment=portlet_calendar.Assignment(
+                search_base_uid=self.portal.eventfolder.UID()
+            )
+        )
+        r.update()
+        rd = r.render()
+        self.assertTrue("e1" not in rd and "e2" in rd)
+
+        # A given search base gives calendar urls without event_listing part
+        self.assertTrue("event_listing?mode=day" not in rd)
+
+        # link to calendar view in rendering
+        self.assertTrue("?mode=day&amp;date=" in rd)
+
+    def test_long_event(self):
+        """Test a long event.
+
+        An event happening on three days, should have an indication on all
+        three days.  That is the main thing we test here.
+
+        But the test may fail on the first day of the month.
+        See https://github.com/plone/plone.app.event/issues/334
+        When generating the portlet on the first day of the month, it seems
+        depending on time zones we might get the previous month.  This would
+        mean our event is not (fully) visible.  So we force showing the
+        calendar of the month that we want.  This test is not about whether
+        the correct month is shown at one minute past midnight.
+
+        Previously we created an event on the first day.  But depending on
+        the day of the week that the new month starts, this may also influence
+        what is visible: we may see Friday 1st and Saturday 2nd, but not
+        Sunday 3rd.  So now we pick a day in the middle of the month.
+        """
+        tz = pytz.timezone(TZNAME)
+        actual = tz.localize(datetime.now())
+        start = tz.localize(datetime(actual.year, actual.month, 15))
+        end = start + timedelta(days=2)
+
+        e1 = createContentInContainer(
+            self.portal, PTYPE, title="e1", start=start, end=end
+        )
+        self.portal.portal_workflow.doActionFor(e1, "publish")
+
+        self.request["year"] = actual.year
+        self.request["month"] = actual.month
+        r = self.renderer(assignment=portlet_calendar.Assignment(state=("published",)))
+        r.update()
+        rd = r.render()
+        self.assertEqual(rd.count("e1"), 3)
+
+    def test_event_created_last_day_of_month_invalidate_cache(self):
+        # First render the calendar portlet when there's no events
+        r = self.renderer(assignment=portlet_calendar.Assignment())
+        r.update()
+        html = r.render()
+
+        # Now let's add a new event on the first day of the current month
+        year, month = r.year_month_display()
+        day = monthrange(year, month)[1]  # (wkday, days)
+
+        tz = pytz.timezone(TZNAME)
+        start = tz.localize(datetime(year, month, day, 23, 0, 0))
+        end = tz.localize(datetime(year, month, day, 23, 30, 0))
+        # Event starts at 23:00 and ends at 23:30
+        createContentInContainer(self.portal, PTYPE, title="e1", start=start, end=end)
+
+        # Try to render the calendar portlet again, it must be different Now
+        r = self.renderer(assignment=portlet_calendar.Assignment())
+        r.update()
+        self.assertNotEqual(html, r.render(), "Cache key wasn't invalidated")
+
+    def test_event_nonascii(self):
+        # test issue with non-ascii event title and location
+        title = "Plön€¢önf München 2012"
+
+        tz = pytz.timezone(TZNAME)
+        start = tz.localize(datetime.now())
+        end = start + timedelta(hours=1)
+        e1 = createContentInContainer(
+            self.portal, PTYPE, title=title, start=start, end=end, location="München"
+        )
+        self.wft.doActionFor(e1, "publish")
+        r = self.renderer(assignment=portlet_calendar.Assignment())
+        r.update()
+        self.assertTrue(title in r.render())
+
+    def test_prev_next_query(self):
+        r = self.renderer(assignment=portlet_calendar.Assignment())
+        r.update()
+
+        year, month = r.year_month_display()
+        prev_expected = "?month={1}&year={0}".format(*r.get_previous_month(year, month))
+        next_expected = "?month={1}&year={0}".format(*r.get_next_month(year, month))
+        self.assertEqual(next_expected, r.next_query)
+        self.assertEqual(prev_expected, r.prev_query)
+
+    def test_invalid_request(self):
+        self.request.form["month"] = [3, 4]
+        self.request.form["year"] = [2011]
+        r = self.renderer()
+        r.update()
+        today = localized_today(self.portal)
+        self.assertEqual(r.year_month_display(), (today.year, today.month))
+
+    def test_with_collection_as_source(self):
+        """When selecting a Collection as source, the events in that collection
+        are used to build the calendar
+        """
+        tz = pytz.timezone(TZNAME)
+
+        start = tz.localize(datetime.now())
+        # Testing at the start or end of the month is prone to a month
+        # rollover condition, which will cause a test failure. This is not
+        # a bug in plone.app.event, but merely a test condition failure.
+        # To avoid this issue, set the start date to roughly in the middle
+        # of the month.
+        start = start.replace(day=15)
+
+        end = start + timedelta(hours=1)
+        start_yesterday = start - timedelta(days=1)
+        end_yesterday = start_yesterday + timedelta(hours=1)
+        start_tomorrow = start + timedelta(days=1)
+        end_tomorrow = end + timedelta(days=1)
+        end_tomorrow_1 = end_tomorrow + timedelta(days=1)
+
+        # Starts today, ends today
+        createContentInContainer(
+            self.portal,
+            PTYPE,
+            title="e1",
+            start=start,
+            end=end,
+        )
+        # 2nd starts today, ends today
+        createContentInContainer(
+            self.portal,
+            PTYPE,
+            title="e2",
+            start=start,
+            end=end,
+        )
+        # Starts yesterday, ends yesterday
+        createContentInContainer(
+            self.portal,
+            PTYPE,
+            title="e3",
+            start=start_yesterday,
+            end=end_yesterday,
+        )
+        # Starts today, ends tomorrow
+        createContentInContainer(
+            self.portal,
+            PTYPE,
+            title="e4",
+            start=start,
+            end=end_tomorrow,
+        )
+        # starts yesterday, ends tomorrow
+        createContentInContainer(
+            self.portal,
+            PTYPE,
+            title="e5",
+            start=start_yesterday,
+            end=end_tomorrow,
+        )
+        # starts tomorrow, ends tomorrow + 1
+        createContentInContainer(
+            self.portal,
+            PTYPE,
+            title="e6",
+            start=start_tomorrow,
+            end=end_tomorrow_1,
+        )
+
+        # self.wft.doActionFor(self.portal.e1, "publish")
+        self.wft.doActionFor(self.portal.e2, "publish")
+        self.wft.doActionFor(self.portal.e3, "publish")
+        self.wft.doActionFor(self.portal.e4, "publish")
+        self.wft.doActionFor(self.portal.e5, "publish")
+        self.wft.doActionFor(self.portal.e6, "publish")
+
+        self.portal.invokeFactory(
+            "Collection",
+            "eventcollection",
+            query=[
+                {
+                    "i": "portal_type",
+                    "o": "plone.app.querystring.operation.selection.any",
+                    "v": [PTYPE],
+                },
+                {
+                    "i": "end",
+                    # Normally you wouldn't restrict the dates in a calendar to
+                    # allow browsing historical dates too.
+                    # And if, you'd probably want to show future events with
+                    # "end after today". But here we want reproducible results
+                    # and not hit by end-of-month boundary effects, where
+                    # tomorrow is already in another month.
+                    "o": "plone.app.querystring.operation.date.largerThan",
+                    "v": start.date().isoformat(),
+                },
+                {
+                    "i": "review_state",
+                    "o": "plone.app.querystring.operation.selection.any",
+                    "v": ["published"],
+                },
+            ],
+        )
+
+        r = self.renderer(
+            # context=self.portal.eventcollection,
+            assignment=portlet_calendar.Assignment(
+                state=("published",),
+                search_base_uid=self.portal.eventcollection.UID(),
+            ),
+        )
+        r.update()
+        rd = r.render()
+
+        self.assertTrue("e1" not in rd)
+        self.assertTrue("e2" in rd)
+        self.assertTrue("e3" in rd)
+        self.assertTrue("e4" in rd)
+        self.assertTrue("e5" in rd)
+        self.assertTrue("e6" in rd)
