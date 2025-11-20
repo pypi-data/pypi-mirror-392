4c82690a0de158111b4fd80af7b98319c2b843e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-20 06:06:44 +0000

    Add package fastdevcli-slim/0.19.3/fastdevcli_slim-0.19.3.tar.gz

---
+++ packages/fastdevcli-slim/fastdevcli_slim-0.19.3.tar.gz/fastdevcli_slim-0.19.3/tests/utils.py
@@ -0,0 +1,69 @@
+import shutil
+import sys
+from collections.abc import Generator
+from contextlib import contextmanager, redirect_stdout
+from io import StringIO
+from pathlib import Path
+
+from asynctor import Shell
+from asynctor.compat import chdir
+
+__all__ = (
+    "capture_stdout",
+    "mock_sys_argv",
+    "prepare_poetry_project",
+    "temp_file",
+)
+
+
+@contextmanager
+def mock_sys_argv(args: list[str]):
+    origin = sys.argv[1:]
+    sys.argv[1:] = args
+    yield
+    sys.argv[1:] = origin
+
+
+@contextmanager
+def capture_stdout():
+    """Redirect sys.stdout to a new StringIO
+
+    Example::
+    ```py
+        with capture_stdout() as stream:
+            GitTag(message="", dry=True).run()
+        assert "git tag -a" in stream.getvalue()
+    ```
+    """
+    stream = StringIO()
+    with redirect_stdout(stream):
+        yield stream
+
+
+@contextmanager
+def temp_file(name: str, text=""):
+    path = Path(__file__).parent / name
+    if text:
+        path.write_text(text)
+    else:
+        path.touch()
+    yield
+    if path.exists():
+        path.unlink()
+
+
+@contextmanager
+def prepare_poetry_project(tmp_path: Path) -> Generator[str]:
+    py = "{}.{}".format(*sys.version_info)
+    poetry = "poetry"
+    if shutil.which(poetry) is None:
+        poetry = "uvx " + poetry
+    with chdir(tmp_path):
+        project = "foo"
+        Shell.run_by_subprocess(f"{poetry} new {project} --python=^{py}")
+        with chdir(tmp_path / project):
+            Shell.run_by_subprocess(
+                f"{poetry} config --local virtualenvs.in-project true"
+            )
+            Shell.run_by_subprocess(f"{poetry} env use {py}")
+            yield poetry
